8fa1e456488c9cc7a242b3f97f645c81b1458f03
Jonas Karlsson <jonas@ozma.(none)>
2012-05-29 14:00:35 -0400

    I guess we need this part

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,26 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table document_set (
+  id                        bigint not null,
+  query                     varchar(255),
+  constraint pk_document_set primary key (id))
+;
+
+create sequence document_set_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists document_set;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists document_set_seq;
+
