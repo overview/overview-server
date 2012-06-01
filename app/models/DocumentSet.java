9be3daef9964e3cc871773595a6e6f7e17ad87c4
Jonas Karlsson <thejonas@gmail.com>
2012-06-01 11:54:22 -0400

    set up the list of DocumentReferences

--- app/models/DocumentSet.java
+++ app/models/DocumentSet.java
@@ -2,6 +2,7 @@ package models;
 
 import java.util.*;
 
+import play.Logger;
 import play.db.ebean.*;
 import play.data.validation.Constraints.*;
 
@@ -16,7 +17,16 @@ public class DocumentSet extends Model {
     @Required
     public String query;
 
+    
+    @OneToMany(mappedBy="documentSetId", cascade=CascadeType.ALL)
+    public List<DocumentReference> documentIds;
+    
+        
     public static Finder<Long, DocumentSet> find = new Finder(Long.class, DocumentSet.class);
+    
+    public DocumentSet() {
+    	documentIds = new ArrayList<DocumentReference>();
+    }
     
     public static List<DocumentSet> all()
     {
@@ -29,6 +39,24 @@ public class DocumentSet extends Model {
     
     public static void delete(Long id) {
     	find.ref(id).delete();
+    }
+    
+    public void addDocument(Document document) {
+    	DocumentReference ref = new DocumentReference();
+    	ref.documentId = document.id;
+    	ref.documentSetId = this;
+    	ref.save();
+    	documentIds.add(ref);
+    }
+    
+    public List<Document> getDocuments() {
+    	List<Document> documents = new ArrayList<Document>();
+
+    	for (DocumentReference ref : documentIds) {
+    		documents.add(Document.find.byId(ref.documentId));
+    	}
+    	
+    	return documents;
     }
     
    
