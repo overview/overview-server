fc81b24a50e7ed99740623dbf0f75067c20872d2
Jonas Karlsson <thejonas@gmail.com>
2012-06-01 11:56:37 -0400

    I wish I knew why we need this.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -11,6 +11,13 @@ create table document (
   constraint pk_document primary key (id))
 ;
 
+create table document_reference (
+  id                        bigint not null,
+  document_set_id_id        bigint,
+  document_id               bigint,
+  constraint pk_document_reference primary key (id))
+;
+
 create table document_set (
   id                        bigint not null,
   query                     varchar(255),
@@ -19,8 +26,12 @@ create table document_set (
 
 create sequence document_seq;
 
+create sequence document_reference_seq;
+
 create sequence document_set_seq;
 
+alter table document_reference add constraint fk_document_reference_document_1 foreign key (document_set_id_id) references document_set (id) on delete restrict on update restrict;
+create index ix_document_reference_document_1 on document_reference (document_set_id_id);
 
 
 
@@ -30,11 +41,15 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists document;
 
+drop table if exists document_reference;
+
 drop table if exists document_set;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists document_seq;
+
+drop sequence if exists document_reference_seq;
 
 drop sequence if exists document_set_seq;
 
