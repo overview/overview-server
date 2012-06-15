85797f82f47b523c5763bece90d7ffa37b2f878f
jstray <jonathanstray@gmail.com>
2012-06-15 13:54:25 -0400

    Create document set in Scala, retrive text in indexing call

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -39,34 +39,10 @@ public class Application extends Controller {
     		documentSet.save();
 
     		// Now retrieve all the documents within this set, creating document objects
-    		// $$ all these code needs to move into worker. Or at least another function, please?
-        	String queryString = documentSet.query;
-        	String documentCloudQuery = "http://www.documentcloud.org/api/search.json";
-        	
-        	Promise<WS.Response> DCcall= WS.url(documentCloudQuery).setQueryParameter("q", queryString).get();
-        	WS.Response response = DCcall.get();	// blocks until result comes back. but does it tie up the thread? not sure, async() may be better
-        	
-    		JsonNode documentReferences = response.asJson().get("documents");
-    		    		
-    		for (JsonNode document : documentReferences) {
-    			String title = document.get("title").toString();
-    			title = title.replace("\"", "");
-    			String canonicalUrl = document.get("canonical_url").toString();
-    			canonicalUrl = canonicalUrl.replace("\"", "");
-
-    			String textUrl = document.get("resources").get("text").toString();
-    			
-    			// Test calling into Scala
-    			DocumentSetIndexer titleObj = new DocumentSetIndexer(textUrl);
-    			titleObj.munge();
-    			
-    			Document newDoc = new Document(documentSet, title, textUrl, canonicalUrl);
-    			newDoc.save();    			
-    			documentSet.documents.add(newDoc);
-    		}
-   
-    		documentSet.update();
-    		
+			DocumentSetIndexer indexer = new DocumentSetIndexer(documentSet);
+			indexer.createDocuments();
+			indexer.indexDocuments();
+			    		
     		return redirect(routes.Application.showDocumentSets());
     	}
     }
--- app/controllers/DocumentSetIndexer.scala
+++ app/controllers/DocumentSetIndexer.scala
@@ -1,16 +1,55 @@
 package controllers
 
-import play.libs.WS;
+import play._
+import play.libs.WS
+import play.libs.F.Promise
+import play.libs.F.Function;
+import play.libs.Json
 
-class DocumentSetIndexer(var textUrl:String) {
-	def munge() = {
-	  
-	  if (textUrl != null) {
-	    textUrl = textUrl.substring(1,textUrl.length-1) // remove quotes
-	    println(textUrl)
-	    
-		val response = WS.url(textUrl).get().get().getBody()
-		println(response + "\n------------------------")
-	  }
-	}
+import models._
+
+import scala.collection.JavaConversions._
+
+
+class DocumentSetIndexer(var documentSet:DocumentSet) {
+
+  // Query documentCloud and create one document object for each doc returned by the query
+  def createDocuments() = {
+    val queryString = documentSet.query
+    val documentCloudQuery = "http://www.documentcloud.org/api/search.json"
+    
+    // Query DocumentCloud for all docs matching query string
+    val DCcall = WS.url(documentCloudQuery).setQueryParameter("q", queryString).get();
+    val response = DCcall.get();  // blocks until result comes back. but does it tie up the thread? not sure, async() may be better
+  
+    // Iterate over returned docs, each one described by a block of JSON
+    val documentReferences  = response.asJson().get("documents")
+    for (document <- documentReferences) { 
+      
+      var title = document.get("title").toString()
+      title = title.replace("\"", "")
+      
+      var canonicalUrl = document.get("canonical_url").toString()
+  
+      canonicalUrl = canonicalUrl.replace("\"", "")
+      var textUrl = document.get("resources").get("text").toString()
+      textUrl = textUrl.substring(1,textUrl.length-1) // remove quotes
+                      
+      val newDoc = new Document(documentSet, title, textUrl, canonicalUrl)
+      newDoc.save()
+      documentSet.documents.add(newDoc)
+    }
+   
+    documentSet.update();     
+  }
+  
+  def indexDocuments() = {
+    for (document <- documentSet.documents) {
+      val textUrl = document.textUrl;
+      println(textUrl)
+     
+      val response = WS.url(textUrl).get().get().getBody()
+      println(response + "\n------------------------")
+    }
+  }
 }
