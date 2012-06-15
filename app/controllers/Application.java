4c24a346830efb4dde883daf93c43c46b607310c
jstray <jonathanstray@gmail.com>
2012-06-15 12:26:56 -0400

    Move document query to creation time

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -35,8 +35,38 @@ public class Application extends Controller {
     				);
     	}
     	else {
-    		filledForm.get().save();
+    	  	final DocumentSet documentSet = filledForm.get();
+    		documentSet.save();
 
+    		// Now retrieve all the documents within this set, creating document objects
+    		// $$ all these code needs to move into worker. Or at least another function, please?
+        	String queryString = documentSet.query;
+        	String documentCloudQuery = "http://www.documentcloud.org/api/search.json";
+        	
+        	Promise<WS.Response> DCcall= WS.url(documentCloudQuery).setQueryParameter("q", queryString).get();
+        	WS.Response response = DCcall.get();	// blocks until result comes back. but does it tie up the thread? not sure, async() may be better
+        	
+    		JsonNode documentReferences = response.asJson().get("documents");
+    		    		
+    		for (JsonNode document : documentReferences) {
+    			String title = document.get("title").toString();
+    			title = title.replace("\"", "");
+    			String canonicalUrl = document.get("canonical_url").toString();
+    			canonicalUrl = canonicalUrl.replace("\"", "");
+
+    			String textUrl = document.get("resources").get("text").toString();
+    			
+    			// Test calling into Scala
+    			DocumentSetIndexer titleObj = new DocumentSetIndexer(textUrl);
+    			titleObj.munge();
+    			
+    			Document newDoc = new Document(documentSet, title, textUrl, canonicalUrl);
+    			newDoc.save();    			
+    			documentSet.documents.add(newDoc);
+    		}
+   
+    		documentSet.update();
+    		
     		return redirect(routes.Application.showDocumentSets());
     	}
     }
@@ -55,39 +85,11 @@ public class Application extends Controller {
     }
         
     public static Result viewDocumentSet(Long id) {
-    	final DocumentSet documentSet = DocumentSet.find.byId(id);
-    	String queryString = documentSet.query;
-    	String documentCloudQuery = "http://www.documentcloud.org/api/search.json";
-    	
-    	Promise<WS.Response> DCcall= WS.url(documentCloudQuery).setQueryParameter("q", queryString).get();
-    	WS.Response response = DCcall.get();	// blocks until result comes back. but does it tie up the thread? not sure, async() may be better
-    	
-		JsonNode documentReferences = response.asJson().get("documents");
-		
-		Document documentToView = null;
-		
-		for (JsonNode document : documentReferences) {
-			//String documentId = document.get("id").toString();
-			String title = document.get("title").toString();
-			title = title.replace("\"", "");
-			String canonicalUrl = document.get("canonical_url").toString();
-			canonicalUrl = canonicalUrl.replace("\"", "");
-
-			String textUrl = document.get("resources").get("text").toString();
-			
-			// Test calling into Scala
-			DocumentSetIndexer titleObj = new DocumentSetIndexer(textUrl);
-			titleObj.munge();
-			
-			Document newDoc = new Document(documentSet, title, textUrl, canonicalUrl);
-			newDoc.save();
-			
-			if (documentToView == null) documentToView = newDoc;
-			
-			documentSet.documents.add(newDoc);
-		}
-		
-		documentSet.update();
-		return redirect(routes.Application.viewDocument(documentToView.id));
+  
+	   	final DocumentSet documentSet = DocumentSet.find.byId(id);
+	   	
+		// Just view the first doc for now
+	   	Document doc = documentSet.documents.iterator().next();
+		return redirect(routes.Application.viewDocument(doc.id));
     }
 }
