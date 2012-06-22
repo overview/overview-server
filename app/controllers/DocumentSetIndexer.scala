5c88c8f5be518555329d2ef836023a64e3526474
jstray <jonathanstray@gmail.com>
2012-06-22 16:43:57 -0700

    Turn each document into tf-idf representation

--- app/controllers/DocumentSetIndexer.scala
+++ app/controllers/DocumentSetIndexer.scala
@@ -8,7 +8,8 @@ import play.libs.Json
 
 import models._
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConversions._  // for iteration through documentReferences JsonNode -- and other things here?
+
 
 
 class DocumentSetIndexer(var documentSet:DocumentSet) {
@@ -43,13 +44,55 @@ class DocumentSetIndexer(var documentSet:DocumentSet) {
     documentSet.update();     
   }
   
+  // Index a collection of documents, TF-IDF style
   def indexDocuments() = {
+    
+    val N = documentSet.documents.size
+    var idf = Map[String, Float]()            // term -> number of docs containing term
+    var tf = Map[Long, Map[String, Float]]()   // docuent ID -> (term -> frequency)
+    
     for (document <- documentSet.documents) {
-      val textUrl = document.textUrl;
-      println(textUrl)
-     
-      val response = WS.url(textUrl).get().get().getBody()
-      println(response + "\n------------------------")
+
+      // Get the doument text
+      val text = WS.url(document.textUrl).get().get().getBody()
+      //println(text)
+
+      // Turn into tokens
+      val tokens = Lexer.make_terms(text)
+      if (tokens.size > 0) {
+        
+        // count how many times each token appears in this doc (term frequency)      
+        var termcounts = Map[String, Int]()
+        for (t <- tokens) {
+          val prev_count = if (termcounts.contains(t)) termcounts(t) else 0
+          termcounts += (t -> (prev_count + 1))
+        }
+        
+        // divide out document length to go from term count to term frequency
+        val termfreqs = termcounts.mapValues(count => count/tokens.size.toFloat) 
+        tf += (document.id.toLong -> termfreqs)
+         
+        // for each unique term in this doc, update how many docs each term appears in (doc frequency)
+        for (t <- termcounts.keys) {
+          val prev_count = if (idf.contains(t)) idf(t) else 0
+          idf += (t -> (prev_count + 1))
+        }
+      }      
     }
+    
+    // Now that we've read all documents, transform the document count into idf
+    idf = idf.mapValues(count => math.log10(N / count).toFloat)
+    
+    // Now we have to run over the stored TF values one more time, multiplying them by the IDF values for each term, and normalizing
+    for ((docid, tf) <- tf) {
+      val tfidf = tf.map({case (term,tf) => (term, tf * idf(term))})
+      val length = math.sqrt(tfidf.values.fold(0f) { (sumsq, weight) => sumsq + weight*weight }).toFloat
+      val normalized = tfidf.mapValues(weight => weight/length)
+      
+      println("------------")
+
+      println(normalized.toList.sorted)
+    }
+    
   }
 }
---
+++ app/controllers/Lexer.scala
@@ -0,0 +1,62 @@
+/*
+ * Lexer.scala
+ * Simple English language tokenization
+ * Break on spaces, remove most punctuation, other characters. Filter stopwords. Optionally parse bigrams from a provided set.
+ * 
+ * Overview Project
+ * 
+ * Created by Jonathan Stray, June 2012
+ * 
+ */
+
+package controllers
+
+import play._
+import scala.util.matching.Regex
+
+object Lexer {
+
+  
+  // Load stopwords on startup
+  var stopWords = Set[String]()
+  val stopWordsFile = Play.application.getFile("conf/stopwords.csv")
+  for (l <- io.Source.fromFile(stopWordsFile).getLines)
+    stopWords += l
+  
+  // Remove certain types of terms. At the moment:
+  //  - we insist that terms are at least three chars
+  //  - discard if a) starts with a digit and b) 40% or more of the characters are digits
+  //  - discard stopwords
+  
+  def term_acceptable(t:String) : Boolean = {  
+    if (t.length < 3)
+      false                                    // too short, unacceptable
+    else if (stopWords.contains(t))
+      false                                    // stopword, unacceptable
+    else
+      if (!t.charAt(0).isDigit)
+        true                                   // doesn't start with digit, acceptable
+      else
+        10*t.count(_.isDigit) < 4*t.length   // < 40% digits, acceptable
+  }
+  
+  // Given a string, returns a list of terms. Basically just splits on space, but also cleans up lots
+  def make_terms(textIn:String) : Array[String] = {
+    if (textIn.length == 0) Array()
+   
+    // lose case, strip HTML/XML tags, 
+    var text = textIn.toLowerCase                   
+    text = "<[^>]*>".r.replaceAllIn(text,"")
+    
+    // collapse runs of spaces (including non-breaking space, U+160) into single space
+    text = "[ \t\n\r\u0160]+".r.replaceAllIn(text," ")
+    
+    // allow only alphanum, dash, apostrophe, space
+    text = text.filter(c => c.isDigit || c.isLetter || " -'".contains(c))
+    
+    // split on space, keep only "acceptable" terms
+    text.split(' ').filter(term_acceptable)
+  }
+  
+}
+  
