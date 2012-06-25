3308f1134006778d57af4789af2e0c39be98aa4d
Adam Hooper <adam@adamhooper.com>
2012-06-25 12:25:55 -0400

    Add stub for /tree/:id/documents, with a stub Selection object

---
+++ common/app/models/Selection.java
@@ -0,0 +1,21 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Selection {
+    public Tree tree;
+    public Set<Node> nodes = new HashSet<Node>();
+    public Set<Document> documents = new HashSet<Document>();
+    public Set<Tag> tags = new HashSet<Tag>();
+
+    public Selection(Tree tree) {
+        this.tree = tree;
+    }
+
+    public List<Document> slice(long start, long end) {
+        return new ArrayList<Document>();
+    }
+}
