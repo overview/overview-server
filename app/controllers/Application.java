d291b163e9945bcb2bba4d8518c1deb9ea4fdb81
Adam Hooper <adam@adamhooper.com>
2012-06-27 17:52:24 -0400

    Remove unused controller code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,7 +20,6 @@ public class Application extends Controller {
     	return ok(documentSets.render(models.DocumentSet.find.all(), documentSetForm));
     }
 
-    
     public static Result deleteDocumentSet(Long id) {
     	models.DocumentSet documentSet = models.DocumentSet.find.ref(id);
     	documentSet.delete();
@@ -28,21 +27,6 @@ public class Application extends Controller {
     	return redirect(routes.Application.showDocumentSets());
     }
 
-    public static Result viewDocument(Long id) {
-    	Document document = Document.find.byId(id);
-
-    	return ok(viewDocument.render(document));
-    }
-        
-    public static Result viewDocumentSet(Long id) {
-  
-	   	final models.DocumentSet documentSet = models.DocumentSet.find.byId(id);
-	   	
-		// Just view the first doc for now
-	   	Document doc = documentSet.documents.iterator().next();
-		return redirect(routes.Application.viewDocument(doc.id));
-    }
-    
     public static Result showJobs() {
     	return ok(viewJobs.render(DocumentSetCreationJob.find.all()));
     	
