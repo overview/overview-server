ac8c6351707eb832f3340eab55868b6aa3b4cb84
Jonas Karlsson <thejonas@gmail.com>
2012-06-28 14:39:16 -0400

    Read connection from properties file or environment

---
+++ worker/src/main/scala/DatabaseConfiguration.scala
@@ -0,0 +1,34 @@
+
+import com.avaje.ebean.config.GlobalProperties
+
+class DatabaseConfiguration {
+  val DatabaseDriverProperty = "datasource.default.databaseDriver"
+  val DatabaseUrlProperty =    "datasource.default.databaseUrl"
+  val UsernameProperty =       "datasource.default.username"
+  val PasswordProperty =       "datasource.default.password"
+  val DATABASE_URL = 		   "DATABASE_URL"
+   
+  var postgresDbUrl: String = null
+
+  
+
+  val databaseDriver = GlobalProperties.get(DatabaseDriverProperty, null) 
+  val (databaseUrl, username, password) = getSettings
+    
+  def getSettings() = {
+	val databaseSetting = sys.props.get(DATABASE_URL)
+	databaseSetting match {
+      case Some(databaseInfo) => {
+        val urlPattern = "\\w+://(\\w+):(\\w+)@(\\w+):(\\w+)/(\\w+)".r
+    
+        val urlPattern(user, password, host, port, database) = sys.props.get(DATABASE_URL).get
+        ("jdbc:postgresql://"+host+":"+port+"/"+database, user, password)
+      }
+      case None => {
+        (GlobalProperties.get(DatabaseUrlProperty, null),
+         GlobalProperties.get(UsernameProperty, null),
+         GlobalProperties.get(PasswordProperty, null))
+      }
+	}
+  }
+}
---
+++ worker/src/test/scala/DatabaseConfigurationSpec.scala
@@ -0,0 +1,57 @@
+
+
+import com.avaje.ebean.config.GlobalProperties
+
+import org.specs2.mutable._
+import org.specs2.specification._
+
+
+
+
+
+class DatabaseConfigurationSpec extends Specification {
+  
+  "DatabaseConfiguration properties" should {
+    
+    "be read from ebean.properties by default" in {
+     
+      val defaultDriver = GlobalProperties.get("datasource.default.databaseDriver", null)
+      val defaultDatabaseUrl = GlobalProperties.get("datasource.default.databaseUrl", null)
+      val defaultUsername = GlobalProperties.get("datasource.default.username", null)
+      val defaultPassword = GlobalProperties.get("datasource.default.password", null)
+      
+      defaultDriver must not beNull; 
+      defaultDatabaseUrl must not beNull;
+      defaultUsername must not beNull;
+      defaultPassword must not beNull;
+      
+      val dbConfig = new DatabaseConfiguration()
+      
+      dbConfig.databaseDriver must beEqualTo(defaultDriver)
+      dbConfig.databaseUrl must beEqualTo(defaultDatabaseUrl)
+      dbConfig.username must beEqualTo(defaultUsername)
+      dbConfig.password must beEqualTo(defaultPassword)
+    }
+  
+    "be read from DATABASE_URL environment variable, if set" in {
+      val username = "DbUser"
+      val password = "DbPassword"
+      val host = "DbHost:port"
+      val database = "Database"
+        
+      val databaseUrl = "postgres://"+username+":"+password+"@"+host+"/"+database
+      sys.props += ("DATABASE_URL" -> databaseUrl)
+      
+      val expectedUrl = "jdbc:postgresql://"+host+"/"+database
+      val dbConfig = new DatabaseConfiguration()
+      
+      sys.props -= "DATABASE_URL"
+
+      dbConfig.databaseUrl must beEqualTo(expectedUrl)
+      dbConfig.username must beEqualTo(username)
+      dbConfig.password must beEqualTo(password)
+      
+    }
+  }
+
+}
