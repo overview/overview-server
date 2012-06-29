a56486beafe14e72a1f622b1cc5edc253fc252f7
Jonas Karlsson <thejonas@gmail.com>
2012-06-29 09:57:02 -0400

    check that documents are saved when documentset is saved

--- common/test/models/DocumentSetTest.java
+++ common/test/models/DocumentSetTest.java
@@ -3,7 +3,12 @@ package models;
 
 import static org.fest.assertions.Assertions.*;
 
+
+import com.avaje.ebean.Ebean;
 import org.junit.Test;
+
+import static play.test.Helpers.*;
+
 
 public class DocumentSetTest {
 
@@ -18,6 +23,36 @@ public class DocumentSetTest {
 	  								   .contains(document);
 	  
 	  assertThat(document.documentSet).isEqualTo(documentSet);
+	}
+	
+	@Test
+	public void saveToDatabase() {
+	  running (fakeApplication(), new Runnable() {
+		
+		public void run() {
+	  
+		  Ebean.beginTransaction();
+		
+		  DocumentSet documentSet = new DocumentSet();
+		
+		  for (int i = 0; i < 3; i++) {
+			Document document = new Document("title" + i, "textUrl" + i, "viewUrl" + i);
+			documentSet.addDocument(document);
+		  }
+		  
+		
+		  documentSet.save();
+
+		  DocumentSet foundDocumentSet = DocumentSet.find.byId(documentSet.id);
+		
+		  assertThat(foundDocumentSet).isNotNull();
+		  assertThat(foundDocumentSet.documents).hasSize(3);
+		  assertThat(foundDocumentSet.documents).onProperty("title")
+		  										.contains("title0", "title1", "title2");
+		  
+		  Ebean.endTransaction();
+		}
+	  });
 	}
 
 }
