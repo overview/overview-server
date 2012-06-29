0fd83b6f2bf4128d6787b22e237e36ff1d809686
Jonas Karlsson <thejonas@gmail.com>
2012-06-29 16:55:32 -0400

    we no longer need to disambiguate model classes from controllers

--- app/controllers/TreeController.scala
+++ app/controllers/TreeController.scala
@@ -10,7 +10,7 @@ import play.api.mvc._
 import play.api.libs.iteratee.Enumerator
 import play.api.libs.json.Json._
 import controllers.TreeJsonWrapper._
-import models.{Node,Tree}
+import models._
 
 object TreeController extends Controller {
 	
@@ -22,22 +22,22 @@ object TreeController extends Controller {
 	  val root = new Node()
 	  root.description = "root"
 
-	  val documentSet = new models.DocumentSet()
+	  val documentSet = new DocumentSet()
 	  for (i <- 1 to 22) {
-	    val document = new models.Document("document-" + i, "textUrl-" + i, "viewUrl-" + i)
+	    val document = new Document("document-" + i, "textUrl-" + i, "viewUrl-" + i)
 	    documentSet.addDocument(document)
 	    document.save
 	  }
 
 	  generateTreeLevel(root, documentSet.documents.toSeq, 4)
 	  
-	  val tree = new models.Tree()
+	  val tree = new Tree()
 	  tree.root = root
 	  tree.save()
 	  Ok(views.html.index.render(toJson(tree).toString))
 	}
 	
-	def generateTreeLevel(root: Node, documents: Seq[models.Document], depth: Int) = {
+	def generateTreeLevel(root: Node, documents: Seq[Document], depth: Int) = {
 	  
       documents.foreach(d => root.addDocument(d))
 
@@ -48,8 +48,8 @@ object TreeController extends Controller {
 	  }
 	}
 
-	def generateChildren(numberOfSiblings: Int, documents: Seq[models.Document], depth: Int) : Seq[Node] = {
-	  val siblings = Seq[models.Node]()
+	def generateChildren(numberOfSiblings: Int, documents: Seq[Document], depth: Int) : Seq[Node] = {
+	  val siblings = Seq[Node]()
 	  if (numberOfSiblings > 0) {
 	    val splitPoint = 
 	      if (documents.size > numberOfSiblings) Random.nextInt(documents.size - numberOfSiblings) + 1 else 1 
@@ -60,7 +60,7 @@ object TreeController extends Controller {
 	    child.description = "node height " + depth
 	    generateTreeLevel(child, childDocuments, depth - 1)
 	    
-	    Seq[models.Node](child) ++ generateChildren(numberOfSiblings - 1, siblingDocuments, depth)
+	    Seq[Node](child) ++ generateChildren(numberOfSiblings - 1, siblingDocuments, depth)
 	  }
 	  else {
 		siblings
@@ -75,7 +75,7 @@ object TreeController extends Controller {
 //            header = ResponseHeader(200, Map(CONTENT_TYPE -> "application/json")),
 //            body = Enumerator(json)
 //        )
-      val tree = models.Tree.find.all.get(0)
+      val tree = Tree.find.all.get(0)
       val json = toJson(tree)
       Ok(json)
 
