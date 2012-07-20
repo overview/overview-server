b593f73c93012449615ef38bc420b1fa4b0cacf3
Jonas Karlsson <thejonas@gmail.com>
2012-07-20 18:20:00 -0400

    added some comments

--- app/models/SubTreeDataParser.scala
+++ app/models/SubTreeDataParser.scala
@@ -1,7 +1,14 @@
 package models
 
+
+/**
+ * Utility class for SubTreeLoader that parses the results from the database queries
+ */
 class SubTreeDataParser {
 
+  /**
+   * @return a list of Nodes created from the passed in data
+   */
   def createNodes(data: List[(Long, Long, String)]) : List[core.Node] = {
     val parentData = data.groupBy(d => d._1)
     val childNodeIds = parentData.map { 
--- app/models/SubTreeLoader.scala
+++ app/models/SubTreeLoader.scala
@@ -8,11 +8,16 @@ import anorm.SqlParser._
 import play.api.Play.current
 import play.api.db.DB
 
-
+/**
+ * Loads data from the database about subTrees
+ */
 class SubTreeLoader(rootId: Long, depth: Int, 
 					loader: SubTreeDataLoader = new SubTreeDataLoader(),
 					parser: SubTreeDataParser = new SubTreeDataParser()) {
   
+  /**
+   * @return a list of all the Nodes in the subTree
+   */
   def loadNodes()(implicit connection : java.sql.Connection) : List[core.Node] = {
 	
     val nodeData = loader.loadNodeData(rootId, depth)
