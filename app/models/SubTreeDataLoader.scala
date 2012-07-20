3785028a9436df1cbb94ea082b10fd5642089e32
Jonas Karlsson <thejonas@gmail.com>
2012-07-20 12:19:15 -0400

    refactored SQL strings into separate values

--- app/models/SubTreeDataLoader.scala
+++ app/models/SubTreeDataLoader.scala
@@ -8,13 +8,10 @@ class SubTreeDataLoader {
 
   type NodeData = (Long, Long, String)
   
+    
   def loadNodeData(rootId: Long, depth: Int)(implicit connection: Connection) : List[NodeData] = {
-    val rootNode = SQL(
-      """
-        SELECT node.id, node.description FROM node WHERE id = {id}
-      """
-    ).on("id" -> rootId).
-    as(long("id") ~ str("description") map(flatten) single)
+    val rootNode = SQL(RootNodeQuery).on("id" -> rootId).
+      as(long("id") ~ str("description") map(flatten) single)
     
     val rootAsChild = (-1l, rootNode._1, rootNode._2)
     
@@ -28,16 +25,25 @@ class SubTreeDataLoader {
                             (implicit connection: Connection) : List[NodeData] = depth match {
     case 0 => Nil
     case _ => {
-      val childNodes = SQL(
-        """
-          SELECT node.parent_id AS id, node.id AS child_id, node.description AS child_description
-	        FROM node WHERE parent_id IN (""" + nodes.mkString(", ") + """)
-        """
-        ).as(long("id") ~ long("child_id") ~ str("child_description") map(flatten) *)
+      val childNodes = SQL(childNodeQuery(nodes)).
+        as(long("id") ~ long("child_id") ~ str("child_description") map(flatten) *)
       
       val childNodeIds = childNodes.map(_._2)
       
       childNodes ++ loadChildNodes(childNodeIds, depth - 1)
     }
   }
+
+  private val RootNodeQuery = 
+    """
+      SELECT node.id, node.description FROM node WHERE id = {id}
+    """
+    
+  private def childNodeQuery(nodeIds: List[Long]) : String = {
+    """
+      SELECT node.parent_id AS id, node.id AS child_id, node.description AS child_description
+        FROM node WHERE parent_id IN (""" + nodeIds.mkString(", ") + """)
+    """
+  }
+
 }
