b79668586273605fb40f2f4fd37708f4df8222f9
Jonas Karlsson <thejonas@gmail.com>
2012-07-27 18:10:49 -0400

    combine selection criteria

--- app/models/PersistentDocumentListDataLoader.scala
+++ app/models/PersistentDocumentListDataLoader.scala
@@ -6,31 +6,35 @@ import DatabaseStructure._
 import java.sql.Connection
 
 class PersistentDocumentListDataLoader(nodeIds: List[Long], documentIds: List[Long]) {
-  
-  def loadDocumentSlice(firstRow: Long, maxRows: Long)
-                       (implicit c: Connection) : List[DocumentData] = {
-    if (nodeIds.isEmpty) {
-	  val documentSelectionWhere =
-	    "WHERE document.id IN " + idList(documentIds)
-	        
-	  documentQueryWhere(documentSelectionWhere)
-    }
-    else {
-      val nodeSelectionWhere = 
-        """
- 		WHERE document.id IN 
-	      (SELECT document_id FROM node_document WHERE node_id IN
-        """ + idList(nodeIds) +
-          ")"
-          
-        documentQueryWhere(nodeSelectionWhere)
-    }
+
+  def loadDocumentSlice(firstRow: Long, maxRows: Long)(implicit c: Connection): List[DocumentData] = {
+    val where = List(nodeSelection(nodeIds),
+      documentSelection(documentIds)).flatMap(_.toList)
+      
+    documentQueryWhere(where.mkString(" AND "))
   }
 
+  private def documentSelection(documentIds: List[Long]) : Option[String] = 
+    documentIds match {
+    case Nil => None
+    case _ => Some("document.id IN " + idList(documentIds))
+  } 
+  
+  private def nodeSelection(nodeIds: List[Long]) : Option[String] =
+    nodeIds match {
+    case Nil => None
+    case _ => Some("""
+    			   document.id IN 
+	                (SELECT document_id FROM node_document WHERE node_id IN""" + 
+	                idList(nodeIds) + 
+    			   ")"
+    		  )
+  }
+  
   private def documentQueryWhere(where: String)(implicit c: Connection) : List[DocumentData] = {
     SQL("""
-        SELECT id, title, text_url, view_url FROM document
-        """ + where
+        SELECT id, title, text_url, view_url FROM document 
+        WHERE """ + where
         ).as(long("id") ~ str("title") ~ str("text_url") ~ str("view_url") map (flatten) *)
   }
   
