02cb197ffbabfdb47e4cff07641bf9c2b669c754
Jonas Karlsson <thejonas@gmail.com>
2012-07-30 18:42:54 -0400

    test basic load/parse

--- test/models/PersistentDocumentListSpec.scala
+++ test/models/PersistentDocumentListSpec.scala
@@ -2,25 +2,48 @@ package models
 
 import org.specs2.mock._
 import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
 
 class PersistentDocumentListSpec extends Specification with Mockito {
   implicit val unusedConnection: java.sql.Connection = null
   
   "PersistDocumentList" should {
     
-    "extract ids from input strings" in {
+    trait MockComponents extends Scope {
       val loader = mock[PersistentDocumentListDataLoader]
       val parser = mock[PersistentDocumentListParser]
-      val dummyDocumentData = Nil
-      
-      loader loadSelectedDocumentSlice(List(1, 2, 4), List(5, 6), 0, 10) returns dummyDocumentData
-      
+      val nodeIds = Seq(1l, 2l, 4l)
+      val documentIds = Seq(5l, 6l)
       val persistentDocumentList = 
         new PersistentDocumentList("1, 2, 4", "5, 6", loader, parser)
 
+      
+    }
+    "extract ids from input strings" in  new MockComponents {
+      val dummyDocumentData = Nil
+      
+      loader loadSelectedDocumentSlice(nodeIds, documentIds, 0, 10) returns
+      	dummyDocumentData
+      
       val documents = persistentDocumentList.loadSlice(0, 10)
       
-      there was one(loader).loadSelectedDocumentSlice(List(1, 2, 4), List(5, 6), 0, 10)
+      there was one(loader).loadSelectedDocumentSlice(nodeIds, documentIds, 0, 10)
+    }
+    
+    "call loader and parser to create Documents" in new MockComponents {
+      val dummyDocumentData = Nil
+      val dummyDocuments = List(core.Document(1l, "title", "text", "view"))
+      
+      loader loadSelectedDocumentSlice(nodeIds, documentIds, 0, 10) returns
+        dummyDocumentData
+      parser createDocuments(dummyDocumentData) returns dummyDocuments
+      
+      val documents = persistentDocumentList.loadSlice(0, 10)
+      
+      there was one(loader).loadSelectedDocumentSlice(nodeIds, documentIds, 0, 10)
+      there was one(parser).createDocuments(dummyDocumentData)
+      
+      documents must be equalTo(dummyDocuments)
     }
     
     "be constructable with default loader and parser" in {
