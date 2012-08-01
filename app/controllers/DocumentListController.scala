e4db252848e720c38ed444e3b4a05d771f9e32fb
Jonas Karlsson <thejonas@gmail.com>
2012-08-01 18:06:39 -0400

    Parsing of input Id string is now done in controller

--- app/controllers/DocumentListController.scala
+++ app/controllers/DocumentListController.scala
@@ -11,7 +11,7 @@ object DocumentListController extends Controller {
     def index(treeId: Long, nodeids: String, tagids: String,
               documentids: String, start: Int, end: Int) = Action {
       DB.withTransaction { implicit connection => 
-        val documents = new PersistentDocumentList(nodeids, documentids)
+        val documents = new PersistentDocumentList(IdList(nodeids), IdList(documentids))
         val selection = documents.loadSlice(start, end)
         val totalItems = documents.loadCount
         
