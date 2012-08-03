484bb2029d89d9bb4d9facbfae192f1a16226fb9
Jonas Karlsson <thejonas@gmail.com>
2012-08-03 14:36:35 -0400

    use collect to get a list with no Nones

--- app/models/PersistentDocumentListDataSelector.scala
+++ app/models/PersistentDocumentListDataSelector.scala
@@ -3,7 +3,7 @@ package models
 class PersistentDocumentListSelector {
 
   protected def combineWhereClauses(whereClauses: Seq[Option[String]]) : String = {
-    val actualWheres = whereClauses.flatMap(_.toList)
+    val actualWheres = whereClauses.collect { case Some(w) => w }
     actualWheres match {
       case Nil => ""
       case _ => actualWheres.mkString("WHERE ", " AND ", " ")
