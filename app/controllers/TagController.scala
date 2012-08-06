0befc23fec16835d949ac46b55697c1fb98d8d82
Jonas Karlsson <thejonas@gmail.com>
2012-08-06 14:39:00 -0400

    Use new PersistentTag

--- app/controllers/TagController.scala
+++ app/controllers/TagController.scala
@@ -1,50 +1,40 @@
 package controllers
 
-import models.{PersistentDocumentList, PersistentTagLoader, PersistentTagSaver}
+import models.{PersistentDocumentList, PersistentTag}
 import play.api.db.DB
 import play.api.mvc.{Action, Controller}
 import play.api.Play.current
+import models.PersistentTagLoader
 
 
 object TagController extends Controller {
 
-  def create(documentSetId: Long, tag: String, 
+  def create(documentSetId: Long, tagName: String, 
              nodeIds: String, tagIds: String, documentIds: String) = Action {
     DB.withTransaction { implicit connection =>
+      val tag = PersistentTag.findOrCreateByName(tagName, documentSetId)
 
-      val tagLoader = new PersistentTagLoader()
-
-      val tagId = tagLoader.loadByName(tag) match {
-        case Some(id) => id
-        case None => {
-          val tagSaver = new PersistentTagSaver()
-          tagSaver.save(tag, documentSetId).get
-        }
-      }  
-      
       val documents = new PersistentDocumentList(IdList(nodeIds),
     		  								     IdList(documentIds))
       
-      val tagCount = documents.addTag(tagId)
+      val tagCount = documents.addTag(tag.id)
 
-      Ok(views.json.Tag.show((tagId, tagCount)))
+      Ok(views.json.Tag.show((tag.id, tagCount)))
     }
   }
   
-  def remove(documentSetId: Long, tag: String,
+  def remove(documentSetId: Long, tagName: String,
              nodeIds: String, tagIds: String, documentIds: String) = Action {
     DB.withTransaction { implicit connection => 
       
-      val tagLoader = new PersistentTagLoader()
-      
-      tagLoader.loadByName(tag) match {
-        case Some(tagId) => {
+      PersistentTag.findByName(tagName, documentSetId) match {
+        case Some(tag) => {
           val documents = new PersistentDocumentList(IdList(nodeIds),
                                                      IdList(documentIds))
           
-          val tagCount = documents.removeTag(tagId)
+          val tagCount = documents.removeTag(tag.id)
           
-          Ok(views.json.Tag.show((tagId, tagCount)))
+          Ok(views.json.Tag.show((tag.id, tagCount)))
         }
         case None => NotFound
       }
