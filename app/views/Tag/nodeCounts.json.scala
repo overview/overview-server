52bb35b58e80df250a63bb5e0a052f8d68319419
Jonas Karlsson <thejonas@gmail.com>
2012-08-07 13:07:56 -0400

    renamed to match protocol

--- app/views/Tag/nodeCounts.json.scala
+++ app/views/Tag/nodeCounts.json.scala
@@ -3,7 +3,7 @@ package views.json.Tag
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json.toJson
 
-object updateNodeCounts {
+object nodeCounts {
 	
   def apply(nodeCounts: Seq[(Long, Long)]) : JsValue = {
     toJson(nodeCounts.flatMap(n => Seq(n._1, n._2)))
