87c00eb4a2951eb93f2b2ba4940c240bc49393c1
Jonas Karlsson <thejonas@gmail.com>
2012-08-09 07:19:23 -0400

    use common JsonDocument

--- app/views/DocumentList/show.json.scala
+++ app/views/DocumentList/show.json.scala
@@ -4,20 +4,10 @@ package views.json.DocumentList
 import models.core.Document
 import play.api.libs.json.{JsValue, Writes}
 import play.api.libs.json.Json.toJson
-
+import views.json.helper.ModelJsonConverters.JsonDocument
 
 object show {
 
-  private[DocumentList] implicit object JsonDocument extends Writes[Document] {
-    def writes(document: Document): JsValue = {
-      toJson(Map(
-          "id" -> toJson(document.id),
-          "title" -> toJson(document.title),
-          "tagids" -> toJson(Seq[String]())
-          ))
-    }
-  }
-  
   def apply(documents: Seq[Document], totalCount: Long): JsValue = {
     toJson(
       Map(
