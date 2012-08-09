4191f380abac5e27b234a9b9feafeb3aed56cbe5
Jonas Karlsson <thejonas@gmail.com>
2012-08-08 20:07:36 -0400

    check that creating with tags works

--- test/models/PersistentDocumentListParserSpec.scala
+++ test/models/PersistentDocumentListParserSpec.scala
@@ -21,19 +21,23 @@ class PersistentDocumentListParserSpec extends Specification {
     		 				 (20l, "title2", "textUrl2", "viewUrl2"),
     		 				 (30l, "title3", "textUrl3", "viewUrl3")
      )
+     val documentTagData = List((10l, 15l), (20l, 5l))
      val persistentDocumentListParser = new DocumentListParser()
      
-     val documents = persistentDocumentListParser.createDocuments(documentData, Nil)
+     val documents = persistentDocumentListParser.createDocuments(documentData, 
+    		 												      documentTagData)
      
      val ids = documents.map(_.id)
      val titles = documents.map(_.title)
      val textUrl = documents.map(_.textUrl)
      val viewUrl = documents.map(_.viewUrl)
+     val tags = documents.map(_.tags)
      
      ids must haveTheSameElementsAs(List(10l, 20l, 30l))
      titles must haveTheSameElementsAs(List("title1", "title2", "title3"))
      textUrl must haveTheSameElementsAs(List("textUrl1", "textUrl2", "textUrl3"))
      viewUrl must haveTheSameElementsAs(List("viewUrl1", "viewUrl2", "viewUrl3"))
+     tags must haveTheSameElementsAs(List(Seq(5l), Seq(15l), Seq()))
    }
  }
 }
