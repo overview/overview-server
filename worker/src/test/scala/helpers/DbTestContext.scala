86c7f732209050ca1ec91eb6b8e67f716d7d6c4d
Jonas Karlsson <thejonas@gmail.com>
2012-08-15 17:49:25 -0400

    use boneCP instead of Squeryl

--- worker/src/test/scala/helpers/DbTestContext.scala
+++ worker/src/test/scala/helpers/DbTestContext.scala
@@ -1,35 +1,26 @@
 package helpers
 
 
-import anorm._
+
 import org.specs2.mutable.Around
 import org.specs2.execute.Result
 import org.squeryl.adapters.PostgreSqlAdapter
-import org.squeryl.PrimitiveTypeMode._
-import org.squeryl.SessionFactory
-import org.squeryl.Session
+import database.DB
 
 
 trait DbTestContext extends Around {
-  Class.forName("org.postgresql.Driver");
-
-  SessionFactory.concreteFactory = Some(() =>
-    Session.create(
-      java.sql.DriverManager.getConnection("jdbc:postgresql://localhost/overview-test",
-        "overview", "overview"),
-      new PostgreSqlAdapter))
-
-  lazy implicit val connection = Session.currentSession.connection
+  lazy implicit val connection = DB.getConnection()
   
   def around[T <% Result](test: => T) = {
-         
-    inTransaction {
-      SQL("TRUNCATE TABLE document_set CASCADE").execute()
-      val result = test
+    try {
+      connection.setAutoCommit(false)
+      test
+    }
+    finally {
       connection.rollback()
-      
-      result
+      connection.close()
     }
   }
   
 }
+    
