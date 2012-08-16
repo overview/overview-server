b97946e7c73474c94c9405e0a9bcb4474eaa2b63
Jonas Karlsson <thejonas@gmail.com>
2012-08-16 18:14:04 -0400

    whitespace correction

--- worker/src/main/scala/database/DataSource.scala
+++ worker/src/main/scala/database/DataSource.scala
@@ -5,18 +5,19 @@ import java.sql.Connection
 
 class DataSource(configuration: DatabaseConfiguration) {
 
-    Class.forName(configuration.databaseDriver)
+  Class.forName(configuration.databaseDriver)
+
+  private val dataSource = new BoneCPDataSource()
+
+  dataSource.setJdbcUrl(configuration.databaseUrl)
+  dataSource.setUsername(configuration.username)
+  dataSource.setPassword(configuration.password)
+  dataSource.setMinConnectionsPerPartition(5)
+  dataSource.setMaxConnectionsPerPartition(30)
+  dataSource.setPartitionCount(1)
+  dataSource.setDisableJMX(true)
+  dataSource.setLogStatementsEnabled(true)
     
-    private val dataSource = new BoneCPDataSource()
-    
-    dataSource.setJdbcUrl(configuration.databaseUrl)
-    dataSource.setUsername(configuration.username)
-    dataSource.setPassword(configuration.password)
-    dataSource.setMinConnectionsPerPartition(5)
-    dataSource.setMaxConnectionsPerPartition(30)
-    dataSource.setPartitionCount(1)
-    dataSource.setDisableJMX(true)
-  
   def getConnection() : Connection = {
     dataSource.getConnection()
   }
