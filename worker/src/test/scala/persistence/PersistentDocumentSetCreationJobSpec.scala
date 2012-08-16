f199714678a5f26ae7b3cf057927c2349593465c
Jonas Karlsson <thejonas@gmail.com>
2012-08-16 19:50:19 -0400

    make sure job has an associated user.

--- worker/src/test/scala/persistence/PersistentDocumentSetCreationJobSpec.scala
+++ worker/src/test/scala/persistence/PersistentDocumentSetCreationJobSpec.scala
@@ -8,27 +8,37 @@
 package persistence
 
 import anorm._
+import anorm.SqlParser._
 import helpers.{DbSpecification, DbTestContext}
+import java.sql.Connection
 import org.specs2.mutable.Specification
 import persistence.DocumentSetCreationJobState._
+
 
 class PersistentDocumentSetCreationJobSpec extends DbSpecification {
 
   step(setupDB)
   
+  def findAdminUserId(implicit connection: Connection): Option[Long] = {
+    SQL("""
+        SELECT id FROM "user" WHERE email = 'admin@overview-project.org'
+        """).as(long("id") singleOpt)
+  }
+  
   "PersistentDocumentSetCreationJob" should {
     
     "find all submitted jobs" in new DbTestContext {
-    
+      val userId = findAdminUserId.get
       val submitted = Submitted.id
       val inProgress = InProgress.id
       
       SQL("""
-          INSERT INTO document_set_creation_job (query, state) VALUES 
-            ('q1', {state1}),
-            ('q2', {state2}),
-            ('q3', {state3})
-          """).on("state1" -> submitted, "state2" -> submitted, "state3" -> inProgress).
+          INSERT INTO document_set_creation_job (query, state, user_id) VALUES 
+            ('q1', {state1}, {user}),
+            ('q2', {state2}, {user}),
+            ('q3', {state3}, {user})
+          """).on("state1" -> submitted, "state2" -> submitted, 
+                  "state3" -> inProgress, "user" -> userId).
                executeUpdate()
                
       val jobs = PersistentDocumentSetCreationJob.findAllSubmitted
@@ -38,14 +48,15 @@ class PersistentDocumentSetCreationJobSpec extends DbSpecification {
     }
     
     "update job state" in new DbTestContext {
+      val userId = findAdminUserId.get
       val submitted = Submitted.id
       val inProgress = InProgress.id
       
       val jobId = SQL(
           """
-          INSERT INTO document_set_creation_job (query, state) VALUES 
-            ('q1', {state})
-          """).on("state" -> submitted).executeInsert()
+          INSERT INTO document_set_creation_job (query, state, user_id) VALUES 
+            ('q1', {state}, {user})
+          """).on("state" -> submitted, "user" -> userId).executeInsert()
                
       val jobs = PersistentDocumentSetCreationJob.findAllSubmitted
       
