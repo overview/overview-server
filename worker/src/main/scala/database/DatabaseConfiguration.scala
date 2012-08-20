ab54fa7790cdd8e0f7980c2f0591f4f792530942
Jonas Karlsson <thejonas@gmail.com>
2012-08-20 15:23:00 -0400

    better regexp for parsing database urls

--- worker/src/main/scala/database/DatabaseConfiguration.scala
+++ worker/src/main/scala/database/DatabaseConfiguration.scala
@@ -15,30 +15,30 @@ import com.avaje.ebean.config.GlobalProperties
  * and converts to jdbc:postgresql://host/database, with user and password in 
  * separate configuration variables.
  */
-class DatabaseConfiguration {
-  val DatabaseDriverProperty = "datasource.default.databaseDriver"
-  val DatabaseUrlProperty =    "datasource.default.databaseUrl"
-  val UsernameProperty =       "datasource.default.username"
-  val PasswordProperty =       "datasource.default.password"
-  val DATABASE_URL = 		   "datasource.default.url"
+class DatabaseConfiguration(
+		databaseDriverProperty: String = "datasource.default.databaseDriver",
+		databaseUrlProperty: String = "datasource.default.databaseUrl",
+		usernameProperty: String = "datasource.default.username",
+		passwordProperty: String = "datasource.default.password",
+		defaultDatabaseUrlProperty: String = "datasource.default.url") {
    
   val databaseDriver = "org.postgresql.Driver"
   val (databaseUrl, username, password) = readSettings()
     
   def readSettings() : (String, String, String) = {
-	val databaseSetting = sys.props.get(DATABASE_URL)
+	val databaseSetting = sys.props.get(defaultDatabaseUrlProperty)
 
 	databaseSetting match {
       case Some(databaseInfo) => {
-        val urlPattern = "\\w+://(\\w+):(\\w+)@([\\w-.]+)/([\\w-]+)".r
-    
+        val urlPattern = """[^:]+://([^:]+):([^@]+)@([^/]+)/(.+)""".r
+
         val urlPattern(user, password, host, database) = databaseInfo
         ("jdbc:postgresql://"+host+"/"+database, user, password)
       }
       case None => {
-        (GlobalProperties.get(DatabaseUrlProperty, null),
-         GlobalProperties.get(UsernameProperty, null),
-         GlobalProperties.get(PasswordProperty, null))
+        (GlobalProperties.get(databaseUrlProperty, null),
+         GlobalProperties.get(usernameProperty, null),
+         GlobalProperties.get(passwordProperty, null))
       }
 	}
   }
