d0652b27bf3afab9cc750b783104d2c24b2499ff
jstray <jonathanstray@gmail.com>
2012-08-21 16:49:06 -0700

    Main JobHandler loop now actor-based

--- worker/src/main/scala/JobHandler.scala
+++ worker/src/main/scala/JobHandler.scala
@@ -5,17 +5,75 @@
  * @author Jonas Karlsson
  */
 
+package overview.clustering
 
+import akka.actor._
+import akka.util.duration._
 import com.jolbox.bonecp._
 
 import database.{DatabaseConfiguration, DataSource, DB}
+import overview.logging._
+import overview.WorkerActorSystem
 import persistence._
 import persistence.DocumentSetCreationJobState._
 
-import overview.logging._
-import overview.clustering._
+case class ScanForJobs()
+case class JobDone()
+case class JobProgress(job:PersistentDocumentSetCreationJob, percentComplete:Int, stageName:String, statusText:String)
 
+class JobHandler extends Actor {
+    
+    val pollingInterval = 500 milliseconds
+    
+    // waits specified interval, then send self a message to look for more jobs
+    def waitAndThenScan : Unit = {
+      WorkerActorSystem().scheduler.scheduleOnce(pollingInterval, self, ScanForJobs);
+    }
+    
+    // Run each job currently listed in the database
+    def handleJobs : Unit  = {
+    
+      val submittedJobs: Seq[PersistentDocumentSetCreationJob] = DB.withConnection { implicit connection =>
+       PersistentDocumentSetCreationJob.findAllSubmitted
+      }
+
+      for (j <- submittedJobs) {
+        handleSingleJob(j)
+      }
+    }
+    
+    // Run a single job
+    def handleSingleJob(j:PersistentDocumentSetCreationJob) : Unit = {
+      val documentSetWriter = new DocumentSetWriter(j.userId)      
+      val documentSetId = DB.withConnection { implicit connection => documentSetWriter.write(j.query) }
+
+      Logger.info("Created document set for query: " + j.query)
+
+      val documentWriter = new DocumentWriter(documentSetId)
+      val nodeWriter = new NodeWriter(documentSetId)
+      val indexer = new DocumentSetIndexer(new DocumentCloudSource(j.query), nodeWriter, documentWriter, self)
+      val tree = indexer.BuildTree()
+
+      j.state = Complete
+      DB.withConnection { implicit connection =>
+        j.update
+      }
+    }
+      
+    def receive = { 
+      case ScanForJobs =>
+       handleJobs
+       waitAndThenScan
+        
+      case prog:JobProgress =>
+        // TODO write status to DB here, for display by client
+        Logger.info("Job progress: " + prog.stageName + ", " + prog.percentComplete + "% done. " + prog.statusText)      
+    }
+  }
+
+  
 object JobHandler {
+  
   def main(args: Array[String]) {
 
     val config = new DatabaseConfiguration()
@@ -23,38 +81,14 @@ object JobHandler {
 	
     DB.connect(dataSource)
     
-	  while (true) {
-      Thread.sleep(500) 
-      
+    // Create the job handling actor and kick it off
+    val jobHandler = WorkerActorSystem().actorOf( Props(new JobHandler), name = "jobHandler")
 
-      val submittedJobs: Seq[PersistentDocumentSetCreationJob] = DB.withConnection { implicit connection =>
-       PersistentDocumentSetCreationJob.findAllSubmitted
-      }
-
-      for (j <- submittedJobs) {
-        
-        val documentSetWriter = new DocumentSetWriter(j.userId)
-        
-        val documentSetId = DB.withConnection { implicit connection => 
-          documentSetWriter.write(j.query)
-        }
-
-
-        Logger.info("Created document set for query: " + j.query)
-
-        val documentWriter = new DocumentWriter(documentSetId)
-        val nodeWriter = new NodeWriter(documentSetId)
-        val indexer = 
-          new DocumentSetIndexer(new DocumentCloudSource(j.query), nodeWriter, documentWriter)
-        
-        val tree = indexer.BuildTree()
-
-        j.state = Complete
-        DB.withConnection { implicit connection =>
-          j.update
-        }
-      }
-      
+    jobHandler ! ScanForJobs
+    
+    while (true) {
+      Thread.sleep(100000)    // sleep this thread forever, as the actor is self-looping
     }
   }
+  
 }
