67298fe9ec511cc24d55c4a00191506f08880776
jstray <jonathanstray@gmail.com>
2012-08-22 17:35:02 -0700

    De-actorify JobHandler

--- worker/src/main/scala/JobHandler.scala
+++ worker/src/main/scala/JobHandler.scala
@@ -5,97 +5,69 @@
  * @author Jonas Karlsson
  */
 
-package overview.clustering
-
-import akka.actor._
-import akka.util.duration._
 import com.jolbox.bonecp._
 
 import database.{DatabaseConfiguration, DataSource, DB}
-import overview.util.{Logger,WorkerActorSystem}
+import overview.clustering._
+import overview.util.Logger
 import overview.util.Progress._
 import persistence._
 import persistence.DocumentSetCreationJobState._
 
-case class ScanForJobs()
-case class JobDone()
-case class JobProgress(percentComplete:Int, stageName:String, statusText:String)
-
-class JobHandler extends Actor {
-    
-    val pollingInterval = 500 milliseconds
-    
-    // waits specified interval, then send self a message to look for more jobs
-    def waitAndThenScan : Unit = {
-      WorkerActorSystem().scheduler.scheduleOnce(pollingInterval, self, ScanForJobs);
+object JobHandler {
+ 
+  // Run a single job
+  def handleSingleJob(j:PersistentDocumentSetCreationJob) : Unit = {
+    j.state = InProgress
+    DB.withConnection { implicit connection =>
+      j.update
     }
-    
-    // Run each job currently listed in the database
-    def handleJobs : Unit  = {
-    
-      val submittedJobs: Seq[PersistentDocumentSetCreationJob] = DB.withConnection { implicit connection =>
-        PersistentDocumentSetCreationJob.findAllSubmitted
-      }
+    val documentSetId = j.documentSetId
 
-      for (j <- submittedJobs) {
-        handleSingleJob(j)
-      }
+    val documentWriter = new DocumentWriter(documentSetId)
+    val nodeWriter = new NodeWriter(documentSetId)
+    def progFn(prog:Progress) = { 
+      println("PROGRESS: " + prog.percent + "% done. " + prog.status + ", " + (if (prog.hasError) "ERROR" else "OK")) ; false 
     }
-    
-    // Run a single job
-    def handleSingleJob(j:PersistentDocumentSetCreationJob) : Unit = {
-      j.state = InProgress
-      DB.withConnection { implicit connection =>
-        j.update
-      }
-      val documentSetId = j.documentSetId
+    val query = DB.withConnection { implicit connection => 
+      DocumentSetLoader.loadQuery(j.documentSetId).get
+    } 
+   
+    val indexer = new DocumentSetIndexer(new DocumentCloudSource(query), nodeWriter, documentWriter, progFn)
 
-      val documentWriter = new DocumentWriter(documentSetId)
-      val nodeWriter = new NodeWriter(documentSetId)
-      def progFn(prog:Progress) = { 
-        println("PROGRESS: " + prog.percent + "% done. " + prog.status + ", " + (if (prog.hasError) "ERROR" else "OK")) ; false 
-      }
-      val query = DB.withConnection { implicit connection => 
-        DocumentSetLoader.loadQuery(j.documentSetId).get
-      } 
-      val indexer = new DocumentSetIndexer(new DocumentCloudSource(query), nodeWriter, documentWriter, progFn)
-      Logger.info("Indexing query: " + query)
-      val tree = indexer.BuildTree()
+    Logger.info("Indexing query: " + query)
+    val tree = indexer.BuildTree()
 
-      j.state = Complete
-      DB.withConnection { implicit connection =>
-        j.update
-      }
-    }
-      
-    def receive = { 
-      case ScanForJobs =>
-       handleJobs
-       waitAndThenScan
-        
-      case prog:Progress =>
-        // TODO write status to DB here, for display by client
-        println("PROGRESS: " + prog.percent + "% done. " + prog.status + ", " + (if (prog.hasError) "ERROR" else "OK"))      
+    j.state = Complete
+    DB.withConnection { implicit connection =>
+      j.update
     }
   }
-
+    
+  // Run each job currently listed in the database
+  def scanForJobs : Unit  = {
   
-object JobHandler {
+    val submittedJobs: Seq[PersistentDocumentSetCreationJob] = DB.withConnection { implicit connection =>
+      PersistentDocumentSetCreationJob.findAllSubmitted
+    }
+  
+    for (j <- submittedJobs) {
+      handleSingleJob(j)
+    }
+  }
   
   def main(args: Array[String]) {
+
+    val pollingInterval = 500 //milliseconds
 
     val config = new DatabaseConfiguration()
 	  val dataSource = new DataSource(config)
 	
     DB.connect(dataSource)
     
-    // Create the job handling actor and kick it off
-    val jobHandler = WorkerActorSystem().actorOf( Props(new JobHandler), name = "jobHandler")
-
-    jobHandler ! ScanForJobs
-    
     while (true) {
-      Thread.sleep(100000)    // sleep this thread forever, as the actor is self-looping
+      scanForJobs
+      Thread.sleep(pollingInterval)
     }
   }
   
