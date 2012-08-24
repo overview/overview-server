94314d684a1d1caac6a2927124ea4e2c3d204ff6
Jonas Karlsson <thejonas@gmail.com>
2012-08-24 14:48:55 -0400

    test controller, using authenticated session

--- test/controllers/DocumentSetControllerSpec.scala
+++ test/controllers/DocumentSetControllerSpec.scala
@@ -3,32 +3,53 @@ package controllers
 import org.specs2.mutable._
 import org.specs2.specification._
 import org.squeryl.PrimitiveTypeMode._
-import play.api.test._
+import play.api.test.{FakeApplication, FakeRequest}
 import play.api.test.Helpers._
+import play.api.Play.{start, stop}
+
 
 import models.orm.{DocumentSetCreationJob, Schema}
-import helpers.DbContext
+import helpers.DbTestContext
 
 class DocumentSetControllerSpec extends Specification {
+  step(start(FakeApplication()))
+  
+  trait AuthorizedSession extends DbTestContext {
+    lazy val login = controllers.SessionController.create() (FakeRequest().
+      withFormUrlEncodedBody(("email", "admin@overview-project.org"),
+                             ("password", "admin@overview-project.org")))
+                               
+    lazy val sessionCookie = cookies(login)("PLAY_SESSION")
+  }
   
   "The DocumentSet Controller" should {
-    "submit a DocumentSetCreationJob when a new query is received" in new DbContext {
-      //val result = controllers.DocumentSetController.create() (FakeRequest().
-      //  withFormUrlEncodedBody(("query", "foo")))	
-      //val foundJob = 
-      //  Schema.documentSetCreationJobs.where(d => d.query === "foo").headOption
+    
+    "submit a DocumentSetCreationJob when a new query is received" in new AuthorizedSession {
+      val authorizedRequest = FakeRequest().withFormUrlEncodedBody(("query", "foo")).
+      	        withCookies(sessionCookie)
 
-      //foundJob must beSome
-      //foundJob.get.query must beEqualTo("foo")
-    }.pendingUntilFixed
+      val maybeDocumentSet = Schema.documentSets.headOption
+      maybeDocumentSet must beSome
+      val documentSet = maybeDocumentSet.get
+      
+      val foundJob = 
+        Schema.documentSetCreationJobs.
+          where(d => d.documentSetId === documentSet.id).headOption
+
+      foundJob must beSome
+      foundJob.get.state must beEqualTo(DocumentSetCreationJob.State.NotStarted)
+    }
     
       
-    "redirect to documentsets view" in new DbContext {
-      val result = controllers.DocumentSetController.create() (FakeRequest().
-        withFormUrlEncodedBody(("query", "foo")))
+    inExample("redirect to documentsets view") in new AuthorizedSession {
+      val authorizedRequest = FakeRequest().withFormUrlEncodedBody(("query", "foo")).
+      	        withCookies(sessionCookie)
 
-      redirectLocation(result).getOrElse("No redirect") must be equalTo("/create")
-    }.pendingUntilFixed
+      val result = controllers.DocumentSetController.create()(authorizedRequest)
+        
+      redirectLocation(result).getOrElse("No redirect") must be equalTo("/documentsets")
+    }
   }
-
+  
+  step(stop)
 }
