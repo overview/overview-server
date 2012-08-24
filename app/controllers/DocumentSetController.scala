1f7f89b0951f3ab40406ff88d46bcae9ec076df0
Jonas Karlsson <thejonas@gmail.com>
2012-08-24 16:44:51 -0400

    unneeded this

--- app/controllers/DocumentSetController.scala
+++ app/controllers/DocumentSetController.scala
@@ -9,7 +9,7 @@ import models.orm.{DocumentSet,DocumentSetCreationJob}
 import models.orm.DocumentSet.ImplicitHelper._
 
 object DocumentSetController extends BaseController {
-  def index() = authorizedAction(anyUser)(user => this.authorizedIndex(user)(_: Request[AnyContent], _: Connection))
+  def index() = authorizedAction(anyUser)(user => authorizedIndex(user)(_: Request[AnyContent], _: Connection))
   def show(id: Long) = authorizedAction(userOwningDocumentSet(id))(user => authorizedShow(user, id)(_: Request[AnyContent], _: Connection))
   def showJson(id: Long) = authorizedAction(userOwningDocumentSet(id))(user => authorizedShowJson(user, id)(_: Request[AnyContent], _: Connection))
   def create() = authorizedAction(anyUser)(user => authorizedCreate(user)(_: Request[AnyContent], _: Connection))
