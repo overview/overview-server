aceb8b244f06a9f45b84cad59a7ee0268e0eb4ee
Jonas Karlsson <thejonas@gmail.com>
2012-08-24 10:47:54 -0400

    Check that user owns the document before showing it

--- app/controllers/DocumentController.scala
+++ app/controllers/DocumentController.scala
@@ -1,19 +1,22 @@
 package controllers
 
+import java.sql.Connection
 import models.DocumentLoader
-import play.api.mvc.{Action,Controller}
+import play.api.mvc.{Action,AnyContent, Request}
 import play.api.db.DB
 import play.api.Play.current
 
-object DocumentController extends Controller {
-    def show(documentId: Long) = Action {
-      DB.withTransaction { implicit connection =>
-      	val documentLoader = new DocumentLoader()
-      	val document = documentLoader.load(documentId)
-      	document match {
-      	  case Some(d) => Ok(views.html.Document.show(d))
-      	  case None => NotFound
-      	}
-      }
+object DocumentController extends BaseController {
+  def show(documentId: Long) =
+    authorizedAction(userOwningDocument(documentId))(user =>
+      this.authorizedShow(documentId)(_: Request[AnyContent], _: Connection))
+
+  def authorizedShow(documentId: Long)(implicit request: Request[AnyContent], connection: Connection) = {
+    val documentLoader = new DocumentLoader()
+    val document = documentLoader.load(documentId)
+    document match {
+      case Some(d) => Ok(views.html.Document.show(d))
+      case None => NotFound
     }
+  }
 }
