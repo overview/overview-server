ce3487e22b595cf71926e8558e7c1ed57eabc9c2
Jonas Karlsson <thejonas@gmail.com>
2012-08-24 11:26:58 -0400

    Get test to work again

--- test/models/orm/DocumentSetCreationJobSpec.scala
+++ test/models/orm/DocumentSetCreationJobSpec.scala
@@ -17,26 +17,34 @@ class DocumentSetCreationJobSpec extends Specification {
     
     // Need inExample because Squeryl messes up implicit conversion
     inExample("save query and user to database") in new DbTestContext {
-      //val adminUser = "admin@overview-project.org"
-      //val query = "query"
-      //  
-      //val maybeUser = Schema.users.where(u => u.email === adminUser).headOption
-      //
-      //maybeUser must beSome
-      //val user = maybeUser.get
-      //
-      //val job = new DocumentSetCreationJob(query)
-      //user.documentSetCreationJobs.associate(job)
-      //
-      //user.documentSetCreationJobs must contain(job)
-      //
-      //val maybeUpdatedUser = Schema.users.where(u => u.email === adminUser).headOption
-      //
-      //maybeUpdatedUser must beSome
-      //val updatedUser = maybeUpdatedUser.get
-      //
-      //updatedUser.documentSetCreationJobs must haveTheSameElementsAs(Seq(job))
-    }.pendingUntilFixed
+      val adminUser = "admin@overview-project.org"
+      val query = "query"
+        
+      val maybeUser = Schema.users.where(u => u.email === adminUser).headOption
+      
+      maybeUser must beSome
+      val user = maybeUser.get
+      
+      val documentSet = user.createDocumentSet(query)
+      val job = documentSet.createDocumentSetCreationJob()
+      
+      val maybeUpdatedUser = Schema.users.where(u => u.email === adminUser).headOption
+      
+      maybeUpdatedUser must beSome
+      val updatedUser = maybeUpdatedUser.get
+      
+      val foundDocumentSet = 
+        updatedUser.documentSets.where(ds => ds.id === documentSet.id).headOption.get
+      
+     foundDocumentSet.documentSetCreationJob must beNone
+     
+     val docSetWithJobLoaded = foundDocumentSet.withCreationJob
+     
+     docSetWithJobLoaded.documentSetCreationJob must beSome
+     val foundJob = docSetWithJobLoaded.documentSetCreationJob.get
+     
+     foundJob.id must be equalTo(job.id)
+    }
   }
   
   step(stop)
