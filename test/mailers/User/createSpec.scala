cabea132e30effb3c9038ddb573e6f5c52b4e2f1
Adam Hooper <adam@adamhooper.com>
2012-08-28 13:18:25 -0400

    Create mailers.User.create()

---
+++ test/mailers/User/createSpec.scala
@@ -0,0 +1,31 @@
+package mailers.User
+
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+import play.api.Play.{start,stop}
+import play.api.i18n.Lang
+import play.api.test.{FakeApplication,FakeRequest}
+
+class createSpec extends Specification {
+  step(start(FakeApplication()))
+
+  trait OurContext extends Scope {
+    val user = new models.orm.User("email@example.org", "password-hash")
+    val lang = Lang("fu", "BA")
+    val request = FakeRequest("POST", "https://example.org/user/create")
+
+    lazy val mailer = create(user)(lang, request)
+  }
+
+  "create()" should {
+    "send to the user and only the user" in new OurContext {
+      mailer.recipients.must(equalTo(Seq(user.email)))
+    }
+
+    "include the confirmation URL" in new OurContext {
+      mailer.text.must(contain("user.confirmationToken")) // FIXME remove the quotes
+    }
+  }
+
+  step(stop)
+}
