0f9e94d520660e684a2c6c811edccdf8c1423d7f
Jonas Karlsson <thejonas@gmail.com>
2012-08-29 14:31:12 -0400

    simplify

--- test/models/orm/UserSpec.scala
+++ test/models/orm/UserSpec.scala
@@ -1,9 +1,8 @@
 package models.orm
 
-import anorm._
-import anorm.SqlParser._
 import helpers.DbTestContext
 import org.specs2.mutable.Specification
+import org.squeryl.PrimitiveTypeMode._
 import play.api.test.FakeApplication
 import play.api.Play.{ start, stop }
 import ua.t3hnar.bcrypt._
@@ -14,48 +13,35 @@ class UserSpec extends Specification {
 	val query = "query"
 	val email = "foo@bar.net"
 	val rawPassword = "raw password"
-	val hashedPassword = "pretend hash"
+	val hashedPassword = rawPassword.bcrypt(7)
+	val user = new User(email, hashedPassword)
   }
   
   step(start(FakeApplication()))
   
   "User" should {
     
-    "Create a DocumentSet" in new DbTestContext {
-      val query = "query"
-      val email = "foo@bar.net"
-      val password = "pencil69"
-      		
-      val user = new User(email, password)
+    inExample("Create a DocumentSet") in new UserContext {
       Schema.users.insert(user)
       
       val documentSet = user.createDocumentSet(query)
       
       documentSet.users must haveTheSameElementsAs(Seq(user))
-
-      val maybeResult = SQL("SELECT document_set_id, user_id FROM document_set_user").
-          as(long("document_set_id") ~ long("user_id") map(flatten) singleOpt)
-          
-      maybeResult must beSome
-      val (documentSetId, userId) = maybeResult.get
       
-      documentSetId must be equalTo(documentSet.id)
-      userId must be equalTo(user.id)
+      val documentSetUserLink = Schema.documentSetUsers.where(dsu => 
+        dsu.documentSetId === documentSet.id and dsu.userId === user.id
+      ).headOption
+      
+      documentSetUserLink must beSome
     }
     
-    "Not create a DocumentSet if not inserted in database" in new DbTestContext {
-      val query = "query"
-      val email = "foo@bar.net"
-      val password = "pencil69"
-      		
-      val user = new User(email, password)
-            
+    "Not create a DocumentSet if not inserted in database" in new UserContext {
       user.createDocumentSet(query) must throwAn[IllegalArgumentException]
     }
     
     "prepare new registration with hashed password" in new UserContext {
-      val user = User.prepareNewRegistration(email, rawPassword)
-      rawPassword.isBcrypted(user.passwordHash) must beTrue
+      val newUser = User.prepareNewRegistration(email, rawPassword)
+      rawPassword.isBcrypted(newUser.passwordHash) must beTrue
     }
   }
   
