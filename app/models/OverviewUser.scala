9453732fb52ce4a35aa00cfeaf217fb183ce18aa
Jonas Karlsson <thejonas@gmail.com>
2012-09-03 06:56:02 -0400

    added scaladocs

--- app/models/OverviewUser.scala
+++ app/models/OverviewUser.scala
@@ -5,34 +5,63 @@ import models.orm.User
 import org.joda.time.DateTime.now
 import ua.t3hnar.bcrypt._
 
+/**
+ * A user that exists in the database
+ */
 trait OverviewUser {
   val id: Long
   val email: String
   
   def passwordMatches(password: String): Boolean
+  
+  /** @return None if the user has no open confirmation request */
   def withConfirmationRequest: Option[OverviewUser with ConfirmationRequest]
   
   def save: Unit
 }
 
+/**
+ * A user that has an open confirmation request (confirmationToken exists)
+ */
 trait ConfirmationRequest {
   val confirmationToken: String
   val confirmationSentAt: Timestamp
   
+  /** 
+   * After confirming, the values in ConfirmationRequest will still
+   * be available, though the actual user will no longer have
+   * a confirmationToken. It's better to work with the returned OverviewUser
+   * to avoid any inconsistencies. The user must be saved before confirm
+   * takes effect.
+   */
   def confirm: OverviewUser
 }
 
+/**
+ * A user that may or may not exist yet, and in an unknown state.
+ * Different methods attempt to convert the user into a known state,
+ * returning None if conversion can't be completed.
+ */
 case class PotentialUser(val email: String, val password: String) {
   private val user = OverviewUser.findByEmail(email)
   
+  /**
+   * @return OverviewUser if exists, without checking password
+   */
   def withValidEmail: Option[OverviewUser] = {
     user  
   }
   
+  /**
+   * @return OverviewUser if password is correct 
+   */
   def withValidCredentials: Option[OverviewUser] = {
     user.find(u => u.passwordMatches(password))
   }
   
+  /**
+   * @return OverviewUser with ConfirmationRequest if the user has an active confirmation request.
+   */
   def withConfirmationRequest: Option[OverviewUser with ConfirmationRequest] = {
     user match {
       case Some(u) => u.withConfirmationRequest
@@ -40,11 +69,21 @@ case class PotentialUser(val email: String, val password: String) {
     }
   }
   
+  
+  /**
+   * @return the OverviewUser with ConfirmationRequest. No matter what the state the user is in
+   * a new confirmation request can always be generated. User must be saved before change takes
+   * effect.
+   */
   def requestConfirmation: OverviewUser with ConfirmationRequest = 
     OverviewUser.prepareNewRegistration(email, password)
 
 }
 
+
+/**
+ * Helpers to get new or existing OverviewUsers 
+ */
 object OverviewUser {
 
   def findById(id: Long): Option[OverviewUser] = create(User.findById(id))
@@ -72,6 +111,30 @@ object OverviewUser {
     userData.map(new OverviewUserImpl(_))
   }
   
+  /**
+   * Underlying implementation that manages the User object that is the conduit to the
+   * database. As the user state is transformed, the underlying User is modified and
+   * passed along
+   */
+  private class OverviewUserImpl(user: User) extends OverviewUser {
+    val id = user.id
+    val email = user.email
+    
+    def passwordMatches(password: String): Boolean = {
+      password.isBcrypted(user.passwordHash)
+    }
+    
+    def withConfirmationRequest: Option[OverviewUser with ConfirmationRequest] = {
+      if (user.confirmationToken.isDefined) Some(new UnconfirmedUser(user))
+      else None
+    }
+    
+    def save: Unit = user.save
+  }
+  
+  /**
+   * A User with an active confirmation request
+   */
   private class UnconfirmedUser(user: User) extends OverviewUserImpl(user) with ConfirmationRequest {
     val confirmationToken = user.confirmationToken.get
     val confirmationSentAt = user.confirmationSentAt.get
@@ -84,21 +147,4 @@ object OverviewUser {
     }
   }
   
-  private class OverviewUserImpl(user: User) extends OverviewUser {
-    val id = user.id
-    val email = user.email
-    
-    def passwordMatches(password: String): Boolean = {
-      password.isBcrypted(user.passwordHash)
-    }
-    
-    def withConfirmationRequest: Option[OverviewUser with ConfirmationRequest] = {
-      if (user.confirmationToken.isDefined) {
-        Some(new UnconfirmedUser(user))
-      }
-      else None
-    }
-    
-    def save: Unit = user.save
-  }
 }
