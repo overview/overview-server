391a06415dc9baf148f8e9f3399a57f7d00ac8da
Jonas Karlsson <thejonas@gmail.com>
2012-09-06 11:02:06 -0400

    Try to enforce https on Heroku

---
+++ app/controllers/HttpsEnforcer.scala
@@ -0,0 +1,23 @@
+package controllers
+
+import play.api.mvc._
+import play.api.mvc.Results.Redirect
+
+trait HttpsEnforcer {
+    
+  def HttpsAction(f: Request[AnyContent] => Result): Action[AnyContent] = {
+    HttpsAction(BodyParsers.parse.anyContent)(f)
+  }
+    
+  
+  def HttpsAction[A](bp: BodyParser[A])(f: Request[A] => Result): Action[A] = {
+    Action(bp) { request =>
+      request.headers.get("x-forwarded-proto") match {
+        case Some("https") => f(request)
+        case Some("http") => Redirect("https://" + request.host + request.uri)
+        case _ => f(request)
+      }
+    }
+  }
+}
+
