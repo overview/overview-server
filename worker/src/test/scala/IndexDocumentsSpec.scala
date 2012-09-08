5b5c50b27615c9b13de18ccf44169fe74c8fa0f7
jstray <jonathanstray@gmail.com>
2012-09-08 16:55:09 -0400

    Store terms in an indexable string table, which should reduce memory usage.

--- worker/src/test/scala/IndexDocumentsSpec.scala
+++ worker/src/test/scala/IndexDocumentsSpec.scala
@@ -51,30 +51,34 @@ class MakeDocumentTreeSpec extends Specification {
       vectorGen.addDocument(3, doc3)
       vectorGen.addDocument(4, doc4)
        
+      // Lookup the ID's of the words we're going to check
+      val catId = vectorGen.stringToId("cat")
+      val ratId = vectorGen.stringToId("rat")
+      
       // Check intermediate inverse document frequency (idf) vals. In this case only terms which appear in 3 docs are preserved
       val idf = vectorGen.Idf()
-      idf.get("cat").get must beEqualTo(computeIDF(4,3)) 
-      idf.get("rat").get must beEqualTo(computeIDF(4,3))
+      idf.get(catId).get must beEqualTo(computeIDF(4,3)) 
+      idf.get(ratId).get must beEqualTo(computeIDF(4,3))
       idf.size must beEqualTo(2)      
       
       // Finally, check actual vectors. 
       val vecs = vectorGen.documentVectors()
        
       // doc1: only cat remains
-      vecs.get(1).get must beEqualTo(Map("cat"->1.0)) 
+      vecs.get(1).get must beEqualTo(Map(catId->1.0)) 
       
       // doc2: cat and rat have same freq, vector normalized
       val sqrhalf = math.sqrt(0.5).toFloat
-      vecs.get(2).get must_== Map("rat"->sqrhalf, "cat"->sqrhalf) 
+      vecs.get(2).get must_== Map(ratId->sqrhalf, catId->sqrhalf) 
 
       // doc3: only rat remains
-      vecs.get(3).get must beEqualTo(Map("rat"->1.0))  // only rat appears in 3 docs
+      vecs.get(3).get must beEqualTo(Map(ratId->1.0))  // only rat appears in 3 docs
       
       // doc4: cat and rat remain, with different weights (which we recompute from scratch here)
-      val ratTfIdf = doc4.count(_ == "rat").toFloat / doc4.length * idf.get("rat").get
-      val catTfIdf = doc4.count(_ == "cat").toFloat / doc4.length * idf.get("cat").get
+      val ratTfIdf = doc4.count(_ == "rat").toFloat / doc4.length * idf.get(ratId).get
+      val catTfIdf = doc4.count(_ == "cat").toFloat / doc4.length * idf.get(catId).get
       val len = math.sqrt(ratTfIdf*ratTfIdf + catTfIdf*catTfIdf).toFloat
-      vecs.get(4).get must beEqualTo(Map("rat"->ratTfIdf/len, "cat"->catTfIdf/len))
+      vecs.get(4).get must beEqualTo(Map(ratId->ratTfIdf/len, catId->catTfIdf/len))
     }
  
 
@@ -84,7 +88,7 @@ class MakeDocumentTreeSpec extends Specification {
       
       // load every doc in the test directory, generate terms and load into vector generator
       val filenames =  new File("worker/src/test/resources/docs").listFiles
-      val docterms = filenames.map(filename => (filename, Lexer.makeTerms(io.Source.fromFile(filename).getLines.reduceLeft(_+"\n"+_))))
+      val docterms = filenames.map(filename => (filename, Lexer.makeTerms(io.Source.fromFile(filename).mkString)))
       for ((filename,terms) <- docterms) {
         vectorGen.addDocument(filename.hashCode, terms)
       }
@@ -93,7 +97,7 @@ class MakeDocumentTreeSpec extends Specification {
       val idf = vectorGen.Idf()
  
       // idf words must be drawn from total document vocabulary         
-      val vocab = docterms.map(_._2).reduceLeft(_ ++ _).toSet
+      val vocab = docterms.map(_._2).reduceLeft(_ ++ _).map(vectorGen.stringToId(_)).toSet
       val idfVocab = idf.map(_._1).toSet
       idfVocab.subsetOf(vocab) must beTrue       
 
@@ -116,7 +120,7 @@ class MakeDocumentTreeSpec extends Specification {
         docvec must not beNull
 
         val docTerms = docvec.get.map(_._1).toSet
-        docTerms.subsetOf(terms.toSet) must beTrue    // all terms in vector must be in doc
+        docTerms.subsetOf(terms.map( vectorGen.stringToId(_) ).toSet) must beTrue    // all terms in vector must be in doc
         docTerms.subsetOf(idfVocab) must beTrue       // all terms in vector must be in IDF vocabulary
         
         val docWeights = docvec.get.map(_._2)
