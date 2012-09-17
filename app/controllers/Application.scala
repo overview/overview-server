bbe491f19757779af35b6c2361b84fe078f50201
Adam Hooper <adam@adamhooper.com>
2012-09-17 16:49:25 -0400

    Remove Application, our first Controller

--- app/controllers/Application.scala
+++
@@ -1,9 +0,0 @@
-package controllers
-
-import play.api.mvc.{Action,Controller}
-
-object Application  extends Controller with HttpsEnforcer {
-  def index() = HttpsAction { implicit request =>
-    Redirect(routes.DocumentSetController.index())
-  }
-}
