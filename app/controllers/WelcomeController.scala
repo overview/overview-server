45af03149ef3a1ab4d14ed5eff48ca9c75bc34b2
Adam Hooper <adam@adamhooper.com>
2012-09-18 11:33:52 -0400

    Revamp the welcome page. It only shows up when you aren't logged in.

--- app/controllers/WelcomeController.scala
+++ app/controllers/WelcomeController.scala
@@ -4,9 +4,16 @@ import java.sql.Connection
 import play.api.mvc.{Request,AnyContent}
 
 object WelcomeController extends BaseController with TransactionActionController {
+  val loginForm = controllers.forms.LoginForm()
+  val userForm = controllers.forms.UserForm()
+
   def show() = optionallyAuthorizedAction({user: Option[User] => optionallyAuthorizedShow(user)(_: Request[AnyContent], _: Connection)})
 
-  private def optionallyAuthorizedShow(user: Option[User])(implicit request: Request[AnyContent], connection: Connection) = {
-    Ok(views.html.Welcome.show(user))
+  private def optionallyAuthorizedShow(optionalUser: Option[User])(implicit request: Request[AnyContent], connection: Connection) = {
+    optionalUser.map(user =>
+      Redirect(routes.DocumentSetController.index())
+    ).getOrElse(
+      Ok(views.html.Welcome.show(loginForm, userForm))
+    )
   }
 }
