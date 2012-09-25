f4cc8cd03d15ec3d38986e1010e6e7e9c537093a
Jonas Karlsson <thejonas@gmail.com>
2012-09-24 20:27:39 -0400

    Characterization test prior to refactoring

---
+++ test/views/DocumentSet/showSpec.scala
@@ -0,0 +1,63 @@
+package views.json.DocumentSet
+
+import helpers.DbTestContext
+import models.orm.{DocumentSet, DocumentSetCreationJob}
+import models.orm.DocumentSetCreationJobState._
+import org.specs2.mock._
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+import play.api.libs.json.Json.toJson
+import play.api.Play.{ start, stop }
+import play.api.test.FakeApplication
+
+class showSpec extends Specification  {
+
+  
+  "DocumentSet view generated Json" should {
+
+    trait DocumentSetContext extends Scope {
+      val documentSet: DocumentSet
+      lazy val documentSetJson = show(documentSet).toString
+    }
+    
+    trait CompleteDocumentSetContext extends DocumentSetContext {
+      override val documentSet = DocumentSet(1, "a title", "a query", Some(20))
+    }
+
+    trait InProgressDocumentSetContext extends DocumentSetContext {
+      val job = DocumentSetCreationJob(1, Some("name"), Some("password"), InProgress,
+	.23, "running")
+      override val documentSet = DocumentSet(1, "a title", "a query", Some(20), Some(job))
+    }
+
+    trait NotStartedDocumentSetContext extends DocumentSetContext {
+      class FakeNotStartedJob extends
+	DocumentSetCreationJob(1, Some("name"), Some("password"), NotStarted, 23, "running") {
+	  override val jobsAheadInQueue = 5l
+	}
+
+      val job = new FakeNotStartedJob
+      override val documentSet = DocumentSet(1, "a title", "a query", Some(20), Some(job))
+    }
+
+  
+    "contain id, query, and html" in new CompleteDocumentSetContext {
+      documentSetJson must /("id" -> documentSet.id)
+      documentSetJson must /("query" -> documentSet.query)
+      documentSetJson must beMatching(""".*"html":"[^<]*<.*>[^>]*".*""")
+      documentSetJson must not contain("state")
+    }
+
+    "show in progress job" in new InProgressDocumentSetContext {
+      documentSetJson must /("state" -> "views.DocumentSet._documentSet.job_state.IN_PROGRESS")
+      documentSetJson must /("percent_complete" -> job.fractionComplete * 100)
+      documentSetJson must /("state_description" -> job.stateDescription)
+      documentSetJson must not contain("n_jobs_ahead_in_queue")
+    }
+
+    "show not started job" in new NotStartedDocumentSetContext {
+      documentSetJson must /("n_jobs_ahead_in_queue" -> 5)
+    }
+  }
+
+}
