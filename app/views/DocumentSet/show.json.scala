a044ba5cc1087ac9c7c679d25f722f14940de6b8
Jonas Karlsson <thejonas@gmail.com>
2012-09-25 09:23:23 -0400

    I18N for state description

--- app/views/DocumentSet/show.json.scala
+++ app/views/DocumentSet/show.json.scala
@@ -1,25 +1,28 @@
 package views.json.DocumentSet
 
+import models.orm.{ DocumentSet, DocumentSetCreationJob }
+import models.orm.DocumentSetCreationJobState._
 import play.api.i18n.Lang
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json.toJson
-
-import models.orm.{DocumentSet,DocumentSetCreationJob}
-import models.orm.DocumentSetCreationJobState._
-
 
 object show {
   private def m(key: String, args: Any*) = {
     play.api.i18n.Messages("views.DocumentSet._documentSet." + key, args: _*)
   }
 
+
+  private def stateDescription(description: String) : String = description match {
+    case "" => ""
+    case _ => m(description)
+  }
+  
   private def documentSetCreationJobProperties(job: DocumentSetCreationJob)(implicit lang: Lang) = {
     val notCompleteMap = Map(
       "state" -> toJson(m("job_state." + job.state.toString)),
       "percent_complete" -> toJson(math.round(job.fractionComplete * 100)),
-      "state_description" -> toJson(job.stateDescription)
+      "state_description" -> toJson(stateDescription(job.stateDescription))
     )
-
     val notStartedMap = job.state match {
       case NotStarted => Map("n_jobs_ahead_in_queue" -> toJson(job.jobsAheadInQueue))
       case _ => Map()
@@ -30,21 +33,23 @@ object show {
 
   private[DocumentSet] def documentSetProperties(documentSet: DocumentSet)(implicit lang: Lang) = {
     Map(
-      "html" -> toJson(views.html.DocumentSet._documentSet(documentSet).toString)
-    )
+      "html" -> toJson(views.html.DocumentSet._documentSet(documentSet).toString))
   }
 
-  private[DocumentSet] implicit def documentSetToJson(documentSet: DocumentSet) : JsValue = {
-    val map = Map(
+  private[DocumentSet] implicit def documentSetToJson(documentSet: DocumentSet): JsValue = {
+    val documentSetMap = Map(
       "id" -> toJson(documentSet.id),
-      "query" -> toJson(documentSet.query)
-    ) ++ (documentSet.documentSetCreationJob.map(documentSetCreationJobProperties)
-          .getOrElse(documentSetProperties(documentSet)))
+      "query" -> toJson(documentSet.query))
 
-    toJson(map)
+    val jobStatusMap = documentSet.documentSetCreationJob match {
+      case Some(documentSetCreationJob) => documentSetCreationJobProperties(documentSetCreationJob)
+      case None => documentSetProperties(documentSet)
+    }
+
+    toJson(documentSetMap ++ jobStatusMap)
   }
 
-  def apply(documentSet: DocumentSet) : JsValue = {
+  def apply(documentSet: DocumentSet): JsValue = {
     documentSetToJson(documentSet)
   }
 }
