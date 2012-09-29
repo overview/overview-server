94dacb999a3f3de984bbddfddaf96167e8904d04
Jonas Karlsson <thejonas@gmail.com>
2012-09-29 15:59:40 -0400

    Extract common setup into context.

--- test/models/PersistentTagSaverSpec.scala
+++ test/models/PersistentTagSaverSpec.scala
@@ -9,55 +9,53 @@ import play.api.Play.{ start, stop }
 import play.api.test.FakeApplication
 
 class PersistentTagSaverSpec extends Specification {
-	
+
   step(start(FakeApplication()))
-  
+
   "PersistentTagSaver" should {
-    
-    "add a new tag to the database, returning id" in new DbTestContext {
-      val documentSetId = insertDocumentSet("TagSaverSpec")
-      val tagSaver = new PersistentTagSaver()
-      val name = "a tag"
-        
-      val id = tagSaver.save(documentSetId, name)
 
-      id must not beNone
-      
-      val tagId  = SQL("SELECT id FROM tag WHERE name = {name}").
-       				on("name" -> name).as(scalar[Long] *).headOption
-      
-      id must be equalTo(tagId)												            
+    trait SavedTag extends DbTestContext {
+      lazy val documentSetId = insertDocumentSet("TagSaverSpec")
+      lazy val name = "a tag"
+      lazy val tagSaver = new PersistentTagSaver()
+      lazy val tagId = tagSaver.save(documentSetId, name)
+
+      def findTag: Option[Long] = SQL("SELECT id FROM tag WHERE name = {name}").
+        on("name" -> name).as(scalar[Long] *).headOption
+
     }
-    
-    "return None if attempting to add already existing tag" in new DbTestContext {
-      val documentSetId = insertDocumentSet("TagSaverSpec")
-      val tagSaver = new PersistentTagSaver()
-      val name = "a tag"
 
-      val goodId = tagSaver.save(documentSetId, name)
+    trait TaggedDocument extends SavedTag {
+      override def setupWithDb = {
+        val documentId = insertDocument(documentSetId, "title", "dcId")
+        tagDocuments(tagId.get, Seq(documentId))
+      }
+    }
+
+    "add a new tag to the database, returning id" in new SavedTag {
+      tagId must not beNone
+
+      val foundId = findTag
+      tagId must be equalTo (foundId)
+    }
+
+    "return None if attempting to add already existing tag" in new SavedTag {
+      tagId must beSome
       val noId = tagSaver.save(documentSetId, name)
-      
+
       noId must beNone
     }
 
-    "delete a tag" in new DbTestContext {
-      val documentSetId = insertDocumentSet("TagSaverSpec")
-      val tagName = "a tag" 
-      val tagId = insertTag(documentSetId, tagName)
-      val documentId = insertDocument(documentSetId, "title", "dcId")
-      tagDocuments(tagId, Seq(documentId))
-      
-      val tagSaver = new PersistentTagSaver()
-      val rowsDeleted = tagSaver.delete(tagId)
+    "delete a tag" in new TaggedDocument {
+      val rowsDeleted = tagSaver.delete(tagId.get)
 
-      rowsDeleted must be equalTo(2)
-      
-      val noTag = SQL("SELECT id FROM tag WHERE id = {tagId}").on("tagId" -> tagId).
-        as(scalar[Long] *).headOption
+      rowsDeleted must be equalTo (2)
 
-      noTag must beNone
+      val deletedTag = findTag
+
+      deletedTag must beNone
     }
   }
-  
+
   step(stop)
 }
