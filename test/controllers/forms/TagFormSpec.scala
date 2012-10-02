ebbb1a589f2dc448f8b3d63c54ca46a198ee74a9
Jonas Karlsson <thejonas@gmail.com>
2012-10-02 12:10:41 -0400

    test for empty values in fields

--- test/controllers/forms/TagFormSpec.scala
+++ test/controllers/forms/TagFormSpec.scala
@@ -1,22 +1,37 @@
 package controllers.forms
 
 import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
 
 class TagFormSpec extends Specification {
 
   "TagForm" should {
 
-    "extract name and color" in {
+    trait ValidForm extends Scope {
       val name = "tagName"
       val color = "#12abcc"
       
       val tagForm = TagForm().bind(Map("name" -> name, "color" -> color))
-      
+    }
+
+    trait EmptyForm extends Scope {
+      val tagForm = TagForm().bind(Map("name" -> "", "color" -> ""))
+    }
+    
+    "extract name and color" in new ValidForm {
       val (nameValue, colorValue) = tagForm.get
       
       nameValue must be equalTo(name)
       colorValue must be equalTo(color)
     }
 
+    "reject empty values" in new EmptyForm {
+      val nameError = tagForm.error("name")
+      val colorError = tagForm.error("color")
+
+      nameError must beSome.like { case e => e.message must be equalTo("error.required") }
+      colorError must beSome.like { case e => e.message must be equalTo("error.required") }
+
+    }
   }
 }
