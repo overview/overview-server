4a6c149b3d51aeda8a084af4d3ce55af209633d3
Jonas Karlsson <thejonas@gmail.com>
2012-10-02 15:59:21 -0400

    Add color field in tag json, if color is set.

--- app/views/json/helper/ModelJsonConverters.scala
+++ app/views/json/helper/ModelJsonConverters.scala
@@ -1,38 +1,42 @@
 package views.json.helper
 
-import models.core.{Document, DocumentIdList, Tag}
-import play.api.libs.json.{JsValue, Writes}
+import models.core.{ Document, DocumentIdList, Tag }
+import play.api.libs.json.{ JsValue, Writes }
 import play.api.libs.json.Json.toJson
 
 object ModelJsonConverters {
-  
+
   implicit object JsonDocumentIdList extends Writes[DocumentIdList] {
-    override def writes(documentIdList: DocumentIdList) : JsValue = {
+    override def writes(documentIdList: DocumentIdList): JsValue = {
       toJson(Map(
-          "docids" -> toJson(documentIdList.firstIds),
-          "n" -> toJson(documentIdList.totalCount)
-      ))
+        "docids" -> toJson(documentIdList.firstIds),
+        "n" -> toJson(documentIdList.totalCount)))
     }
   }
 
   implicit object JsonTag extends Writes[Tag] {
     override def writes(tag: Tag): JsValue = {
-      toJson(Map(
+      val tagValues = Map(
         "id" -> toJson(tag.id),
         "name" -> toJson(tag.name),
-        "doclist" -> toJson(tag.documentIds)
-      ))
+        "doclist" -> toJson(tag.documentIds))
+
+      val tagColorValue = tag.color match {
+	case Some(c) => Map("color" -> toJson('#' + c))
+	case None => Nil
+      }
+
+      toJson(tagValues ++ tagColorValue)
     }
   }
 
   implicit object JsonDocument extends Writes[Document] {
-    override def writes(document: Document) : JsValue = {
+    override def writes(document: Document): JsValue = {
       toJson(Map(
         "id" -> toJson(document.id),
         "title" -> toJson(document.title),
         "tagids" -> toJson(document.tags),
-        "documentcloud_id" -> toJson(document.documentCloudId)
-      ))
+        "documentcloud_id" -> toJson(document.documentCloudId)))
     }
   }
 
--- test/views/json/helper/ModelJsonConvertersSpec.scala
+++ test/views/json/helper/ModelJsonConvertersSpec.scala
@@ -4,55 +4,65 @@ import models.core._
 import org.specs2.mutable.Specification
 import play.api.libs.json.Json.toJson
 
-
 class ModelJsonConvertersSpec extends Specification {
-  
+
   "JsonDocumentIdList" should {
     import views.json.helper.ModelJsonConverters.JsonDocumentIdList
-    
+
     "write documentIdList attributes" in {
       val ids = List(10l, 20l, 34l)
       val count = 45l
       val documentIdList = DocumentIdList(ids, count)
-      
+
       val documentIdListJson = toJson(documentIdList).toString
-      
+
       documentIdListJson must contain("\"docids\":" + ids.mkString("[", ",", "]"))
       documentIdListJson must /("n" -> count)
     }
   }
-    
+
   "JsonDocument" should {
     import views.json.helper.ModelJsonConverters.JsonDocument
-    
+
     "write document attributes" in {
       val id = 39l
       val title = "title"
       val document = Document(id, title, "unused by Tree", Seq(1, 2, 3))
-      
+
       val documentJson = toJson(document).toString
-      
+
       documentJson must /("id" -> id)
       documentJson must /("title" -> title)
       documentJson must contain(""""tagids":[1,2,3]""")
     }
   }
-  
-    
+
   "JsonTag" should {
     import views.json.helper.ModelJsonConverters.JsonTag
-    
+
     "write tag attributes" in {
       val id = 5l
       val name = "tag"
+      val color = "013370"
+      val colorForJs = "#" + color
       val documentCount = 22l
-      val tag = Tag(id, name, None, DocumentIdList(Seq(10l), documentCount))
-      
+      val tag = Tag(id, name, Some(color), DocumentIdList(Seq(10l), documentCount))
+
       val tagJson = toJson(tag).toString
-      
+
       tagJson must /("id" -> id)
       tagJson must /("name" -> name)
-      tagJson must /("doclist") */("n" -> documentCount)
+      tagJson must /("color" -> colorForJs)
+      tagJson must /("doclist") */ ("n" -> documentCount)
+    }
+
+    "omit color value if color is not set" in {
+      val noColor = None
+      val tag = Tag(0, "", noColor, DocumentIdList(Seq(10l), 4))
+
+      val tagJson = toJson(tag).toString
+
+      tagJson must not / ("color")
     }
   }
 }
