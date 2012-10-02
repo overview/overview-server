a8c4232791ca0bb0fee383f9ade4c65fc991360c
Jonas Karlsson <thejonas@gmail.com>
2012-10-02 13:56:49 -0400

    Clear up test setup

--- test/models/PersistentTagLoaderSpec.scala
+++ test/models/PersistentTagLoaderSpec.scala
@@ -16,6 +16,35 @@ class PersistentTagLoaderSpec extends Specification {
   trait TagSetup extends DbTestContext {
     lazy val documentSetId = insertDocumentSet("TagLoaderSpec")
     lazy val tagLoader = new PersistentTagLoader()
+    var tagId: Long = _
+
+    override def setupWithDb = {
+      tagId = insertTag(documentSetId, tagName)
+    }
+  }
+
+  trait NodesWithDocuments extends TagSetup {
+    var nodeIds: Seq[Long] = _
+    var documentIds: Seq[Long] = _
+
+    val numberOfNodes = 4 
+    val documentsPerNode = 4
+    
+    override def setupWithDb = {
+      super.setupWithDb
+      
+      nodeIds = insertNodes(documentSetId, numberOfNodes)
+      documentIds = insertDocumentsForeachNode(documentSetId, nodeIds, documentsPerNode)
+    }
+  }
+
+  trait TaggedDocuments extends NodesWithDocuments {
+
+    override def setupWithDb = {
+      super.setupWithDb
+
+      tagDocuments(tagId, documentIds)
+    }
   }
 
   step(start(FakeApplication()))
@@ -23,98 +52,71 @@ class PersistentTagLoaderSpec extends Specification {
   "PersistentTagLoader" should {
 
     "get tag id by name if it exists" in new TagSetup {
-      val tagId = insertTag(documentSetId, tagName)
-
       val foundTag = tagLoader.loadByName(documentSetId, tagName)
 
       foundTag must be equalTo (Some(tagId))
     }
 
     "get tag id by name from the correct document set" in new TagSetup {
-      val tagId1 = insertTag(documentSetId, tagName)
       val documentSetId2 = insertDocumentSet("OtherDocumentSet")
       val tagId2 = insertTag(documentSetId2, tagName)
       
       val foundTag1 = tagLoader.loadByName(documentSetId, tagName)
       val foundTag2 = tagLoader.loadByName(documentSetId2, tagName)
       
-      foundTag1 must beSome.like{ case t => t must be equalTo(tagId1) }
+      foundTag1 must beSome.like{ case t => t must be equalTo(tagId) }
       foundTag2 must beSome.like{ case t => t must be equalTo(tagId2) }
     }
 
     "get None if tag does not exist" in new TagSetup {
-      val missingTag = tagLoader.loadByName(documentSetId, tagName)
+      val missingTag = tagLoader.loadByName(documentSetId, "no tag")
 
       missingTag must beNone
     }
 
-    "count total number of documents tagged" in new TagSetup {
-      val documentIds = for (i <- 1 to 5) yield insertDocument(documentSetId, "title", "documentCloudId")
-
-      val tagId = insertTag(documentSetId, tagName)
-
+    "count total number of documents tagged" in new NodesWithDocuments {
       val initialCount = tagLoader.countDocuments(tagId)
-
       initialCount must be equalTo (0)
 
       tagDocuments(tagId, documentIds.take(3))
-
-      val count = tagLoader.countDocuments(tagId)
-      count must be equalTo (3)
+      val countAfterTag = tagLoader.countDocuments(tagId)
+      countAfterTag must be equalTo (3)
     }
 
-    "count tagged documents per node" in new TagSetup {
-      val nodeIds = insertNodes(documentSetId, 4)
-      val documentIds = insertDocumentsForeachNode(documentSetId, nodeIds, 4)
-
-      val tagId = insertTag(documentSetId, tagName)
-
-      tagDocuments(tagId, documentIds)
+    "count tagged documents per node" in new TaggedDocuments {
+      val expectedCounts = nodeIds.map((_, documentsPerNode))
       val counts = tagLoader.countsPerNode(nodeIds, tagId)
-      val expectedCounts = nodeIds.map((_, 4l))
 
       counts must haveTheSameElementsAs(expectedCounts)
     }
 
-    "insert 0 values for nodes with no tagged documents" in new TagSetup {
-      val nodeIds = insertNodes(documentSetId, 4)
-      val documentIds = insertDocumentsForeachNode(documentSetId, nodeIds, 4)
-
-      val tagId = insertTag(documentSetId, tagName)
-
-      val initialCounts = tagLoader.countsPerNode(nodeIds, tagId)
+    "insert 0 values for nodes with no tagged documents" in new NodesWithDocuments {
       val expectedInitialCounts = nodeIds.map((_, 0l))
+      val initialCounts = tagLoader.countsPerNode(nodeIds, tagId)
 
       initialCounts must haveTheSameElementsAs(expectedInitialCounts)
 
-      tagDocuments(tagId, documentIds.take(8))
+      tagDocuments(tagId, documentIds.take(2 * documentsPerNode))
 
+      val taggedNodeCounts = nodeIds.take(2).map((_, documentsPerNode))
+      val notTaggedNodeCounts = nodeIds.drop(2).map((_, 0l))
+      val expectedCounts = taggedNodeCounts ++ notTaggedNodeCounts
+      
       val counts = tagLoader.countsPerNode(nodeIds, tagId)
-      val expectedCounts = nodeIds.take(2).map((_, 4l)) ++ nodeIds.drop(2).map((_, 0l))
-
       counts must haveTheSameElementsAs(expectedCounts)
     }
 
-    "empty list returns counts for all nodes" in new TagSetup {
-      val nodeIds = insertNodes(documentSetId, 4)
-      val documentIds = insertDocumentsForeachNode(documentSetId, nodeIds, 4)
-
-      val tagId = insertTag(documentSetId, tagName)
-
-      tagDocuments(tagId, documentIds)
+    "empty list returns counts for all nodes" in new TaggedDocuments {
+      val expectedCounts = nodeIds.map((_, documentsPerNode))
       val counts = tagLoader.countsPerNode(Nil, tagId)
-      val expectedCounts = nodeIds.map((_, 4l))
 
       counts must haveTheSameElementsAs(expectedCounts)
     }
 
-    "return tag data for tag id" in new TagSetup {
-      val nodeIds = insertNodes(documentSetId, 1)
-      val documentIds = insertDocumentsForeachNode(documentSetId, nodeIds, 4)
-      val tagId = insertTag(documentSetId, tagName)
-      tagDocuments(tagId, documentIds)
-
-      val expectedTagData = documentIds.map(d => (tagId, tagName, 4, Some(d)))
+    "return tag data for tag id" in new TaggedDocuments {
+      val totalTagged = numberOfNodes * documentsPerNode
+      
+      val expectedTagData = documentIds.take(10).map(d => (tagId, tagName, totalTagged, Some(d)))
       val tagData = tagLoader.loadTag(tagId)
 
       tagData must haveTheSameElementsAs(expectedTagData)
