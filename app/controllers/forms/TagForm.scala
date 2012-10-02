69aa14b05b3a751899670782a6483f865d79b690
Jonas Karlsson <thejonas@gmail.com>
2012-10-02 12:24:29 -0400

    Extract valid color format regexp

--- app/controllers/forms/TagForm.scala
+++ app/controllers/forms/TagForm.scala
@@ -4,12 +4,13 @@ import play.api.data.Form
 import play.api.data.Forms._
 
 object TagForm {
-
+  private val colorFormat = "^#[0-9a-fA-F]{6}$"
+  
   def apply(): Form[(String, String)] = 
     Form(
       tuple(
 	"name" -> nonEmptyText,
-	"color" -> nonEmptyText.verifying("color.invalid_format", { c => c.matches("^#[0-9a-fA-F]{6}$")})
+	"color" -> nonEmptyText.verifying("color.invalid_format", { c => c.matches(colorFormat)})
 	)
       )
 }
