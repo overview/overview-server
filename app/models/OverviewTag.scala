15d69161898792512b9113be97803280ed60b5fb
Jonas Karlsson <thejonas@gmail.com>
2012-10-09 16:48:56 -0400

    Turn tag into a trait

--- app/models/OverviewTag.scala
+++ app/models/OverviewTag.scala
@@ -1,19 +1,77 @@
 package models
 
+import java.sql.Connection
+import models.orm.Tag
+
 trait OverviewTag {
   val id: Long
   val name: String
-  val color: Option[String]
+
+  def withName(newName: String): OverviewTag
+  def withColor(newColor: String): OverviewTag with TagColor
+  def withColor: Option[OverviewTag with TagColor]
+
+  def withDocumentInformation(implicit c: Connection): OverviewTag with TaggedDocumentInformation
+  
+  def save: Unit
+}
+
+trait TagColor {
+  val color: String
+}
+
+trait TaggedDocumentInformation {
   val documentIds: models.core.DocumentIdList
+}
+
+  
+case class PotentialTag(name: String) {
+
+  def inDocumentSet(documentSetId: Long): Option[OverviewTag] = {
+    OverviewTag.findByName(documentSetId, name)
+  }
+
+  def create(documentSetId: Long): OverviewTag = OverviewTag.create(documentSetId, name)
+
 }
 
 object OverviewTag {
 
-  private class OverviewImpl(val id: Long, val name: String, val color: Option[String],
-    val documentIds: models.core.DocumentIdList) extends OverviewTag 
+  def findByName(documentSetId: Long, tagName: String): Option[OverviewTag] =
+    Tag.findByName(documentSetId, tagName).map(new OverviewTagImpl(_))
+
+  def create(documentSetId: Long, tagName: String): OverviewTag = {
+    val newTag = Tag(documentSetId = documentSetId, name = tagName).save
+
+    new OverviewTagImpl(newTag)
+
+  }
+
+  private[models] class OverviewTagImpl(tag: Tag) extends OverviewTag {
+    private class ColoredTag(source: Tag, override val color: String) extends OverviewTagImpl(source.copy(color = Some(color))) with TagColor
+
+    override val id: Long = tag.id
+    override val name: String = tag.name
+
+    override def withName(newName: String): OverviewTag = new OverviewTagImpl(tag.copy(name = newName))
+    override def withColor(newColor: String): OverviewTag with TagColor = new ColoredTag(tag, newColor)
+
+    override def withColor: Option[OverviewTag with TagColor] = tag.color.map(new ColoredTag(tag, _))
+
+    override def save = tag.save
+
+    override def withDocumentInformation(implicit c: Connection): TaggedDocumentInformationImpl =
+      new TaggedDocumentInformationImpl(tag)
+
+  }
+
+  private[models] class TaggedDocumentInformationImpl(tag: Tag, loader: PersistentTagLoader = new PersistentTagLoader, parser: DocumentListParser = new DocumentListParser)(implicit c: Connection) extends OverviewTagImpl(tag) with TaggedDocumentInformation {
+    
+    override val documentIds: models.core.DocumentIdList = {
+      val documentListData = loader.loadDocumentList(id)
+      parser.createDocumentIdList(documentListData)
+    }
+  }
 
   
-  def apply(id: Long, name: String, color: Option[String], documentIds: models.core.DocumentIdList):
-    OverviewTag = new OverviewImpl(id, name, color, documentIds)
-      
 }
---
+++ test/models/OverviewTagSpec.scala
@@ -0,0 +1,94 @@
+package models
+
+import helpers.DbSetup._
+import helpers.DbTestContext
+import models.orm.Tag
+import org.specs2.mutable.Specification
+import play.api.Play.{ start, stop }
+import play.api.test.FakeApplication
+
+class OverviewTagSpec extends Specification {
+
+  step(start(FakeApplication()))
+
+  "OverviewTag" should {
+
+    trait DocumentSetContext extends DbTestContext {
+      val tagName = "name of tag"
+      lazy val documentSetId = insertDocumentSet("OverviewTagSpec")
+      val potentialTag = PotentialTag(tagName)
+    }
+
+    trait ExistingTag extends DocumentSetContext {
+      var tagId: Long = _
+
+      override def setupWithDb = tagId = insertTag(documentSetId, tagName)
+    }
+
+    trait LoadedTag extends ExistingTag {
+      var tag: OverviewTag = _
+
+      override def setupWithDb = {
+        super.setupWithDb
+        tag = PotentialTag(tagName).inDocumentSet(documentSetId).getOrElse(throw new Exception("Missing tag"))
+      }
+    }
+
+    "be created from a PotentialTag" in new DocumentSetContext {
+      val newTag: OverviewTag = potentialTag.create(documentSetId)
+
+      newTag.id must not be equalTo(0)
+    }
+
+    "be loaded from document set if exists" in new ExistingTag {
+      val foundTag: Option[OverviewTag] = potentialTag.inDocumentSet(documentSetId)
+
+      foundTag must beSome.like { case t => t.id must be equalTo (tagId) }
+    }
+
+    "load None if not in document set" in new DocumentSetContext {
+      val foundTag: Option[OverviewTag] = potentialTag.inDocumentSet(documentSetId)
+
+      foundTag must beNone
+    }
+
+    "be renameable" in new LoadedTag {
+      val newName = "new name"
+
+      val renamedTag: OverviewTag = tag.withName(newName)
+
+      renamedTag.name must be equalTo (newName)
+    }
+
+    "allow color change" in new LoadedTag {
+      val newColor = "d0bed0"
+      val coloredTag: OverviewTag with TagColor = tag.withColor(newColor)
+
+      coloredTag.color must be equalTo (newColor)
+    }
+
+    "return None if color is not set" in new LoadedTag {
+      val coloredTag = tag.withColor
+
+      coloredTag must beNone
+    }
+
+    "return tag with color if color is set" in new LoadedTag {
+      val newColor = "d0bed0"
+      val tagWithColor: OverviewTag = tag.withColor(newColor)
+      val coloredTag = tagWithColor.withColor
+
+      coloredTag must beSome.like { case t => t.color must be equalTo(newColor) }
+    }
+
+    "be saveable" in new LoadedTag {
+      val newName = "new name"
+      tag.withName(newName).save
+
+      val renamedTag = PotentialTag(newName).inDocumentSet(documentSetId)
+      renamedTag must beSome.like { case t => t.id must be equalTo tag.id }
+    }
+  }
+
+  step(stop)
+}
