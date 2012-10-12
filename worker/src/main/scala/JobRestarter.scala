0135ad93f29cfe5aa891230f6470f83de330d336
Jonas Karlsson <thejonas@gmail.com>
2012-10-12 13:40:25 -0400

    Add comments

--- worker/src/main/scala/JobRestarter.scala
+++ worker/src/main/scala/JobRestarter.scala
@@ -1,3 +1,9 @@
+/*
+ * JobRestarter.scala
+ *
+ * Overview Project
+ * Created by Jonas Karlsson, Oct 2012
+ */
 package overview.util
 
 import java.sql.Connection
@@ -5,6 +11,9 @@ import persistence.DocumentSetCleaner
 import persistence.PersistentDocumentSetCreationJob
 import persistence.DocumentSetCreationJobState._
 
+/**
+ * Removes data related to documentsets in jobs, and resets job state to Submitted.
+ */
 class JobRestarter(cleaner: DocumentSetCleaner) {
 
   def restart(jobs: Seq[PersistentDocumentSetCreationJob])(implicit c: Connection) {
