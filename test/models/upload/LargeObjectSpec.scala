767d5ab52140459575b212ac0f9a5f2dcb784ebe
Jonas Karlsson <thejonas@gmail.com>
2012-10-25 21:06:19 -0400

    Simplify use of scopes

--- test/models/upload/LargeObjectSpec.scala
+++ test/models/upload/LargeObjectSpec.scala
@@ -16,12 +16,18 @@ class LargeObjectSpec extends Specification {
 
   "LargeObject" should {
 
+    // We can't use DbTestContext, because it uses a connection
+    // returned from DB.withConnection, which is an AutoCleanConnection wrapper
+    // around bonecp.ConnectionHandle
     trait PGConnectionContext extends Around {
       implicit var pgConnection: PGConnection = _
 
       def setupWithDb = {}
       
       def around[T <% Result](test: => T) = {
+	// DB.getConnection returns an unwrapped bonecp.ConnectionHandle
+	// hopefully we are not leaking statements, but as long as we
+	// just use the LargeObjectAPI, we should be ok...
         val connection = DB.getConnection(autocommit = false)
         try {
           val connectionHandle = connection.asInstanceOf[ConnectionHandle]
@@ -41,40 +47,26 @@ class LargeObjectSpec extends Specification {
       var oid: Long = _
       
       override def setupWithDb = {
-	LO.withLargeObject { largeObject =>
-	  oid = largeObject.oid
-	}
+	oid = LO.withLargeObject { _.oid }.get
       }
 
       def addData(data: Array[Byte]): Long = {
-	LO.withLargeObject(oid) { largeObject =>
-	  largeObject.add(data)
-	}.get
+	LO.withLargeObject(oid) { _.add(data) }.get
       }
-
-
     }
 
     "create a new instance" in new PGConnectionContext {
-      val oid = LO.withLargeObject { largeObject =>
-	largeObject.oid 
-      }
-
-      oid must not be equalTo(-1)
+      LO.withLargeObject { _.oid must not be equalTo(-1) }
     }
 
     // See if there are better ways to handle errors.
     "return None for non existent oid" in new PGConnectionContext {
-      val noid = LO.withLargeObject(234) { largeObject =>
-	largeObject.oid
-      }
+      val noid = LO.withLargeObject(234) { _.oid }
       noid must beNone
     }
 
     "return a LargeObject if oid exists" in new LoContext {
-      LO.withLargeObject(oid) { largeObject =>
-        largeObject.oid must be equalTo (oid)
-      }
+      LO.withLargeObject(oid) { _.oid must be equalTo (oid) }
     }
 
     "add data" in new LoContext {
@@ -97,10 +89,7 @@ class LargeObjectSpec extends Specification {
       addData(allData.take(4)) must be equalTo(4)
       addData(allData.drop(4)) must be equalTo(8)
 
-      LO.withLargeObject(oid) { largeObject =>
-	val readData = largeObject.read(10)
-	readData must be equalTo(allData)
-      }
+      LO.withLargeObject(oid) { _.read(10) must be equalTo(allData) }
     }
 
     "truncate the stored data" in new LoContext {
@@ -119,9 +108,7 @@ class LargeObjectSpec extends Specification {
 
       LO.delete(oid)
 
-      LO.withLargeObject(oid) { largeObject =>
-	largeObject.read(10)
-      } must beNone
+      LO.withLargeObject(oid) { _.read(10) } must beNone
     }
 
     "throw exception when deleting nonexisting objects" in new PGConnectionContext {
