39903ec2d18691665123aaa04b0c3d5b7d4e3339
Jonas Karlsson <thejonas@gmail.com>
2012-11-01 10:03:59 -0400

    Use authorize instead of authenticate in names

--- app/controllers/UploadController.scala
+++ app/controllers/UploadController.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import com.jolbox.bonecp.ConnectionHandle
+import java.sql.Connection
 import java.util.UUID
 import models.orm.SquerylPostgreSqlAdapter
 import org.squeryl.PrimitiveTypeMode._
@@ -7,28 +9,36 @@ import org.squeryl.Session
 import play.api.Play.current
 import play.api.db.DB
 import play.api.libs.iteratee.Iteratee
+import play.api.mvc.Request
+import play.api.mvc.RequestHeader
 import play.api.mvc.{Action, BodyParser, BodyParsers}
 
 object UploadController extends BaseController {
 
-  def authenticatedBodyParser[A](p: BodyParser[A]) = parse.using { implicit request =>
+
+  protected def authorizeInTransaction(authority: Authority)(implicit r: RequestHeader) = {
     DB.withTransaction { implicit connection =>
       val adapter = new SquerylPostgreSqlAdapter()
       val session = new Session(connection, adapter)
       using(session) { // sets thread-local variable
-	authorized(anyUser) match {
-	  case Left(e) => parse.error(e)
-	  case Right(u) => p
-	}
+	authorized(authority)
       }
     }
   }
+  
+  def authorizedBodyParser[A](p: BodyParser[A], authority: Authority) = parse.using { implicit request =>
+    authorizeInTransaction(authority) match {
+      case Left(e) => parse.error(e)
+      case Right(u) => p
+    }
+  }
+
 
   def show(uuid: UUID) = Action(BodyParsers.parse.anyContent) { request =>
     Ok("ok")
   }
 
-  def create(uuid: UUID) = Action(authenticatedBodyParser(fileUpload)) { request =>
+  def create(uuid: UUID) = ActionInTransaction(authorizedBodyParser(fileUpload, anyUser)) { (request: Request[Long], connection: Connection) =>
     println("Total: " + request.body)
     Ok("ok")
   }
