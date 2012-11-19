e487b0a518a18e670e458e24bb46a9b894cfb262
Adam Hooper <adam@adamhooper.com>
2012-11-19 15:37:36 -0500

    Make OverviewUploadedFile immutable

--- app/models/upload/OverviewUploadedFile.scala
+++ app/models/upload/OverviewUploadedFile.scala
@@ -4,7 +4,7 @@ import java.sql.Timestamp
 import models.orm.UploadedFile
 
 trait OverviewUploadedFile {
-  def id: Long
+  val id: Long
   val uploadedAt: Timestamp
   val contentsOid: Long
   val contentDisposition: String
@@ -18,38 +18,36 @@ trait OverviewUploadedFile {
 }
 
 object OverviewUploadedFile {
-  
-  def apply(oid: Long, contentDisposition: String, contentType: String): OverviewUploadedFile = {
-    val uploadedFile = UploadedFile(uploadedAt = now, contentsOid = oid, contentDisposition = contentDisposition, contentType = contentType, size = 0)
-    		
+  def apply(uploadedFile: UploadedFile) : OverviewUploadedFile = {
     new OverviewUploadedFileImpl(uploadedFile)
   }
-  
-  
+
+  def apply(oid: Long, contentDisposition: String, contentType: String): OverviewUploadedFile = {
+    val uploadedFile = UploadedFile(uploadedAt = now, contentsOid = oid, contentDisposition = contentDisposition, contentType = contentType, size = 0)
+    apply(uploadedFile)
+  }
+
   def findById(id: Long): Option[OverviewUploadedFile] = {
     UploadedFile.findById(id).map(new OverviewUploadedFileImpl(_))
   }
-  
+
   private def now: Timestamp = new Timestamp(System.currentTimeMillis())
 
   private class OverviewUploadedFileImpl(uploadedFile: UploadedFile) extends OverviewUploadedFile  {
-    def id = uploadedFile.id
+    val id = uploadedFile.id
     val uploadedAt = uploadedFile.uploadedAt
     val contentsOid = uploadedFile.contentsOid
     val contentDisposition = uploadedFile.contentDisposition
     val contentType = uploadedFile.contentType
     val size = uploadedFile.size
-    
+
     def withSize(size: Long): OverviewUploadedFile = new OverviewUploadedFileImpl(uploadedFile.copy(uploadedAt = now, size = size))
-    
+
     def withContentInfo(contentDisposition: String, contentType: String): OverviewUploadedFile = 
       new OverviewUploadedFileImpl(uploadedFile.copy(contentDisposition = contentDisposition, contentType = contentType))
-    
-    def save: OverviewUploadedFile = {
-      uploadedFile.save
-      this
-    }
-    
+
+    def save: OverviewUploadedFile = new OverviewUploadedFileImpl(uploadedFile.save)
+
     def delete { uploadedFile.delete }
   }
 }
