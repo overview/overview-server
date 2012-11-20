b65379ace84dc1e0a439b576c3cc69483728b1f6
Jonas Karlsson <thejonas@gmail.com>
2012-11-20 16:27:10 -0500

    Produce CsvImportDocuments

--- worker/src/main/scala/csv/CsvImportDocumentProducer.scala
+++ worker/src/main/scala/csv/CsvImportDocumentProducer.scala
@@ -1,9 +1,25 @@
 package csv
 
-import overview.util.{DocumentConsumer, DocumentProducer}
+import overview.util.{ DocumentConsumer, DocumentProducer }
 import overview.util.Progress._
+import database.DB
 
 class CsvImportDocumentProducer(documentSetId: Long, uploadedFileId: Long, consumer: DocumentConsumer, progAbort: ProgressAbortFn) extends DocumentProducer {
 
-  def produce() {}
+  private val uploadReader = new UploadReader(uploadedFileId)
+
+  def produce() {
+    DB.withConnection { implicit connection =>
+      uploadReader.read { reader =>
+        val documentSource = new CsvImportSource(reader)
+        
+        documentSource.foreach { doc =>
+          val documentId = doc.write(documentSetId)
+          consumer.processDocument(documentId, doc.text)
+        }
+        
+        consumer.productionComplete()
+      }
+    }
+  }
 }
