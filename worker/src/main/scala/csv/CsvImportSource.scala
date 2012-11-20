78bc428405ca1f47cdd13f058f2bf5a61659eae5
Jonas Karlsson <thejonas@gmail.com>
2012-11-20 15:48:33 -0500

    Add some gratuitous recursion

--- worker/src/main/scala/csv/CsvImportSource.scala
+++ worker/src/main/scala/csv/CsvImportSource.scala
@@ -3,6 +3,7 @@ package csv
 import scala.collection.Iterable
 import java.io.Reader
 import au.com.bytecode.opencsv.CSVReader
+import scala.annotation.tailrec
 
 class CsvImportSource(reader: Reader) extends Iterable[String] {
   private val TextColumn: String = "text"
@@ -18,14 +19,19 @@ class CsvImportSource(reader: Reader) extends Iterable[String] {
     def next(): String = {
       require(columns.get(TextColumn).isDefined)
       
-      var currentLine: Array[String] = null
-      do {
-        currentLine = readRow
-      } while (currentLine != null && currentLine.length <= columns(TextColumn))
-
+      var currentLine: Array[String] = nextValidRow
       Option(currentLine).map(_(columns(TextColumn))).orNull
     }
 
+    @tailrec
+    private def nextValidRow: Array[String] = {
+      readRow match {
+        case null => null
+        case row if row.length > columns(TextColumn) => row
+        case _ => nextValidRow
+      }
+    }
+    
     private def readRow: Array[String] = {
       val row = nextLine
       nextLine = csvParser.readNext()
