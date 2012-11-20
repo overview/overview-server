661e015f9b20e7fa993aa1bdb6b338c29d5761b4
Jonas Karlsson <thejonas@gmail.com>
2012-11-20 10:04:23 -0500

    Allow reading of uploaded file

---
+++ worker/src/main/scala/csv/UploadReader.scala
@@ -0,0 +1,25 @@
+package csv
+
+import java.io.Reader
+import database.DB
+import overview.largeobject.LO
+import persistence.UploadedFileLoader
+import java.sql.Connection
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import overview.largeobject.LargeObject
+
+class UploadReader(uploadedFileId: Long) {
+
+  def read[T](block: Reader => T)(implicit connection: Connection): T = {
+    implicit val pgc = DB.pgConnection
+
+    val oid = UploadedFileLoader.load(uploadedFileId)
+
+    LO.withLargeObject(oid) { largeObject =>
+      val reader = new BufferedReader(new InputStreamReader(largeObject.inputStream))
+      block(reader)
+    }
+  }
+
+}
---
+++ worker/src/test/scala/csv/UploadReaderSpec.scala
@@ -0,0 +1,36 @@
+package csv
+
+import helpers.DbSpecification
+import org.specs2.execute.PendingUntilFixed
+import overview.largeobject.LO
+import database.DB
+import testutil.DbSetup._
+import java.io.InputStreamReader
+
+class UploadReaderSpec extends DbSpecification {
+
+  step(setupDb)
+
+  "UploadInputStream" should {
+
+    "create reader from uploaded file" in new DbTestContext {
+      val data = Array[Byte](74, 79, 78, 75)
+      implicit val pgc = DB.pgConnection
+      val loid = LO.withLargeObject { lo =>
+        lo.add(data)
+        lo.oid
+      }
+      val uploadId = insertUploadedFile(loid, "content-disposition", "content-type")
+
+      val uploadReader = new UploadReader(uploadId)
+      uploadReader.read { reader =>
+        val buffer = new Array[Char](128)
+        val numRead = reader.read(buffer)
+        numRead must be equalTo(data.size)
+        buffer.take(numRead) must be equalTo(data.map(_.toChar))
+      }
+    }
+  }
+
+  step(shutdownDb)
+}
