59c919a1ec71b8b8dca183e2e0363662ba1484ee
Jonas Karlsson <thejonas@gmail.com>
2012-11-20 20:46:54 -0500

    Write supplied_id if available

--- worker/src/main/scala/persistence/PersistentCsvImportDocument.scala
+++ worker/src/main/scala/persistence/PersistentCsvImportDocument.scala
@@ -5,12 +5,13 @@ import java.sql.Connection
 
 trait PersistentCsvImportDocument {
   val text: String
-  val title: String = "Implement Title"
+  val title: String = ""
+  val suppliedId: Option[String]
   
   def write(documentSetId: Long)(implicit c: Connection): Long = {
     SQL("""
-        INSERT INTO document (type, text, title, document_set_id) VALUES
-          ('CsvImportDocument', {text}, {title}, {documentSetId})
-        """).on("text" -> text, "title" -> title, "documentSetId" -> documentSetId).executeInsert().get
+        INSERT INTO document (type, text, title, supplied_id, document_set_id) VALUES
+          ('CsvImportDocument', {text}, {title}, {suppliedId}, {documentSetId})
+        """).on("text" -> text, "title" -> title,  "suppliedId" -> suppliedId, "documentSetId" -> documentSetId).executeInsert().get
   }
 }
--- worker/src/test/scala/persistence/PersistentCsvImportDocumentSpec.scala
+++ worker/src/test/scala/persistence/PersistentCsvImportDocumentSpec.scala
@@ -7,23 +7,62 @@ import testutil.DbSetup._
 
 class PersistentCsvImportDocumentSpec extends DbSpecification {
   step(setupDb)
-  
-  "PersistentCsvImportDocument" should {
-    "write text to document table" in new DbTestContext {
-      val documentSetId = insertDocumentSet("PersistentCsvImportDocumentSpec")
-      val documentText = "some text"
-        
-      val document = new PersistentCsvImportDocument {
-        val text = documentText
-      }
-      
-      val id = document.write(documentSetId)
-      val documents = 
-        SQL("SELECT id, text FROM document").as(long("id") ~ str("text") map (flatten) *)
-        
-      documents must haveTheSameElementsAs(Seq((id, documentText)))
+
+  trait Setup extends DbTestContext {
+    var documentSetId: Long = _
+    val documentText = "some text"
+
+    override def setupWithDb = documentSetId = insertDocumentSet("PersistentCsvImportDocumentSpec")
+  }
+
+  trait DocumentWithNoId extends Setup {
+    val document = new PersistentCsvImportDocument {
+      val text = documentText
+      val suppliedId = None
+    }
+
+    var id: Long = _
+    override def setupWithDb = {
+      super.setupWithDb
+      id = document.write(documentSetId)
     }
   }
-  
+
+  trait DocumentWithId extends Setup {
+    val document = new PersistentCsvImportDocument {
+      val text = documentText
+      val suppliedId = Some("user id")
+    }
+
+    var id: Long = _
+    override def setupWithDb = {
+      super.setupWithDb
+      id = document.write(documentSetId)
+    }
+  }
+
+  "PersistentCsvImportDocument" should {
+
+    "write text to document table" in new DocumentWithNoId {
+      val documents =
+        SQL("SELECT id, text FROM document").as(long("id") ~ str("text") map (flatten) *)
+
+      documents must haveTheSameElementsAs(Seq((id, documentText)))
+    }
+
+    "leave supplied_id empty if not set" in new DocumentWithNoId {
+      val documents =
+        SQL("SELECT id, supplied_id FROM document").as(long("id") ~ (str("supplied_id")?) map (flatten) *)
+
+      documents must haveTheSameElementsAs(Seq((id, None)))
+    }
+
+    "write supplied_id if set" in new DocumentWithId {
+      val documents =
+        SQL("SELECT id, supplied_id FROM document").as(long("id") ~ (str("supplied_id")?) map (flatten) *)
+      documents must haveTheSameElementsAs(Seq((id, document.suppliedId)))
+    }
+  }
+
   step(shutdownDb)
 }
