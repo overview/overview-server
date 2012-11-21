5d18fea5f4dddc803946c44a5c0eed0157a8f5fb
Jonas Karlsson <thejonas@gmail.com>
2012-11-21 07:37:03 -0500

    Provide upload size when known

--- worker/src/main/scala/csv/UploadReader.scala
+++ worker/src/main/scala/csv/UploadReader.scala
@@ -11,11 +11,14 @@ import overview.largeobject.LargeObject
 
 class UploadReader(uploadedFileId: Long) {
 
+  var size: Option[Long] = None
+  
   def read[T](block: Reader => T)(implicit connection: Connection): T = {
     implicit val pgc = DB.pgConnection
 
     val upload = UploadedFileLoader.load(uploadedFileId)
-
+    size = Some(upload.size)
+        
     LO.withLargeObject(upload.contentsOid) { largeObject =>
       val reader = new BufferedReader(new InputStreamReader(largeObject.inputStream))
       block(reader)
--- worker/src/test/scala/csv/UploadReaderSpec.scala
+++ worker/src/test/scala/csv/UploadReaderSpec.scala
@@ -6,28 +6,45 @@ import overview.largeobject.LO
 import database.DB
 import testutil.DbSetup._
 import java.io.InputStreamReader
+import persistence.UploadedFile
 
 class UploadReaderSpec extends DbSpecification {
 
   step(setupDb)
 
-  "UploadInputStream" should {
+  "UploadReader" should {
 
-    "create reader from uploaded file" in new DbTestContext {
+    trait UploadContext extends DbTestContext {
       val data = Array[Byte](74, 79, 78, 75)
-      implicit val pgc = DB.pgConnection
-      val loid = LO.withLargeObject { lo =>
-        lo.add(data)
-        lo.oid
-      }
-      val uploadId = insertUploadedFile(loid, "content-disposition", "content-type", 100)
+      val uploadSize = 100l
 
-      val uploadReader = new UploadReader(uploadId)
+      var uploadReader: UploadReader = _
+
+      override def setupWithDb = {
+        implicit val pgc = DB.pgConnection
+        val loid = LO.withLargeObject { lo =>
+          lo.add(data)
+          lo.oid
+        }
+        val uploadId = insertUploadedFile(loid, "content-disposition", "content-type", uploadSize)
+        uploadReader = new UploadReader(uploadId)
+      }
+    }
+
+    "create reader from uploaded file" in new UploadContext {
       uploadReader.read { reader =>
         val buffer = new Array[Char](128)
         val numRead = reader.read(buffer)
-        numRead must be equalTo(data.size)
-        buffer.take(numRead) must be equalTo(data.map(_.toChar))
+        numRead must be equalTo (data.size)
+        buffer.take(numRead) must be equalTo (data.map(_.toChar))
+      }
+    }
+
+    "return upload size inside read block" in new UploadContext {
+      uploadReader.size must beNone
+
+      uploadReader.read { reader =>
+        uploadReader.size must beSome.like { case s => s must be equalTo (uploadSize) }
       }
     }
   }
