987c09517f8306e4e079bce9f07e7f05efa255de
jstray <jonathanstray@gmail.com>
2012-11-28 16:55:39 -0500

    Created SmallNodeBundler, aka the DustBuster

---
+++ worker/src/main/scala/SmallNodeBundler.scala
@@ -0,0 +1,60 @@
+/**
+ * SmallNodeBundler.scala
+ *
+ * Take all but the largest child nodes and group them under a new node labeled "other", 
+ * for every node in the tree. User specifies resulting maximum number of children per node (must be >= 2)
+ * Might also be called the DustBuster (dust == single document nodes)
+ * 
+ * Overview Project, created November 2012
+ *
+ * @author Jonathan Stray
+ *
+ */
+
+package overview.clustering
+
+import overview.clustering.ClusterTypes._
+import scala.collection.mutable.Set
+
+object SmallNodeBundler {
+  
+  def apply(root:DocTreeNode, desiredMaxChildren:Int) : Unit = limitTreeMaxChildren(root, desiredMaxChildren)
+  
+  
+  def limitNodeMaxChildren(n:DocTreeNode, maxChildren:Int) : Unit = {
+    require(maxChildren >= 2)
+  
+    var kidsRemaining = n.children.size
+
+    if (kidsRemaining > maxChildren) {
+      kidsRemaining += 1                              // we create a new node to put the bundled kids under
+      
+      var kids = n.orderedChildren.reverse            //  this list goes from smallest to biggest
+      var bundledNodes = Set[DocTreeNode]()
+      var bundledDocs = Set[DocumentID]()
+      
+      while (kidsRemaining > maxChildren) {           // each iteration bundles a kid, until only maxChildren left
+        val eatKid = kids.head
+        bundledNodes += eatKid
+        bundledDocs ++= eatKid.docs
+        kids = kids.tail
+        kidsRemaining -= 1
+      }
+
+      var otherNode = new DocTreeNode(bundledDocs)    // create new node that will hold all the kids we bundled
+      otherNode.children = bundledNodes
+      otherNode.description = "(other)"
+        
+      n.children = Set(kids: _*) + otherNode
+    } 
+  }
+  
+  def limitTreeMaxChildren(n:DocTreeNode, maxChildren:Int) : Unit = {
+    limitNodeMaxChildren(n, maxChildren)
+    n.children foreach {
+      limitTreeMaxChildren(_, maxChildren)
+    }
+  }
+  
+  
+}
---
+++ worker/src/test/scala/SmallNodeBundlerSpec.scala
@@ -0,0 +1,65 @@
+/**
+ * SmallNodeBundlerSpec.scala
+ * 
+ * Overview Project, created November 2012
+ * @author Jonathan Stray
+ * 
+ */
+
+import scala.collection.mutable.Set
+import overview.clustering.ClusterTypes.DocumentID
+import overview.clustering.{SmallNodeBundler, DocTreeNode}
+import org.specs2.mutable.Specification
+import org.specs2.specification._
+
+class SmallNodeBundlerSpec extends Specification {
+
+  "SmallNodeBundler" should {
+        
+    def makeNodeWithKids(totalDocs:Int, docsPerKid:Int) : DocTreeNode = {
+      var allDocs = Set[DocumentID](1.asInstanceOf[DocumentID] until totalDocs.asInstanceOf[DocumentID]: _*)
+      val root = new DocTreeNode(allDocs)
+      
+      while (!allDocs.isEmpty) {
+        root.children += new DocTreeNode(allDocs.take(docsPerKid))
+        allDocs = allDocs.drop(docsPerKid)
+      }
+
+      root
+    }
+    
+    "bundle a node" in {
+      var n = makeNodeWithKids(100, 10)   // ten children, each with 10 nodes
+      
+      SmallNodeBundler.limitNodeMaxChildren(n, 5)  
+      n.children.size must beEqualTo(5)
+      (n.children find { _.description == "(other)" }).isDefined must beTrue // we must have made an "other" node
+      
+      SmallNodeBundler.limitNodeMaxChildren(n, 2)  
+      n.children.size must beEqualTo(2)
+
+      SmallNodeBundler.limitNodeMaxChildren(n, 1) must throwA[java.lang.IllegalArgumentException]
+    }
+    
+    def maxTreeArity(n:DocTreeNode) : Int  = {
+      n.children.foldLeft (n.children.size) { (maxSoFar,child) => math.max(maxSoFar, maxTreeArity(child)) }
+    }
+    
+    "bundle a tree" in {
+      // Make a tree three levels deep, where each node has 20 children of 10 docs each -- 8000 nodes total
+      // (it's not a valid document tree, because children don't have strict subsets of parent docs, but that does not matter here)
+      var root = makeNodeWithKids(200, 10)  
+      root.children foreach { child => 
+        child.children = makeNodeWithKids(200,10).children
+        child.children foreach { child2 => 
+          child2.children = makeNodeWithKids(200,10).children
+        }
+      }
+
+      // If we force no more than 5 children of each node, then the top level "other" nodes will need to be split
+      SmallNodeBundler.limitTreeMaxChildren(root, 5)
+      
+      maxTreeArity(root) must beEqualTo(5)
+    }
+  }
+}
