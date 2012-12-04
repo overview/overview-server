cba16c6965335168231052ca44aafbe28f7556a3
Adam Hooper <adam@adamhooper.com>
2012-12-03 19:54:24 -0500

    Remove useless methods in AuthConfigImpl

--- app/controllers/AuthConfigImpl.scala
+++ app/controllers/AuthConfigImpl.scala
@@ -1,23 +1,13 @@
 package controllers
 
-import jp.t2v.lab.play20.auth.{CookieRelationResolver, RelationResolver}
 import play.api.mvc.{PlainResult, Request, RequestHeader}
 import play.api.mvc.Results.{Forbidden, Redirect}
 
 import models.OverviewUser
 
 trait AuthConfigImpl {
-  type Id = Long
   type User = OverviewUser
   type Authority = OverviewUser => Boolean
-
-  val idManifest : ClassManifest[Id] = classManifest[Id]
-
-  val sessionTimeoutInSeconds : Int = 3600
-
-  def resolveUser(id: Id) : Option[OverviewUser] = {
-    OverviewUser.findById(id)
-  }
 
   def loginSucceeded(request: RequestHeader): PlainResult = {
     val uri = request.session.get(AuthConfigImpl.AccessUriKey).getOrElse(routes.DocumentSetController.index.url)
@@ -33,12 +23,9 @@ trait AuthConfigImpl {
   def authorizationFailed(request: RequestHeader): PlainResult = {
     Forbidden(views.html.http.forbidden())
   }
-
-  def authorize(user: User, authority: Authority) : Boolean = throw new Exception("This method should never be called")
-
-  def resolver(implicit request: RequestHeader): RelationResolver[Id] = new CookieRelationResolver[Id](request)
 }
 
 object AuthConfigImpl {
   private val AccessUriKey = "access_uri"
+  private[controllers] val AuthUserIdKey = "AUTH_USER_ID"
 }
