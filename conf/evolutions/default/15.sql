3efb6cd621f13c8c3528f5e737c6311362b8faf5
Jonas Karlsson <thejonas@gmail.com>
2012-12-05 17:36:43 -0500

    Add cache to node

---
+++ conf/evolutions/default/15.sql
@@ -0,0 +1,24 @@
+# --- !Ups
+
+ALTER TABLE node 
+  ADD cached_document_ids BIGINT[],
+  ADD cached_size INT;
+
+UPDATE node SET cached_document_ids = ( 
+  SELECT ARRAY_AGG(id) FROM ( 
+    SELECT id FROM document
+    INNER JOIN node_document ON
+     node_document.node_id = node.id AND 
+     node_document.document_id = document.id
+    ORDER BY NULLIF(document.title, '') 
+    LIMIT 10) sorted_docs);
+
+UPDATE node SET cached_size = (
+  SELECT COUNT(*) FROM node_document WHERE node.id = node_id);
+
+# --- !Downs
+
+ALTER TABLE node 
+  DROP cached_document_ids,
+  DROP cached_size;
+
