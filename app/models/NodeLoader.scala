977954a7c70dbfa780a04743c61858dc5fb19003
Jonas Karlsson <thejonas@gmail.com>
2012-12-06 14:26:16 -0500

    Load subtree using squeryl

--- app/models/NodeLoader.scala
+++ app/models/NodeLoader.scala
@@ -6,7 +6,17 @@ import org.overviewproject.postgres.CustomTypes._
 class NodeLoader {
 
   import models.orm.Schema._
+  
   def loadRoot(documentSetId: Long): Option[Node] = {
-    nodes.where(n => n.documentSetId === documentSetId and (n.parentId === Option.empty[Long])).headOption
+    nodes.where(n => n.documentSetId === documentSetId and n.parentId === Option.empty[Long]).headOption
+  }
+  
+  def loadTree(documentSetId: Long, root: Node, depth: Int): Seq[Node] = {
+    if (depth == 0) Seq(root)
+    else {
+      val children = nodes.where(n => n.documentSetId === documentSetId and n.parentId === root.id).iterator
+      if (children.isEmpty) Seq(root)
+      else root +: children.flatMap(c => loadTree(documentSetId, c, depth - 1)).toSeq
+    }
   }
 }
--- test/models/NodeLoaderSpec.scala
+++ test/models/NodeLoaderSpec.scala
@@ -12,15 +12,68 @@ class NodeLoaderSpec extends Specification {
 
   "NodeLoader" should {
 
-    "find the root Node" in new DbTestContext {
-      val documentSetId = insertDocumentSet("SubTreeDataLoaderSpec")
-      val root = Node(documentSetId, None, "root", 0, Array[Long]()).save
-      
+    trait NodeContext extends DbTestContext {
+      var documentSetId: Long = _
+      var root: Node = _
+
       val nodeLoader = new NodeLoader
-      
+
+      override def setupWithDb = {
+        documentSetId = insertDocumentSet("SubTreeDataLoaderSpec")
+        root = Node(documentSetId, None, "root", 0, Array[Long]()).save
+      }
+    }
+
+    trait SmallTreeContext extends NodeContext {
+      var nodeIds: Seq[Long] = _
+
+      override def setupWithDb = {
+        super.setupWithDb
+        nodeIds = root.id +: createNextLevel(root.id)
+      }
+
+      protected def createNextLevel(nodeId: Long): Seq[Long] = {
+        for (i <- 1 to 2) yield {
+          val c = Node(documentSetId, Some(nodeId), "child", 0, Array[Long]()).save
+          c.id
+        }
+      }
+    }
+
+    trait LargerTreeContext extends SmallTreeContext {
+      override def setupWithDb = {
+        super.setupWithDb
+
+        val level2 = nodeIds.tail.flatMap(createNextLevel)
+        val level3 = level2.flatMap(createNextLevel)
+        nodeIds ++= level2 ++ level3
+      }
+    }
+
+    "find the root Node" in new NodeContext {
       val node = nodeLoader.loadRoot(documentSetId)
-      
+
       node must beSome.like { case n => n.id must be equalTo root.id }
+    }
+
+    "load a small tree" in new SmallTreeContext {
+      val nodes = nodeLoader.loadTree(documentSetId, root, 1)
+      nodes.map(_.id) must haveTheSameElementsAs(nodeIds)
+    }
+
+    "should handle too much depth" in new SmallTreeContext {
+      val nodes = nodeLoader.loadTree(documentSetId, root, 10)
+      nodes.map(_.id) must haveTheSameElementsAs(nodeIds)
+    }
+
+    "return root if depth is 0" in new SmallTreeContext {
+      val nodes = nodeLoader.loadTree(documentSetId, root, 0)
+      nodes.map(_.id) must contain(root.id).only
+    }
+
+    "handle multiple levels" in new LargerTreeContext {
+      val nodes = nodeLoader.loadTree(documentSetId, root, 2)
+      nodes.map(_.id) must haveTheSameElementsAs(nodeIds.take(7))
     }
   }
   step(stop)
