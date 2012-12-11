57a2aedbecf039b8e2683159e5e644c9c623a335
Adam Hooper <adam@adamhooper.com>
2012-12-11 10:02:51 -0500

    :s/User/OverviewUser

--- app/controllers/admin/DocumentSetCreationJobController.scala
+++ app/controllers/admin/DocumentSetCreationJobController.scala
@@ -4,11 +4,12 @@ import java.sql.Connection
 import play.api.mvc.{AnyContent,Request}
 
 import models.orm.DocumentSetCreationJob
+import models.OverviewUser
 
 object DocumentSetCreationJobController extends AdminController {
-  def index() = adminAction((user: User) => authorizedIndex(user)(_: Request[AnyContent], _: Connection))
+  def index() = adminAction((user: OverviewUser) => authorizedIndex(user)(_: Request[AnyContent], _: Connection))
 
-  def authorizedIndex(user: User)(implicit request: Request[AnyContent], connection: Connection) = {
+  def authorizedIndex(user: OverviewUser)(implicit request: Request[AnyContent], connection: Connection) = {
     val jobs = DocumentSetCreationJob.all.iterator.toSeq
     Ok(views.html.admin.DocumentSetCreationJob.index(user, jobs))
   }
