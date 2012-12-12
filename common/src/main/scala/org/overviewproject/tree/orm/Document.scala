9fb9bde116b7248073b908e790f74bf573151119
Jonas Karlsson <thejonas@gmail.com>
2012-12-12 13:45:07 -0500

    Define DocumentType

--- common/src/main/scala/org/overviewproject/tree/orm/Document.scala
+++ common/src/main/scala/org/overviewproject/tree/orm/Document.scala
@@ -6,16 +6,20 @@ import org.overviewproject.postgres.PostgresqlEnum
 
 class DocumentType(v: String) extends PostgresqlEnum(v, "document_type")
 
+object DocumentType {
+  val DocumentCloudDocument = new DocumentType("DocumentCloudDocument")
+  val CsvImportDocument = new DocumentType("CsvImportDocument")
+}
+
 case class Document(
-    @Column("type") val documentType: DocumentType,
-    override val id: Long = 0L,
-    @Column("document_set_id") val documentSetId: Long = 0L,
-    val title: String = "",
-    @Column("supplied_id") val suppliedId: Option[String] = None,
-    val text: Option[String] = None,
-    val url: Option[String] = None,
-    @Column("documentcloud_id") val documentcloudId: Option[String] = None
-    ) extends KeyedEntity[Long] {
+  @Column("type") val documentType: DocumentType,
+  @Column("document_set_id") val documentSetId: Long = 0L,
+  val title: String = "",
+  @Column("supplied_id") val suppliedId: Option[String] = None,
+  val text: Option[String] = None,
+  val url: Option[String] = None,
+  @Column("documentcloud_id") val documentcloudId: Option[String] = None,
+  override val id: Long = 0L) extends KeyedEntity[Long] {
 
   // https://www.assembla.com/spaces/squeryl/tickets/68-add-support-for-full-updates-on-immutable-case-classes#/followers/ticket:68
   override def isPersisted(): Boolean = (id > 0)
