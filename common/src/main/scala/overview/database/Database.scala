52a9598030fbd659994a390c56f8fe3092eef169
Jonas Karlsson <thejonas@gmail.com>
2012-12-12 14:49:28 -0500

    TransactionProvider for non-Play apps

---
+++ common/src/main/scala/overview/database/Database.scala
@@ -0,0 +1,11 @@
+package overview.database
+
+import java.sql.Connection
+
+object Database extends TransactionProvider {
+
+  def transactionBlock[A](block: Connection => A): A = {
+    DB.withTransaction(block)
+  }
+
+}
