3d174a8ab5b827d7f0f2e366e4273e6cd6cb3d76
Jonas Karlsson <thejonas@gmail.com>
2012-12-12 13:44:38 -0500

    Add Document to Schema

--- worker/src/main/scala/persistence/Schema.scala
+++ worker/src/main/scala/persistence/Schema.scala
@@ -1,10 +1,11 @@
 package persistence
 
 import org.overviewproject.postgres.CustomTypes._
-import org.overviewproject.tree.orm.Node
+import org.overviewproject.tree.orm.{ Document, Node }
 
 object Schema extends org.squeryl.Schema {
   override def tableNameFromClassName(className: String) = NamingConventionTransforms.snakify(className)
   
   val nodes = table[Node]
+  val documents = table[Document]
 }
