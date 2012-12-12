ac20fbff21964252ab94c6fb866ca39e3168f50f
Jonas Karlsson <thejonas@gmail.com>
2012-12-12 13:45:19 -0500

    Use Document to write and update document

--- worker/src/main/scala/persistence/DocumentWriter.scala
+++ worker/src/main/scala/persistence/DocumentWriter.scala
@@ -7,8 +7,11 @@
 
 package persistence
 
-import anorm._
 import java.sql.Connection
+import anorm.{ SQL, sqlToSimple, toParameterValue }
+import org.overviewproject.tree.orm.Document
+import org.squeryl.PrimitiveTypeMode._
+import persistence.Schema.documents
 
 /**
  * Writes out document information associated with the documentSetId
@@ -18,5 +21,19 @@ class DocumentWriter(documentSetId: Long) {
   def updateDescription(id: Long, description: String)(implicit c: Connection): Long = {
     SQL("UPDATE document SET title = {description} WHERE id = {id}").
       on("description" -> description, "id" -> id).executeUpdate()
+  }
+}
+
+object DocumentWriter {
+  import persistence.Schema.documents
+
+  def write(document: Document) {
+    documents.insert(document)
+  }
+
+  def updateDescription(documentId: Long, description: String) {
+    update(documents)(d =>
+      where(d.id === documentId)
+      set (d.title := description))
   }
 }
--- worker/src/test/scala/persistence/DocumentWriterSpec.scala
+++ worker/src/test/scala/persistence/DocumentWriterSpec.scala
@@ -12,6 +12,8 @@ import anorm.SqlParser._
 import org.overviewproject.test.DbSpecification
 import org.specs2.mutable.Specification
 import org.overviewproject.test.DbSetup._
+import org.overviewproject.tree.orm.DocumentType._
+import org.overviewproject.tree.orm.Document
 
 class DocumentWriterSpec extends DbSpecification {
 
@@ -19,23 +21,42 @@ class DocumentWriterSpec extends DbSpecification {
 
   "DocumentWriter" should {
 
-    "update description of document" in new DbTestContext {
-      val documentSetId = insertDocumentSet("DocumentWriterSpec")
+    trait Setup extends DbTestContext {
+      var documentSetId: Long = _
 
-      val writer = new DocumentWriter(documentSetId)
+      override def setupWithDb = {
+        documentSetId = insertDocumentSet("DocumentWriterSpec")
+      }
+    }
+
+    "update description of document" in new Setup {
       val title = "title"
       val documentCloudId = "documentCloud-id"
       val description = "some,terms,together"
-      
+
       val id = insertDocument(documentSetId, title, documentCloudId)
-      writer.updateDescription(id, description)
+      DocumentWriter.updateDescription(id, description)
 
       val documents =
         SQL("SELECT id, title, documentcloud_id FROM document").
           as(long("id") ~ str("title") ~ str("documentcloud_id") map (flatten) *)
 
       documents must haveTheSameElementsAs(Seq((id, description, documentCloudId)))
+
+    }
+
+    "write a document cloud document" in new Setup {
+      val document = Document(DocumentCloudDocument, documentSetId, documentcloudId = Some("dcId"))
+      DocumentWriter.write(document)
+
+      document.id must not be equalTo(0)
+    }
+
+    "write a csv import document" in new Setup {
+      val document = Document(CsvImportDocument, documentSetId, text = Some("text"), url = None)
+      DocumentWriter.write(document)
       
+      document.id must not be equalTo(0)
     }
   }
 
