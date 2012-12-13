cdf67bf26023226bc089d97eaa166e3a93e47d92
Jonas Karlsson <thejonas@gmail.com>
2012-12-13 14:44:13 -0500

    Save and read DocumentSetCreationJobs

--- app/models/OverviewDocumentSetCreationJob.scala
+++ app/models/OverviewDocumentSetCreationJob.scala
@@ -1,20 +1,38 @@
 package models
 
 import models.orm.DocumentSetCreationJobState._
+import models.orm.DocumentSetCreationJob
 
 trait OverviewDocumentSetCreationJob {
+  val id: Long
   val documentSetId: Long
   val state: DocumentSetCreationJobState
+
+  def save: OverviewDocumentSetCreationJob
 }
 
 object OverviewDocumentSetCreationJob {
-  
-  
+  import org.squeryl.PrimitiveTypeMode._
+  import models.orm.Schema.documentSetCreationJobs
+
   def apply(documentSet: OverviewDocumentSet): OverviewDocumentSetCreationJob = {
-    new OverviewDocumentSetCreationJobImpl(documentSet.id)
+    val documentSetCreationJob = DocumentSetCreationJob(documentSet.id, state = NotStarted)
+    new OverviewDocumentSetCreationJobImpl(documentSetCreationJob)
   }
-  
-  private class OverviewDocumentSetCreationJobImpl(val documentSetId: Long) extends OverviewDocumentSetCreationJob {
-    val state: DocumentSetCreationJobState = NotStarted
+
+  def all: Seq[OverviewDocumentSetCreationJob] = {
+    from(documentSetCreationJobs)(j => select(j).orderBy(j.id.asc)).toSeq.map(OverviewDocumentSetCreationJobImpl)
+  }
+
+  private case class OverviewDocumentSetCreationJobImpl(documentSetCreationJob: DocumentSetCreationJob) extends OverviewDocumentSetCreationJob {
+    val id: Long = documentSetCreationJob.id
+    val documentSetId: Long = documentSetCreationJob.documentSetId
+    val state: DocumentSetCreationJobState = documentSetCreationJob.state
+
+    def save: OverviewDocumentSetCreationJob = {
+      documentSetCreationJobs.insertOrUpdate(documentSetCreationJob)
+
+      copy(documentSetCreationJob)
+    }
   }
 }
--- test/models/OverviewDocumentSetCreationJobSpec.scala
+++ test/models/OverviewDocumentSetCreationJobSpec.scala
@@ -1,6 +1,6 @@
 package models
 
-import play.api.Play.{start, stop}
+import play.api.Play.{ start, stop }
 import play.api.test.FakeApplication
 import org.overviewproject.test.Specification
 import helpers.DbTestContext
@@ -10,20 +10,46 @@ import models.orm.DocumentSetCreationJobState._
 
 class OverviewDocumentSetCreationJobSpec extends Specification {
   step(start(FakeApplication()))
-  
+
   "OverviewDocumentSetCreationJob" should {
-    
-    "create a job with a document set" in new DbTestContext {
-      import models.orm.Schema.documentSets
-      
+    import models.orm.Schema.documentSets
+
+    trait DocumentSetContext extends DbTestContext {
       val documentSet = DocumentSet(DocumentCloudDocumentSet, title = "title", query = Some("query"))
-      documentSets.insert(documentSet)
+      var job: OverviewDocumentSetCreationJob = _
+
+      override def setupWithDb = {
+        documentSets.insert(documentSet)
+        job = OverviewDocumentSetCreationJob(OverviewDocumentSet(documentSet))
+      }
+    }
+
+    trait MultipleJobs extends DbTestContext {
+      var jobs: Seq[OverviewDocumentSetCreationJob] = _  
       
-      val job = OverviewDocumentSetCreationJob(OverviewDocumentSet(documentSet))
-      
-      job.documentSetId must be equalTo(documentSet.id)
-      job.state must be equalTo(NotStarted)
-      
+      override def setupWithDb = {
+        val sets = Seq.fill(10)(DocumentSet(DocumentCloudDocumentSet, title = "title", query = Some("query")))
+        jobs = sets.map { d =>
+          documentSets.insert(d)
+          OverviewDocumentSetCreationJob(OverviewDocumentSet(d)).save
+        }    
+            
+      }
+    }
+    
+    "create a job with a document set" in new DocumentSetContext {
+      job.documentSetId must be equalTo (documentSet.id)
+      job.state must be equalTo (NotStarted)
+    }
+
+    "save job to database" in new DocumentSetContext {
+      val savedJob = job.save
+
+      savedJob.id must not be equalTo(0)
+    }
+    
+    "list all saved jobs ordered by ascending ids" in new MultipleJobs {
+      OverviewDocumentSetCreationJob.all must be equalTo(jobs.sortBy(_.id))
     }
   }
 
