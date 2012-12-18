af1d2e5e985c62405bc51a17579f1e43ed578fe2
Jonas Karlsson <thejonas@gmail.com>
2012-12-18 11:58:18 -0500

    Make updateable

--- common/src/main/scala/org/overviewproject/tree/orm/DocumentSetCreationJob.scala
+++ common/src/main/scala/org/overviewproject/tree/orm/DocumentSetCreationJob.scala
@@ -5,27 +5,29 @@ import org.squeryl.KeyedEntity
 import org.squeryl.dsl.ManyToOne
 import org.squeryl.PrimitiveTypeMode._
 
-
 object DocumentSetCreationJobState extends Enumeration {
   type DocumentSetCreationJobState = Value
 
   val NotStarted = Value(0, "NOT_STARTED")
   val InProgress = Value(1, "IN_PROGRESS")
   val Error = Value(2, "ERROR")
+  val Cancelled = Value(3, "CANCELLED")
 }
 
 import DocumentSetCreationJobState._
 
 case class DocumentSetCreationJob(
-  val documentSetId: Long = 0,
-  val documentcloudUsername: Option[String] = None,
-  val documentcloudPassword: Option[String] = None,
-  val state: DocumentSetCreationJobState = NotStarted,
-  val fractionComplete: Double = 0.0,
-  val statusDescription: String = "") extends KeyedEntity[Long] {
-  override val id: Long = 0
+  documentSetId: Long = 0,
+  documentcloudUsername: Option[String] = None,
+  documentcloudPassword: Option[String] = None,
+  state: DocumentSetCreationJobState = NotStarted,
+  fractionComplete: Double = 0.0,
+  statusDescription: String = "",
+  id: Long = 0) extends KeyedEntity[Long] {
 
   def this() = this(state = NotStarted)
+
+  override def isPersisted(): Boolean = (id > 0)
 
 }
 
