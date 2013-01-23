cdfec9fd96c225dac9678db9fcf2c20564a608f5
Jonas Karlsson <thejonas@gmail.com>
2013-01-22 19:06:22 -0500

    Return failed url retrievals grouped by error message

---
+++ app/models/OverviewDocumentProcessingError.scala
@@ -0,0 +1,94 @@
+package models
+
+import models.orm.Schema.documentProcessingErrors
+import org.overviewproject.tree.orm.DocumentProcessingError
+
+trait OverviewDocumentProcessingError {
+  val statusCode: Option[Int]
+}
+
+object OverviewDocumentProcessingError {
+  def sortedByStatus(documentSetId: Long): Seq[(String, Seq[OverviewDocumentProcessingError])] = {
+    import org.overviewproject.postgres.SquerylEntrypoint._
+
+    val errors = documentProcessingErrors.where(dpe => dpe.documentSetId === documentSetId).map(new OverviewDocumentProcessingErrorImpl(_))
+
+    val errorGroups = errors.groupBy(_.statusCode.getOrElse(-1))
+    val errorGroupsSortedByStatusCode = errorGroups.toSeq.sortBy(_._1)
+    errorGroupsSortedByStatusCode.map(e => (message(e._1), e._2.toSeq))
+  }
+
+  private def message(statusCode: Int): String = 
+    if (statusCode == -1) InternalError
+    else "%d %s".format(statusCode, HttpStatusCode.getOrElse(statusCode, UnknownStatus))
+    
+  private class OverviewDocumentProcessingErrorImpl(documentProcessingError: DocumentProcessingError) extends OverviewDocumentProcessingError {
+    val statusCode: Option[Int] = documentProcessingError.statusCode
+  }
+
+  private val InternalError: String = "Overview Internal Error"
+  private val UnknownStatus: String = "Unknown Status"
+  private val HttpStatusCode: Map[Int, String] = Map(
+    (100 -> "Continue"),
+    (101 -> "Switching Protocols"),
+    (102 -> "Processing"),
+    (200 -> "OK"),
+    (201 -> "Created"),
+    (202 -> "Accepted"),
+    (203 -> "Non-Authoritative Information"),
+    (204 -> "No Content"),
+    (205 -> "Reset Content"),
+    (206 -> "Partial Content"),
+    (207 -> "Multi-Status"),
+    (208 -> "Already Reported"),
+    (226 -> "IM Used"),
+    (300 -> "Multiple Choices"),
+    (301 -> "Moved Permanently"),
+    (302 -> "Found"),
+    (303 -> "See Other"),
+    (304 -> "Not Modified"),
+    (305 -> "Use Proxy"),
+    (306 -> "Reserved"),
+    (307 -> "Temporary Redirect"),
+    (308 -> "Permanent Redirect"),
+    (400 -> "Bad Request"),
+    (401 -> "Unauthorized"),
+    (402 -> "Payment Required"),
+    (403 -> "Forbidden"),
+    (404 -> "Not Found"),
+    (405 -> "Method Not Allowed"),
+    (406 -> "Not Acceptable"),
+    (407 -> "Proxy Authentication Required"),
+    (408 -> "Request Timeout"),
+    (409 -> "Conflict"),
+    (410 -> "Gone"),
+    (411 -> "Length Required"),
+    (412 -> "Precondition Failed"),
+    (413 -> "Request Entity Too Large"),
+    (414 -> "Request-URI Too Long"),
+    (415 -> "Unsupported Media Type"),
+    (416 -> "Requested Range Not Satisfiable"),
+    (417 -> "Expectation Failed"),
+    (422 -> "Unprocessable Entity"),
+    (423 -> "Locked"),
+    (424 -> "Failed Dependency"),
+    (425 -> "Unassigned"),
+    (426 -> "Upgrade Required"),
+    (427 -> "Unassigned"),
+    (428 -> "Precondition Required"),
+    (429 -> "Too Many Requests"),
+    (430 -> "Unassigned"),
+    (431 -> "Request Header Fields Too Large"),
+    (500 -> "Internal Server Error"),
+    (501 -> "Not Implemented"),
+    (502 -> "Bad Gateway"),
+    (503 -> "Service Unavailable"),
+    (504 -> "Gateway Timeout"),
+    (505 -> "HTTP Version Not Supported"),
+    (506 -> "Variant Also Negotiates (Experimental)"),
+    (507 -> "Insufficient Storage"),
+    (508 -> "Loop Detected"),
+    (509 -> "Unassigned"),
+    (510 -> "Not Extended"),
+    (511 -> "Network Authentication Required"))
+}
---
+++ test/models/OverviewDocumentProcessingErrorSpec.scala
@@ -0,0 +1,63 @@
+package models
+
+import helpers.DbTestContext
+import models.orm.Schema
+import org.overviewproject.test.DbSetup._
+import org.overviewproject.test.Specification
+import play.api.Play.{ start, stop }
+import play.api.test.FakeApplication
+import org.overviewproject.tree.orm.DocumentProcessingError
+
+class OverviewDocumentProcessingErrorSpec extends Specification {
+
+  step(start(FakeApplication()))
+
+  "OverviewDocumentProcessingError" should {
+
+    trait HttpErrors extends DbTestContext {
+      var documentSetId: Long = _
+      val statusCodes = Seq(400, 302, 400, 500, 403, 999)
+      def errors = statusCodes.map(c => DocumentProcessingError(documentSetId, "url-" + c, "message", Some(c), Some("header")))
+      var errorGroups: Seq[(String, Seq[OverviewDocumentProcessingError])] = _
+
+      override def setupWithDb = {
+        documentSetId = insertDocumentSet("OverviewDocumentProcessingErrorSpec")
+        Schema.documentProcessingErrors.insert(errors)
+        errorGroups = OverviewDocumentProcessingError.sortedByStatus(documentSetId)
+      }
+    }
+
+    trait InternalErrors extends HttpErrors {
+      override def errors = DocumentProcessingError(documentSetId, "url", "exception thrown", None, None) +: super.errors
+    }
+
+    "return errors and reasons sorted by status code" in new HttpErrors {
+      errorGroups must have size (statusCodes.groupBy(identity).size)
+      errorGroups.map(e => e._2.head.statusCode) must beSorted
+    }
+
+    "return internal errors first in list" in new InternalErrors {
+      val internalErrors = errorGroups.head
+      internalErrors._2 must have size (1)
+      internalErrors._2.head.statusCode must beNone
+    }
+
+    "map message for internal errors" in new InternalErrors {
+      val internalErrors = errorGroups.head
+      internalErrors._1 must be equalTo ("Overview Internal Error")
+    }
+
+    "map message for http status codes" in new HttpErrors {
+      val expectedMessages = Seq(
+        "302 Found",
+        "400 Bad Request",
+        "403 Forbidden",
+        "500 Internal Server Error",
+        "999 Unknown Status")
+
+      errorGroups.map(_._1) must be equalTo (expectedMessages)
+    }
+  }
+
+  step(stop)
+}
