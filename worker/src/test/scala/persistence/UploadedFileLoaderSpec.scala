23d16def24b896762ec3289aa675a6cbbe9ae3cf
Jonas Karlsson <thejonas@gmail.com>
2013-01-23 16:40:21 -0500

    Extract setup into trait

--- worker/src/test/scala/persistence/UploadedFileLoaderSpec.scala
+++ worker/src/test/scala/persistence/UploadedFileLoaderSpec.scala
@@ -1,46 +1,59 @@
 package persistence
 
+import org.overviewproject.postgres.LO
 import org.overviewproject.test.DbSpecification
 import org.overviewproject.test.DbSetup._
+import org.overviewproject.database.DB
 
 class UploadedFileLoaderSpec extends DbSpecification {
 
   step(setupDb)
-  
+
   "UploadedFileLoader" should {
-    
-    "load uploaded file values" in new DbTestContext {
-      val oid = 100l
+
+    trait UploadedFileContext extends DbTestContext {
       val size = 1999l
-      val contentType =  "application/octet-stream"
-        
-      val uploadedFileId = insertUploadedFile(oid, "content-disposition", contentType, size)
-      
-      val uploadedFile = EncodedUploadFile.load(uploadedFileId) 
+      val contentType = "application/octet-stream"
+
+      var uploadedFileId: Long = _
+      var oid: Long = _
+
+      override def setupWithDb {
+        implicit val pgc = DB.pgConnection
+        LO.withLargeObject { lo =>
+          uploadedFileId = insertUploadedFile(lo.oid, "content-disposition", contentType, size)
+          oid = lo.oid
+        }
+      }
+    }
+
+    "load uploaded file values" in new UploadedFileContext {
+      val uploadedFile = EncodedUploadFile.load(uploadedFileId)
       uploadedFile.contentsOid must be equalTo oid
       uploadedFile.contentType must be equalTo contentType
       uploadedFile.size must be equalTo size
     }
+    
   }
-  
+
   step(shutdownDb)
-  
+
   case class TestUploadFile(contentType: String) extends EncodedUploadFile {
     val contentsOid: Long = 0l
     val size: Long = 100
   }
-  
+
   "UploadedFile" should {
-    
+
     "return None if no encoding can be found" in {
       val uploadedFile = TestUploadFile("application/octet-stream")
       uploadedFile.encoding must beNone
     }
-    
+
     "return specified encoding" in {
       val encoding = "someEncoding"
       val uploadedFile = TestUploadFile("application/octet-stream; charset=" + encoding)
-      uploadedFile.encoding must beSome.like { case c => c must be equalTo(encoding) }
+      uploadedFile.encoding must beSome.like { case c => c must be equalTo (encoding) }
     }
   }
 }
