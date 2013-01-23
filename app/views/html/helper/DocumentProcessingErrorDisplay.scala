a4c6aa3d66814890e00e3cbe1566fe4d64e23355
Jonas Karlsson <thejonas@gmail.com>
2013-01-22 19:34:43 -0500

    Improve formatting of errors

--- app/views/html/helper/DocumentProcessingErrorDisplay.scala
+++ app/views/html/helper/DocumentProcessingErrorDisplay.scala
@@ -3,18 +3,8 @@ package views.html.helper
 import play.api.i18n.Lang
 
 object DocumentProcessingErrorDisplay {
-  private val m = views.ScopedMessages("views.DocumentProcessingError.error.td")
-  private val UrlDisplayLength: Int = 40
+  private val UrlDisplayLength: Int = 65
   private val Ellipsis: String = "\u2026"
-    
-  def status(statusCode: Option[Int])(implicit lang: Lang): String = {
-    statusCode match {
-      case Some(403) => m("access_denied")
-      case Some(404) => m("not_found")
-      case Some(_) => m("server_error")
-      case _ => m("internal_error")
-    }
-  }
   
   def url(textUrl: String) = 
     if (textUrl.length <= UrlDisplayLength) textUrl
--- test/views/html/helper/DocumentProcessingErrorDisplaySpec.scala
+++ test/views/html/helper/DocumentProcessingErrorDisplaySpec.scala
@@ -4,37 +4,16 @@ import org.overviewproject.test.Specification
 
 class DocumentProcessingErrorDisplaySpec extends Specification {
   
-  val prefix: String = "views.DocumentProcessingError.error.td."
-  val internalError: String = prefix + "internal_error"
-  val accessDenied: String = prefix + "access_denied"
-  val notFound: String = prefix + "not_found"
-  val serverError: String = prefix + "server_error"
   
   "ErrorStatusMessage" should {
-    
-    "show internal error if no status code is given" in {
-      DocumentProcessingErrorDisplay.status(None) must be equalTo internalError
-    }
-    
-    "show access denied error on 403" in {
-      DocumentProcessingErrorDisplay.status(Some(403)) must be equalTo accessDenied
-    }
-    
-    "show not found error on 404" in {
-      DocumentProcessingErrorDisplay.status(Some(404)) must be equalTo notFound
-    }
-    
-    "show server error for other errors" in {
-      DocumentProcessingErrorDisplay.status(Some(406)) must be equalTo serverError
-    }
-    
-    "show complete URL if less than 40 characters" in {
+
+    "show complete URL if less than 65 characters" in {
       val url = "short url"
       DocumentProcessingErrorDisplay.url(url) must be equalTo url
     }
     
-   "show last 40 chars of a url, preceeded by ellipsis" in {
-     val urlShown = "*" * 40
+   "show last 65 chars of a url, preceeded by ellipsis" in {
+     val urlShown = "*" * 65
      val url = "not shown" + urlShown 
      
      DocumentProcessingErrorDisplay.url(url) must be equalTo "\u2026" + urlShown
