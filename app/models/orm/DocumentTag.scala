8731c4bd380db275dec1109eea8c82010eeda241
Jonas Karlsson <thejonas@gmail.com>
2013-02-01 17:54:17 -0500

    Remove unneeded annotation

--- app/models/orm/DocumentTag.scala
+++ app/models/orm/DocumentTag.scala
@@ -1,12 +1,11 @@
 package models.orm
 
-import org.squeryl.annotations.Column
 import org.squeryl.dsl.CompositeKey2
 import org.squeryl.KeyedEntity
 import org.overviewproject.postgres.SquerylEntrypoint.compositeKey
 
 case class DocumentTag(
-  @Column("document_id") documentId: Long,
-  @Column("tag_id") tagId: Long) extends KeyedEntity[CompositeKey2[Long, Long]] {
+  documentId: Long,
+  tagId: Long) extends KeyedEntity[CompositeKey2[Long, Long]] {
   override def id = compositeKey(documentId, tagId)
 }
