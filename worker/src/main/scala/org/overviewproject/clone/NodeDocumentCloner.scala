7ddbe59753b02aa4534eef5b51010617ae14dded
Jonas Karlsson <thejonas@gmail.com>
2013-02-01 17:19:28 -0500

    Clone nodeDocuments

---
+++ worker/src/main/scala/org/overviewproject/clone/NodeDocumentCloner.scala
@@ -0,0 +1,24 @@
+package org.overviewproject.clone
+
+import org.overviewproject.postgres.SquerylEntrypoint._
+import persistence.Schema
+import persistence.NodeDocumentBatchInserter
+
+object NodeDocumentCloner {
+
+  def clone(documentMapping: Map[Long, Long], nodeMapping: Map[Long, Long]) {
+    val batchInserter = new NodeDocumentBatchInserter(500)
+    val sourceNodes = nodeMapping.keys
+
+    val sourceNodeDocuments =
+      from(Schema.nodeDocuments)(nd => where(nd.nodeId in sourceNodes) select nd)
+
+    val cloneNodeDocuments = for {
+      nd <- sourceNodeDocuments
+      nodeId <- nodeMapping.get(nd.nodeId)
+      documentId <- documentMapping.get(nd.documentId)
+    } batchInserter.insert(nodeId, documentId)
+
+    batchInserter.flush
+  }
+}
---
+++ worker/src/test/scala/org/overviewproject/clone/NodeDocumentClonerSpec.scala
@@ -0,0 +1,55 @@
+package org.overviewproject.clone
+
+import org.overviewproject.postgres.SquerylEntrypoint._
+import org.overviewproject.test.DbSetup._
+import org.overviewproject.test.DbSpecification
+import org.overviewproject.tree.orm.{ Document, Node }
+import org.overviewproject.tree.orm.DocumentType._
+import persistence.NodeDocument
+import persistence.Schema
+import org.squeryl.KeyedEntity
+
+class NodeDocumentClonerSpec extends DbSpecification {
+
+  step(setupDb)
+
+  def createNodeDocuments(nodes: Seq[Node], documents: Seq[Document]): Seq[NodeDocument] = {
+    nodes.zip(documents).map { nd =>
+      Schema.nodes.insert(nd._1)
+      Schema.documents.insert(nd._2)
+      
+      NodeDocument(nd._1.id, nd._2.id)
+    }
+  }
+  
+  def createMapping(source: Seq[KeyedEntity[Long]], clone: Seq[KeyedEntity[Long]]): Map[Long, Long] = 
+    source.map(_.id).zip(clone.map(_.id)).toMap
+
+  "NodeDocumentCloner" should {
+
+    "clone the node_document table" in new DbTestContext {
+      val documentSetId = insertDocumentSet("NodeDocumentClonerSpec")
+      val cloneDocumentSetId = insertDocumentSet("ClonedNodeDocumentClonerSpec")
+      val sourceNodes = Seq.tabulate(10)(i => Node(documentSetId, None, "node-" + i, 100, Array()))
+      val sourceDocuments = Seq.tabulate(10)(i => Document(CsvImportDocument, documentSetId, text = Some("text-" + i)))
+      val cloneNodes = sourceNodes.map(_.copy(documentSetId = cloneDocumentSetId))
+      val cloneDocuments = sourceDocuments.map(_.copy(documentSetId = cloneDocumentSetId))
+
+      val sourceNodeDocuments = createNodeDocuments(sourceNodes, sourceDocuments)
+      val cloneNodeDocuments = createNodeDocuments(cloneNodes, cloneDocuments)
+      
+      Schema.nodeDocuments.insert(sourceNodeDocuments)
+      
+      val nodeMapping =  createMapping(sourceNodes, cloneNodes)
+      val documentMapping = createMapping(sourceDocuments, cloneDocuments)
+      
+      NodeDocumentCloner.clone(documentMapping, nodeMapping)
+      
+      val allNodeDocuments = Schema.nodeDocuments.allRows.toSeq
+      
+      allNodeDocuments must haveTheSameElementsAs(sourceNodeDocuments ++ cloneNodeDocuments)
+    }
+  }
+
+  step(shutdownDb)
+}
