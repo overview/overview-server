dbcc57c1da47c28a10bf93f9247f63fe0ce85947
Jonas Karlsson <thejonas@gmail.com>
2013-02-07 09:56:19 -0500

    Update DocumentSet with form

---
+++ app/controllers/forms/DocumentSetUpdateForm.scala
@@ -0,0 +1,22 @@
+package controllers.forms
+
+import play.api.data._
+import play.api.data.Forms._
+import models.orm.DocumentSet
+
+object DocumentSetUpdateForm {
+
+  def apply(documentSet: DocumentSet): Form[DocumentSet] = Form(
+    mapping(
+      "public" -> optional(boolean),
+      "title" -> optional(text))
+      ((isPublic, title) => update(documentSet, isPublic, title))
+      (documentSet => Some((Some(documentSet.isPublic), Some(documentSet.title)))))
+
+  private def update(documentSet: DocumentSet, isPublic: Option[Boolean], title: Option[String]): DocumentSet = {
+    val isPublicUpdate = isPublic.getOrElse(documentSet.isPublic)
+    val titleUpdate = title.getOrElse(documentSet.title)
+
+    documentSet.copy(isPublic = isPublicUpdate, title = titleUpdate)
+  }
+}
---
+++ test/controllers/forms/DocumentSetUpdateFormSpec.scala
@@ -0,0 +1,47 @@
+package controllers.forms
+
+import org.overviewproject.test.Specification
+import org.specs2.specification.Scope
+import models.orm.DocumentSet
+import models.orm.DocumentSetType._
+import play.api.data.Form
+import org.specs2.execute.Result
+
+class DocumentSetUpdateFormSpec extends Specification {
+
+  "DocumentSetUpdateForm" should {
+
+    trait UpdateFormContext extends Scope {
+      val documentSet = DocumentSet(DocumentCloudDocumentSet, 1l, title = "title", isPublic = false)
+      val form = DocumentSetUpdateForm(documentSet)
+    }
+    
+    def checkFormValues(form: Form[DocumentSet], data: Map[String, String])(check: DocumentSet => Result): Result = 
+      form.bind(data).fold(f => failure("Couldn't bind form"),
+        check
+      )
+    
+
+    "accept all parameters" in new UpdateFormContext {
+
+      val data = Map(
+        "public" -> "false",
+        "title" -> "new title")
+
+      checkFormValues(form, data) { d => 
+          d.isPublic must beFalse
+          d.title must be equalTo ("new title")
+        }
+    }
+
+    "accept one parameter" in new UpdateFormContext {
+      val data = Map("public" -> "true")
+
+      checkFormValues(form, data) { d => 
+        d.isPublic must beTrue
+        d.title must be equalTo documentSet.title
+      }
+    }
+
+  }
+}
