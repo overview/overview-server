e1f481e5fd288c787aaea87c8157e395e79af732
Adam Hooper <adam@adamhooper.com>
2013-02-13 15:46:46 -0500

    Add files from previous commit. WHOOPS.

---
+++ app/models/ResultPage.scala
@@ -0,0 +1,66 @@
+package models
+
+import org.squeryl.Query
+
+case class ResultPageDetails(
+  val pageSize: Int,
+  val pageNum: Int,
+  val totalLength: Long
+) {
+  def isFirst : Boolean = pageNum == 1
+  def isLast : Boolean = pageNum == lastPageNum
+  def isOnlyPage : Boolean = isFirst && isLast
+  def lastPageNum : Int = math.ceil(1.0 * totalLength / pageSize).toInt
+}
+
+case class ResultPage[A](
+  val items: Iterable[A],
+  val pageDetails: ResultPageDetails
+) extends Iterable[A] {
+  override def iterator = items.iterator
+  def map[B](f: (A) => B) = ResultPage(items.map(f), pageDetails)
+}
+
+trait ResultPageSet[T] {
+  val pageSize: Int
+
+  def totalLength: Long
+  def page(n: Int) : ResultPage[T]
+}
+
+object ResultPageSet {
+  case class SquerylQueryResultPageSet[T](val query: Query[T], override val pageSize: Int) extends ResultPageSet[T] {
+    override lazy val totalLength : Long = {
+      import org.overviewproject.postgres.SquerylEntrypoint._
+
+      from(query)(_ => compute(count()))
+    }
+
+    override def page(n: Int) = {
+      import org.overviewproject.postgres.SquerylEntrypoint._ // for implicit queryToIterable
+
+      val items = query.page(pageSize * (n - 1), pageSize)
+      val details = ResultPageDetails(pageSize, n, totalLength)
+
+      ResultPage(items, details)
+    }
+  }
+
+  case class SeqResultPageSet[T](val seq: Seq[T], override val pageSize: Int) extends ResultPageSet[T] {
+    override lazy val totalLength : Long = seq.length
+    override def page(n: Int) = {
+      val items = seq.slice((n - 1) * pageSize, n * pageSize)
+      val details = ResultPageDetails(pageSize, n, totalLength)
+
+      ResultPage(items, details)
+    }
+  }
+
+  def apply[T](query: Query[T], pageSize: Int) = SquerylQueryResultPageSet(query, pageSize)
+  def apply[T](seq: Seq[T], pageSize: Int) = SeqResultPageSet(seq, pageSize)
+}
+
+object ResultPage {
+  def apply[T](query: Query[T], pageSize: Int, page: Int) : ResultPage[T] = ResultPageSet(query, pageSize).page(page)
+  def apply[T](seq: Seq[T], pageSize: Int, page: Int) : ResultPage[T] = ResultPageSet(seq, pageSize).page(page)
+}
---
+++ app/views/html/helper/Pagination.scala
@@ -0,0 +1,64 @@
+package views.html.helper
+
+import play.api.templates.Html
+
+import models.ResultPageDetails
+
+object Pagination {
+  /** Draws navigation links, a la http://twitter.github.com/bootstrap/components.html#pagination
+    *
+    * @param pageDetails Details from the ResultPage
+    * @param pageNumToUrl: Function returning the URL of the specified page
+    * @param options Options. Valid keys:
+    *                previousText: String to show in "previous" button (default "«")
+    *                nextText: Text to show in "next" button (default "»")
+    */
+  def links(pageDetails: ResultPageDetails, pageNumToUrl : Int => String, options: Map[String, String] = Map.empty) = {
+    if (pageDetails.isOnlyPage) {
+      Html("")
+    } else {
+      val prevText = options.get("previousText").getOrElse("«")
+      val nextText = options.get("nextText").getOrElse("»")
+
+      def disabledLi(text: String) = <li class="disabled"><a>{text}</a></li>
+      def textLi(url: String, text: String) = <li><a href={url}>{text}</a></li>
+
+      def prevLi = {
+        if (pageDetails.isFirst) {
+          disabledLi(prevText)
+        } else {
+          val prevHref = pageNumToUrl(pageDetails.pageNum - 1)
+          textLi(prevHref, prevText)
+        }
+      }
+
+      def nextLi = {
+        if (pageDetails.isLast) {
+          disabledLi(nextText)
+        } else {
+          val nextHref = pageNumToUrl(pageDetails.pageNum + 1)
+          textLi(nextHref, nextText)
+        }
+      }
+
+      def liFor(n: Int) = {
+        if (n == pageDetails.pageNum) {
+          <li class="active"><a>{n}</a></li>
+        } else {
+          <li><a href={pageNumToUrl(n)}>{n}</a></li>
+        }
+      }
+
+      Html(
+        <div class="pagination">
+          <ul>
+            {prevLi}
+            {(1 until pageDetails.lastPageNum + 1).map { pageNum =>
+              liFor(pageNum)
+            }}
+            {nextLi}
+          </ul>
+        </div>.buildString(false))
+    }
+  }
+}
