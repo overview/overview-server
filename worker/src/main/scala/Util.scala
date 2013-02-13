dd3afc431d7f205f024801fef63fcf0d4fbc8163
jstray <jonathanstray@gmail.com>
2013-02-13 17:24:02 -0500

    New control structure: logExecutionTime will, um, do that

--- worker/src/main/scala/Util.scala
+++ worker/src/main/scala/Util.scala
@@ -25,10 +25,20 @@ object Logger {
   
   def logElapsedTime(op: String, t0: Long) {
     val t1 = System.nanoTime()
-    info(op + ", time: " + ("%.2f" format (t1 - t0) / 1e9) + " seconds")
+    info(op + ", time: " + ("%.3f" format (t1 - t0) / 1e9) + " seconds")
   }
 
+  // Version of the above that can be used as a custom control structure. Takes a string and a code block
+  // optional boolean can be used for conditional logging
+  def logExecutionTime(op:String, logIt:Boolean = true)(fn : => Unit) : Unit = {
+    val t0 = System.nanoTime()
+    fn
+    if (logIt)
+      logElapsedTime(op, t0)
+  }
 }
+
+
 
 // Singleton Akka actor system object. One per process, managing all actors.
 object WorkerActorSystem {
