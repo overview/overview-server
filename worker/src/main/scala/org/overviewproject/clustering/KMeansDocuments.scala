806449cec97835416e642bcafb5de35d1fa033cb
jstray <jonathanstray@gmail.com>
2013-02-13 18:27:48 -0500

    10x speed up in assignClusters. Use DocumentVectorMap as centroid representation, then iterate through doc terms, not centroid terms, to compute cosine distance, because centroid will have fill-in.

--- worker/src/main/scala/org/overviewproject/clustering/KMeansDocuments.scala
+++ worker/src/main/scala/org/overviewproject/clustering/KMeansDocuments.scala
@@ -12,16 +12,27 @@ package org.overviewproject.clustering
 
 import ClusterTypes._
 
-// Short and sweet
-class KMeansDocuments(protected val docVecs:DocumentSetVectors) extends KMeans[DocumentID,DocumentVector] {
+// Use DocumentVectorMap as centroid type, but just document ID as element type. 
+// We reference into a set of vectors given in the ctor
+
+class KMeansDocuments(protected val docVecs:DocumentSetVectors) extends KMeans[DocumentID,DocumentVectorMap] {
   
-  def distance(a:DocumentID, b:DocumentVector) : Double = {
-    DistanceFn.CosineDistance(docVecs(a),b)
+  // Custom cosine distance function: always index against a, as the centroid will have fill-in
+  def distance(aId:DocumentID, b:DocumentVectorMap) : Double = {    
+    val a = docVecs(aId)
+    var dot = 0.0
+    for (i <- 0 until a.length) {
+      val weight = b.get(a.terms(i))
+      if (weight.isDefined)
+        dot += a.weights(i) * weight.get
+    }
+    
+    1.0 - dot
   }
   
   // To compute mean, we accumulate in a DocumentVectorMap, much more suited to modification than DocumentVector
   // result is not very sparse after we sum all those terms -- we get fill-in here
-  def mean(elems: Iterable[DocumentID]) : DocumentVector = {
+  def mean(elems: Iterable[DocumentID]) : DocumentVectorMap = {
     var m = DocumentVectorMap()
     elems foreach { docId => m.accumulate(DocumentVectorMap(docVecs(docId))) }  // ...could save conversion time if we had accumulate(v:DocumentVector) 
 
@@ -31,7 +42,7 @@ class KMeansDocuments(protected val docVecs:DocumentSetVectors) extends KMeans[D
 //    println("New centroid has " +  m.size + " terms.")
 //    println("Top terms: " + m.toSeq.sortWith(_._2 > _._2).take(20))
     
-    DocumentVector(m)
+    m
   }
 }
 
