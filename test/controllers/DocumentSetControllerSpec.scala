63efec65e8c44641fe7a27f173a2a143d270688c
Jonas Karlsson <thejonas@gmail.com>
2013-02-25 16:38:53 -0500

    Test that controller loads the viewers

--- test/controllers/DocumentSetControllerSpec.scala
+++ test/controllers/DocumentSetControllerSpec.scala
@@ -29,9 +29,9 @@ class DocumentSetControllerSpec extends Specification with Mockito {
     var savedDocumentSet: Option[DocumentSet] = None
     var createdJobOwnerId: Option[Long] = None
     var loadedViewers: Int = 0
-    
+
     private var documentSets: Map[Long, DocumentSet] = Map((1l, DocumentSet(DocumentCloudDocumentSet, 1l, "title", Some("query"))))
-    
+
     override protected def loadDocumentSet(id: Long): Option[DocumentSet] = {
       documentSets.get(id)
     }
@@ -40,16 +40,19 @@ class DocumentSetControllerSpec extends Specification with Mockito {
       savedDocumentSet.get
     }
     override protected def setDocumentSetOwner(documentSet: DocumentSet, ownerEmail: String) {}
-    override protected def createDocumentSetCreationJob(documentSet: DocumentSet, credentials: Credentials): Unit =  createdJobOwnerId = Some(documentSet.id)
+    override protected def createDocumentSetCreationJob(documentSet: DocumentSet, credentials: Credentials): Unit = createdJobOwnerId = Some(documentSet.id)
     override protected def loadDocumentSetViewers(id: Long): Iterable[DocumentSetUser] = {
       loadedViewers += 1
       Nil
     }
   }
 
-  trait AuthorizedSession extends Scope {
+  trait ControllerScope extends Scope {
     val controller = new TestDocumentSetController
     val user = mock[OverviewUser]
+  }
+
+  trait AuthorizedSession extends ControllerScope {
     def request = new AuthorizedRequest(
       FakeRequest()
         .withSession("AUTH_USER_ID" -> user.id.toString)
@@ -69,12 +72,12 @@ class DocumentSetControllerSpec extends Specification with Mockito {
     val newTitle = "New Title"
     override def sessionForm = Seq("public" -> "true", "title" -> newTitle)
   }
-  
+
   trait BadUpdateRequest extends AuthorizedSession {
     val documentSetId: Long = 1l
     override def sessionForm = Seq("public" -> "not a boolean")
   }
-
+  
   "The DocumentSet Controller" should {
 
     "submit a DocumentSetCreationJob when a new query is received" in new CreateRequest {
@@ -93,37 +96,35 @@ class DocumentSetControllerSpec extends Specification with Mockito {
       val result = controller.create()(request)
       redirectLocation(result).getOrElse("No redirect") must be equalTo ("/documentsets")
     }
-    
+
     "update the DocumentSet" in new UpdateRequest {
       val result = controller.update(documentSetId)(request)
-      
-      status(result) must be equalTo(OK)
+
+      status(result) must be equalTo (OK)
       controller.savedDocumentSet must beSome
       val documentSet = controller.savedDocumentSet.get
-      
+
       documentSet.isPublic must beTrue
-      documentSet.title must be equalTo(newTitle)
+      documentSet.title must be equalTo (newTitle)
     }
-    
+
     "return NotFound if document set is bad" in new UpdateRequest {
       val result = controller.update(-1l)(request)
-      status(result) must be equalTo(NOT_FOUND)
+      status(result) must be equalTo (NOT_FOUND)
     }
-    
+
     "return BadRequest if form input is bad" in new BadUpdateRequest {
       val result = controller.update(documentSetId)(request)
-      status(result) must be equalTo(BAD_REQUEST)
+      status(result) must be equalTo (BAD_REQUEST)
     }
-    
-    "return viewers" in {
-      val controller = new TestDocumentSetController
-      val user = mock[OverviewUser]
+
+    "return viewers" in new ControllerScope {
       val request = new AuthorizedRequest(FakeRequest(), user)
-      
+
       val result = controller.showUsers(1l)(request)
-      status(result) must be equalTo(OK)
-      
-      controller.loadedViewers must be equalTo(1)
+      status(result) must be equalTo (OK)
+
+      controller.loadedViewers must be equalTo (1)
     }
   }
 
