3ac16753b9846f406861189cb6e714ee38353b79
jstray <jonathanstray@gmail.com>
2013-02-26 16:11:42 -0500

    Add help page and user forum links to registration email. Fixes #350

--- app/mailers/User/create.scala
+++ app/mailers/User/create.scala
@@ -2,16 +2,21 @@ package mailers.User
 
 import play.api.i18n.Lang
 import play.api.mvc.RequestHeader
-
+import play.api.Play
+import play.api.Play.current
 import mailers.Mailer
 import models.{ConfirmationRequest, OverviewUser}
+import scala.xml.Unparsed
 
 case class create(val user: OverviewUser with ConfirmationRequest)(implicit val lang: Lang, val request: RequestHeader) extends Mailer {
   private val m = views.Magic.scopedMessages("mailers.User.create")
 
   private val url = controllers.routes.ConfirmationController.show(user.confirmationToken).absoluteURL() 
+  private val helpUrl = Play.configuration.getString("overview.help_url").getOrElse(throw new Exception("overview.help_url not configured"))
+  private val userForumUrl = Play.configuration.getString("overview.user_forum_url").getOrElse(throw new Exception("overview.user_forum_url not configured"))
+
   private val body = m("body")
-  private val signoff = m("signoff")
+  private val signoff = Unparsed(m("signoff_html", helpUrl, userForumUrl))
   private val signature = m("signature")
 
   override val recipients = Seq(user.email)
