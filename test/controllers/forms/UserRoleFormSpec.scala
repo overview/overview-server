a9c6810dd1835e35e50d2c5660f52488ca1e5734
Jonas Karlsson <thejonas@gmail.com>
2013-02-26 10:00:00 -0500

    Validate email in form

--- test/controllers/forms/UserRoleFormSpec.scala
+++ test/controllers/forms/UserRoleFormSpec.scala
@@ -2,18 +2,32 @@ package controllers.forms
 
 import org.specs2.mutable.Specification
 import models.orm.DocumentSetUserRoleType._
+import org.specs2.specification.Scope
+import play.api.data.Form
+import models.orm.DocumentSetUser
 
 class UserRoleFormSpec extends Specification {
 
   "UserRoleForm" should {
     
-    "create a DocumentSetUser" in {
-      val form = UserRoleForm(1l).bind(Map("email" -> "user@host.com", "role" -> "Viewer"))
+    trait FormContext extends Scope {
+      def bindForm(email: String, role: String): Form[DocumentSetUser] =
+        UserRoleForm(1l).bind(Map("email" -> email, "role" -> role))
+    }
+    
+    "create a DocumentSetUser" in new FormContext {
+      val form = bindForm("user@host.com", "Viewer")
       
       val documentSetUser = form.get
       documentSetUser.documentSetId must be equalTo(1l)
       documentSetUser.userEmail must be equalTo("user@host.com")
       documentSetUser.role must be equalTo(Viewer)
+    }
+    
+    "reject bad email format" in new FormContext {
+      val form = bindForm("bad email format", "Viewer")
+      
+      form.error("email") must beSome.like { case e => e.message must be equalTo ("error.email") }
     }
   }
 }
