f9a05ad02ee7dcc8b0bf35b3124802052fb74a2b
jstray <jonathanstray@gmail.com>
2013-03-10 01:11:10 -0500

    New utilities: toMutableSet implicit, Range object with .clip method

--- worker/src/main/scala/org/overviewproject/util/Util.scala
+++ worker/src/main/scala/org/overviewproject/util/Util.scala
@@ -12,6 +12,7 @@ package org.overviewproject.util
 
 import akka.actor._
 import org.slf4j.LoggerFactory
+import scala.language.implicitConversions  // for toMutableSet
 
 // Worker logging singleton object. Pass-through to LogBack
 object Logger {
@@ -62,6 +63,27 @@ class LoopedIterator[T](makeIter: => Iterator[T]) extends Iterator[T] {
       current = makeIter
     current.next
   }  
+}
+
+
+// An implicit that adds toMutableSet to any Iterable (toSet almost always returns immutable set)
+class ToMutableSet[T](val container:Iterable[T]) {
+  def toMutableSet() : scala.collection.mutable.Set[T] = {
+    val s = scala.collection.mutable.Set[T]()
+    container foreach { s += _ }
+    s
+  }
+}
+object ToMutableSet {
+  implicit def augmentIterable[T](x: Iterable[T]) : ToMutableSet[T] = new ToMutableSet(x)
+}
+
+// Some basic utility functions for dealing with ranges
+object Ranges {
+  def clip(a:Int, x:Int, b:Int) = Math.min(b, Math.max(a, x)) 
+  def clip(a:Long, x:Long, b:Long) = Math.min(b, Math.max(a, x)) 
+  def clip(a:Float, x:Float, b:Float) = Math.min(b, Math.max(a, x)) 
+  def clip(a:Double, x:Double, b:Double) = Math.min(b, Math.max(a, x)) 
 }
 
 // Trait that knows how to compose an error key 
@@ -115,4 +137,6 @@ object Progress {
   // stub that you can pass in when you don't case about progress reporting
   def NoProgressReporting(p: Progress): Boolean = { false }
 }
+
+
 
