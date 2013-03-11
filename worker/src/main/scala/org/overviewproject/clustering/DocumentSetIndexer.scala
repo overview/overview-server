ddee62b6552183186a7a2daef1234247d05eb6b9
jstray <jonathanstray@gmail.com>
2013-03-11 02:31:48 -0400

    Keep terms with fewer instances if the docset is 'small', currently <200 docs

--- worker/src/main/scala/org/overviewproject/clustering/DocumentSetIndexer.scala
+++ worker/src/main/scala/org/overviewproject/clustering/DocumentSetIndexer.scala
@@ -30,8 +30,10 @@ class DocumentSetIndexer(nodeWriter: NodeWriter, progAbort: ProgressAbortFn) ext
   private val fetchingFraction = 0.5 // what percent done do we say when we're all done fetching docs?
   private val savingFraction = 0.98
 
-  private val vectorGen = new DocumentVectorGeneratorWithBigrams
+  private val smallDocsetSize = 200   // when docset is smaller than this, reduce minimum count thresholds for keeping terms
 
+  private val vectorGen = new DocumentVectorGeneratorWithBigrams
+  
   // When we get the document text back, we add the document to the database and feed the text to the vector generator
   def processDocument(documentId: Long, text: String): Unit = {
     vectorGen.addDocument(documentId, Lexer.makeTerms(text))
@@ -46,24 +48,32 @@ class DocumentSetIndexer(nodeWriter: NodeWriter, progAbort: ProgressAbortFn) ext
 
   def productionComplete() {
     Logger.logElapsedTime("Retrieved " + vectorGen.numDocs, t0)
-
+    
     if (!progAbort(Progress(fetchingFraction, Clustering))) {
       val t1 = System.nanoTime()
+      
+      // keep more of the terms in the tail if the docset is small
+      if (vectorGen.numDocs < smallDocsetSize) {
+        vectorGen.minDocsToKeepTerm = 2
+        vectorGen.minBigramOccurrences = 3
+      }
+        
       val docVecs = vectorGen.documentVectors()
       val docTree = BuildDocTree(docVecs, makeNestedProgress(progAbort, fetchingFraction, savingFraction))
-      DB.withConnection { implicit connection => addDocumentDescriptions(docTree) }
-
+      
       Logger.logElapsedTime("Clustered documents", t1)
-
+    
       // Save tree to database
       if (!progAbort(Progress(savingFraction, Saving))) {
         val t2 = System.nanoTime()
+        
+        DB.withConnection { implicit connection => addDocumentDescriptions(docTree) }
         Database.inTransaction {
           implicit val connection = Database.currentConnection
           nodeWriter.write(docTree)
         }
+        
         Logger.logElapsedTime("Saved DocumentSet to DB", t2)
-
         progAbort(Progress(1, Done))
       }
     }
