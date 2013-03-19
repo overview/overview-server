b6b098586cbaf420afb1c9e2c48ab83eea6d8c0d
Adam Hooper <adam@adamhooper.com>
2013-03-19 17:46:02 -0400

    Make console.log messages in specs go to the console

--- test/assets/javascripts/framework/init.js
+++ test/assets/javascripts/framework/init.js
@@ -1,4 +1,7 @@
 (function() {
+  if (!window.console) console = {};
+  console.log = jasmine.log;
+
   Timecop.MockDate.now = function() {
     return (new Date()).getTime(); // the mocked date
   };
