56cd53f289293f9842c9cca12d91c6ea9f81c22c
Adam Hooper <adam@adamhooper.com>
2013-04-01 13:46:16 -0400

    Add new "Ownership" enum, for DocumentSetUser.role

---
+++ common/src/main/scala/org/overviewproject/tree/Ownership.scala
@@ -0,0 +1,7 @@
+package org.overviewproject.tree
+
+object Ownership extends Enumeration {
+  // Constants copied from "ownership" table
+  val Owner = Value(1, "Owner")
+  val Viewer = Value(2, "Viewer")
+}
