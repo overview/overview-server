27d0be9abf0356a007e15575287e5dacb9a7c8c4
Jonas Karlsson <thejonas@gmail.com>
2013-04-09 12:36:20 -0400

    Interfaces for AsyncHttpClient

---
+++ worker/src/main/scala/org/overviewproject/http/Client.scala
@@ -0,0 +1,16 @@
+package org.overviewproject.http
+
+import com.ning.http.client.AsyncCompletionHandler
+
+case class Credentials(userName: String, password: String)
+
+trait Response2 {
+  def status: Int
+  def headers(name: String): Seq[String]
+  def body: String
+}
+
+trait Client {
+  def submit(url: String, responseHandler: AsyncCompletionHandler[Unit]): Unit
+  def submitWithAuthentication(url: String, credentials: Credentials, responseHandler: AsyncCompletionHandler[Unit]): Unit
+}
