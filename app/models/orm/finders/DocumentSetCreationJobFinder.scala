119a6785d4a9e8e3b29f50994e541558758e5e56
Adam Hooper <adam@adamhooper.com>
2013-04-10 12:25:51 -0400

    Return a DocumentSetCreationJobFinderResult

--- app/models/orm/finders/DocumentSetCreationJobFinder.scala
+++ app/models/orm/finders/DocumentSetCreationJobFinder.scala
@@ -1,19 +1,25 @@
 package models.orm.finders
 
+import org.squeryl.Query
 import org.overviewproject.postgres.SquerylEntrypoint._
+import scala.language.postfixOps
+import scala.language.implicitConversions
 
 import org.overviewproject.tree.orm.DocumentSetCreationJob
 import org.overviewproject.tree.Ownership
 import models.orm.Schema
 
-object DocumentSetCreationJobFinder {
-  import scala.language.postfixOps
+object DocumentSetCreationJobFinder extends Finder {
+  class DocumentSetCreationJobFinderResult(query: Query[DocumentSetCreationJob]) extends FinderResult(query)
+  object DocumentSetCreationJobFinderResult {
+    implicit def fromQuery(query: Query[DocumentSetCreationJob]) = new DocumentSetCreationJobFinderResult(query)
+  }
 
   /** @return All DocumentSetCreationJobs with the given ID.
     *
     * Since ID is a unique key, the return value can only have 0 or 1 row.
     */
-  def byDocumentSet(documentSet: Long) = {
+  def byDocumentSet(documentSet: Long) : DocumentSetCreationJobFinderResult = {
     from(Schema.documentSetCreationJobs)(dscj =>
       where(dscj.documentSetId === documentSet)
       select(dscj)
@@ -24,7 +30,7 @@ object DocumentSetCreationJobFinder {
     *
     * These are ordered from newest to oldest.
     */
-  def all = {
+  def all : DocumentSetCreationJobFinderResult = {
     from(Schema.documentSetCreationJobs)(dscj =>
       select(dscj)
       orderBy(dscj.id desc)
