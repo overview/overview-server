975217bcb75f3b9a1b4a3e9546ae7a4bc8c37901
Jonas Karlsson <thejonas@gmail.com>
2013-04-11 09:03:12 -0400

    Request private document with credentials

--- worker/src/main/scala/org/overviewproject/documentcloud/DocumentRetriever.scala
+++ worker/src/main/scala/org/overviewproject/documentcloud/DocumentRetriever.scala
@@ -3,12 +3,18 @@ package org.overviewproject.documentcloud
 import akka.actor._
 import org.overviewproject.http.PublicRequest
 import org.overviewproject.http.RequestQueueProtocol.AddToEnd
+import org.overviewproject.http.Credentials
+import org.overviewproject.http.PrivateRequest
 
-class DocumentRetriever(requestQueue: ActorRef) extends Actor {
+class DocumentRetriever(requestQueue: ActorRef, credentials: Option[Credentials]) extends Actor {
+  
+  private val PublicAccess: String = "public"
   private def DocumentQuery(d: Document): String = s"https://www.documentcloud.org/api/documents/${d.id}.txt"
   
   def receive = {
-    case d: Document => requestQueue ! AddToEnd(PublicRequest(DocumentQuery(d)))
+    case d: Document if isPublic(d) => requestQueue ! AddToEnd(PublicRequest(DocumentQuery(d)))
+    case d: Document => credentials.map { c => requestQueue ! AddToEnd(PrivateRequest(DocumentQuery(d), c)) }
   }
 
+  private def isPublic(d: Document): Boolean = d.access == PublicAccess
 }
--- worker/src/test/scala/org/overviewproject/documentcloud/DocumentRetrieverSpec.scala
+++ worker/src/test/scala/org/overviewproject/documentcloud/DocumentRetrieverSpec.scala
@@ -1,10 +1,11 @@
 package org.overviewproject.documentcloud
 
-import org.specs2.mutable.Specification
-import org.overviewproject.test.ActorSystemContext
-import akka.testkit.TestActorRef
+import org.overviewproject.http.{Credentials, PrivateRequest, PublicRequest}
 import org.overviewproject.http.RequestQueueProtocol.AddToEnd
-import org.overviewproject.http.PublicRequest
+import org.overviewproject.test.ActorSystemContext
+import org.specs2.mutable.Specification
+
+import akka.testkit.TestActorRef
 
 class DocumentRetrieverSpec extends Specification {
 
@@ -13,11 +14,23 @@ class DocumentRetrieverSpec extends Specification {
     "queue a request for a public url" in new ActorSystemContext {
       val document = Document("id", "title", "public", "http://canonical-url")
       val documentUrl = "https://www.documentcloud.org/api/documents/id.txt"
-      val retriever = TestActorRef(new DocumentRetriever(testActor))
+      val retriever = TestActorRef(new DocumentRetriever(testActor, None))
       
       retriever ! document
       
       expectMsg(AddToEnd(PublicRequest(documentUrl)))
+    }
+    
+    "queue a request for a private url" in new ActorSystemContext {
+      val credentials = Credentials("user@host", "dcpassword")
+      val document = Document("id", "title", "private", "http://canonical-url")
+      val documentUrl = "https://www.documentcloud.org/api/documents/id.txt"
+      val retriever = TestActorRef(new DocumentRetriever(testActor, Some(credentials)))
+      
+      retriever ! document
+      
+      expectMsg(AddToEnd(PrivateRequest(documentUrl, credentials)))
+      
     }
   }
 }
