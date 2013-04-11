1c36b62478f2ff36f6dd323a9c485a1ae91f7e9f
Jonas Karlsson <thejonas@gmail.com>
2013-04-11 09:34:59 -0400

    Handle redirect after requesting private doc

--- worker/src/main/scala/org/overviewproject/documentcloud/DocumentRetriever.scala
+++ worker/src/main/scala/org/overviewproject/documentcloud/DocumentRetriever.scala
@@ -1,20 +1,32 @@
 package org.overviewproject.documentcloud
 
+import org.overviewproject.http.{Credentials, PrivateRequest, PublicRequest}
+import org.overviewproject.http.RequestQueueProtocol._
 import akka.actor._
-import org.overviewproject.http.PublicRequest
-import org.overviewproject.http.RequestQueueProtocol.AddToEnd
-import org.overviewproject.http.Credentials
-import org.overviewproject.http.PrivateRequest
+import org.overviewproject.http.SimpleResponse
 
 class DocumentRetriever(requestQueue: ActorRef, credentials: Option[Credentials]) extends Actor {
   
   private val PublicAccess: String = "public"
+  private val LocationHeader: String = "Location"
+  private val RedirectStatus: Int = 302
+  
   private def DocumentQuery(d: Document): String = s"https://www.documentcloud.org/api/documents/${d.id}.txt"
   
   def receive = {
-    case d: Document if isPublic(d) => requestQueue ! AddToEnd(PublicRequest(DocumentQuery(d)))
-    case d: Document => credentials.map { c => requestQueue ! AddToEnd(PrivateRequest(DocumentQuery(d), c)) }
+    case d: Document if isPublic(d) => requestPublicDocument(d)
+    case d: Document => requestPrivateDocument(d)
+    case Result(r) if isRedirect(r) => redirectRequest(r) 
   }
 
   private def isPublic(d: Document): Boolean = d.access == PublicAccess
+  private def isRedirect(r: SimpleResponse): Boolean = r.status == RedirectStatus
+  
+  private def requestPublicDocument(d: Document): Unit = requestQueue ! AddToEnd(PublicRequest(DocumentQuery(d)))
+  private def requestPrivateDocument(d: Document): Unit = credentials.map { c => requestQueue ! AddToFront(PrivateRequest(DocumentQuery(d), c)) }
+  
+  private def redirectRequest(response: SimpleResponse): Unit = response.headers(LocationHeader).map { url =>
+    requestQueue ! AddToEnd(PublicRequest(url))
+  }
+  
 }
--- worker/src/test/scala/org/overviewproject/documentcloud/DocumentRetrieverSpec.scala
+++ worker/src/test/scala/org/overviewproject/documentcloud/DocumentRetrieverSpec.scala
@@ -1,11 +1,11 @@
 package org.overviewproject.documentcloud
 
 import org.overviewproject.http.{Credentials, PrivateRequest, PublicRequest}
-import org.overviewproject.http.RequestQueueProtocol.AddToEnd
+import org.overviewproject.http.RequestQueueProtocol._
 import org.overviewproject.test.ActorSystemContext
 import org.specs2.mutable.Specification
-
 import akka.testkit.TestActorRef
+import org.overviewproject.http.PublicRequest
 
 class DocumentRetrieverSpec extends Specification {
 
@@ -21,7 +21,7 @@ class DocumentRetrieverSpec extends Specification {
       expectMsg(AddToEnd(PublicRequest(documentUrl)))
     }
     
-    "queue a request for a private url" in new ActorSystemContext {
+    "put request for a private url in front of the queue" in new ActorSystemContext {
       val credentials = Credentials("user@host", "dcpassword")
       val document = Document("id", "title", "private", "http://canonical-url")
       val documentUrl = "https://www.documentcloud.org/api/documents/id.txt"
@@ -29,8 +29,21 @@ class DocumentRetrieverSpec extends Specification {
       
       retriever ! document
       
-      expectMsg(AddToEnd(PrivateRequest(documentUrl, credentials)))
+      expectMsg(AddToFront(PrivateRequest(documentUrl, credentials)))
+    }
+    
+    "handle redirect from a private document request" in new ActorSystemContext {
+      val credentials = Credentials("user@host", "dcpassword")
+      val document = Document("id", "title", "private", "http://canonical-url")
+      val documentUrl = "https://www.documentcloud.org/api/documents/id.txt"
+      val redirectUrl = "https://someting.s3.amazon.com/doc.txt"
+      val response = TestSimpleResponse(302, "ignored body", Map(("Location" -> redirectUrl)))
+      val retriever = TestActorRef(new DocumentRetriever(testActor, Some(credentials)))
       
+      retriever ! document
+      expectMsg(AddToFront(PrivateRequest(documentUrl, credentials)))
+      retriever ! Result(response)      
+      expectMsg(AddToEnd(PublicRequest(redirectUrl)))
     }
   }
 }
