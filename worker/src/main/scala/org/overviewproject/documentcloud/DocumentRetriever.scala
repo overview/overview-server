3a57da5c264606e3f76d8dcd6cb234974ef8e018
Jonas Karlsson <thejonas@gmail.com>
2013-04-10 20:14:03 -0400

    Put magic query format at the top of the class

--- worker/src/main/scala/org/overviewproject/documentcloud/DocumentRetriever.scala
+++ worker/src/main/scala/org/overviewproject/documentcloud/DocumentRetriever.scala
@@ -5,7 +5,10 @@ import org.overviewproject.http.PublicRequest
 import org.overviewproject.http.RequestQueueProtocol.AddToEnd
 
 class DocumentRetriever(requestQueue: ActorRef) extends Actor {
+  private def DocumentQuery(d: Document): String = s"https://www.documentcloud.org/api/documents/${d.id}.txt"
+  
   def receive = {
-    case d: Document => requestQueue ! AddToEnd(PublicRequest(s"https://www.documentcloud.org/api/documents/${d.id}.txt"))
+    case d: Document => requestQueue ! AddToEnd(PublicRequest(DocumentQuery(d)))
   }
+
 }
