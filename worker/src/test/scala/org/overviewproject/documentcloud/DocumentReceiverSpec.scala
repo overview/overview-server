b9b6b04f261738b4a2377b98b634a64f87318b04
Jonas Karlsson <thejonas@gmail.com>
2013-04-12 14:25:43 -0400

    Extract common setup

--- worker/src/test/scala/org/overviewproject/documentcloud/DocumentReceiverSpec.scala
+++ worker/src/test/scala/org/overviewproject/documentcloud/DocumentReceiverSpec.scala
@@ -7,69 +7,60 @@ import akka.testkit.TestActorRef
 import org.overviewproject.documentcloud.DocumentRetrieverProtocol._
 import scala.concurrent.{Await, Promise}
 import org.specs2.time.NoTimeConversions
+import org.specs2.mutable.Before
 
 
 class DocumentReceiverSpec extends Specification with NoTimeConversions {
 
   "DocumentReceiver" should {
 
-    "call callback when receiving documents" in new ActorSystemContext {
+    
+    abstract class ReceiverContext extends ActorSystemContext with Before {
+      val ExpectedDocuments = 2
       val document = Document("id", "title", "public", "http://canonical-url")
       val text = "document text"
-      def callback(d: Document, t: String): Unit = testActor ! t
-      val retrievalDone = Promise[Seq[DocumentRetrievalError]]
-      val receiver = TestActorRef(new DocumentReceiver(callback, 5, retrievalDone))
+      val url = "url"      
+      
+      var retrievalDone: Promise[Seq[DocumentRetrievalError]] = _
+      var receiver: TestActorRef[DocumentReceiver] = _
+  
+      def  callback(d: Document, t: String): Unit = testActor ! t
+      
+      def before = {
+    	retrievalDone = Promise[Seq[DocumentRetrievalError]]
+    	receiver = TestActorRef(new DocumentReceiver(callback, ExpectedDocuments, retrievalDone))
+      }
+    }
+    
 
+    
+    "call callback when receiving documents" in new ReceiverContext {
       receiver ! GetTextSucceeded(document, text)
 
       expectMsg(text)
     }
     
-    "not call callback when retrieval failed" in new ActorSystemContext {
-      val document = Document("id", "title", "public", "http://canonical-url")
-      val text = "Failure"
-      def callback(d: Document, t: String): Unit = testActor ! t
-      val retrievalDone = Promise[Seq[DocumentRetrievalError]]
-      val receiver = TestActorRef(new DocumentReceiver(callback, 5, retrievalDone))
-
+    "not call callback when retrieval failed" in new ReceiverContext {
       receiver ! GetTextFailed("failed url", text)
 
       expectNoMsg
-      
     }
 
-    "complete future when specified number of documents have been processed" in new ActorSystemContext {
-      val document = Document("id", "title", "public", "http://canonical-url")
-      val text = "document text"
-      def callback(d: Document, t: String): Unit = {}
-
-      val retrievalDone = Promise[Seq[DocumentRetrievalError]]
-      val receiver = TestActorRef(new DocumentReceiver(callback, 2, retrievalDone))
-
+    "complete future when specified number of documents have been processed" in new ReceiverContext {
       receiver ! GetTextSucceeded(document, text)
       retrievalDone.isCompleted must beFalse
 
       receiver ! GetTextSucceeded(document, text)
       retrievalDone.isCompleted must beTrue
-      
     }
     
-    "return retrieval errors in future" in new ActorSystemContext {
-      val document = Document("id", "title", "public", "http://canonical-url")
-      val text = "document text"
-      val errorText = "some failure"
-      val failedUrl = "url"
-      def callback(d: Document, t: String): Unit = {}
-      
-      val retrievalDone = Promise[Seq[DocumentRetrievalError]]
-      val receiver = TestActorRef(new DocumentReceiver(callback, 2, retrievalDone))
-
+    "return retrieval errors in future" in new ReceiverContext {
       receiver ! GetTextSucceeded(document, text)
-      receiver ! GetTextFailed(failedUrl, errorText)
+      receiver ! GetTextFailed(url, text)
 
       retrievalDone.isCompleted must beTrue
       val retrievalErrors: Seq[DocumentRetrievalError] = Await.result(retrievalDone.future, 1 millis)
-      retrievalErrors must haveTheSameElementsAs(Seq(DocumentRetrievalError(failedUrl, errorText)))
+      retrievalErrors must haveTheSameElementsAs(Seq(DocumentRetrievalError(url, text)))
     }
     
   }
