fe1da54db279ecc9f5cacf12891ea99b6cfe786a
Adam Hooper <adam@adamhooper.com>
2013-04-16 10:11:03 -0400

    Fix string_agg() types, and add format() Postgres function

--- common/src/main/scala/org/overviewproject/postgres/SquerylPostgresFunctions.scala
+++ common/src/main/scala/org/overviewproject/postgres/SquerylPostgresFunctions.scala
@@ -1,14 +1,25 @@
 package org.overviewproject.postgres
 
-import org.squeryl.dsl.{ TypedExpression, TypedExpressionFactory, TOptionString, TString }
+import org.squeryl.PrimitiveTypeMode
+import org.squeryl.dsl._
 import org.squeryl.dsl.ast.FunctionNode
 
 trait SquerylPostgresFunctions {
-  def string_agg(
-    e: TypedExpression[Option[String],TOptionString],
+  self: PrimitiveTypeMode => 
+
+  def string_agg[T2 >: TOptionString, T1 <: T2, A1, A2](
+    e: TypedExpression[A1,T1],
     separator: TypedExpression[String,TString]
-    )(implicit f: TypedExpressionFactory[Option[String],TOptionString]) = {
+    )(implicit f: TypedExpressionFactory[A2,T2]) = {
 
     f.convert(new FunctionNode("string_agg", Seq(e, separator)))
+  }
+
+  def format[T2 >: TOptionString, T1 <: T2, A1, A2](
+    format: TypedExpression[String,TString],
+    e: TypedExpression[A1,T1]
+    )(implicit f: TypedExpressionFactory[String,TString]) = {
+
+    f.convert(new FunctionNode("format", Seq(format, e)))
   }
 }
