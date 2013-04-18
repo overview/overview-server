155330ed23e72d64e521f656af0a027613433e5b
Jonas Karlsson <thejonas@gmail.com>
2013-04-18 10:59:36 -0400

    Separate out request monitoring code

--- worker/src/main/scala/org/overviewproject/http/RequestQueue.scala
+++ worker/src/main/scala/org/overviewproject/http/RequestQueue.scala
@@ -51,8 +51,22 @@ class RequestQueue(client: Client, maxInFlightRequests: Int, superTimeout: Finit
   private case class CancelRequest(requestId: Long) // Cancel stuck request
 
 
+  /**
+   * Container for monitoring a request and canceling it on superTimeout
+   * 
+   * @param monitor The scheduled event that will trigger request cancellation
+   * @param request The Request being monitored
+   */
+  private class RequestMonitor(monitor: Cancellable, request: Request) {
+    /** Stop the monitor from triggering the cancellation event */
+    def cancelMonitor: Unit = monitor.cancel
+    
+    /** Cancel the request */
+    def cancelRequest: Unit = request.cancel
+  }
+  
   private var queuedRequests: Seq[(ActorRef, Request)] = Seq.empty // FIXME: mutable seq will be more efficient with many requests
-  private var inFlightRequests: scala.collection.mutable.Map[Long, (Cancellable, Request)] = HashMap.empty
+  private var inFlightRequests: scala.collection.mutable.Map[Long, RequestMonitor] = HashMap.empty
   private var nextInFlightRequestId: Long = 0
 
   /**
@@ -70,41 +84,67 @@ class RequestQueue(client: Client, maxInFlightRequests: Int, superTimeout: Finit
     }
   }
 
+  /** 
+   *  A received request is submitted immediately if the number of inflight requests is smaller
+   *  than `maxInFlightRequests`. Otherwise, it is added to the queue.
+   */
   def receive = {
     case AddToEnd(request) if inFlightRequests.size < maxInFlightRequests => submitRequest(sender, request)
     case AddToEnd(request) => queueRequest(sender, request)
     case AddToFront(request) if inFlightRequests.size < maxInFlightRequests => submitRequest(sender, request)
     case AddToFront(request) => queueRequestInFront(sender, request)
-    case RequestCompleted(requestId) => handleNextRequest(requestId)
+    case RequestCompleted(requestId) => completeRequest(requestId)
     case CancelRequest(requestId) => cancelRequest(requestId)
   }
 
+  /** Shutdown the client and close all connections when the actor is stopped */
   override def postStop = client.shutdown()
 
-  private def submitRequest(requestor: ActorRef, request: Request): Unit = {
-    request.execute(client, new ResultHandler(nextInFlightRequestId, requestor))
 
+  private def submitRequest(requester: ActorRef, request: Request): Unit = {
+    request.execute(client, new ResultHandler(nextInFlightRequestId, requester))
+    addToInFlightRequests(request)
+  }
+
+  private def queueRequest(requester: ActorRef, url: Request): Unit = 
+    queuedRequests = queuedRequests :+ (requester, url)
+
+  private def queueRequestInFront(requester: ActorRef, url: Request): Unit = 
+    queuedRequests = (requester, url) +: queuedRequests
+
+  private def completeRequest(requestId: Long): Unit = {
+    removeFromInFlightRequests(requestId)
+    startNextRequest
+  }
+
+  private def cancelRequest(requestId: Long): Unit = inFlightRequests.get(requestId).map { _.cancelRequest }
+  
+  /**
+   * Schedule an event to trigger request cancellation if the request lives past
+   * the superTimeout
+   */
+  private def addToInFlightRequests(request: Request): Unit = {
     import context.dispatcher
+
     val superTimeoutEvent = context.system.scheduler.scheduleOnce(superTimeout, self, CancelRequest(nextInFlightRequestId))
-    inFlightRequests += (nextInFlightRequestId -> (superTimeoutEvent, request))
+    inFlightRequests += (nextInFlightRequestId -> new RequestMonitor(superTimeoutEvent, request))
 
     nextInFlightRequestId += 1
   }
-
-  private def queueRequest(requestor: ActorRef, url: Request): Unit = queuedRequests = queuedRequests :+ (sender, url)
-
-  private def queueRequestInFront(requestor: ActorRef, url: Request): Unit = queuedRequests = (requestor, url) +: queuedRequests
-
-  private def handleNextRequest(requestId: Long): Unit = {
-    inFlightRequests.get(requestId).map { r => r._1.cancel }
+  
+  /** Stop the cancellation event for a request */
+  private def removeFromInFlightRequests(requestId: Long): scala.collection.mutable.Map[Long, RequestMonitor] = {
+    inFlightRequests.get(requestId).map { _.cancelMonitor }
     inFlightRequests -= requestId
-
-    queuedRequests.headOption.map { r =>
-      submitRequest(r._1, r._2)
-      queuedRequests = queuedRequests.tail
-    }
   }
+  
+  
+  /** Submit next request in queue */
+  private def startNextRequest: Unit = 
+    for ((requester, request) <- queuedRequests.headOption) { 
+       submitRequest(requester, request)
+       queuedRequests = queuedRequests.tail
+     }
 
-  private def cancelRequest(requestId: Long): Unit = inFlightRequests.get(requestId).map { r => r._2.cancel }
 
 }
