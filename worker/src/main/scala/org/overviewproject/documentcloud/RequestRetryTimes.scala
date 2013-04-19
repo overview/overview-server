de6dbfe5c448bf5212572a0ea76b819894fc414d
Jonas Karlsson <thejonas@gmail.com>
2013-04-19 15:50:33 -0400

    Add some comments

--- worker/src/main/scala/org/overviewproject/documentcloud/RequestRetryTimes.scala
+++ worker/src/main/scala/org/overviewproject/documentcloud/RequestRetryTimes.scala
@@ -3,14 +3,22 @@ package org.overviewproject.documentcloud
 import scala.concurrent.duration._
 import scala.language.postfixOps
   
+/**
+ * Provide times to wait before retrying a documentcloud request
+ */
 trait RequestRetryTimes {
   protected val times: Seq[FiniteDuration]
   
+  /**
+   * Return a wait duration for the specified attempt.
+   * @return None if no more attempts should be made
+   */
   def apply(attempt: Int): Option[FiniteDuration] = times.lift(attempt)
 
 }
   
 
+/** Provide default retry times */
 object RequestRetryTimes {
   private val DefaultTimes: Seq[FiniteDuration] = Seq(
     1 second,
