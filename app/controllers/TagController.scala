37bc6855ff49a9cc5554e6df5787d15e6553e9a6
Adam Hooper <adam@adamhooper.com>
2013-05-08 16:52:48 -0400

    Add route for CSV of tag counts. Refs #468
    There were no unit tests in TagController, and it needs a rewrite.
    So I didn't add any unit tests for my method :).

--- app/controllers/TagController.scala
+++ app/controllers/TagController.scala
@@ -8,16 +8,17 @@
 
 package controllers
 
-import models.{ OverviewTag, PotentialTag }
-import models.orm.User
+import au.com.bytecode.opencsv.CSVWriter
+import java.io.StringWriter
 import play.api.data.{ Form, FormError }
 import play.api.mvc.Controller
 
+import models.orm.finders.TagFinder
 import controllers.auth.AuthorizedAction
 import controllers.auth.Authorities.userOwningDocumentSet
 import controllers.forms.TagForm
 import controllers.util.IdList
-import models.{ PersistentDocumentList, PersistentTag, PersistentTagLoader }
+import models.{ OverviewTag, PotentialTag, PersistentDocumentList, PersistentTag, PersistentTagLoader }
 
 object TagController extends Controller {
   private val idListFormat = new play.api.data.format.Formatter[Seq[Long]] {
@@ -50,6 +51,24 @@ object TagController extends Controller {
         val tag = PersistentTag(updatedTag)
         Ok(views.json.Tag.create(tag.id, tag.name))
       })
+  }
+
+  def indexCsv(documentSetId: Long) = AuthorizedAction(userOwningDocumentSet(documentSetId)) { implicit request =>
+    val tags = TagFinder.byDocumentSet(documentSetId).withCounts
+
+    val stringWriter = new StringWriter()
+    val csvWriter = new CSVWriter(stringWriter)
+    csvWriter.writeNext(Array("id", "name", "count", "color"))
+    tags.foreach({ case (tag, count) =>
+      csvWriter.writeNext(Array(tag.id.toString, tag.name, count.toString, tag.color.getOrElse("")))
+    })
+    csvWriter.close()
+    Ok(stringWriter.toString())
+      .as("text/csv")
+      .withHeaders(
+        CACHE_CONTROL -> "max-age=0",
+        CONTENT_DISPOSITION -> "attachment; filename=overview-tags.csv"
+      )
   }
 
   def add(documentSetId: Long, tagId: Long) = AuthorizedAction(userOwningDocumentSet(documentSetId)) { implicit request =>
--- app/models/orm/finders/TagFinder.scala
+++ app/models/orm/finders/TagFinder.scala
@@ -1,6 +1,7 @@
 package models.orm.finders
 
 import org.squeryl.Query
+import org.squeryl.dsl.GroupWithMeasures
 import scala.language.implicitConversions
 import scala.language.postfixOps
 
@@ -8,8 +9,25 @@ import org.overviewproject.postgres.SquerylEntrypoint._
 import models.orm.{ Schema, Tag }
 
 object TagFinder extends Finder {
+  class TagResult(query: Query[Tag]) extends FinderResult(query) {
+    def withCounts : FinderResult[(Tag,Long)] = {
+      val tagCounts : Query[GroupWithMeasures[Long,Long]] = from(Schema.documentTags)(t =>
+        groupBy(t.tagId)
+        compute(org.overviewproject.postgres.SquerylEntrypoint.count) // NOT FinderResult.count
+      )
+
+      join(toQuery, tagCounts.leftOuter)((tags, counts) =>
+        select(tags, &(nvl(counts.map(_.measures), 0L)))
+        on(tags.id === counts.map(_.key))
+      )
+    }
+  }
+  object TagResult {
+    implicit def fromQuery(query: Query[Tag]) = new TagResult(query)
+  }
+
   /** @return All `Tag`s with the given DocumentSet. */
-  def byDocumentSet(documentSet: Long) : FinderResult[Tag] = {
+  def byDocumentSet(documentSet: Long) : TagResult = {
     from(Schema.tags)(t =>
       where(t.documentSetId === documentSet)
       select(t)
