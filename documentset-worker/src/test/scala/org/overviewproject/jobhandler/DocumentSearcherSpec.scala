5e4bc11edcc1c24bb7cbace9feca0e590d4f5dfa
Jonas Karlsson <thejonas@gmail.com>
2013-05-16 10:06:56 -0400

    Extract common setup

--- documentset-worker/src/test/scala/org/overviewproject/jobhandler/DocumentSearcherSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/jobhandler/DocumentSearcherSpec.scala
@@ -1,9 +1,6 @@
 package org.overviewproject.jobhandler
 
-
-
 import scala.concurrent.duration._
-
 import org.specs2.mutable.Specification
 import org.overviewproject.test.ActorSystemContext
 import akka.actor._
@@ -13,17 +10,24 @@ import org.overviewproject.documentcloud.QueryProcessorProtocol._
 import org.overviewproject.documentcloud.{ Document, SearchResult }
 import org.specs2.mock.Mockito
 import org.specs2.time.NoTimeConversions
+import org.specs2.mutable.Before 
+import org.specs2.specification.Scope
 
 class DocumentSearcherSpec extends Specification with NoTimeConversions with Mockito {
 
   "DocumentSearcher" should {
 
-    "send a query request" in new ActorSystemContext {
-
-      val queryProcessor = TestProbe()
-
-      case class QueryProcessorMessage(query: String, msg: Any)
+    
+    abstract class SearcherContext extends ActorSystemContext with Before {
+      val documentSetId = 6
+      val query = "query terms"
+      val expectedQuery = s"projectid:$documentSetId $query"
       
+      var queryProcessor: TestProbe = _
+      var documentSearcher: ActorRef = _
+      
+      case class QueryProcessorMessage(query: String, msg: Any)
+
       trait TestQueryProcessorFactory extends QueryProcessorFactory {
         override def produce(query: String, requestQueue: ActorRef): Actor = new Actor {
           def receive = {
@@ -31,45 +35,32 @@ class DocumentSearcherSpec extends Specification with NoTimeConversions with Moc
           }
         }
       }
-
-      val documentSetId = 6
-      val query = "query terms"
-      val expectedQuery = s"projectid:$documentSetId $query"
       
-      val documentSearcher = TestActorRef(new DocumentSearcher(documentSetId, query, testActor) with TestQueryProcessorFactory)
+      override def before = {
+        queryProcessor = TestProbe()
+        documentSearcher = TestActorRef(new DocumentSearcher(documentSetId, query, testActor) with TestQueryProcessorFactory)
+      }
+
+    }
+    
+
+
+    "send a query request" in new SearcherContext {
 
       queryProcessor.expectMsg(QueryProcessorMessage(expectedQuery, GetPage(1)))
     }
-  }
-  
-  "request all remaining pages after first page is received" in new ActorSystemContext {
-      val queryProcessor = TestProbe()
 
-      case class QueryProcessorMessage(query: String, msg: Any)
-      
-      trait TestQueryProcessorFactory extends QueryProcessorFactory {
-        override def produce(query: String, requestQueue: ActorRef): Actor = new Actor {
-          def receive = {
-            case msg => queryProcessor.ref ! QueryProcessorMessage(query, msg)
-          }
-        }
-      }
-
-      val documentSetId = 6
-      val query = "query terms"
-      val expectedQuery = s"projectid:$documentSetId $query"
-      
-      val documentSearcher = TestActorRef(new DocumentSearcher(documentSetId, query, testActor) with TestQueryProcessorFactory)
+    "request all remaining pages after first page is received" in new SearcherContext {
 
       val document: Document = mock[Document]
       val documents: Seq[Document] = Seq.fill(10)(document)
-      
+
       queryProcessor.expectMsg(QueryProcessorMessage(expectedQuery, GetPage(1)))
-      
+
       documentSearcher ! SearchResult(150, 1, documents)
-      
+
       queryProcessor.expectMsg(QueryProcessorMessage(expectedQuery, GetPage(2)))
       queryProcessor.expectMsg(QueryProcessorMessage(expectedQuery, GetPage(3)))
-
+    }
   }
 }
