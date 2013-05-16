5e7095f7a29d694e3ecce0db5a42a931a74688c5
Jonas Karlsson <thejonas@gmail.com>
2013-05-16 12:19:17 -0400

    Don't request pages after maxDocuments has been reached

--- documentset-worker/src/main/scala/org/overviewproject/jobhandler/DocumentSearcher.scala
+++ documentset-worker/src/main/scala/org/overviewproject/jobhandler/DocumentSearcher.scala
@@ -9,34 +9,32 @@ trait QueryProcessorFactory {
   def produce(query: String, requestQueue: ActorRef): Actor
 }
 
-class DocumentSearcher(documentSetId: Long, query: String, requestQueue: ActorRef) extends Actor {
+class DocumentSearcher(documentSetId: Long, query: String, requestQueue: ActorRef,
+  val pageSize: Int = Configuration.pageSize, val maxDocuments: Int = Configuration.maxDocuments) extends Actor {
   this: QueryProcessorFactory =>
 
   import org.overviewproject.documentcloud.QueryProcessorProtocol._
 
-  private val PageSize: Int = Configuration.pageSize
   
   private val queryProcessor = context.actorOf(Props(produce(createQuery, requestQueue)))
-  
+
   queryProcessor ! GetPage(1)
 
   def receive = {
     case SearchResult(total, page, documents) => if (page == 1) requestRemainingPages(total)
   }
-  
+
   private def createQuery: String = s"projectid:$documentSetId $query"
-  
+
   private def requestRemainingPages(total: Int): Unit = {
-    val totalPages: Int = total / PageSize 
-    for (p <- 2 to totalPages) 
+    val totalPages: Int = scala.math.min(total, maxDocuments) / pageSize
+    for (p <- 2 to totalPages)
       queryProcessor ! GetPage(p)
   }
 }
-
 
 trait ActualQueryProcessorFactory extends QueryProcessorFactory {
   override def produce(query: String, requestQueue: ActorRef): Actor = new QueryProcessor(query, None, requestQueue)
 }
 
-class ActualDocumentSearcher(documentSetId: Long, query: String, requestQueue: ActorRef) extends
-  DocumentSearcher(documentSetId, query, requestQueue) with ActualQueryProcessorFactory
+class ActualDocumentSearcher(documentSetId: Long, query: String, requestQueue: ActorRef) extends DocumentSearcher(documentSetId, query, requestQueue) with ActualQueryProcessorFactory
--- documentset-worker/src/test/scala/org/overviewproject/jobhandler/DocumentSearcherSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/jobhandler/DocumentSearcherSpec.scala
@@ -10,57 +10,94 @@ import org.overviewproject.documentcloud.QueryProcessorProtocol._
 import org.overviewproject.documentcloud.{ Document, SearchResult }
 import org.specs2.mock.Mockito
 import org.specs2.time.NoTimeConversions
-import org.specs2.mutable.Before 
+import org.specs2.mutable.Before
 import org.specs2.specification.Scope
+import org.overviewproject.documentcloud.QueryProcessor
+import org.overviewproject.util.Configuration
 
 class DocumentSearcherSpec extends Specification with NoTimeConversions with Mockito {
 
   "DocumentSearcher" should {
 
     
+    trait TestConfig {
+      val maxDocuments: Int = Configuration.maxDocuments
+      val pageSize: Int = Configuration.pageSize
+    }
+    
+    trait NotAllPagesNeeded extends TestConfig {
+      val pagesNeeded: Int = 2
+      override val pageSize: Int = 5
+      override val maxDocuments: Int = pageSize * pagesNeeded 
+      
+      val totalDocuments = 10 * maxDocuments 
+    }
+    
     abstract class SearcherContext extends ActorSystemContext with Before {
+      this: TestConfig  => 
+        
       val documentSetId = 6
-      val query = "query terms"
-      val expectedQuery = s"projectid:$documentSetId $query"
+      val queryTerms = "query terms"
+      val expectedQuery = s"projectid:$documentSetId $queryTerms"
+      
       
       var queryProcessor: TestProbe = _
-      var documentSearcher: ActorRef = _
+      var documentSearcher: TestActorRef[TestDocumentSearcher] = _
       
       case class QueryProcessorMessage(query: String, msg: Any)
 
       trait TestQueryProcessorFactory extends QueryProcessorFactory {
-        override def produce(query: String, requestQueue: ActorRef): Actor = new Actor {
-          def receive = {
-            case msg => queryProcessor.ref ! QueryProcessorMessage(query, msg)
+        var query: Option[String] = None
+        
+        override def produce(queryString: String, requestQueue: ActorRef): Actor = {
+          query = Some(queryString)  
+        
+          new Actor {
+            def receive = {
+              case msg => queryProcessor.ref forward msg
+            }
           }
         }
       }
       
+      type TestDocumentSearcher = DocumentSearcher with TestQueryProcessorFactory
+      
       override def before = {
         queryProcessor = TestProbe()
-        documentSearcher = TestActorRef(new DocumentSearcher(documentSetId, query, testActor) with TestQueryProcessorFactory)
+        documentSearcher = 
+          TestActorRef(new DocumentSearcher(documentSetId, queryTerms, testActor, pageSize, maxDocuments) with TestQueryProcessorFactory)
       }
 
     }
-    
+     abstract class SearcherSetup extends SearcherContext with TestConfig
 
-
-    "send a query request" in new SearcherContext {
-
-      queryProcessor.expectMsg(QueryProcessorMessage(expectedQuery, GetPage(1)))
+    "send a query request" in new SearcherSetup {
+      documentSearcher.underlyingActor.query must beSome(expectedQuery)
+      queryProcessor.expectMsg(GetPage(1))
     }
 
-    "request all remaining pages after first page is received" in new SearcherContext {
-
+    "request all remaining pages after first page is received" in new SearcherSetup {
       val document: Document = mock[Document]
       val documents: Seq[Document] = Seq.fill(10)(document)
 
-      queryProcessor.expectMsg(QueryProcessorMessage(expectedQuery, GetPage(1)))
-
       documentSearcher ! SearchResult(150, 1, documents)
+      
+      val messages = Seq.tabulate(3)(p => GetPage(p + 1))
+      val receivedMessages = queryProcessor.receiveN(3)
+      receivedMessages must haveTheSameElementsAs(messages)
+    }
+    
+    "only request pages needed to get maxDocuments results" in new SearcherContext with NotAllPagesNeeded {
+      
+      val document: Document = mock[Document]
+      val documents: Seq[Document] = Seq.fill(10)(document)
 
-      queryProcessor.expectMsg(QueryProcessorMessage(expectedQuery, GetPage(2)))
-      queryProcessor.expectMsg(QueryProcessorMessage(expectedQuery, GetPage(3)))
+      documentSearcher ! SearchResult(totalDocuments, 1, documents)
+      
+      queryProcessor.receiveN(pagesNeeded)
+      queryProcessor.expectNoMsg(100 millis)
+
+      
     }
   }
 }
