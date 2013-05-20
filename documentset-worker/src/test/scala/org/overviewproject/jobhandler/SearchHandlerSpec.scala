992eb274cbfa72b1d6efeb1b75d7f1e9fe645938
Jonas Karlsson <thejonas@gmail.com>
2013-05-20 18:40:59 -0400

    Extract common setup

--- documentset-worker/src/test/scala/org/overviewproject/jobhandler/SearchHandlerSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/jobhandler/SearchHandlerSpec.scala
@@ -1,6 +1,5 @@
 package org.overviewproject.jobhandler
 
-
 import org.specs2.mutable.Specification
 import org.overviewproject.test.ActorSystemContext
 import org.specs2.mock.Mockito
@@ -11,68 +10,69 @@ import akka.actor._
 import akka.testkit.TestActor
 import akka.testkit.TestProbe
 import org.overviewproject.jobhandler.DocumentSearcherProtocol.StartSearch
-
+import org.specs2.specification.Scope
 
 class SearchHandlerSpec extends Specification with Mockito {
-  
+
   "SearchHandler" should {
-    
-      class TestSearchHandler(searchExists: Boolean, documentSearcherProbe: ActorRef) extends SearchHandler with SearchHandlerComponents {
-        val storage = mock[Storage]
-        storage.searchExists(anyLong, anyString) returns searchExists
-        
-        val actorCreator = new ActorCreator { // can't mock creation of actors
-          override def produceDocumentSearcher(documentSetId: Long, query: String, requestQueue: ActorRef): Actor = 
-            new ForwardingActor(documentSearcherProbe)  
-        }
-        
+
+    class TestSearchHandler(searchExists: Boolean, documentSearcherProbe: ActorRef) extends SearchHandler with SearchHandlerComponents {
+      val storage = mock[Storage]
+      storage.searchExists(anyLong, anyString) returns searchExists
+
+      val actorCreator = new ActorCreator { // can't mock creation of actors
+        override def produceDocumentSearcher(documentSetId: Long, query: String, requestQueue: ActorRef): Actor =
+          new ForwardingActor(documentSearcherProbe)
       }
-      
-      class ForwardingActor(target: ActorRef) extends Actor {
-        def receive = {
-          case msg => target forward msg
-        }
+
+    }
+
+    class ForwardingActor(target: ActorRef) extends Actor {
+      def receive = {
+        case msg => target forward msg
       }
-    
-    "send Done to parent if SearchResult already exists" in new ActorSystemContext {
+    }
+
+    class SearchHandlerParent(searchExists: Boolean, parentProbe: ActorRef, documentSearcherProbe: ActorRef) extends Actor {
+      val searchHandler = context.actorOf(Props(new TestSearchHandler(searchExists, documentSearcherProbe)))
+
+      def receive = {
+        case msg if sender == searchHandler => parentProbe forward msg
+        case msg => searchHandler forward msg
+      }
+
+    }
+
+    trait SearchHandlerSetup extends Scope {
       val documentSetId = 5l
       val query = "projectid:123 search terms"
-      val documentSearcherProbe = TestProbe()
       
-      val parent = system.actorOf(Props(new Actor {
-        val searchHandler = context.actorOf(Props(new TestSearchHandler(searchExists = true, documentSearcherProbe.ref)))
+      def createSearchHandlerParent(searchExists: Boolean, parentProbe: ActorRef, documentSearcherProbe: ActorRef)
+        (implicit system: ActorSystem): ActorRef = 
+          system.actorOf(Props(new SearchHandlerParent(searchExists, parentProbe, documentSearcherProbe)))
         
-        def receive = {
-          case msg if sender == searchHandler => testActor forward msg
-          case msg => searchHandler forward msg
-        }
-      }))
+    }
+
+    "send Done to parent if SearchResult already exists" in new ActorSystemContext with SearchHandlerSetup {
+      val documentSearcherProbe = TestProbe()
+      val parent = createSearchHandlerParent(searchExists = true, testActor, documentSearcherProbe.ref)
       
       parent ! Search(documentSetId, query, testActor)
-      
+
       expectMsg(Done)
     }
-    
-    "send StartSearch to a new DocumentSearcher" in new ActorSystemContext {
-      val documentSetId = 5l
-      val query = "projectid:123 search terms"
+
+    "send StartSearch to a new DocumentSearcher" in new ActorSystemContext with SearchHandlerSetup {
       val documentSearcherProbe = TestProbe()
-      
-      val parent = system.actorOf(Props(new Actor {
-        val searchHandler = context.actorOf(Props(new TestSearchHandler(searchExists = false, documentSearcherProbe.ref)))
-        
-        def receive = {
-          case msg if sender == searchHandler => testActor forward msg
-          case msg => searchHandler forward msg
-        }
-      }))
-      
+
+      val parent = createSearchHandlerParent(searchExists = false, testActor, documentSearcherProbe.ref)
+
       parent ! Search(documentSetId, query, testActor)
-      
+
       documentSearcherProbe.expectMsg(StartSearch())
-      
+
     }
-    
+
   }
 
 }
