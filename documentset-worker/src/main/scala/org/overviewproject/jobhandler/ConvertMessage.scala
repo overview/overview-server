b6786ad3cb6ed0b79a93e17924547a0b2b99e1b1
Jonas Karlsson <thejonas@gmail.com>
2013-05-23 12:17:21 -0400

    Clean up and add comments

--- documentset-worker/src/main/scala/org/overviewproject/jobhandler/ConvertMessage.scala
+++ documentset-worker/src/main/scala/org/overviewproject/jobhandler/ConvertMessage.scala
@@ -3,12 +3,16 @@ package org.overviewproject.jobhandler
 import org.overviewproject.jobhandler.JobHandlerProtocol.SearchCommand
 import play.api.libs.json._
 
+
+/** Converts messages from the queue into specific Command Messages */
 object ConvertMessage {
   private case class Message(cmd: String, args: SearchCommand)
   
   implicit private val searchCommandReads = Json.reads[SearchCommand]
   implicit private val searchMessageReads = Json.reads[Message]
   
+  // Once we have more than one command, we'll actually have to look at the 
+  // message to decide how to convert it.
   def apply(message: String): SearchCommand = {
     val m = Json.parse(message).as[Message]
     m.args
--- documentset-worker/src/main/scala/org/overviewproject/jobhandler/JobHandler.scala
+++ documentset-worker/src/main/scala/org/overviewproject/jobhandler/JobHandler.scala
@@ -1,65 +1,35 @@
 package org.overviewproject.jobhandler
 
-import org.fusesource.stomp.jms.{ StompJmsConnectionFactory, StompJmsDestination }
+import org.fusesource.stomp.jms.{StompJmsConnectionFactory, StompJmsDestination}
+import org.overviewproject.jobhandler.DocumentSearcherProtocol.Done
+import org.overviewproject.jobhandler.SearchHandlerProtocol.Search
+
 import akka.actor._
 import javax.jms._
-import org.overviewproject.jobhandler.SearchHandlerProtocol.Search
-import org.overviewproject.jobhandler.DocumentSearcherProtocol.Done
 
-class ActualSearchHandler extends SearchHandler with SearchHandlerComponents {
-  override val storage: Storage = new Storage
-  override val actorCreator: ActorCreator = new ActorCreator
-}
 
+
+/**
+ * Messages the JobHandler can process 
+ */
 object JobHandlerProtocol {
+  /** Starts listening to the connection on the message queue */
   case object StartListening
+  
+  // Internal messages that should really be private, but are 
+  // public for easier testing. 
   case class CommandMessage(message: TextMessage)
   case class SearchCommand(documentSetId: Long, query: String)
 }
 
-trait MessageServiceComponent {
 
-  val messageService: MessageService
-
-  class MessageService {
-    private val connection: Connection = createConnection
-    private val consumer: MessageConsumer = createConsumer
-
-    def startListening(messageHandler: MessageListener): Unit = {
-      consumer.setMessageListener(messageHandler)
-      connection.start()
-    }
-    
-    def complete(message: Message): Unit = {
-      message.acknowledge()
-    }
-
-    private def createConnection: Connection = {
-      val factory = new StompJmsConnectionFactory()
-      factory.setBrokerURI("tcp://localhost:61613")
-      factory.createConnection("admin", "password")
-    }
-
-    private def createConsumer: MessageConsumer = {
-      val session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
-      val destination = new StompJmsDestination("/queue/myqueue")
-
-      session.createConsumer(destination)
-    }
-  }
-}
-
-trait SearchComponent {
-  val actorCreator: ActorCreator
-  
-  class ActorCreator {
-    def produceSearchHandler: Actor = new SearchHandler with SearchHandlerComponents {
-      override val storage: Storage = new Storage
-      override val actorCreator: ActorCreator = new ActorCreator()
-    }
-  }
-}
-
+/**
+ * `JobHandler` goes through the following state transitions:
+ * Idle -> Listening: after startup and having received the StartListening message
+ * Listening -> WaitingForCompletion: after a message has been received from the queue
+ *   and a handler has been dispatched to deal with the command
+ * WaitingForCompletion -> Listening: when the command handler is done
+ */
 object JobHandlerFSM {
   sealed trait State
   case object Idle extends State
@@ -68,12 +38,53 @@ object JobHandlerFSM {
   
   sealed trait Data
   case object NoMessageReceived extends Data
+  /** Keep track of the message received so it can be ACK/NACKed */
   case class MessageReceived(message: Message) extends Data
   
 }
 
+
+
+/**
+ * A component that listens for and responds to incoming messages.
+ */
+trait MessageServiceComponent {
+
+  val messageService: MessageService
+
+  trait MessageService {
+    /** 
+     *  Only call `startListening` once. To stop listening, stop the Actor, which should
+     *  disconnect the connection.
+     */
+    def startListening(messageHandler: MessageListener): Unit
+    
+    /** Call to indicate successful handling of the message */
+    def complete(message: Message): Unit
+  }
+}
+
+
+/**
+ * Component for creating a SearchHandler actor
+ */
+trait SearchComponent {
+  val actorCreator: ActorCreator
+  
+  trait ActorCreator {
+    def produceSearchHandler: Actor
+  }
+}
+
 import JobHandlerFSM._
 
+/**
+ * The `JobHandler` listens for messages on the queue, and then spawns an appropriate
+ * handler for the incoming command. When the command handler sends a Done message,
+ * the message is acknowledged.
+ * To handle new types of command, expand `ConvertMessage` to generate new message type,
+ * and add a new case to the `Listening` state to handle the new type.
+ */
 class JobHandler(requestQueue: ActorRef) extends Actor  with FSM[State, Data] {
   this: MessageServiceComponent with SearchComponent =>
 
@@ -108,18 +119,64 @@ class JobHandler(requestQueue: ActorRef) extends Actor  with FSM[State, Data] {
   initialize
   
   class MessageHandler extends MessageListener {
-    def onMessage(message: Message): Unit = {}//convertMessage(message).map { cmd => self ! cmd }
+    def onMessage(message: Message): Unit =  self ! CommandMessage(message.asInstanceOf[TextMessage])
   }
 
 }
 
-object JobHandler {
-  class ActualJobHandler(requestQueue: ActorRef) extends JobHandler(requestQueue) 
-    with MessageServiceComponent with SearchComponent {
+
+/**
+ * Implementation of connecting to the message queue and receiving and responding to
+ * messages.
+ */
+trait MessageServiceComponentImpl extends MessageServiceComponent {
+  class MessageServiceImpl extends MessageService  {
     
-    override val messageService = new MessageService
-    override val actorCreator = new ActorCreator
+    private val connection: Connection = createConnection
+    private val consumer: MessageConsumer = createConsumer
+
+    override def startListening(messageHandler: MessageListener): Unit = {
+      consumer.setMessageListener(messageHandler)
+      connection.start()
+    }
+    
+    override def complete(message: Message): Unit = {
+      message.acknowledge()
+    }
+
+    private def createConnection: Connection = {
+      val factory = new StompJmsConnectionFactory()
+      factory.setBrokerURI("tcp://localhost:61613")
+      factory.createConnection("admin", "password")
+    }
+
+    private def createConsumer: MessageConsumer = {
+      val session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
+      val destination = new StompJmsDestination("/queue/myqueue")
+
+      session.createConsumer(destination)
+    }
+  }
+}
+
+
+/** Create a SearchHandler */
+trait SearchComponentImpl extends SearchComponent {
+  class ActorCreatorImpl extends ActorCreator {
+    override def produceSearchHandler: Actor = new SearchHandler with SearchHandlerComponents {
+      override val storage: Storage = new Storage
+      override val actorCreator: ActorCreator = new ActorCreator()
+    }
+  }
+}
+
+object JobHandler {
+  class JobHandlerImpl(requestQueue: ActorRef) extends JobHandler(requestQueue) 
+    with MessageServiceComponentImpl with SearchComponentImpl {
+    
+    override val messageService = new MessageServiceImpl
+    override val actorCreator = new ActorCreatorImpl
   }
   
-  def apply(requestQueue: ActorRef): Props = Props(new ActualJobHandler(requestQueue))
+  def apply(requestQueue: ActorRef): Props = Props(new JobHandlerImpl(requestQueue))
 }
