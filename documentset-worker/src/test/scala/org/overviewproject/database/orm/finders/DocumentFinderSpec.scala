cb64b8ab87d511a20e26edac03365415472e9e09
Jonas Karlsson <thejonas@gmail.com>
2013-05-24 12:02:12 -0400

    Test that empty input list works

--- documentset-worker/src/test/scala/org/overviewproject/database/orm/finders/DocumentFinderSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/database/orm/finders/DocumentFinderSpec.scala
@@ -3,23 +3,38 @@ package org.overviewproject.database.orm.finders
 import org.specs2.mutable.Specification
 import org.overviewproject.test.DbSetup._
 import org.overviewproject.test.DbSpecification
+import org.overviewproject.tree.orm.Document
 
 class DocumentFinderSpec extends DbSpecification {
-  
+
   step(setupDb)
-  
+
   "DocumentFinder" should {
-    
-    "find all documents with specified DocumentCloudIds" in new DbTestContext {
-      val documentSetId = insertDocumentSet("DocumentFinderSpec")
-      val allDocumentIds = insertDocuments(documentSetId, 10)
+
+    trait DocumentSetup extends DbTestContext {
+      var documentSetId: Long = _
+      var allDocumentIds: Seq[Long] = _
+
+      override def setupWithDb = {
+        documentSetId = insertDocumentSet("DocumentFinderSpec")
+        allDocumentIds = insertDocuments(documentSetId, 10)
+      }
+    }
+
+    "find all documents with specified DocumentCloudIds" in new DocumentSetup {
       val documentCloudIds = Seq.tabulate(5)(n => s"documentCloudId-${n + 1}")
       val foundDocuments = DocumentFinder.byDocumentSetAndDocumentCloudIds(documentSetId, documentCloudIds)
-      
+
       foundDocuments.map(_.id) must haveTheSameElementsAs(allDocumentIds.take(5))
     }
+
+    "return empty list if documentCloudIds list is empty" in new DocumentSetup {
+      val foundDocuments = DocumentFinder.byDocumentSetAndDocumentCloudIds(documentSetId, Seq.empty).toSeq
+
+      foundDocuments must have size (0)
+    }
   }
-  
+
   step(shutdownDb)
 
 }
