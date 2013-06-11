98c29c670fa86ae4793d3a5f4c0b2dd96b23a205
Adam Hooper <adam@adamhooper.com>
2013-06-11 13:56:05 -0400

    Fix bug: editing tags would create a new tag.

--- app/models/orm/Tag.scala
+++ app/models/orm/Tag.scala
@@ -5,37 +5,10 @@ import org.squeryl.KeyedEntity
 import org.overviewproject.postgres.SquerylEntrypoint._
 
 case class Tag(
-  val id: Long = 0l,
+  val id: Long = 0L,
   @Column("document_set_id") val documentSetId: Long,
   val name: String,
   val color: Option[String] = None) extends KeyedEntity[Long] {
 
-  def save: Tag = {
-    if (id == 0l) {
-      Schema.tags.insert(this)
-    }
-    else {
-      Schema.tags.update(this)
-    }
-    this
-  }
-
-  def delete {
-    Schema.documentTags.deleteWhere(dt => dt.tagId === id)
-    Schema.tags.deleteWhere(t => t.id === id)
-  }
-  
-  def withUpdate(newName: String, color: String): Tag = copy(name = newName, color = Some(color))
-}
- 
-object Tag {
-
-  def findByName(documentSetId: Long, name: String): Option[Tag] = {
-    Schema.tags.where(t => t.documentSetId === documentSetId and t.name === name).headOption
-  }
-
-  def findById(documentSetId: Long, tagId: Long): Option[Tag] = {
-    Schema.tags.where(t => t.documentSetId === documentSetId and t.id === tagId).headOption
-  }
-
+  override def isPersisted(): Boolean = id != 0L
 }
