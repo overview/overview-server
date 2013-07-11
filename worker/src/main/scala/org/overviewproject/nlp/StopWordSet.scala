01cb855f814d17520ac5e7985119edfbeb624dda
Jonas Karlsson <thejonas@gmail.com>
2013-07-11 11:53:18 -0400

    Don't crash if no stopword file is found

--- worker/src/main/scala/org/overviewproject/nlp/StopWordSet.scala
+++ worker/src/main/scala/org/overviewproject/nlp/StopWordSet.scala
@@ -1,22 +1,28 @@
 package org.overviewproject.nlp
 
 import java.io.InputStream
+import java.io.ByteArrayInputStream
+import org.overviewproject.util.Logger
 
 object StopWordSet {
   private val DefaultStopWordsFile: String = "/stopwords-en.csv"
-    
-  private def stopWordsFile(lang: String): InputStream = {
-	val filename = s"/stopwords-$lang.csv"
-	val stopWordStream = getClass.getResourceAsStream(filename)
-	
-	if (stopWordStream == null) getClass.getResourceAsStream(DefaultStopWordsFile)
-	else stopWordStream
-  }
-    
+  private val EmptyStopWordsStream: InputStream = new ByteArrayInputStream(Array.empty)
   def apply(lang: String): Set[String] = {
     val stopWordLines = io.Source.fromInputStream(stopWordsFile(lang)).getLines
-  
+
     stopWordLines.toSet
   }
-  
+
+  private def stopWordsFile(lang: String): InputStream = {
+    val filename = s"/stopwords-$lang.csv"
+    val possibleStopWordStreams: Seq[InputStream] = Seq(filename, DefaultStopWordsFile).map(fileAsStream) 
+
+    possibleStopWordStreams.find(_ != null).getOrElse {
+      Logger.error("No stopwords file found")
+      EmptyStopWordsStream
+    }
+  }
+
+  private def fileAsStream(filename: String): InputStream = getClass.getResourceAsStream(filename)
+
 }
