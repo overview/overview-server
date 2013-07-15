c87924ebfe9482daafeedf1ae055f8c2c96460b8
Jonas Karlsson <thejonas@gmail.com>
2013-07-15 07:04:29 -0400

    Don't allow underscore in stopwords

--- worker/src/main/scala/org/overviewproject/nlp/StopWordSet.scala
+++ worker/src/main/scala/org/overviewproject/nlp/StopWordSet.scala
@@ -7,35 +7,34 @@ import org.overviewproject.util.Logger
 object StopWordSet {
   private val DefaultStopWordsFile: String = "/stopwords-en.csv"
   private val EmptyStopWordsStream: InputStream = new ByteArrayInputStream(Array.empty)
-  
+
   def apply(lang: String, suppliedStopWordString: Option[String]): Set[String] = {
     val stopWordLines = io.Source.fromInputStream(stopWordsFile(lang)).getLines
     val suppliedStopWords = extractStopWords(suppliedStopWordString)
-    
+
     stopWordLines.toSet ++ suppliedStopWords
   }
 
   private def stopWordsFile(lang: String): InputStream = {
     val filename = s"/stopwords-$lang.csv"
-    val possibleStopWordStreams: Seq[InputStream] = Seq(filename, DefaultStopWordsFile).map(fileAsStream) 
+    val possibleStopWordStreams: Seq[InputStream] = Seq(filename, DefaultStopWordsFile).map(fileAsStream)
 
     possibleStopWordStreams.find(_ != null).getOrElse {
       Logger.error("No stopwords file found")
       EmptyStopWordsStream
     }
   }
-  
+
   private def fileAsStream(filename: String): InputStream = getClass.getResourceAsStream(filename)
 
   private def extractStopWords(wordString: Option[String]): Set[String] = wordString.map { s =>
     val separators = """[\s\u00A0,]+""".r
-    val invalidChars = """[^\w\d'-]""".r
-    
+    val allowedPunctuation = "-'"
+    def allowedChar(c: Char): Boolean = c.isDigit || c.isLetter || allowedPunctuation.contains(c)
+
     val words = separators.split(s.trim).toSet
     
-    words.map { w =>
-      invalidChars.replaceAllIn(w, "").toLowerCase
-    }
-  }.getOrElse(Set.empty[String]) 
-    
+    words.map(_.filter(allowedChar).toLowerCase)
+  }.getOrElse(Set.empty[String])
+
 }
