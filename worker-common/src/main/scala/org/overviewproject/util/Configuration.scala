6a65d4fe066da2b08b1a2c88f8ea2e6a3c961898
Jonas Karlsson <thejonas@gmail.com>
2013-08-22 14:18:06 -0400

    Use builtin functionality to provide configuration default values

--- worker-common/src/main/scala/org/overviewproject/util/Configuration.scala
+++ worker-common/src/main/scala/org/overviewproject/util/Configuration.scala
@@ -1,60 +1,105 @@
 package org.overviewproject.util
 
+import scala.collection.JavaConverters._
+
 import com.typesafe.config.{ Config, ConfigFactory }
 
-class WorkerConfiguration(val configuration: Config) extends DefaultConfigurable {
-  val prefix: String = ""
+trait ConfigurationDefault {
+  def path: Option[String]
+  def defaultValues: Map[String, Any]
+}
 
-  private val MaxDocuments: String = "max_documents"
-  private val MaxInFlightRequests: String = "max_inflight_requests"
-  private val PageSize: String = "page_size"
+trait ConfigurationWithDefaults {
+  def defaultConfigs: Iterable[ConfigurationDefault]
 
-  val defaultValues: Map[String, Any] = Map(
+  def createWithDefaults: Config = {
+    val definedConfiguration = ConfigFactory.load()
+
+    defaultConfigs.foldLeft(definedConfiguration) { (c, d) =>
+      val defaults = ConfigFactory.parseMap(d.defaultValues.asJava)
+      val defaultsAtPath = d.path.map(defaults.atPath(_)).getOrElse(defaults)
+
+      c.withFallback(defaultsAtPath)
+    }
+  }
+}
+
+object WorkerDefaults extends ConfigurationDefault {
+  override def path: Option[String] = None
+
+  val MaxDocuments: String = "max_documents"
+  val MaxInFlightRequests: String = "max_inflight_requests"
+  val PageSize: String = "page_size"
+
+  override def defaultValues: Map[String, Any] = Map(
     (MaxDocuments -> 50000),
     (PageSize -> 50),
     (MaxInFlightRequests -> 4))
-
-  val maxDocuments: Int = getInt(MaxDocuments)
-  val pageSize: Int = getInt(PageSize)
-  val maxInFlightRequests: Int = getInt(MaxInFlightRequests)
-
 }
 
-class SearchIndexConfiguration(val configuration: Config) extends DefaultConfigurable {
-  val prefix: String = "search_index"
+object MessageQueueDefaults extends ConfigurationDefault {
+  override def path: Option[String] = Some("message_queue")
 
-  private val ConfigFile = "config_file"
+  val BrokerUri = "broker_uri"
+  val QueueName = "queue_name"
+  val Username: String = "username"
+  val Password: String = "password"
 
-  val defaultValues: Map[String, Any] = Map(
-    (ConfigFile -> "elasticsearch.yml")    
-  )
-   
-  val configFile: String = getString(ConfigFile)
-}
-
-class MessageQueueConfiguration(val configuration: Config) extends DefaultConfigurable {
-  val prefix: String = "message_queue"
-  private val BrokerUri = "broker_uri"
-  private val QueueName = "queue_name"
-  private val Username: String = "username"
-  private val Password: String = "password"
-
-  val defaultValues: Map[String, Any] = Map(
+  override def defaultValues: Map[String, Any] = Map(
     (BrokerUri -> "tcp://localhost:61613"),
     (Username -> "admin"),
     (Password -> "password"),
     (QueueName -> "/queue/document-set-commands"))
-
-  val brokerUri: String = getString(BrokerUri)
-  val username: String = getString(Username)
-  val password: String = getString(Password)
-  val queueName: String = getString(QueueName)
 }
 
-object Configuration {
-  private val configuration: Config = ConfigFactory.load()
-  private val globalConfig = new WorkerConfiguration(configuration)
+object SearchIndexDefaults extends ConfigurationDefault {
+  override def path: Option[String] = Some("search_index")
 
+  val ConfigFile = "config_file"
+
+  override def defaultValues: Map[String, Any] = Map(
+    (ConfigFile -> "elasticsearch.yml"))
+}
+
+class WorkerConfiguration(configuration: Config) {
+  import WorkerDefaults._
+  
+  val maxDocuments: Int = configuration.getInt(MaxDocuments)
+  val maxInFlightRequests = configuration.getInt(MaxInFlightRequests)
+  val pageSize: Int = configuration.getInt(PageSize)
+}
+
+class MessageQueueConfiguration(configuration: Config) {
+  import MessageQueueDefaults._
+  val prefix: String = "message_queue"
+
+  private val config = configuration.getConfig(prefix) 
+
+  val brokerUri: String = config.getString(BrokerUri)
+  val username: String = config.getString(Username)
+  val password: String = config.getString(Password)
+  val queueName: String = config.getString(QueueName)
+}
+
+class SearchIndexConfiguration(configuration: Config) {
+  import SearchIndexDefaults.ConfigFile
+  
+  val prefix: String = "search_index"
+
+  private val config = configuration.getConfig(prefix)
+
+  val configFile: String = config.getString(ConfigFile)
+}
+
+object Configuration extends ConfigurationWithDefaults {
+  override def defaultConfigs = Seq(
+    WorkerDefaults,
+    MessageQueueDefaults,
+    SearchIndexDefaults)
+
+  private val configuration: Config = createWithDefaults
+  private val globalConfig = new WorkerConfiguration(configuration)
+  
   val maxDocuments = globalConfig.maxDocuments
   val pageSize = globalConfig.pageSize
   val maxInFlightRequests = globalConfig.maxInFlightRequests
--- worker-common/src/test/scala/org/overviewproject/util/ConfigurationSpec.scala
+++ worker-common/src/test/scala/org/overviewproject/util/ConfigurationSpec.scala
@@ -1,26 +1,55 @@
 package org.overviewproject.util
 
+import scala.collection.JavaConverters._
 import org.specs2.mutable.Specification
+import com.typesafe.config.ConfigFactory
 
 class ConfigurationSpec extends Specification {
 
-  
   "Configuration" should {
-    
+    object RootDefaults extends ConfigurationDefault {
+      override def path: Option[String] = None
+      override def defaultValues: Map[String, Any] = Map(
+        "str" -> "value",
+        "int" -> 5)
+    }
+
+    object SubDefaults extends ConfigurationDefault {
+      override def path: Option[String] = Some("path")
+      override def defaultValues: Map[String, Any] = Map(
+        "sub" -> "subvalue")
+    }
+
+    object TestConfiguration extends ConfigurationWithDefaults {
+      override val defaultConfigs = Seq(RootDefaults, SubDefaults)
+
+      val configuration = createWithDefaults
+    }
+
     "have BrokerUri value" in {
-      Configuration.messageQueue.brokerUri must be equalTo("tcp://localhost:61613")
+
+      Configuration.messageQueue.brokerUri must be equalTo ("tcp://localhost:61613")
     }
-    
+
     "have MaxDocuments value" in {
-      Configuration.maxDocuments must be equalTo(50000)
+      Configuration.maxDocuments must be equalTo (50000)
     }
-    
+
     "have pageSize value" in {
-      Configuration.pageSize must be equalTo(50)
+      Configuration.pageSize must be equalTo (50)
     }
-    
+
     "have maxInFlightRequests value" in {
-      Configuration.maxInFlightRequests must be equalTo(4)
+      Configuration.maxInFlightRequests must be equalTo (4)
+    }
+
+    "read default values" in {
+      TestConfiguration.configuration.getString("str") must be equalTo ("value")
+      TestConfiguration.configuration.getInt("int") must be equalTo (5)
+    }
+
+    "read values in sub path" in {
+      TestConfiguration.configuration.getString("path.sub") must be equalTo ("subvalue")
     }
   }
 }
