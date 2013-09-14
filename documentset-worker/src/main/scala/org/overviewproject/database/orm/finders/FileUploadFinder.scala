b71e23dc9997ad7bc3b42e3a1e96773365db7bd9
Jonas Karlsson <thejonas@gmail.com>
2013-09-14 11:46:32 -0400

    count fileuploads

--- documentset-worker/src/main/scala/org/overviewproject/database/orm/finders/FileUploadFinder.scala
+++ documentset-worker/src/main/scala/org/overviewproject/database/orm/finders/FileUploadFinder.scala
@@ -2,6 +2,15 @@ package org.overviewproject.database.orm.finders
 
 
 import org.overviewproject.database.orm.Schema
+import org.overviewproject.postgres.SquerylEntrypoint._
 import org.overviewproject.tree.orm.FileUpload
+import org.overviewproject.tree.orm.finders.FinderResult
 
-object FileUploadFinder extends FinderById[FileUpload](Schema.fileUploads)
+object FileUploadFinder extends FinderById[FileUpload](Schema.fileUploads) {
+
+  def countsByFileGroup(fileGroupId: Long): Long = {
+    from(Schema.fileUploads)(f =>
+      where(f.fileGroupId === fileGroupId)
+      compute(count))
+  }
+}
---
+++ documentset-worker/src/test/scala/org/overviewproject/database/orm/finders/FileUploadFinderSpec.scala
@@ -0,0 +1,40 @@
+package org.overviewproject.database.orm.finders
+
+import org.overviewproject.test.DbSpecification
+import org.overviewproject.tree.orm.FileGroup
+import org.overviewproject.tree.orm.FileJobState._
+import java.util.UUID
+import org.overviewproject.tree.orm.stores.BaseStore
+import org.overviewproject.database.orm.Schema
+import org.overviewproject.tree.orm.FileUpload
+import java.sql.Timestamp
+
+class FileUploadFinderSpec extends DbSpecification {
+
+  object FileGroupStore extends BaseStore(Schema.fileGroups)
+  object FileUploadStore extends BaseStore(Schema.fileUploads)
+  step(setupDb)
+  
+  "FileUploadFinder" should {
+    
+    
+    "count FileUploads" in new DbTestContext {
+      val numberOfFileUploads = 10
+      val fileGroup = FileGroupStore.insertOrUpdate(FileGroup(UUID.randomUUID, 1l, Complete))
+      
+      val fileUploads = Seq.tabulate(numberOfFileUploads)(n => FileUpload(
+          fileGroup.id,
+          UUID.randomUUID,
+          s"filename=file$n",
+          "application/pdf",
+          n * 1000,
+          new Timestamp(scala.compat.Platform.currentTime),
+          n))
+      FileUploadStore.insertBatch(fileUploads)
+      
+      FileUploadFinder.countsByFileGroup(fileGroup.id) must be equalTo(numberOfFileUploads)
+    }
+  }
+  
+  step(shutdownDb)
+}
