def04307586bcd4ff48ac574af0f83fc5ded9f60
Jonas Karlsson <thejonas@gmail.com>
2013-10-03 12:07:54 -0400

    Extract information from headers

--- app/controllers/util/MassUploadFileIteratee.scala
+++ app/controllers/util/MassUploadFileIteratee.scala
@@ -3,7 +3,9 @@ package controllers.util
 import org.overviewproject.tree.orm.FileGroup
 import org.overviewproject.tree.orm.GroupedFileUpload
 import play.api.libs.iteratee.Iteratee
-
+import play.api.mvc.RequestHeader
+import play.api.http.HeaderNames._
+import org.overviewproject.util.ContentDisposition
 
 trait MassUploadFileIteratee {
 
@@ -11,16 +13,33 @@ trait MassUploadFileIteratee {
 
   trait Storage {
     def findCurrentFileGroup: Option[FileGroup]
-    def createUpload(fileGroupId: Long): GroupedFileUpload
+    def createUpload(fileGroupId: Long, contentType: String, filename: String, size: Long): GroupedFileUpload
     def appendData(upload: GroupedFileUpload, data: Array[Byte]): GroupedFileUpload
   }
 
-  def apply(): Iteratee[Array[Byte], GroupedFileUpload] = {
+  def apply(request: RequestHeader): Iteratee[Array[Byte], GroupedFileUpload] = {
     val fileGroup = storage.findCurrentFileGroup.get
-    val initialUpload = storage.createUpload(fileGroup.id)
+    val info = RequestInformation(request)
+    val initialUpload =
+      storage.createUpload(fileGroup.id, info.contentType, info.filename, info.total)
 
     Iteratee.fold(initialUpload) { (upload, data) =>
       storage.appendData(upload, data)
+    }
+  }
+
+  private case class RequestInformation(filename: String, contentType: String, start: Long, end: Long, total: Long)
+  private object RequestInformation {
+    def apply(request: RequestHeader): RequestInformation = {
+      val contentType = request.headers.get(CONTENT_TYPE).get
+      val contentDisposition = request.headers.get(CONTENT_DISPOSITION).get
+      val contentRange = request.headers.get(CONTENT_RANGE).get
+      val range = """(\d+)-(\d+)/(\d+)""".r // start-end/length
+      val rangeMatch = range.findFirstMatchIn(contentRange).get
+      val List(start, end, length) = rangeMatch.subgroups.take(3)
+
+      RequestInformation(ContentDisposition.filename(contentDisposition).get, contentType,
+        start.toLong, end.toLong, length.toLong)
     }
   }
 }
--- test/controllers/util/MassUploadFileIterateeSpec.scala
+++ test/controllers/util/MassUploadFileIterateeSpec.scala
@@ -9,6 +9,10 @@ import org.overviewproject.tree.orm.{ FileGroup, GroupedFileUpload }
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
+import play.api.mvc.RequestHeader
+import play.api.test.FakeHeaders
+import play.api.test.Helpers._
+import java.util.UUID
 
 
 class MassUploadFileIterateeSpec extends Specification with Mockito {
@@ -23,11 +27,18 @@ class MassUploadFileIterateeSpec extends Specification with Mockito {
       fileGroup.id returns 1l
 
       storage.findCurrentFileGroup returns Some(fileGroup)
-      storage.createUpload(any) returns fileUpload
+      storage.createUpload(any, any, any, any) returns fileUpload
       storage.appendData(any, any) returns fileUpload
     }
 
     trait UploadContext extends Scope {
+      val contentType = "ignoredForNow"
+      val filename = "filename.ext"
+      val contentDisposition = s"attachement; filename=$filename"
+      val start  = 0
+      val end = 999
+      val total = 1000
+
       val data = new Array[Byte](100)
       Random.nextBytes(data)
 
@@ -36,18 +47,31 @@ class MassUploadFileIterateeSpec extends Specification with Mockito {
       val input = new ByteArrayInputStream(data)
       val enumerator = Enumerator.fromStream(input)
 
+      val headers: Seq[(String, Seq[String])] = Seq(
+        (CONTENT_TYPE, Seq(contentType)),
+        (CONTENT_RANGE, Seq(s"$start-$end/$total")),
+        (CONTENT_LENGTH, Seq(s"$total")),
+        (CONTENT_DISPOSITION, Seq(contentDisposition))     
+      )
+
+      val request: RequestHeader = {
+        val r = mock[RequestHeader]
+        r.headers returns FakeHeaders(headers)
+        r
+      }
       
       def result = {
-        val resultFuture = enumerator.run(iteratee())
+        val resultFuture = enumerator.run(iteratee(request))
         Await.result(resultFuture, Duration.Inf)
       }
       
     }
     
-    
+
     "produce a MassUploadFile" in new UploadContext {
-      result must be equalTo(iteratee.fileUpload)
+      result
       there was one(iteratee.storage).findCurrentFileGroup
+      there was one(iteratee.storage).createUpload(1l, contentType, filename, total)
       there was one(iteratee.storage).appendData(iteratee.fileUpload, data)
     }
   }
