118109222adb6aab2ee97345cd2b718f579a9ba4
Jonas Karlsson <thejonas@gmail.com>
2013-10-07 18:28:27 -0400

    Extract more common setup

--- test/controllers/MassUploadControllerSpec.scala
+++ test/controllers/MassUploadControllerSpec.scala
@@ -16,6 +16,7 @@ import play.api.test.Helpers._
 import play.api.mvc.RequestHeader
 import org.overviewproject.tree.orm.FileGroup
 import org.specs2.specification.Scope
+import org.specs2.mutable.Before
 
 class MassUploadControllerSpec extends Specification with Mockito {
 
@@ -25,55 +26,68 @@ class MassUploadControllerSpec extends Specification with Mockito {
     // We can leave this undefined, since it will not be called.
     def massUploadFileIteratee(userEmail: String, request: RequestHeader, guid: UUID, lastModifiedDate: String): Iteratee[Array[Byte], Either[Result, GroupedFileUpload]] =
       ???
-      
+
     override val storage = smartMock[Storage]
   }
 
   "MassUploadController.show" should {
 
     trait UploadContext extends Scope {
+
       val guid = UUID.randomUUID
       val user = OverviewUser(User(1l))
       val request = new AuthorizedRequest(FakeRequest(), user)
       val controller = new TestMassUploadController
-      
+
       lazy val result: Result = controller.show(guid)(request)
     }
-    
-    "return NOT_FOUND if upload does not exist" in new UploadContext {
+
+    trait NoFileGroup {
+      this: UploadContext =>
+
+      controller.storage.findFileGroupInProgress(user.email) returns None
+    }
+
+    trait InProgressFileGroup {
+      this: UploadContext =>
+
       val fileGroup = smartMock[FileGroup]
+      controller.storage.findFileGroupInProgress(user.email) returns Some(fileGroup)
+
+    }
+
+    trait NoUpload {
+      this: UploadContext =>
 
       controller.storage.findGroupedFileUpload(guid) returns None
-      controller.storage.findFileGroupInProgress(user.email) returns Some(fileGroup)
-      
-      status(result) must be equalTo(NOT_FOUND)
     }
+    
+    trait CompleteUpload {
+      this: UploadContext =>
 
-    "return NOT_FOUND if no InProgress FileGroup exists" in new UploadContext {
-      val upload = smartMock[GroupedFileUpload]
-
-      controller.storage.findGroupedFileUpload(guid) returns Some(upload)
-      controller.storage.findFileGroupInProgress(user.email) returns None
-      
-      status(result) must be equalTo(NOT_FOUND)
-
-    }
-
-    "return Ok with content length if upload is complete" in new UploadContext {
       val uploadSize = 1000l
       val filename = "foo.pdf"
       val contentDisposition = s"attachment ; filename=$filename"
-      val fileGroup = smartMock[FileGroup]
       val upload = smartMock[GroupedFileUpload]
 
-      controller.storage.findFileGroupInProgress(user.email) returns Some(fileGroup)
       controller.storage.findGroupedFileUpload(guid) returns Some(upload)
-      
+
       upload.size returns uploadSize
       upload.uploadedSize returns uploadSize
       upload.name returns filename
-      
-      status(result) must be equalTo(OK)
+
+    }
+
+    "return NOT_FOUND if upload does not exist" in new UploadContext with NoUpload with InProgressFileGroup {
+      status(result) must be equalTo (NOT_FOUND)
+    }
+
+    "return NOT_FOUND if no InProgress FileGroup exists" in new UploadContext with CompleteUpload with NoFileGroup {
+      status(result) must be equalTo (NOT_FOUND)
+    }
+
+    "return Ok with content length if upload is complete" in new UploadContext with CompleteUpload with InProgressFileGroup {
+      status(result) must be equalTo (OK)
       header(CONTENT_LENGTH, result) must beSome(s"$uploadSize")
       header(CONTENT_DISPOSITION, result) must beSome(contentDisposition)
     }
@@ -81,7 +95,7 @@ class MassUploadControllerSpec extends Specification with Mockito {
     "return PartialContent with content range if upload is not complete" in {
       pending
     }
-    
+
     "return NOT_FOUND if user does not own upload" in {
       pending
     }
