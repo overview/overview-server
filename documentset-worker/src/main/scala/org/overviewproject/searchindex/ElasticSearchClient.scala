84b54686a9fa688ff4d40f230a901f466f09818e
Jonas Karlsson <thejonas@gmail.com>
2013-10-29 16:02:30 -0400

    Use TransportClient

--- documentset-worker/src/main/scala/org/overviewproject/searchindex/ElasticSearchClient.scala
+++ documentset-worker/src/main/scala/org/overviewproject/searchindex/ElasticSearchClient.scala
@@ -3,6 +3,9 @@ package org.overviewproject.searchindex
 import org.elasticsearch.common.settings.ImmutableSettings
 import org.elasticsearch.node.NodeBuilder.nodeBuilder
 import org.overviewproject.util.Configuration
+import org.elasticsearch.client.transport.TransportClient
+import org.overviewproject.util.Logger
+import org.elasticsearch.common.transport.InetSocketTransportAddress
 
 /**
  * Keeps a singleton client for ElasticSearch.
@@ -10,8 +13,19 @@ import org.overviewproject.util.Configuration
  */
 object ElasticSearchClient {
   private val ConfigFile = Configuration.searchIndex.getString("config_file")
-  
-  private val node = nodeBuilder.settings(ImmutableSettings.settingsBuilder.loadFromClasspath(ConfigFile)).node
-  
-  val client = node.client  
+  private val SearchIndexHost = Configuration.searchIndex.getString("host")
+  private val SearchIndexPort = Configuration.searchIndex.getInt("port")
+
+  val client: TransportClient = createTransportClient
+
+  private def createTransportClient: TransportClient = {
+    val settings = ImmutableSettings.settingsBuilder.loadFromClasspath(ConfigFile)
+    new TransportClient(settings)
+
+    Logger.info(s"Connecting to Search Index [${settings.get("cluster.name")}] at $SearchIndexHost:$SearchIndexPort")
+
+    val transportClient = new TransportClient(settings)
+    transportClient.addTransportAddress(new InetSocketTransportAddress(SearchIndexHost, SearchIndexPort))
+  }
+
 }
