7f3ff79675333375119d519d2690e9af8165d757
Jonas Karlsson <thejonas@gmail.com>
2013-10-30 19:47:46 -0400

    Extract common setup

--- documentset-worker/src/test/scala/org/overviewproject/jobhandler/documentset/SearchHandlerSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/jobhandler/documentset/SearchHandlerSpec.scala
@@ -2,7 +2,6 @@ package org.overviewproject.jobhandler.documentset
 
 import akka.actor._
 import akka.testkit.{ TestActorRef, TestProbe }
-
 import org.overviewproject.jobhandler.JobProtocol._
 import org.overviewproject.jobhandler.documentset.SearchHandlerProtocol.SearchDocumentSet
 import org.overviewproject.jobhandler.documentset.SearchIndexSearcherProtocol._
@@ -10,30 +9,29 @@ import org.overviewproject.test.{ ActorSystemContext, ForwardingActor }
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
+import org.specs2.mutable.Before
 
 class SearchHandlerSpec extends Specification with Mockito {
 
   "SearchHandler" should {
 
-    class TestSearchHandler(searchExists: Boolean, documentSearcherProbe: ActorRef) extends SearchHandler with SearchHandlerComponents {
-      val storage = mock[Storage]
+    abstract class MockStorageSearchHandler(searchExists: Boolean) extends SearchHandler with SearchHandlerComponents {
+      override val storage = mock[Storage]
 
       storage.searchExists(anyLong, anyString) returns searchExists
       storage.createSearchResult(anyLong, anyString) returns 1l
       storage.queryForProject(anyLong, anyString) returns "search terms"
 
+    }
+
+    class TestSearchHandler(searchExists: Boolean, documentSearcherProbe: ActorRef) extends MockStorageSearchHandler(searchExists) {
       val actorCreator = new ActorCreator { // can't mock creation of actors
         override def produceDocumentSearcher(documentSetId: Long, query: String): Actor =
           new ForwardingActor(documentSearcherProbe)
       }
     }
 
-    class SearchHandlerWithSearchFailure extends SearchHandler with SearchHandlerComponents {
-      override val storage = mock[Storage]
-
-      storage.searchExists(anyLong, anyString) returns false
-      storage.createSearchResult(anyLong, anyString) returns 1l
-      storage.queryForProject(anyLong, anyString) returns "search terms"
+    class SearchHandlerWithSearchFailure extends MockStorageSearchHandler(false) {
 
       val actorCreator = new ActorCreator { // can't mock creation of actors
         override def produceDocumentSearcher(documentSetId: Long, query: String): Actor =
@@ -55,91 +53,104 @@ class SearchHandlerSpec extends Specification with Mockito {
 
     }
 
-    trait SearchHandlerSetup extends Scope {
-      val documentSetId = 5l
-      val searchTerms = "search terms"
-
-      def createSearchHandlerParent(searchExists: Boolean, parentProbe: ActorRef, documentSearcherProbe: ActorRef)(implicit system: ActorSystem): ActorRef =
-        system.actorOf(Props(new SearchHandlerParent(searchExists, parentProbe, documentSearcherProbe)))
-
+    trait SearchExistence {
+      def searchExists: Boolean
     }
 
-    abstract class SearchHandlerContext extends ActorSystemContext with SearchHandlerSetup
+    trait ExistingSearch extends SearchExistence {
+      override def searchExists: Boolean = true
+    }
 
-    "send JobDone to parent if SearchResult already exists" in new SearchHandlerContext {
-      val documentSearcherProbe = TestProbe()
-      val parent = createSearchHandlerParent(searchExists = true, testActor, documentSearcherProbe.ref)
+    trait NoExistingSearch extends SearchExistence {
+      override def searchExists: Boolean = false
+    }
 
+    trait SearchInfo {
+      val documentSetId = 5l
+      val searchTerms = "search terms"
+    }
+
+    abstract class SearchHandlerWithParent extends ActorSystemContext with SearchInfo with Before {
+      self: SearchExistence =>
+
+      var parent: ActorRef = _
+      var documentSearcher: TestProbe = _
+
+      def before = {
+        documentSearcher = TestProbe()
+        parent = TestActorRef(new SearchHandlerParent(searchExists, testActor, documentSearcher.ref))
+
+      }
+    }
+
+    abstract class MonitoredSearchHandler extends ActorSystemContext with SearchInfo with Before {
+      var searchHandler: TestActorRef[MockStorageSearchHandler] = _
+      var monitor: TestProbe = _
+
+      var storage: MockStorageSearchHandler#Storage = _
+
+      def before = {
+        searchHandler = TestActorRef(createSearchHandler)
+        storage = searchHandler.underlyingActor.storage
+        monitor = TestProbe()
+
+        monitor watch searchHandler
+      }
+
+      protected def createSearchHandler: MockStorageSearchHandler = {
+        val documentSearcher = TestProbe()
+
+        new TestSearchHandler(searchExists = false, documentSearcher.ref)
+      }
+    }
+
+    abstract class MonitoredFailingSearchHandler extends MonitoredSearchHandler {
+      override protected def createSearchHandler: MockStorageSearchHandler = {
+        new SearchHandlerWithSearchFailure
+      }
+    }
+
+    "send JobDone to parent if SearchResult already exists" in new SearchHandlerWithParent with ExistingSearch {
       parent ! SearchDocumentSet(documentSetId, searchTerms)
 
       expectMsg(JobDone(documentSetId))
     }
 
-    "create a new SearchResult and start Searcher if SearchResult doesn't exist" in new SearchHandlerContext {
-      val documentSearcherProbe = TestProbe()
-
-      val parent = createSearchHandlerParent(searchExists = false, testActor, documentSearcherProbe.ref)
-
+    "create a new SearchResult and start Searcher if SearchResult doesn't exist" in new SearchHandlerWithParent with NoExistingSearch {
       parent ! SearchDocumentSet(documentSetId, searchTerms)
 
-      documentSearcherProbe.expectMsg(StartSearch(1l, documentSetId, searchTerms))
+      documentSearcher.expectMsg(StartSearch(1l, documentSetId, searchTerms))
     }
 
-    "send JobDone to parent when receiving SearchComplete from Searcher" in new SearchHandlerContext {
-      val documentSearcherProbe = TestProbe()
-
-      val parent = createSearchHandlerParent(searchExists = false, testActor, documentSearcherProbe.ref)
-
+    "send JobDone to parent when receiving SearchComplete from Searcher" in new SearchHandlerWithParent with NoExistingSearch {
       parent ! SearchDocumentSet(documentSetId, searchTerms)
       parent ! SearchComplete
 
       expectMsg(JobDone(documentSetId))
     }
 
-    "set SearchResult state to Complete when receiving SearchComplete from Searcher" in new SearchHandlerContext {
-
-      val searchHandler = TestActorRef(new TestSearchHandler(searchExists = false, testActor))
-      val searchHandlerWatcher = TestProbe()
-
-      searchHandlerWatcher watch searchHandler
-
+    "set SearchResult state to Complete when receiving SearchComplete from Searcher" in new MonitoredSearchHandler {
       searchHandler ! SearchDocumentSet(documentSetId, searchTerms)
       searchHandler ! SearchComplete
 
-      val storage = searchHandler.underlyingActor.storage
-
+      monitor.expectMsgType[Terminated]
       there was one(storage).completeSearch(1l, documentSetId, searchTerms)
-      searchHandlerWatcher.expectMsgType[Terminated]
     }
 
-    "set SearchResultState to Error when receiving SearchFailure from Searcher" in new SearchHandlerContext {
-      val searchHandler = TestActorRef(new TestSearchHandler(searchExists = false, testActor))
-      val searchHandlerWatcher = TestProbe()
-
-      searchHandlerWatcher watch searchHandler
-
+    "set SearchResultState to Error when receiving SearchFailure from Searcher" in new MonitoredSearchHandler {
       val error = new Exception("exception from RequestQueue")
 
       searchHandler ! SearchDocumentSet(documentSetId, searchTerms)
       searchHandler ! SearchFailure(error)
 
-      val storage = searchHandler.underlyingActor.storage
-
+      monitor.expectMsgType[Terminated]
       there was one(storage).failSearch(1l, documentSetId, searchTerms)
-      searchHandlerWatcher.expectMsgType[Terminated]
     }
 
-    "set SearchResultState to Error if Searcher dies unexpectedly" in new ActorSystemContext {
-      val searchHandler = TestActorRef(new SearchHandlerWithSearchFailure)
-      val searchHandlerWatcher = TestProbe()
-
-      searchHandlerWatcher watch searchHandler
-      
+    "set SearchResultState to Error if Searcher dies unexpectedly" in new MonitoredFailingSearchHandler {
       searchHandler ! SearchDocumentSet(1l, "search terms")
-      
-      val storage = searchHandler.underlyingActor.storage
-      
-      searchHandlerWatcher.expectMsgType[Terminated]
+
+      monitor.expectMsgType[Terminated]
       there was one(storage).failSearch(1l, 1l, "search terms")
     }
   }
