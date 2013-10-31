5201263fee1e9601926a596b8a3cf0ac25f4f81e
Jonas Karlsson <thejonas@gmail.com>
2013-10-31 16:52:39 -0400

    Extract common setup

--- documentset-worker/src/test/scala/org/overviewproject/jobhandler/documentset/DocumentSetMessageHandlerSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/jobhandler/documentset/DocumentSetMessageHandlerSpec.scala
@@ -2,19 +2,17 @@ package org.overviewproject.jobhandler.documentset
 
 import akka.actor._
 import akka.testkit._
-
 import org.overviewproject.jobhandler.JobProtocol._
 import org.overviewproject.messagequeue.MessageHandlerProtocol._
 import org.overviewproject.jobhandler.documentset.DeleteHandlerProtocol.DeleteDocumentSet
 import org.overviewproject.jobhandler.documentset.DocumentSetJobHandlerProtocol._
 import org.overviewproject.jobhandler.documentset.SearchHandlerProtocol.SearchDocumentSet
 import org.overviewproject.test.{ ActorSystemContext, ForwardingActor }
-
 import org.specs2.mutable.Specification
+import org.specs2.mutable.Before
 
 class DocumentSetMessageHandlerSpec extends Specification {
-  
-  
+
   class TestMessageHandler(specificHandlerProbe: ActorRef) extends DocumentSetMessageHandler
       with SearchComponent {
 
@@ -23,52 +21,71 @@ class DocumentSetMessageHandlerSpec extends Specification {
       override def produceDeleteHandler: Actor = new ForwardingActor(specificHandlerProbe)
     }
   }
-  
+
   "DocumentSetMessageHandler" should {
-    
-    "start search handler" in new ActorSystemContext {
+
+    trait DocumentSetInfo {
       val documentSetId = 1l
+    }
+
+    abstract class SearchContext extends ActorSystemContext with DocumentSetInfo with Before {
       val query = "query string"
       val searchCommand = SearchCommand(documentSetId, query)
       val searchMessage = SearchDocumentSet(documentSetId, query)
 
-      val searchHandler = TestProbe()
+      var searchHandler: TestProbe = _
+      var messageHandler: TestActorRef[TestMessageHandler] = _
+
+      def before = {
+        searchHandler = TestProbe()
+        messageHandler = TestActorRef(new TestMessageHandler(searchHandler.ref))
+      }
+    }
+
+    trait DeleteInfo extends DocumentSetInfo {
+      val deleteCommand = DeleteCommand(documentSetId)
+      val deleteMessage = DeleteDocumentSet(documentSetId)
+
+      var deleteHandler: TestProbe = _
+    }
+
+    abstract class DeleteContext extends ActorSystemContext with DeleteInfo with Before {
+      var messageHandler: TestActorRef[TestMessageHandler] = _
+
+      def before = {
+        deleteHandler = TestProbe()
+        messageHandler = TestActorRef(new TestMessageHandler(deleteHandler.ref))
+      }
+    }
+
+    abstract class DeleteWithParentContext extends ActorSystemContext with DeleteInfo with Before {
+      var parent: TestProbe = _
+      var messageHandler: TestActorRef[Nothing] = _
       
-      val messageHandler = TestActorRef(new TestMessageHandler(searchHandler.ref))
-      
+      def before = {
+        parent = TestProbe()
+        deleteHandler = TestProbe()
+        messageHandler = TestActorRef(Props(new TestMessageHandler(deleteHandler.ref)), parent.ref, "Message Handler")
+      }
+    }
+
+    "start search handler" in new SearchContext {
       messageHandler ! searchCommand
-      
+
       searchHandler.expectMsg(searchMessage)
     }
-    
-    "start delete handler" in new ActorSystemContext {
-      val documentSetId = 1l
-      val deleteCommand = DeleteCommand(documentSetId)
-      val deleteMessage = DeleteDocumentSet(documentSetId)
-      
-      val deleteHandler = TestProbe()
-      
-      val messageHandler = TestActorRef(new TestMessageHandler(deleteHandler.ref))
-      
+
+    "start delete handler" in new DeleteContext {
       messageHandler ! deleteCommand
-      
+
       deleteHandler.expectMsg(deleteMessage)
     }
-    
-    "tell parent MessageHandled when JobDone is received" in new ActorSystemContext {
-      val parentProbe = TestProbe()
-      val documentSetId = 1l
-      val deleteCommand = DeleteCommand(documentSetId)
-      val deleteMessage = DeleteDocumentSet(documentSetId)
-      
-      val deleteHandler = TestProbe()
-      
-      val messageHandler = TestActorRef(Props(new TestMessageHandler(deleteHandler.ref)), parentProbe.ref, "Message Handler")
-      
+
+    "tell parent MessageHandled when JobDone is received" in new DeleteWithParentContext {
       messageHandler ! deleteCommand
       messageHandler ! JobDone(documentSetId)
-      
-      parentProbe.expectMsg(MessageHandled)
+
+      parent.expectMsg(MessageHandled)
     }
   }
 
