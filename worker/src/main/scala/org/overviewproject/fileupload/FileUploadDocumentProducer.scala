178b8c39a448f4af472d80ab2857cbc171dc90bd
Jonas Karlsson <thejonas@gmail.com>
2013-11-19 17:47:06 -0500

    Make sure indexing is complete before finishing job

--- worker/src/main/scala/org/overviewproject/fileupload/FileUploadDocumentProducer.scala
+++ worker/src/main/scala/org/overviewproject/fileupload/FileUploadDocumentProducer.scala
@@ -1,19 +1,24 @@
 package org.overviewproject.fileupload
 
+import scala.language.postfixOps
+import scala.concurrent.duration._
 import org.overviewproject.database.Database
 import org.overviewproject.documentcloud.DocumentRetrievalError
 import org.overviewproject.persistence._
 import org.overviewproject.persistence.orm.finders.FileFinder
 import org.overviewproject.tree.orm.{ Document, GroupedProcessedFile }
 import org.overviewproject.tree.orm.FileJobState._
-import org.overviewproject.util.{ DocumentConsumer, DocumentProducer, DocumentSetIndexingSession } 
+import org.overviewproject.util.{ DocumentConsumer, DocumentProducer, DocumentSetIndexingSession }
 import org.overviewproject.util.DocumentSetCreationJobStateDescription.Parsing
 import org.overviewproject.util.Progress.{ Progress, ProgressAbortFn }
 import org.overviewproject.util.SearchIndex
+import scala.concurrent.Await
 
 class FileUploadDocumentProducer(documentSetId: Long, fileGroupId: Long,
                                  consumer: DocumentConsumer, progAbort: ProgressAbortFn) extends DocumentProducer
     with PersistentDocumentSet {
+
+  private val IndexingTimeout = 3 minutes
 
   private val UpdateInterval = 1000l
   private val PreparingFraction = 0.25
@@ -42,13 +47,12 @@ class FileUploadDocumentProducer(documentSetId: Long, fileGroupId: Long,
         file.text.map { text =>
           val documentId = writeAndCommitDocument(documentSetId, file)
           indexingSession.indexDocument(documentSetId, documentId, text, Some(file.name), None)
-          
+
           consumer.processDocument(documentId, text)
         } getOrElse {
           val error = file.errorMessage.getOrElse(s"Inconsistent GroupedProcessFile in Database: ${file.id}")
-          fileErrors = DocumentRetrievalError(file.name, error) +: fileErrors            
+          fileErrors = DocumentRetrievalError(file.name, error) +: fileErrors
         }
-        
 
         numberOfDocumentsRead += 1
 
@@ -58,6 +62,9 @@ class FileUploadDocumentProducer(documentSetId: Long, fileGroupId: Long,
     indexingSession.complete
 
     consumer.productionComplete()
+
+    Await.result(indexingSession.requestsComplete, IndexingTimeout)
+
     updateDocumentSetCounts(documentSetId, numberOfDocumentsRead, 0)
 
     Database.inTransaction {
@@ -77,12 +84,12 @@ class FileUploadDocumentProducer(documentSetId: Long, fileGroupId: Long,
   }
 
   private def writeAndCommitDocument(documentSetId: Long, file: GroupedProcessedFile): Long = Database.inTransaction {
-    val document = Document(documentSetId, id = ids.next, title = Some(file.name), text = file.text, 
-        contentsOid = Some(file.contentsOid), contentLength = Some(file.size))
+    val document = Document(documentSetId, id = ids.next, title = Some(file.name), text = file.text,
+      contentsOid = Some(file.contentsOid), contentLength = Some(file.size))
 
     DocumentWriter.write(document)
 
     document.id
   }
-  
+
 }
