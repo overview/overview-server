877f8c2da2aedbdd4367eb74acfdef6843b27979
Jonas Karlsson <thejonas@gmail.com>
2013-11-27 16:15:38 -0500

    Filter NodeDocuments by documentSet

--- app/models/orm/finders/NodeDocumentFinder.scala
+++ app/models/orm/finders/NodeDocumentFinder.scala
@@ -9,84 +9,87 @@ import org.overviewproject.tree.orm.finders.{ Finder, FinderResult }
 import org.squeryl.Query
 import org.squeryl.dsl.GroupWithMeasures
 
-import models.orm.{NodeDocument,Schema}
+import models.orm.{ NodeDocument, Schema }
 
 object NodeDocumentFinder extends Finder {
   class NodeDocumentFinderResult(query: Query[NodeDocument]) extends FinderResult(query) {
     /** @return (nodeId, count) pairs. */
-    def tagCountsByNodeId(tag: Long) : FinderResult[(Long,Long)] = {
-      val main : Query[GroupWithMeasures[Long,Long]] = join(query, Schema.documentTags)((nd, dt) =>
+    def tagCountsByNodeId(tag: Long): FinderResult[(Long, Long)] = {
+      val main: Query[GroupWithMeasures[Long, Long]] = join(query, Schema.documentTags)((nd, dt) =>
         where(dt.tagId === tag)
-        groupBy(nd.nodeId)
-        compute(org.overviewproject.postgres.SquerylEntrypoint.count)
-        on(nd.documentId === dt.documentId)
-      )
+          groupBy (nd.nodeId)
+          compute (org.overviewproject.postgres.SquerylEntrypoint.count)
+          on (nd.documentId === dt.documentId))
 
       from(main)(row => select(row.key, row.measures))
     }
 
     /** @return (nodeId, tagId, count) tuples. */
-    def allTagCountsByNodeId: FinderResult[(Long,Long,Long)] = {
-      val main : Query[GroupWithMeasures[Product2[Long,Long],Long]] = join(query, Schema.documentTags)((nd, dt) =>
+    def allTagCountsByNodeId: FinderResult[(Long, Long, Long)] = {
+      val main: Query[GroupWithMeasures[Product2[Long, Long], Long]] = join(query, Schema.documentTags)((nd, dt) =>
         groupBy(nd.nodeId, dt.tagId)
-        compute(org.overviewproject.postgres.SquerylEntrypoint.count)
-        on(nd.documentId === dt.documentId)
-      )
+          compute (org.overviewproject.postgres.SquerylEntrypoint.count)
+          on (nd.documentId === dt.documentId))
 
       from(main)(row => select(row.key._1, row.key._2, row.measures))
     }
 
     /** @return (nodeId, count) pairs. */
-    def searchResultCountsByNodeId(searchResult: Long) : FinderResult[(Long,Long)] = {
-      val main : Query[GroupWithMeasures[Long,Long]] = join(query, Schema.documentSearchResults)((nd, ds) =>
+    def searchResultCountsByNodeId(searchResult: Long): FinderResult[(Long, Long)] = {
+      val main: Query[GroupWithMeasures[Long, Long]] = join(query, Schema.documentSearchResults)((nd, ds) =>
         where(ds.searchResultId === searchResult)
-        groupBy(nd.nodeId)
-        compute(org.overviewproject.postgres.SquerylEntrypoint.count)
-        on(nd.documentId === ds.documentId)
-      )
+          groupBy (nd.nodeId)
+          compute (org.overviewproject.postgres.SquerylEntrypoint.count)
+          on (nd.documentId === ds.documentId))
 
       from(main)(row => select(row.key, row.measures))
     }
 
     /** @return (nodeId, tagId, count) tuples. */
-    def allSearchResultCountsByNodeId: FinderResult[(Long,Long,Long)] = {
-      val main : Query[GroupWithMeasures[Product2[Long,Long],Long]] = join(query, Schema.documentSearchResults)((nd, ds) =>
+    def allSearchResultCountsByNodeId: FinderResult[(Long, Long, Long)] = {
+      val main: Query[GroupWithMeasures[Product2[Long, Long], Long]] = join(query, Schema.documentSearchResults)((nd, ds) =>
         groupBy(nd.nodeId, ds.searchResultId)
-        compute(org.overviewproject.postgres.SquerylEntrypoint.count)
-        on(nd.documentId === ds.documentId)
-      )
+          compute (org.overviewproject.postgres.SquerylEntrypoint.count)
+          on (nd.documentId === ds.documentId))
 
       from(main)(row => select(row.key._1, row.key._2, row.measures))
     }
-    
+
     def untaggedDocumentCountsByNodeId: FinderResult[(Long, Long)] = {
       val main: Query[GroupWithMeasures[Long, Long]] = join(query, Schema.documentTags.leftOuter)((nd, dt) =>
-        where(dt.map(_.tagId).getOrElse(-1l) isNull)  // Not clear if get would be safe here
-        groupBy(nd.nodeId)
-        compute(org.overviewproject.postgres.SquerylEntrypoint.count)
-        on(nd.documentId === dt.map(_.documentId)))
+        where(dt.map(_.tagId).getOrElse(-1l) isNull) // Not clear if get would be safe here
+          groupBy (nd.nodeId)
+          compute (org.overviewproject.postgres.SquerylEntrypoint.count)
+          on (nd.documentId === dt.map(_.documentId)))
 
-     from(main)(row => select(row.key, row.measures))
+      from(main)(row => select(row.key, row.measures))
     }
   }
-  implicit private def queryToNodeDocumentFinderResult(query: Query[NodeDocument]) : NodeDocumentFinderResult = new NodeDocumentFinderResult(query)
+  implicit private def queryToNodeDocumentFinderResult(query: Query[NodeDocument]): NodeDocumentFinderResult = new NodeDocumentFinderResult(query)
 
-  def byDocumentSet(documentSet: Long) : NodeDocumentFinderResult = {
+  def byDocumentSet(documentSet: Long): NodeDocumentFinderResult = {
     // join through documents, not nodes: there are fewer documents than nodes
     // Select as WHERE with a subquery, to circumvent Squeryl delete() missing the join
     val documentIds = from(Schema.documents)(d =>
       where(d.documentSetId === documentSet)
-      select(d.id)
-    )
+        select (d.id))
 
     from(Schema.nodeDocuments)(nd =>
       where(nd.documentId in documentIds)
+        select (nd))
+  }
+
+  /** @return All NodeDocuments with the given Node IDs */
+  def byNodeIds(nodeIds: Traversable[Long]): NodeDocumentFinderResult = {
+    Schema.nodeDocuments.where(_.nodeId in nodeIds)
+  }
+
+  /** @ return All NodeDocuments with the given Node IDs in the given DocumentSet. */
+  def byNodeIdsInDocumentSet(nodeIds: Traversable[Long], documentSetId: Long): NodeDocumentFinderResult = {
+    from(Schema.nodeDocuments, Schema.nodes)((nd, n) =>
+      where((nd.nodeId in nodeIds) and (nd.nodeId === n.id) and (n.documentSetId === documentSetId))
       select(nd)
     )
   }
 
-  /** @return All NodeDocuments with the given Node IDs in the given DocumentSet. */
-  def byNodeIds(nodeIds: Traversable[Long]) : NodeDocumentFinderResult = {
-    Schema.nodeDocuments.where(_.nodeId in nodeIds)
-  }
 }
--- test/models/orm/finders/NodeDocumentFinderSpec.scala
+++ test/models/orm/finders/NodeDocumentFinderSpec.scala
@@ -15,7 +15,20 @@ class NodeDocumentFinderSpec extends Specification {
 
   "NodeDocumentFinder" should {
 
-    trait TaggedDocumentsContext extends DbTestContext {
+    trait NodeSetup {
+      def createNode(nodeId: Long, documentSetId: Long): Unit = nodes.insert(
+        Node(id = nodeId,
+          documentSetId = documentSetId,
+          parentId = None, description = "", cachedSize = 0, cachedDocumentIds = Array.empty, isLeaf = true))
+
+      def addDocumentsToNode(documentIds: Seq[Long], nodeId: Int): Unit =
+        nodeDocuments.insert(documentIds.map(d => NodeDocument(nodeId, d)))
+        
+      def createDocument(id: Long, documentSetId: Long): Unit = 
+        documents.insert(Document(id = id, documentSetId = documentSetId))
+    }
+
+    trait TaggedDocumentsContext extends DbTestContext with NodeSetup {
       val documentIds = 0l to 9l
       val nodeIds = 0l to 2l
 
@@ -24,7 +37,7 @@ class NodeDocumentFinderSpec extends Specification {
       override def setupWithDb = {
         documentSet = documentSets.insertOrUpdate(DocumentSet())
 
-        documentIds.foreach(n => documents.insert(Document(id = n, documentSetId = documentSet.id)))
+        documentIds.foreach(n => createDocument(n, documentSet.id))
         nodeIds.foreach(n => createNode(n, documentSet.id))
         val tag = tags.insertOrUpdate(Tag(documentSetId = documentSet.id, name = "tag", color = "000000"))
 
@@ -35,23 +48,44 @@ class NodeDocumentFinderSpec extends Specification {
 
       }
 
-      private def createNode(nodeId: Long, documentSetId: Long): Unit = nodes.insert(
-        Node(id = nodeId,
-          documentSetId = documentSetId,
-          parentId = None, description = "", cachedSize = 0, cachedDocumentIds = Array.empty, isLeaf = true))
-
-      private def addDocumentsToNode(documentIds: Seq[Long], nodeId: Int): Unit =
-        nodeDocuments.insert(documentIds.map(d => NodeDocument(nodeId, d)))
-
     }
 
-    
+    trait NodesInTwoDocumentSets extends DbTestContext with NodeSetup {
+      var documentSet1: DocumentSet = _
+      var documentSet2: DocumentSet = _
+      val documentIds1 = 100l to 109l
+      val documentIds2 = 200l to 209l
+      val nodeId1 = 100
+      val nodeId2 = 200
+      
+      override def setupWithDb = {
+        documentSet1 = documentSets.insertOrUpdate(DocumentSet())
+        documentSet2 = documentSets.insertOrUpdate(DocumentSet())
+        
+        documentIds1.foreach(n => createDocument(n, documentSet1.id))
+        documentIds2.foreach(n => createDocument(n, documentSet2.id))
+        
+        createNode(nodeId1, documentSet1.id)
+        createNode(nodeId2, documentSet2.id)
+        
+        addDocumentsToNode(documentIds1, nodeId1)
+        addDocumentsToNode(documentIds2, nodeId2)
+      }
+    }
+
     "count untagged documents in nodes" in new TaggedDocumentsContext {
       val counts = NodeDocumentFinder.byNodeIds(nodeIds).untaggedDocumentCountsByNodeId.toMap
-      
+
       counts.get(0) must beSome(3)
       counts.get(1) must beNone
       counts.get(2) must beSome(3)
+    }
+    
+    "find NodeDocuments in DocumentSet only" in new NodesInTwoDocumentSets {
+      val nodeDocuments = NodeDocumentFinder.byNodeIdsInDocumentSet(documentIds1 ++ documentIds2, documentSet1.id)
+      
+      nodeDocuments.toSeq must haveTheSameElementsAs(documentIds1.map(NodeDocument(nodeId1, _)))
+      
     }
   }
 
