d05d3f9741dab88f75c7528e30b374cfd5b918bc
Jonas Karlsson <thejonas@gmail.com>
2013-11-27 16:19:17 -0500

    Count untagged documents in Nodes

---
+++ app/controllers/UntaggedDocumentsController.scala
@@ -0,0 +1,44 @@
+package controllers
+
+import play.api.mvc.Controller
+import controllers.auth.Authorities.userOwningDocumentSet
+import controllers.auth.AuthorizedAction
+import controllers.forms.NodeIdsForm
+import models.orm.finders.NodeDocumentFinder
+
+trait UntaggedDocumentsController extends Controller {
+  val MaxPageSize = 100
+  
+  trait Storage {
+    def untaggedCountsByNodeId(nodeIds: Iterable[Long], documentSetId: Long): Iterable[(Long, Int)]
+  }
+  
+  val storage: Storage
+  
+
+  def nodeCounts(documentSetId: Long) = AuthorizedAction(userOwningDocumentSet(documentSetId)) { implicit request => 
+    NodeIdsForm().bindFromRequest.fold(
+      formWithErrors => BadRequest,
+      nodeIds => {
+        val counts = storage.untaggedCountsByNodeId(nodeIds, documentSetId)
+        Ok(views.json.helper.nodeCounts(counts))
+      }
+    )
+  }
+
+}
+
+object UntaggedDocumentsController extends UntaggedDocumentsController {
+  override val storage = new Storage {
+    
+    override def untaggedCountsByNodeId(nodeIds: Iterable[Long], documentSetId: Long): Iterable[(Long, Int)] = {
+      
+      val counts = NodeDocumentFinder
+        .byNodeIdsInDocumentSet(nodeIds, documentSetId)
+        .untaggedDocumentCountsByNodeId
+        .toMap
+        
+      nodeIds.map(nodeId => (nodeId -> counts.getOrElse(nodeId, 0l).toInt))
+    }
+  }
+}
