b563a40165c0766165fd610ac8913c8f6fd35648
Jonas Karlsson <thejonas@gmail.com>
2013-12-17 16:34:03 -0500

    Document deletion process

--- documentset-worker/src/main/scala/org/overviewproject/jobhandler/documentset/DeleteHandler.scala
+++ documentset-worker/src/main/scala/org/overviewproject/jobhandler/documentset/DeleteHandler.scala
@@ -8,7 +8,33 @@ import akka.actor.Actor
 import org.overviewproject.jobhandler.JobProtocol._
 import org.overviewproject.util.Logger
 
-
+/**
+ * [[DeleteHandler]] deletes a document set and all associated data if deletion is requested after
+ * clustering begins. 
+ * Since the clustering worker is currently completely independent, we can't directly control the 
+ * status of the clustering process. We want to assume that no more data is being added to the 
+ * document set while it is deleted, so we want to wait until the clustering job has stopped.
+ * The initial, hacky way to accomplish this goal:
+ *   # The server sets the document set creation job state to `CANCELLED`
+ *   # The clustering worker will delete the document set creation job, when it detects cancellation
+ *   # The DeleteHandler will wait until the job has been deleted before deleting the document set
+ *   # The DeleteHandler will fail if the job does not get deleted after a given timeout interval
+ * Once we unify all worker processes, we can use a more rigorous approach.
+ * 
+ * We also assume that the server will have sent cancellation notices to any jobs trying to clone the
+ * document set being deleted. Clone jobs will notice that they're being cancelled before they notice
+ * that the source has been deleted.
+ * 
+ * Data to be deleted:
+ *   * documents in search index and search index alias
+ *   * Nodes created by clustering
+ *   * LogEntries
+ *   * Tags
+ *   * Uploaded documents
+ *   * Documents
+ *   * DocumentProcessingErrors
+ *   * SearchResults
+ */
 object DeleteHandlerProtocol {
   case class DeleteDocumentSet(documentSetId: Long)
 }
