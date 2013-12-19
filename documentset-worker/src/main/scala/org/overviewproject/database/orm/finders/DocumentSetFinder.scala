f25ad87e46c77d84e160eb0969ff63cde689466a
Jonas Karlsson <thejonas@gmail.com>
2013-12-19 17:00:04 -0500

    Delete unused finder

--- documentset-worker/src/main/scala/org/overviewproject/database/orm/finders/DocumentSetFinder.scala
+++
@@ -1,7 +0,0 @@
-package org.overviewproject.database.orm.finders
-
-
-import org.overviewproject.database.orm.Schema
-import org.overviewproject.tree.orm.DocumentSet
-
-object DocumentSetFinder extends FinderById[DocumentSet](Schema.documentSets)
