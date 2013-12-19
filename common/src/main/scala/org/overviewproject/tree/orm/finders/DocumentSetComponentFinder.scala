d9ad73b80c888d5eafb5615db977257ef24172c7
Jonas Karlsson <thejonas@gmail.com>
2013-12-19 17:15:04 -0500

    Enable subclassing

--- common/src/main/scala/org/overviewproject/tree/orm/finders/DocumentSetComponentFinder.scala
+++ common/src/main/scala/org/overviewproject/tree/orm/finders/DocumentSetComponentFinder.scala
@@ -4,19 +4,19 @@ import org.overviewproject.tree.orm.DocumentSetComponent
 import org.squeryl.Table
 import org.overviewproject.postgres.SquerylEntrypoint._
 
-trait DocumentSetComponentFinder[A] extends Finder {
-  def byDocumentSet(documentSetId: Long): FinderResult[A]   
+trait DocumentSetComponentFinder[A <: DocumentSetComponent] extends Finder {
+  protected val table: Table[A]
+
+  def byDocumentSet(documentSetId: Long): FinderResult[A] =
+    from(table)(c =>
+      where(c.documentSetId === documentSetId)
+        select (c))
+
 }
 
 object DocumentSetComponentFinder {
-  def apply[A <: DocumentSetComponent](table: Table[A]) = new DocumentSetComponentFinder[A] {
-    
-    def byDocumentSet(documentSetId: Long): FinderResult[A] = 
-      from(table)(c =>
-        where(c.documentSetId === documentSetId)
-        select(c)
-      )
-  } 
-    
-    
+  def apply[A <: DocumentSetComponent](componentTable: Table[A]) = new DocumentSetComponentFinder[A] {
+	override val table: Table[A] = componentTable
+  }
+
 }
