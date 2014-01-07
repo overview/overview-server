e59cb8e26c3fed72cd4d29f5a9f19acf3d5aa812
Jonas Karlsson <thejonas@gmail.com>
2014-01-07 16:12:55 -0500

    Test deletion of clustering generated data

--- documentset-worker/src/test/scala/org/overviewproject/jobhandler/documentset/DocumentSetDeleterSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/jobhandler/documentset/DocumentSetDeleterSpec.scala
@@ -8,41 +8,92 @@ import org.overviewproject.tree.orm.finders.DocumentSetComponentFinder
 import org.overviewproject.test.IdGenerator._
 import org.overviewproject.database.orm.Schema._
 import org.overviewproject.database.orm.finders.DocumentTagFinder
+import org.squeryl.Table
+import org.overviewproject.database.orm.finders.FindableByDocumentSet
+import org.overviewproject.database.orm.finders.NodeDocumentFinder
 
 class DocumentSetDeleterSpec extends DbSpecification {
-  
+
   step(setupDb)
-  
+
   "DocumentSetDeleter" should {
-    
-    
-    inExample("delete client generated information") in new DbTestContext {
-      val documentSet = documentSets.insertOrUpdate(DocumentSet(title =  "document set"))
-      val documentSetId = documentSet.id
-      val document = Document(documentSetId, documentcloudId = Some("dcId"), id = nextDocumentId(documentSetId))
-      documents.insert(document)
-      
-      val logEntry = logEntries.insertOrUpdate(
-          LogEntry(documentSetId = documentSet.id, userId = 1l, date = new Timestamp(0l), component = ""))
-      val tag = tags.insertOrUpdate(Tag(documentSetId = documentSet.id, name = "", color = "ffffff"))
-      documentTags.insert(DocumentTag(document.id, tag.id))
-      searchResults.insert(SearchResult(SearchResultState.Complete, documentSet.id, "query"))
-      
+
+    trait DocumentSetContext extends DbTestContext {
+      var documentSet: DocumentSet = _
+
+      protected var document: Document = _
+
+      override def setupWithDb = {
+        createDocumentSet
+      }
+
+      protected def createDocumentSet = {
+        documentSet = documentSets.insertOrUpdate(DocumentSet(title = "document set"))
+        document = Document(documentSet.id, documentcloudId = Some("dcId"), id = nextDocumentId(documentSet.id))
+        documents.insert(document)
+      }
+
+      def addClientGeneratedInformation = {
+        logEntries.insert(
+          LogEntry(documentSetId = documentSet.id,
+            userId = 1l,
+            date = new Timestamp(0l),
+            component = ""))
+        val tag = tags.insertOrUpdate(Tag(documentSetId = documentSet.id, name = "", color = "ffffff"))
+        documentTags.insert(DocumentTag(document.id, tag.id))
+        searchResults.insert(SearchResult(SearchResultState.Complete, documentSet.id, "query"))
+      }
+
+      def addClusteringGeneratedInformation = {
+        val node = Node(nextNodeId(documentSet.id),
+          documentSet.id, None, "", 1, Array(document.id), true)
+
+        nodes.insert(node)
+        nodeDocuments.insert(NodeDocument(node.id, document.id))
+
+        documentProcessingErrors.insert(
+          DocumentProcessingError(documentSet.id, "url", "message", None, None))
+      }
+
+      def findAll[A <: DocumentSetComponent](table: Table[A]): Seq[A] =
+        DocumentSetComponentFinder(table).byDocumentSet(documentSet.id).toSeq
+
+      def findAllWithFinder[A](finder: FindableByDocumentSet[A]): Seq[A] =
+        finder.byDocumentSet(documentSet.id).toSeq
+    }
+
+    "delete client generated information" in new DocumentSetContext {
+      addClientGeneratedInformation
       DocumentSetDeleter().deleteClientGeneratedInformation(documentSet.id)
 
-      val remainingLogEntries = DocumentSetComponentFinder(logEntries).byDocumentSet(documentSet.id)
-      val remainingDocumentTags = DocumentTagFinder.byDocumentSet(documentSet.id)
-      val remainingTags = DocumentSetComponentFinder(tags).byDocumentSet(documentSet.id)
-      val remainingSearchResults = DocumentSetComponentFinder(searchResults).byDocumentSet(documentSet.id)
-      
-      remainingLogEntries must beEmpty
-      remainingDocumentTags must beEmpty
-      remainingTags must beEmpty
-      remainingSearchResults must beEmpty
+      findAll(logEntries) must beEmpty
+      findAllWithFinder(DocumentTagFinder) must beEmpty
+      findAll(tags) must beEmpty
+      findAll(searchResults) must beEmpty
+    }
+
+    "delete clustering generated information" in new DocumentSetContext {
+      addClusteringGeneratedInformation
+      DocumentSetDeleter().deleteClusteringGeneratedInformation(documentSet.id)
+
+      findAll(nodes) must beEmpty
+      findAllWithFinder(NodeDocumentFinder) must beEmpty
+      findAll(documentProcessingErrors) must beEmpty
+    }
+
+    "delete document set" in new DbTestContext {
+      todo
+    }
+
+    "delete document set with CSV upload" in new DbTestContext {
+      todo
+    }
+
+    "delete document set with uploaded PDFs" in new DbTestContext {
+      todo
     }
   }
-  
-  
+
   step(shutdownDb)
 
 }
