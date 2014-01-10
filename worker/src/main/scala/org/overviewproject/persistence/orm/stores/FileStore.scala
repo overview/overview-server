0db163173f230b269b4ff70c53a4d144d61b6428
Jonas Karlsson <thejonas@gmail.com>
2014-01-10 12:06:38 -0500

    Delete uploads where text extraction failed

--- worker/src/main/scala/org/overviewproject/persistence/orm/stores/FileStore.scala
+++ worker/src/main/scala/org/overviewproject/persistence/orm/stores/FileStore.scala
@@ -1,6 +1,25 @@
 package org.overviewproject.persistence.orm.stores
 
-import org.overviewproject.tree.orm.stores.BaseStore
-import org.overviewproject.persistence.orm.Schema
+import scala.language.postfixOps
 
-object FileStore extends BaseStore(Schema.groupedProcessedFiles)
+import org.overviewproject.postgres.SquerylEntrypoint._
+import org.overviewproject.tree.orm.stores.BaseStore
+import org.overviewproject.tree.orm.GroupedProcessedFile
+import org.overviewproject.persistence.orm.Schema
+import org.squeryl.Query
+
+object FileStore extends BaseStore(Schema.groupedProcessedFiles) {
+  
+  /**
+   * Delete large objects associated with failed pdf extraction, otherwise
+   * we lose all references to the oid.
+   */
+  override def delete(query: Query[GroupedProcessedFile]): Int = {
+    from(query)(g =>
+      where (g.text isNull)
+      select (&(lo_unlink(Some(g.contentsOid))))).toIterable
+
+    
+    super.delete(query)
+  }
+}
