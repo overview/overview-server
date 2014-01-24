ce8f358ac29f695d1db31d3598dcad0dfd50320f
Jonas Karlsson <thejonas@gmail.com>
2014-01-24 17:26:37 -0500

    Remove redundant NodeDocument class

--- worker/src/main/scala/org/overviewproject/persistence/orm/NodeDocument2.scala
+++
@@ -1,7 +0,0 @@
-package org.overviewproject.persistence.orm
-
-import org.squeryl.annotations.Column
-
-case class NodeDocument2(
-    @Column("node_id") nodeId: Long,
-    @Column("document_id") documentId: Long) 
