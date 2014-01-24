9e24f5d815a1c5b7d9aff1424296ef323bedfd81
Jonas Karlsson <thejonas@gmail.com>
2014-01-24 18:58:48 -0500

    Cleanup imports

--- worker/src/main/scala/org/overviewproject/persistence/DocumentWriter.scala
+++ worker/src/main/scala/org/overviewproject/persistence/DocumentWriter.scala
@@ -7,18 +7,15 @@
 
 package org.overviewproject.persistence
 
-import java.sql.Connection
-import anorm.{ SQL, sqlToSimple, toParameterValue }
-import org.overviewproject.tree.orm.Document
-import org.overviewproject.postgres.SquerylEntrypoint._
 import org.overviewproject.persistence.orm.Schema.documents
+import org.overviewproject.postgres.SquerylEntrypoint._
+import org.overviewproject.tree.orm.Document
 
 /**
  * Writes out document information associated with the documentSetId
  */
 
 object DocumentWriter {
-  import org.overviewproject.persistence.orm.Schema.documents
 
   def write(document: Document) {
     documents.insert(document)
