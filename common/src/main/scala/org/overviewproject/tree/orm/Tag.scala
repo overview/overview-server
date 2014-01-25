c07f530f39556a5ce35e3b3b0cb18ff61d7bbe07
Jonas Karlsson <thejonas@gmail.com>
2014-01-24 20:06:03 -0500

    Make parameter order easier to work with

--- common/src/main/scala/org/overviewproject/tree/orm/Tag.scala
+++ common/src/main/scala/org/overviewproject/tree/orm/Tag.scala
@@ -1,14 +1,13 @@
 package org.overviewproject.tree.orm
 
-import org.squeryl.annotations.Column
 import org.squeryl.KeyedEntity
 import org.overviewproject.postgres.SquerylEntrypoint._
 
 case class Tag(
-  val id: Long = 0L,
-  @Column("document_set_id") val documentSetId: Long,
-  val name: String,
-  val color: String) extends KeyedEntity[Long] with DocumentSetComponent {
+  documentSetId: Long,
+  name: String,
+  color: String,
+  id: Long = 0L) extends KeyedEntity[Long] with DocumentSetComponent {
 
   override def isPersisted(): Boolean = id != 0L
 }
