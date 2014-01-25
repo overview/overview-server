9bcd5af56006d1449ad514a5d8365a9134880fd8
Jonas Karlsson <thejonas@gmail.com>
2014-01-24 19:18:30 -0500

    Make parameter order easier to work with

--- common/src/main/scala/org/overviewproject/tree/orm/UploadedFile.scala
+++ common/src/main/scala/org/overviewproject/tree/orm/UploadedFile.scala
@@ -1,18 +1,16 @@
 package org.overviewproject.tree.orm
 
 import java.sql.Timestamp
-import org.squeryl.annotations.Column
 import org.squeryl.KeyedEntity
 import org.overviewproject.postgres.SquerylEntrypoint._
 import java.util.Date
 
-
 case class UploadedFile(
-  id: Long = 0L,
-  @Column("uploaded_at") uploadedAt: Timestamp = new Timestamp(new Date().getTime),
-  @Column("content_disposition") contentDisposition: String,
-  @Column("content_type") contentType: String,
-  size: Long) extends KeyedEntity[Long] {
+    contentDisposition: String,
+    contentType: String,
+    size: Long,
+    uploadedAt: Timestamp = new Timestamp(new Date().getTime),
+    id: Long = 0L) extends KeyedEntity[Long] {
 
   // https://www.assembla.com/spaces/squeryl/tickets/68-add-support-for-full-updates-on-immutable-case-classes#/followers/ticket:68
   override def isPersisted(): Boolean = (id > 0)
