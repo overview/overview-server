7260e31ef03c5f6633392a0807a616ea3837ff94
Adam Hooper <adam@adamhooper.com>
2014-01-27 10:32:41 -0500

    Trim overview-server logging a bit. [finishes #64352364]

--- app/Global.scala
+++ app/Global.scala
@@ -1,8 +1,29 @@
-import play.api.GlobalSettings
-import play.api.mvc.{Handler,RequestHeader,WithFilters}
+import play.api.{GlobalSettings,Logger}
+import play.api.mvc._
 import play.filters.csrf.CSRFFilter
+import scala.concurrent.ExecutionContext.Implicits.global
 
-object Global extends WithFilters(CSRFFilter()) with GlobalSettings {
+object LoggingFilter extends Filter {
+  /* Straight from http://www.playframework.com/documentation/2.1.1/ScalaHttpFilters */
+  def apply(next: (RequestHeader) => Result)(rh: RequestHeader) = {
+    val start = System.currentTimeMillis
+
+    def logTime(result: PlainResult): Result = {
+      val time = System.currentTimeMillis - start
+      Logger.info(s"${rh.method} ${rh.uri} took ${time}ms and returned ${result.header.status}")
+      result.withHeaders("Request-Time" -> time.toString)
+    }
+
+    Logger.info(s"${rh.method} ${rh.uri} ...")
+
+    next(rh) match {
+      case plain: PlainResult => logTime(plain)
+      case async: AsyncResult => async.transform(logTime)
+    }
+  }
+}
+
+object Global extends WithFilters(LoggingFilter, CSRFFilter()) with GlobalSettings {
   private val HttpOverrideKey = "X-HTTP-Method-Override"
 
   override def onRouteRequest(request: RequestHeader): Option[Handler] = {
