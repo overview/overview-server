96613e4203842647f45ee205ae8f9e60ac6c0b0a
Jonas Karlsson <thejonas@gmail.com>
2014-01-30 17:32:15 -0500

    Make test pass with new database config

--- common/src/test/scala/org/overviewproject/database/DatabaseConfigurationSpec.scala
+++ common/src/test/scala/org/overviewproject/database/DatabaseConfigurationSpec.scala
@@ -11,51 +11,50 @@ import org.specs2.mutable.Specification
 import org.specs2.specification._
 
 class DatabaseConfigurationSpec extends Specification {
-  
+
   "DatabaseConfiguration properties" should {
 
-  
     "be read from datasource.default.url property, if set" in {
       val username = "overview"
-      val password = "overview"      
+      val password = "overview"
       val host = "localhost"
+      val port = 9010
       val database = "overview-test"
 
-      
       val systemDefault = sys.props.get("datasource.default.url")
-      
+
       val defaultDriver = "org.postgresql.Driver"
-      val expectedUrl = "jdbc:postgresql://"+host+"/"+database
-      
+      val expectedUrl = s"jdbc:postgresql://$host:$port/$database"
+
       val dbConfig = new SystemPropertiesDatabaseConfiguration()
 
       dbConfig.databaseUrl must beEqualTo(expectedUrl)
       dbConfig.username must beEqualTo(username)
       dbConfig.password must beEqualTo(password)
-      dbConfig.databaseDriver must beEqualTo(defaultDriver) 
+      dbConfig.databaseDriver must beEqualTo(defaultDriver)
     }
-    
+
     "handle real urls" in {
       val testDatabaseUrlProperty = "testdatasource.default.url"
 
       val username = "overview-user"
-      val password = "overview123-"      
+      val password = "overview123-"
       val host = "localhost.my-domain.org:1244"
       val database = "overview-test"
-     
-      val databaseUrl = "postgres://"+username+":"+password+"@"+host+"/"+database        
+
+      val databaseUrl = "postgres://" + username + ":" + password + "@" + host + "/" + database
       sys.props += testDatabaseUrlProperty -> databaseUrl
-      
-      val dbConfig = 
+
+      val dbConfig =
         new SystemPropertiesDatabaseConfiguration(defaultDatabaseUrlProperty = testDatabaseUrlProperty)
-      
+
       sys.props -= testDatabaseUrlProperty
-      val expectedUrl = "jdbc:postgresql://"+host+"/"+database
-      
+      val expectedUrl = "jdbc:postgresql://" + host + "/" + database
+
       dbConfig.databaseUrl must beEqualTo(expectedUrl)
       dbConfig.username must beEqualTo(username)
       dbConfig.password must beEqualTo(password)
-      
+
     }
   }
 
