87f4a0eb590d923d1c1268d4d3c4d9cfa2795fc9
Adam Hooper <adam@adamhooper.com>
2014-03-04 13:57:00 -0500

    Use Travis to test JS. [refs #66793774]

---
+++ auto/setup-grunt.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+npm install -g grunt-cli coffee-script
+((cd "$(dirname $0)/../test/assets/javascripts/autotest" && npm install))
--- auto/test-coffee.sh
+++ auto/test-coffee.sh
@@ -3,4 +3,4 @@
 # Before running this script, follow instructions in
 # test/assets/javascripts/autotest/README.md to set up your test environment.
 
-((cd test/assets/javascripts/autotest && grunt))
+((cd "$(dirname $0)/../test/assets/javascripts/autotest" && grunt))
