8174b55624ac96dbdc4f6b365085ddba091d496f
Adam Hooper <adam@adamhooper.com>
2014-03-04 13:37:16 -0500

    Make script to invoke grunt [refs #66793774]

---
+++ auto/test-coffee-once.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Before running this script, follow instructions in
+# test/assets/javascripts/autotest/README.md to set up your test environment.
+
+((cd test/assets/javascripts/autotest && grunt test))
