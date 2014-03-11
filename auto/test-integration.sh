048a1e4eb610a78df7041723fcd0706583d02ffe
Adam Hooper <adam@adamhooper.com>
2014-03-11 14:43:41 -0400

    Allow arguments for "npm test"

--- auto/test-integration.sh
+++ auto/test-integration.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# Runs integration tests. There must be an Overview server running at
+# http://localhost:9000.
+
 # Before running this script, run setup-integration-tests.sh in this
 # same directory to install dependencies. (If integration tests fail, please
 # re-run setup-integration-tests.sh before filing a bug, in case the bug has
 # to do with dependencies' versions.)
 
-((cd "$(dirname $0)/../test/integration" && npm test))
+((cd "$(dirname $0)/../test/integration" && npm test "$@"))
