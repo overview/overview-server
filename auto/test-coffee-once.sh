5acbc8faa0750ec888341b3a05702930b1227ef0
Jonas Karlsson <thejonas@gmail.com>
2014-03-12 11:02:06 -0400

    Remove double parens to work with old version of bash used by OS X

--- auto/test-coffee-once.sh
+++ auto/test-coffee-once.sh
@@ -3,4 +3,4 @@
 # Before running this script, follow instructions in
 # test/assets/javascripts/autotest/README.md to set up your test environment.
 
-((cd "$(dirname $0)/../test/assets/javascripts/autotest" && grunt test))
+(cd "$(dirname $0)/../test/assets/javascripts/autotest" && grunt test)
