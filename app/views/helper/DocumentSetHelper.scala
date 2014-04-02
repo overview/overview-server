1cf75c7cf58e8a0725f389b88bb9bb4760b80bde
Adam Hooper <adam@adamhooper.com>
2014-04-02 17:23:26 -0400

    Don't show waiting when processing has begun
    [finishes #60001830]

--- app/views/helper/DocumentSetHelper.scala
+++ app/views/helper/DocumentSetHelper.scala
@@ -2,7 +2,7 @@ package views.helper
 
 import play.api.i18n.Lang
 
-import org.overviewproject.tree.orm.DocumentSetCreationJob
+import org.overviewproject.tree.orm.{DocumentSetCreationJob, DocumentSetCreationJobState}
 
 object DocumentSetHelper {
   /**
@@ -26,7 +26,9 @@ object DocumentSetHelper {
    * @return A translated string, like "Clustering (4)"
    */
   def jobDescriptionMessage(job: DocumentSetCreationJob, nAheadInQueue: Long)(implicit lang: Lang): String = {
-    if (nAheadInQueue > 0) {
+    if (
+        (job.state == DocumentSetCreationJobState.NotStarted && nAheadInQueue > 0) ||
+        (job.state == DocumentSetCreationJobState.Preparing && job.statusDescription == "file_processing_not_started")) {
       views.Magic.t("views.ImportJob._documentSetCreationJob.jobs_to_process", nAheadInQueue)
     } else {
       jobDescriptionKeyToMessage(job.statusDescription)
---
+++ test/views/helper/DocumentSetHelperSpec.scala
@@ -0,0 +1,66 @@
+package views.helper
+
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
+import org.specs2.specification.{Scope, Step}
+import play.api.Play.{start,stop}
+import play.api.i18n.Lang
+import play.api.test.FakeApplication
+
+import org.overviewproject.tree.orm.{DocumentSetCreationJob, DocumentSetCreationJobState}
+
+class DocumentSetHelperSpec extends Specification with Mockito {
+  step(start(FakeApplication())) // to enable translation
+  // Neater would be to use dependency injection on views.Magic.t()....
+
+  trait BaseScope extends Scope {
+    implicit val lang = Lang("en")
+    val state = DocumentSetCreationJobState
+
+    val job = mock[DocumentSetCreationJob]
+    val nAheadInQueue = 0
+
+    def message = DocumentSetHelper.jobDescriptionMessage(job, nAheadInQueue)
+  }
+
+  "DocumentSetHelper" should {
+    "show waiting at the start" in new BaseScope {
+      job.state returns state.NotStarted
+      job.statusDescription returns "file_processing_not_started"
+      override val nAheadInQueue = 2
+
+      message must beEqualTo("Waiting for 2 jobs to complete before processing can begin")
+    }
+
+    "show waiting when preparing other jobs" in new BaseScope {
+      job.state returns state.Preparing
+      job.statusDescription returns "file_processing_not_started"
+      override val nAheadInQueue = 2
+
+      message must beEqualTo("Waiting for 2 jobs to complete before processing can begin")
+    }
+
+    "show processing when preparing" in new BaseScope {
+      job.state returns state.Preparing
+      job.statusDescription returns "processing_files:2:4"
+
+      message must beEqualTo("Extracting text from file 2/4")
+    }
+
+    "show retrieving when retrieving" in new BaseScope {
+      job.state returns state.InProgress
+      job.statusDescription returns "retrieving_documents:2:4"
+
+      message must beEqualTo("Storing file 2/4")
+    }
+
+    "show clustering when clustering" in new BaseScope {
+      job.state returns state.InProgress
+      job.statusDescription returns "clustering"
+
+      message must beEqualTo("Clustering")
+    }
+  }
+
+  step(stop)
+}
