3a9e00db0cfd39a9cbba0bfb59a913987da9d810
Jonas Karlsson <thejonas@gmail.com>
2014-04-18 09:57:51 -0400

    Submit job by setting state to NotStarted

--- documentset-worker/src/main/scala/org/overviewproject/jobhandler/filegroup/ClusteringJobQueue.scala
+++ documentset-worker/src/main/scala/org/overviewproject/jobhandler/filegroup/ClusteringJobQueue.scala
@@ -2,16 +2,40 @@ package org.overviewproject.jobhandler.filegroup
 
 import akka.actor.Actor
 import org.overviewproject.jobhandler.filegroup.ClusteringJobQueueProtocol.ClusterDocumentSet
+import org.overviewproject.database.Database
+import org.overviewproject.database.orm.finders.DocumentSetCreationJobFinder
+import org.overviewproject.tree.orm.DocumentSetCreationJobState._
+import org.overviewproject.database.orm.stores.DocumentSetCreationJobStore
+import akka.actor.Props
 
 trait ClusteringJobQueue extends Actor {
-	
+
   protected val storage: Storage
-  
+
   trait Storage {
-    def submitJob(documentSetId: Long)
+    def submitJob(documentSetId: Long): Unit
   }
-  
+
   def receive = {
     case ClusterDocumentSet(documentSetId) => storage.submitJob(documentSetId)
   }
+}
+
+class ClusteringJobQueueImpl extends ClusteringJobQueue {
+
+  override protected val storage: Storage = new DatabaseStorage
+
+  class DatabaseStorage extends Storage {
+    override def submitJob(documentSetId: Long) = Database.inTransaction {
+      val job = DocumentSetCreationJobFinder.byDocumentSetAndState(documentSetId, Preparing).headOption
+      job.map { j =>
+        DocumentSetCreationJobStore.insertOrUpdate(j.copy(state = NotStarted))
+      }
+    }
+  }
+}
+
+object ClusteringJobQueue {
+
+  def apply(): Props = Props[ClusteringJobQueueImpl]
 }
