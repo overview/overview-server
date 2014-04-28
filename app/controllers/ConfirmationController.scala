4279a355f9cfd324ee0c1ad6032373daf91c607d
Adam Hooper <adam@adamhooper.com>
2014-04-28 18:31:53 -0400

    Eliminate flash on confirm when using Intercom.
    [finishes #70209172]
    
    This is untested, because it's hard to imagine a test that's less
    fragile than the code itself.

--- app/controllers/ConfirmationController.scala
+++ app/controllers/ConfirmationController.scala
@@ -6,7 +6,7 @@ import play.api.mvc.Controller
 import controllers.auth.{OptionallyAuthorizedAction,AuthResults}
 import controllers.auth.Authorities.anyUser
 import controllers.util.TransactionAction
-import models.{MailChimp, OverviewUser}
+import models.{IntercomConfiguration, MailChimp, OverviewUser}
 import models.orm.Session
 import models.orm.stores.SessionStore
 
@@ -41,10 +41,27 @@ object ConfirmationController extends Controller {
             MailChimp.subscribe(u.email).getOrElse(Logger.info(s"Did not attempt requested subscription for ${u.email}"))
           }
 
-          AuthResults.loginSucceeded(request, session).flashing(
-            "success" -> m("show.success"),
-            "event" -> "confirmation-update"
-          )
+          val result = AuthResults.loginSucceeded(request, session)
+
+          // We have an Intercom welcome message -- see
+          // https://www.pivotaltracker.com/story/show/70209172
+          //
+          // So if we're using Intercom, don't flash a welcome.
+          //
+          // This is untested because the most likely source of user-facing
+          // error is a change to our Intercom settings, which our test suite
+          // can't control.
+          IntercomConfiguration.settingsForUser(savedUser.toUser) match {
+            case Some(_) =>
+              result.flashing(
+                "event" -> "confirmation-update"
+              )
+            case None =>
+              result.flashing(
+                "success" -> m("show.success"),
+                "event" -> "confirmation-update"
+              )
+          }
         }
         case None => BadRequest(views.html.Confirmation.show())
       }
