0fc4c908feba6005d90d3a3f90c2e609c1d24ea8
Adam Hooper <adam@adamhooper.com>
2014-04-29 14:34:10 -0400

    Fix AuthorizedBodyParser for single-user mode
    [refs #70126990]

--- app/controllers/auth/AuthorizedBodyParser.scala
+++ app/controllers/auth/AuthorizedBodyParser.scala
@@ -3,20 +3,35 @@ package controllers.auth
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc.{BodyParser,SimpleResult}
 import play.api.mvc.BodyParsers.parse
+import play.api.Play
 import scala.concurrent.Future
 
 import models.OverviewDatabase
 import models.orm.{Session, User}
 
-object AuthorizedBodyParser {
+trait AuthorizedBodyParser {
+  protected val sessionFactory: SessionFactory
+
   def apply[A](authority: Authority)(parserGenerator: User => BodyParser[A]): BodyParser[A] = {
     parse.using { implicit request =>
       OverviewDatabase.inTransaction {
-        SessionFactory.loadAuthorizedSession(request, authority) match {
+        sessionFactory.loadAuthorizedSession(request, authority) match {
           case Left(e) => parse.error(Future(e))
           case Right((session: Session, user: User)) => parserGenerator(user)
         }
       }
+    }
+  }
+}
+
+object AuthorizedBodyParser extends AuthorizedBodyParser {
+  private val isMultiUser = Play.current.configuration.getBoolean("overview.multi_user").getOrElse(true)
+
+  override val sessionFactory = {
+    if (isMultiUser) {
+      SessionFactory
+    } else {
+      SingleUserSessionFactory
     }
   }
 }
