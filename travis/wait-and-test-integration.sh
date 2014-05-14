4aee92f52f5bc9fd96e8b063506b35504e4fd616
Adam Hooper <adam@adamhooper.com>
2014-05-14 17:48:52 -0400

    Use a Maven/Ivy proxy to speed up Travis build

--- travis/wait-and-test-integration.sh
+++ travis/wait-and-test-integration.sh
@@ -4,7 +4,7 @@
 # then runs the given command.
 #
 # Why? Because this is the easiest way to get a Travis system up and running:
-# "./run --sh auto/wait-and-test-integration.sh". A better way would be for the
+# "./run --sh travis/wait-and-test-integration.sh". A better way would be for the
 # runner to poll port 9000 and only run the shell script when the server is up,
 # but that would take more work.
 
