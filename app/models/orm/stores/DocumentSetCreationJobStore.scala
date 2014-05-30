72a73380c0910f3d67d44b95f3fcc3028f884efb
Jonas Karlsson <thejonas@gmail.com>
2014-05-30 10:14:53 -0400

    Remove unused methods

--- app/models/orm/stores/DocumentSetCreationJobStore.scala
+++ app/models/orm/stores/DocumentSetCreationJobStore.scala
@@ -12,44 +12,6 @@ import models.OverviewDatabase
 import models.orm.Schema
 
 object DocumentSetCreationJobStore extends BaseStore(models.orm.Schema.documentSetCreationJobs) {
-  /**
-   * Deletes a DocumentSetCreationJob.
-   *
-   * This only works for a NotStarted or an Error job. If the job is already
-   * running, cancellation must be communicated to the worker, via cancel().
-   *
-   * Because of this complex logic, this method is not public. Use
-   * DocumentSetStore to manage a document set's lifecycle. Note, in
-   * particular, that DocumentSetStore selects the job FOR UPDATE.
-   */
-  private[stores] def deletePending(job: DocumentSetCreationJob): Unit = {
-    require(job.state == DocumentSetCreationJobState.NotStarted || job.state == DocumentSetCreationJobState.Error)
-
-    import org.overviewproject.postgres.SquerylEntrypoint._
-
-    from(Schema.documentSetCreationJobs)(dscj =>
-      where(dscj.id === job.id)
-        select (&(lo_unlink(dscj.contentsOid)))).toIterable // toIterable() executes it
-
-    Schema.documentSetCreationJobs.delete(job.id)
-  }
-
-  /**
-   * Tells the worker to delete a DocumentSetCreationJob and some parts of its
-   * associated DocumentSet.
-   *
-   * This only works for an InProgress or Cancelled job. If the job is not
-   * running, use deletePending() instead.
-   *
-   * Because of this complex logic, this method is not public. Use
-   * DocumentSetStore to manage a document set's lifecycle. Note, in
-   * particular, that DocumentSetStore selects the job FOR UPDATE.
-   */
-  private[stores] def cancel(job: DocumentSetCreationJob): Unit = {
-    require(job.state == DocumentSetCreationJobState.InProgress || job.state == DocumentSetCreationJobState.Cancelled)
-
-    insertOrUpdate(job.copy(state = DocumentSetCreationJobState.Cancelled))
-  }
 
   /**
    * Finds a cancellable job for the given document set, and sets its state to CANCELLED.
@@ -84,10 +46,10 @@ object DocumentSetCreationJobStore extends BaseStore(models.orm.Schema.documentS
   }
 
   override def delete(query: Query[DocumentSetCreationJob]): Int = {
-    throw new AssertionError("Do not delete() a DocumentSetCreationJob; cancel() or deletePending() it.")
+    throw new AssertionError("Do not delete() a DocumentSetCreationJob; deletion is handled by workers.")
   }
 
   override def delete[K](k: K)(implicit ked: KeyedEntityDef[DocumentSetCreationJob, K]): Unit = {
-    throw new AssertionError("Do not delete() a DocumentSetCreationJob; cancel() or deletePending() it.")
+    throw new AssertionError("Do not delete() a DocumentSetCreationJob; deletion is handled by workers.")
   }
 }
