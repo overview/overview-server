2869c1c27f2c32bcc14bcd07d2db909cf8a207a0
Jonas Karlsson <thejonas@gmail.com>
2014-06-10 17:04:01 -0400

    Add comment

--- documentset-worker/src/test/scala/org/overviewproject/test/ParameterStore.scala
+++ documentset-worker/src/test/scala/org/overviewproject/test/ParameterStore.scala
@@ -7,15 +7,19 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 import org.specs2.matcher.MatchResult
 
-
+/**
+ * When testing Actor behavior, Mockito can't be used to test calls to mocked out components (like database storage).
+ * If the mock update is occurring on a different thread than the test, the test result may become unpredictable.
+ * `ParameterStore` provides a synchronous mechanism to store and check method calls.
+ * `store` is used in the test method to store the parameters in the call. The `was*` methods wait for all outstanding 
+ * update requests to complete before checking the value.
+ */
 class ParameterStore[A] extends Specification {
   import ExecutionContext.Implicits.global
   
   private val storedParameters: Agent[Queue[A]] = Agent(Queue.empty)
   
   def store(parameters: A): Unit = storedParameters send (_.enqueue(parameters))
-  
-  def history: Future[Queue[A]] = storedParameters.future
   
   def wasCalledWith(parameters: A) = storedParameters.future must contain(parameters).await
   def wasCalledWithMatch[B](f: A => MatchResult[B]) = storedParameters.future must contain(f).await
