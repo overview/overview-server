8cad804dce2f324182aadde8682db1d370768712
Jonas Karlsson <thejonas@gmail.com>
2014-06-17 15:47:45 -0400

    Pass the whole upload

--- documentset-worker/src/main/scala/org/overviewproject/jobhandler/filegroup/task/CreateFile.scala
+++ documentset-worker/src/main/scala/org/overviewproject/jobhandler/filegroup/task/CreateFile.scala
@@ -1,42 +1,53 @@
 package org.overviewproject.jobhandler.filegroup.task
 
+import scala.util.control.Exception._
 import org.overviewproject.postgres.LargeObjectInputStream
 import org.overviewproject.tree.orm.File
 import java.io.InputStream
+import org.overviewproject.tree.orm.GroupedFileUpload
 
 trait CreateFile {
   val PdfMagicNumber: Array[Byte] = "%PDF".getBytes
-  
-  def apply(name: String, oid: Long): File = {
-    val stream = storage.getLargeObjectInputStream(oid)
-    
+
+  def apply(upload: GroupedFileUpload): File = {
+    val stream = storage.getLargeObjectInputStream(upload.contentsOid)
+
     val magicNumber = peekAtMagicNumber(stream)
-    
-    if (magicNumber.sameElements(PdfMagicNumber)) File(1, oid, oid, name)
-    else converter.createFileWithPdfView(name, oid, stream)
+
+    if (magicNumber.sameElements(PdfMagicNumber)) storage.createFile(upload.name, upload.contentsOid)
+    else converter.createFileWithPdfView(upload, stream)
 
   }
-  
+
   private def peekAtMagicNumber(inputStream: InputStream): Array[Byte] = {
     val magicNumberBytes = 4
     val allowedReadAhead = magicNumberBytes * 2
     val magicNumber = new Array[Byte](4)
-    
+
     inputStream.mark(allowedReadAhead)
     inputStream.read(magicNumber, 0, magicNumberBytes)
     inputStream.reset
-    
+
     magicNumber
   }
-  
-  protected val storage: Storage 
-  protected val converter: DocumentConverter
-  
-  protected trait Storage {
-    def getLargeObjectInputStream(oid: Long): InputStream 
+
+  private def withLargeObjectInputStream[T](oid: Long)(f: InputStream => T): T = {
+    val stream = storage.getLargeObjectInputStream(oid)
+
+    ultimately(stream.close) {
+      f(stream)
+    }
   }
-  
+
+  protected val storage: Storage
+  protected val converter: DocumentConverter
+
+  protected trait Storage {
+    def getLargeObjectInputStream(oid: Long): InputStream
+    def createFile(name: String, oid: Long): File
+  }
+
   protected trait DocumentConverter {
-    def createFileWithPdfView(name: String, contentsOid: Long, inputStream: InputStream): File
+    def createFileWithPdfView(upload: GroupedFileUpload, inputStream: InputStream): File
   }
 }
--- documentset-worker/src/test/scala/org/overviewproject/jobhandler/filegroup/task/CreateFileSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/jobhandler/filegroup/task/CreateFileSpec.scala
@@ -6,55 +6,61 @@ import java.io.ByteArrayInputStream
 import org.overviewproject.tree.orm.File
 import java.io.InputStream
 import org.specs2.specification.Scope
+import org.overviewproject.tree.orm.GroupedFileUpload
 
 class CreateFileSpec extends Specification with Mockito {
 
   "CreatePdfView" should {
 
     "return original oid if original is a pdf" in new PdfFileContext {
-      val file = createFile(name, oid)
+      val file = createFile(upload)
 
-      file.viewOid must be equalTo (oid)
-      file.contentsOid must be equalTo (oid)
+      there was one(createFile.storage).createFile(name, oid)
     }
 
     "convert rewound stream to pdf if original is not pdf" in new NoPdfFileContext {
-      val file = createFile(name, oid)
+      val file = createFile(upload)
 
-      file.viewOid must be equalTo (viewOid)
-      file.contentsOid must be equalTo (oid)
-
+      there was one(createFile.converter).createFileWithPdfView(upload, inputStream)
       inputStream.available must be equalTo(fileText.length)
     }
 
     trait CreateFileContext extends Scope {
       val fileText: String
-
+      val createdFile: File 
+      
       val name = "filename"
       val oid = 1l
+      val upload = smartMock[GroupedFileUpload]
+      upload.name returns name
+      upload.contentsOid returns oid
 
       lazy val inputStream = new ByteArrayInputStream(fileText.getBytes)
-      lazy val createFile: CreateFile = new TestCreateFile(inputStream)
+      lazy val createFile: TestCreateFile = new TestCreateFile(inputStream, createdFile)
     }
 
     trait PdfFileContext extends CreateFileContext {
       override val fileText: String = "%PDF and stuff"
+      override val createdFile: File = File(1, oid, oid, name)
     }
     
     trait NoPdfFileContext extends CreateFileContext {
       val viewOid = 2l
+      override val createdFile: File = File(1, oid, viewOid, name)
+      
       override val fileText: String = "Not a PDF"
     }
   }
 
-  class TestCreateFile(inputStream: InputStream) extends CreateFile {
+  class TestCreateFile(inputStream: InputStream, createdFile: File) extends CreateFile {
 
     override val storage = mock[Storage]
 
     storage.getLargeObjectInputStream(any) returns inputStream
-
+    storage.createFile(any, any) returns createdFile
+    
     override val converter = mock[DocumentConverter]
-    converter.createFileWithPdfView(any, any, any) returns File(1, 1l, 2l, "filename")
+    converter.createFileWithPdfView(any, any) returns createdFile
   }
 
 }
