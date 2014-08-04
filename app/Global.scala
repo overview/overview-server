5695cbbdd13d848d0818413b59731572eef95b2b
Adam Hooper <adam@adamhooper.com>
2014-08-04 16:16:12 -0400

    Handle OPTIONS requests to /api/*
    It's a bit of a hack, but that shouldn't cause problems.

--- app/Global.scala
+++ app/Global.scala
@@ -1,5 +1,6 @@
 import play.api.{GlobalSettings,Logger}
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.iteratee.Iteratee
 import play.api.mvc._
 import play.filters.csrf.CSRFFilter
 import scala.concurrent.Future
@@ -25,10 +26,51 @@ object LoggingFilter extends EssentialFilter {
   }
 }
 
-object Global extends WithFilters(LoggingFilter, CSRFFilter()) with GlobalSettings {
+object CorsFilter extends EssentialFilter {
+  private val CorsResponseHeaders = Seq(
+    "Access-Control-Allow-Origin" -> "*",
+    "Access-Control-Allow-Headers" -> "origin, authorization, content-type, accept",
+    "Access-Control-Allow-Credentials" -> "true"
+  )
+
+  def apply(nextFilter: EssentialAction) = new EssentialAction {
+    def apply(requestHeader: RequestHeader) = {
+      if (requestHeader.path.startsWith("/api/")) {
+        nextFilter(requestHeader).map(_.withHeaders(CorsResponseHeaders: _*))
+      } else {
+        nextFilter(requestHeader)
+      }
+    }
+  }
+}
+
+object Global extends WithFilters(LoggingFilter, CorsFilter, CSRFFilter()) with GlobalSettings {
   private val HttpOverrideKey = "X-HTTP-Method-Override"
 
+  private val CorsOptionsHeaders = Seq(
+    "Access-Control-Allow-Origin" -> "*",
+    "Access-Control-Allow-Methods" -> "DELETE, PUT, POST, GET, OPTIONS", // laziness
+    "Access-Control-Allow-Credentials" -> "true",
+    "Access-Control-Allow-Headers" -> "origin, authorization",
+    "Access-Control-Max-Age" -> "1728000"
+  )
+
+  private val optionsHandler = new EssentialAction {
+    def apply(requestHeader: RequestHeader) = {
+      Iteratee.ignore[Array[Byte]].map(Unit => Results.NoContent.withHeaders(
+        CorsOptionsHeaders: _*
+      ))
+    }
+  }
+
   override def onRouteRequest(request: RequestHeader): Option[Handler] = {
+    // Handle OPTIONS requests.
+    // This should be in Play. In the meantime, we just say every HTTP method
+    // works on any /api/* URL.
+    if (request.method == "OPTIONS" && request.path.startsWith("/api/")) {
+      return Some(optionsHandler)
+    }
+
     // Handle DELETE and PUT requests.
     // This should be in Play. See
     // http://play.lighthouseapp.com/projects/82401/tickets/761-work-with-x-http-method-override
