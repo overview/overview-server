5a94c22f48282bc41998c35fc44b0bfe9c0b9731
Jonas Karlsson <thejonas@gmail.com>
2014-09-18 17:01:35 -0400

    Write local file header

--- app/models/archive/streamingzip/Zip64LocalFileEntry.scala
+++ app/models/archive/streamingzip/Zip64LocalFileEntry.scala
@@ -2,16 +2,61 @@ package models.archive.streamingzip
 
 import models.archive.CRCInputStream
 import models.archive.ArchiveEntry
+import models.archive.streamingzip.HexByteString._
 import java.nio.charset.StandardCharsets
+import java.io.InputStream
+import java.io.ByteArrayInputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder._
 
 case class Zip64LocalFileEntry(fileName: String, fileSize: Long, data: CRCInputStream) {
   private val HeaderSize = 30
   private val ExtraFieldSize = 32
   private val DataDescriptorSize = 24
-
+  private val emptyField: Int = 0x00000000
+  
+  val signature: Int = 0x04034b50
+  val extractorVersion: Short = 10
+  val flags: Short = 0x0808 // bit 3: Use Data Descriptor, bit 11: Use UTF-8
+  val compression: Short = 0
+  val fileNameLength: Short = fileNameSize
+  def crc32 = data.crc32
+  
   def size: Long = HeaderSize + fileNameSize + ExtraFieldSize + fileSize + DataDescriptorSize
 
-  private def fileNameSize = fileName.getBytes(StandardCharsets.UTF_8).size
+  def stream: InputStream = headerStream
+  
+  private def fileNameSize = fileName.getBytes(StandardCharsets.UTF_8).size.toShort
+  
+  private def headerStream: InputStream = new ByteArrayInputStream(
+    writeInt(signature) ++
+    writeShort(extractorVersion) ++
+    writeShort(flags) ++
+    writeShort(compression) ++
+    writeShort(0) ++
+    writeShort(0) ++
+    writeInt(emptyField) ++
+    writeInt(emptyField) ++
+    writeInt(emptyField) ++
+    writeShort(fileNameLength) ++
+    writeShort(ExtraFieldSize.toShort)
+    
+  )
+  
+  private def writeInt(value: Int): Array[Byte] = {
+    val byteBuffer = ByteBuffer.allocate(4).order(LITTLE_ENDIAN)
+    byteBuffer.putInt(value).array()
+  }
+
+  private def writeShort(value: Short): Array[Byte] = {
+    val byteBuffer = ByteBuffer.allocate(2).order(LITTLE_ENDIAN)
+    byteBuffer.putShort(value).array()
+  }
+  
+  
+  
+    
+    
 }
 
 object Zip64LocalFileEntry {
--- test/models/archive/streamingzip/Zip64LocalFileEntrySpec.scala
+++ test/models/archive/streamingzip/Zip64LocalFileEntrySpec.scala
@@ -5,30 +5,67 @@ import org.specs2.mock.Mockito
 import models.archive.CRCInputStream
 import java.nio.charset.StandardCharsets
 import org.specs2.specification.Scope
+import models.archive.streamingzip.HexByteString._
 
 class Zip64LocalFileEntrySpec extends Specification with Mockito {
 
   "Zip64LocalFileEntry" should {
 
     "return size" in new FileEntryContext {
-      val fileName = "12334567890"
-
       val entry = new Zip64LocalFileEntry(fileName, fileSize, data)
 
       entry.size must be equalTo (entrySize(fileName.size))
     }
 
     "count UTF-8 filename size correctly" in new FileEntryContext {
-      val utf8Bytes = Array(0xd0, 0xad, 0xd0, 0x9c, 0xd0, 0x98, 0xd0, 0x9b, 0xd0, 0x98).map(_.toByte)
-      val utf8FileName = new String(utf8Bytes, StandardCharsets.UTF_8)
+      val utf8Bytes = "d0add09cd098d09bd098".hex
 
+      val utf8FileName = new String(utf8Bytes, StandardCharsets.UTF_8)
+      
       val entry = new Zip64LocalFileEntry(utf8FileName, fileSize, data)
 
       entry.size must be equalTo (entrySize(utf8FileName.size * 2))
 
     }
    
+    "set values" in new FileEntryContext {
+      val entry = new Zip64LocalFileEntry(fileName, fileSize, data)
+
+      entry.signature must be equalTo(0x04034b50)
+      entry.extractorVersion must be equalTo(10)
+      entry.flags must be equalTo(0x0808)
+      entry.compression must be equalTo(0)
+      entry.fileNameLength must be equalTo(fileName.length.toShort)
+    }
+    
+    "write header in stream" in new FileEntryContext {
+      val entry = new Zip64LocalFileEntry(fileName, fileSize, data)
+
+      val output = new Array[Byte](localFileHeaderSize)
+      
+      val n = entry.stream.read(output)
+      val expectedHeader = 
+        "504b0304" +     // signature
+        "0a00" +         // version
+        "0808" +         // flag
+        "0000" +         // compression
+        "0000" +         // time
+        "0000" +         // date
+        "00000000" +     // crc32
+        "00000000" +     // compressed size
+        "00000000" +     // uncompressed size
+        "0a00" +         // filename length
+        "2000"           // extra field length
+       
+        // don't check date and time fields
+      output.take(10) must be equalTo(expectedHeader.hex.take(10))
+      output.drop(14) must be equalTo(expectedHeader.hex.drop(14))
+    }
+    
+    
     trait FileEntryContext extends Scope {
+      val fileName = "1234567890"
+      
       val localFileHeaderSize = 30
       val extraFieldSize = 32
       val dataDescriptorSize = 24
@@ -37,7 +74,6 @@ class Zip64LocalFileEntrySpec extends Specification with Mockito {
       val data = mock[CRCInputStream]
       
       def entrySize(fileNameSize: Int) = localFileHeaderSize + extraFieldSize + dataDescriptorSize + fileSize + fileNameSize
-
     }
   }
 
