ff48dbc28ea8a6d2f038b478ff04d4f896df7100
Jonas Karlsson <thejonas@gmail.com>
2014-09-22 17:33:02 -0400

    Convert to DOS date format

--- app/models/archive/streamingzip/DosDate.scala
+++ app/models/archive/streamingzip/DosDate.scala
@@ -7,13 +7,13 @@ class DosDate(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: I
 
   val date: Int =
     day << 0 |
-    month << 5 |
-    (year - 1980) << 9
-    
-    val time: Int =
-      (second / 2) << 0 | 
-      minute << 5 |       
-      hour << 11          
+      month << 5 |
+      (year - 1980) << 9
+
+  val time: Int =
+    (second / 2) << 0 |
+      minute << 5 |
+      hour << 11
 
 }
 
@@ -25,4 +25,19 @@ object DosDate {
     date.get(HOUR_OF_DAY),
     date.get(MINUTE),
     date.get(SECOND))
+  
+  def toCalendar(date: Int, time: Int): Calendar = {
+    val year = ((date >> 9) & 0x007F) + 1980 
+    val month = (date >> 5) & 0x000F
+    val day = (date >> 0) & 0x001F
+    
+    val hour = (time >> 11) & 0x001F
+    val minute = (time >> 5) & 0x003F
+    val second = ((time >> 0) & 0x001F) * 2
+    
+    val convertedDate = Calendar.getInstance()
+    convertedDate.set(year, month, day, hour, minute, second)
+    
+    convertedDate
+  }
 }
--- test/models/archive/streamingzip/DosDateSpec.scala
+++ test/models/archive/streamingzip/DosDateSpec.scala
@@ -2,22 +2,44 @@ package models.archive.streamingzip
 
 import org.specs2.mutable.Specification
 import java.util.Calendar
+import java.util.Calendar._
 import HexByteString._
+import org.specs2.specification.Scope
 
 class DosDateSpec extends Specification {
   
   "DosDate" should {
     
-    "convert a calendar date to DOS date format" in {
+    "convert a calendar date to DOS date format" in new DosDateContext {
       
       val date = Calendar.getInstance()
       
-      date.set(1995, 10, 19, 16, 6, 32)
+      date.set(year, month, day, hour, minute, second)
+      
       val dosDate = DosDate(date)
 
-      dosDate.date must be equalTo 0x1F53 
-      dosDate.time must be equalTo 0x80D0 
+      dosDate.date must be equalTo dosDay 
+      dosDate.time must be equalTo dosTime 
     }
   }
 
+  "convert a DOS date to a calendar" in new DosDateContext {
+    
+    val date = DosDate.toCalendar(dosDay, dosTime)
+    
+    date.get(YEAR) must be equalTo year
+    date.get(MONTH) must be equalTo month
+    date.get(DAY_OF_MONTH) must be equalTo day
+    
+    date.get(HOUR_OF_DAY) must be equalTo hour
+    date.get(MINUTE) must be equalTo minute
+    date.get(SECOND) must be equalTo second
+  }
+  
+  trait DosDateContext extends Scope {
+    
+    val (year, month, day, hour, minute, second) = (1995, 10, 19, 16, 6, 32)
+    val dosDay = 0x1F53
+    val dosTime = 0x80D0
+  }
 }
