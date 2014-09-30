9ca4519ba722ada8e63c3b274edcb9cab5f08097
Adam Hooper <adam@adamhooper.com>
2014-09-30 10:34:42 -0400

    Add deps/redis/dev.sh to run Redis on port 9020

---
+++ deps/README.md
@@ -0,0 +1,5 @@
+Dependencies
+============
+
+The projects in these subdirectories have their own licenses. They are included
+here to ease distribution and deployment.
---
+++ deps/redis/dev.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Redis runner, for Mac OS X and Linux.
+#
+# Usage: `./dev.sh` (or call it from another directory).
+#
+# It will run with the config in `redis.conf`.
+
+VERSION="2.8.17"
+
+DIR="$(dirname "$0")"
+SRCDIR="$DIR/redis-$VERSION"
+
+(cd "$SRCDIR" && make redis-server && ./src/redis-server ../redis.conf)
