17aa185caf9ba351a7c43252044612c4f4f82ae6
Jonas Karlsson <thejonas@gmail.com>
2014-10-04 17:31:10 -0400

    Write file and data descriptor

--- app/models/archive/streamingzip/LocalFileEntry.scala
+++ app/models/archive/streamingzip/LocalFileEntry.scala
@@ -4,6 +4,10 @@ import java.io.InputStream
 import java.io.ByteArrayInputStream
 import java.util.Calendar
 import java.nio.charset.StandardCharsets
+import java.io.SequenceInputStream
+import scala.collection.JavaConverters._
+import java.util.zip.CheckedInputStream
+import java.util.zip.CRC32
 
 class LocalFileEntry(fileName: String, fileSize: Long, data: InputStream) extends LittleEndianWriter with ZipFormat {
 
@@ -11,9 +15,16 @@ class LocalFileEntry(fileName: String, fileSize: Long, data: InputStream) extend
   protected val flags = useDataDescriptor | useUTF8
   protected val timeStamp = DosDate(Calendar.getInstance())
   protected val fileNameLength = fileName.getBytes(StandardCharsets.UTF_8).size.toShort
-  
-  val stream: InputStream = headerStream
-    
+
+  protected val checkedData = new CheckedInputStream(data, new CRC32)
+  protected def crc32: Int = checkedData.getChecksum.getValue.toInt
+
+  val stream: InputStream = new SequenceInputStream(Iterator(
+    headerStream,
+    fileNameStream,
+    checkedData,
+    dataDescriptorStream).asJavaEnumeration)
+
   private def headerStream: InputStream =
     new ByteArrayInputStream(
       writeInt(localFileEntrySignature) ++
@@ -27,5 +38,13 @@ class LocalFileEntry(fileName: String, fileSize: Long, data: InputStream) extend
         writeInt(empty) ++
         writeShort(fileNameLength) ++
         writeShort(empty))
+
+  private def fileNameStream: InputStream = new ByteArrayInputStream(fileName.getBytes(StandardCharsets.UTF_8))
+
+  private def dataDescriptorStream: InputStream = new LazyByteArrayInputStream(
+    writeInt(dataDescriptorSignature) ++
+      writeInt(crc32) ++
+      writeLong(fileSize) ++
+      writeLong(fileSize))
 
 }
--- test/models/archive/streamingzip/LocalFileEntrySpec.scala
+++ test/models/archive/streamingzip/LocalFileEntrySpec.scala
@@ -7,49 +7,78 @@ import java.nio.ByteOrder._
 import java.util.zip.CRC32
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
-
+import java.util.Calendar
 
 class LocalFileEntrySpec extends Specification {
 
   "write header" in new LocalFileEntryContext {
     val output = readStream(entry.stream)
-    
+
     val expectedHeader = {
       writeInt(0x04034b50) ++
-      writeShort(10) ++
-      writeShort(0x0808) ++
-      writeShort(0) ++
-      writeShort(0) ++
-      writeShort(0) ++
-      writeInt(0) ++
-      writeInt(0) ++
-      writeInt(0) ++
-      writeShort(fileName.length) ++
-      writeShort(0)
+        writeShort(10) ++
+        writeShort(0x0808) ++
+        writeShort(0) ++
+        writeShort(0) ++
+        writeShort(0) ++
+        writeInt(0) ++
+        writeInt(0) ++
+        writeInt(0) ++
+        writeShort(fileName.length) ++
+        writeShort(0)
     }
-    
+
     output.take(10) must be equalTo expectedHeader.take(10)
-    output.drop(14) must be equalTo expectedHeader.drop(14)
+    output.slice(14, localFileHeaderSize) must be equalTo expectedHeader.drop(14)
   }
 
   "write date and time in stream" in new LocalFileEntryContext {
-    todo
+    val output = readStream(entry.stream)
+
+    val now = Calendar.getInstance()
+
+    val timeBytes = output.slice(10, 12)
+    val dateBytes = output.slice(12, 14)
+
+    val dosTime = bytesToInt(timeBytes)
+    val dosDate = bytesToInt(dateBytes)
+
+    val fileTime = DosDate.toCalendar(dosDate, dosTime)
+
+    now.getTimeInMillis must be closeTo (fileTime.getTimeInMillis, 5000)
+
   }
 
   "write filename" in new LocalFileEntryContext {
-    todo
+    val output = readStream(entry.stream)
+
+    output.slice(localFileHeaderSize, localFileHeaderSize + fileName.size) must be equalTo fileName.getBytes
+
   }
 
   "write file" in new LocalFileEntryContext {
-    todo
+    val output = readStream(entry.stream)
+
+    output.slice(localFileHeaderSize + fileName.size,
+      localFileHeaderSize + fileName.size + fileSize) must be equalTo fileData
   }
 
   "write data descriptor" in new LocalFileEntryContext {
-    todo
+    val output = readStream(entry.stream)
+
+    val expectedDataDescriptor =
+      writeInt(0x08074b50) ++ // signature
+        writeInt(crc32.toInt) ++ // crc32
+        writeLong(fileSize) ++ // original size
+        writeLong(fileSize) // compressed size 	
+
+    output.drop(localFileHeaderSize + fileName.size + fileSize) must be equalTo expectedDataDescriptor
   }
 
   trait LocalFileEntryContext extends Scope with LittleEndianWriter {
     val fileName = "1234567890"
+
+    val localFileHeaderSize = 30
 
     val fileSize = 100
     val fileData = Array.tabulate(fileSize)(_.toByte)
@@ -65,6 +94,16 @@ class LocalFileEntrySpec extends Specification {
 
     def readStream(stream: InputStream): Array[Byte] =
       Stream.continually(stream.read).takeWhile(_ != -1).map(_.toByte).toArray
+
+    // assumes 2 bytes in array
+    def bytesToInt(bytes: Array[Byte]): Int = {
+      val byteBuffer = ByteBuffer.allocate(4).order(LITTLE_ENDIAN)
+
+      byteBuffer.put(bytes)
+
+      byteBuffer.getShort(0)
+    }
+
   }
 
 }
