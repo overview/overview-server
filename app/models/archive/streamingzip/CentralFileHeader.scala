9536c76dd679e4c933f835af71a78d0cce92378a
Jonas Karlsson <thejonas@gmail.com>
2014-10-06 10:01:38 -0400

    Write zip32 central file header

---
+++ app/models/archive/streamingzip/CentralFileHeader.scala
@@ -0,0 +1,42 @@
+package models.archive.streamingzip
+
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import java.io.SequenceInputStream
+import java.nio.charset.StandardCharsets
+import scala.collection.JavaConverters._
+
+class CentralFileHeader(fileName: String, fileSize: Long, offset: Long, timeStamp: DosDate, crcFunction: => Int)
+    extends LittleEndianWriter with ZipFormat {
+
+  val versionMadeBy = unix | zipSpecification
+  val extractorVersion = defaultVersion
+  val flags = useUTF8
+  val fileNameSize = fileNameBytes.size
+
+  val stream: InputStream = new SequenceInputStream(Iterator(
+      headerStream,
+      fileNameStream).asJavaEnumeration)
+
+  private def headerStream = new LazyByteArrayInputStream(
+    writeInt(centralFileHeaderSignature) ++
+      writeShort(versionMadeBy) ++
+      writeShort(extractorVersion) ++
+      writeShort(flags) ++
+      writeShort(noCompression) ++
+      writeShort(timeStamp.time) ++
+      writeShort(timeStamp.date) ++
+      writeInt(crcFunction) ++
+      writeInt(fileSize.toInt) ++
+      writeInt(fileSize.toInt) ++
+      writeShort(fileNameSize) ++
+      writeShort(empty) ++
+      writeShort(empty) ++
+      writeShort(diskNumber) ++
+      writeShort(empty) ++
+      writeInt(empty) ++
+      writeInt(offset.toInt))
+  
+  private def fileNameBytes = fileName.getBytes(StandardCharsets.UTF_8)
+  private def fileNameStream: InputStream = new ByteArrayInputStream(fileNameBytes)
+}
---
+++ test/models/archive/streamingzip/CentralFileHeaderSpec.scala
@@ -0,0 +1,71 @@
+package models.archive.streamingzip
+
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+import java.util.zip.CRC32
+import java.io.InputStream
+import java.util.Calendar
+import java.util.zip.CheckedInputStream
+import java.io.ByteArrayInputStream
+
+
+class CentralFileHeaderSpec extends Specification {
+  
+  "CentralFileHeader" should {
+    
+    "write header in stream" in new CentralFileHeaderContext {
+      val crc = new CRC32
+      crc.update(data)
+      readStream(fileStream)
+
+      val output = readStream(centralFileHeader.stream)
+
+      val expectedHeader =
+        writeInt(0x02014b50) ++
+          writeShort(0x033F) ++
+          writeShort(10) ++
+          writeShort(0x0800) ++
+          writeShort(0) ++
+          writeShort(timeStamp.time.toShort) ++
+          writeShort(timeStamp.date.toShort) ++
+          writeInt(crc.getValue.toInt) ++
+          writeInt(fileSize) ++
+          writeInt(fileSize) ++
+          writeShort(fileName.length.toShort) ++
+          writeShort(0) ++
+          writeShort(0) ++
+          writeShort(0) ++
+          writeShort(0) ++
+          writeInt(0) ++
+          writeInt(offset)
+
+      output.take(fixedHeaderSize) must be equalTo expectedHeader
+    }
+    
+    "write fileName in stream" in new CentralFileHeaderContext {
+       val output = readStream(centralFileHeader.stream)
+       
+       output.drop(fixedHeaderSize) must be equalTo fileName.getBytes
+    }
+    
+    trait CentralFileHeaderContext extends Scope with LittleEndianWriter {
+      val fixedHeaderSize = 46
+      val extraFieldLength = 28
+      val extraFieldDataLength = extraFieldLength - 4 
+      
+      val offset = 0x12345678
+      val fileSize = 10
+      val timeStamp = DosDate(Calendar.getInstance())
+      val fileName = "file name"
+      val data = Array.fill[Byte](fileSize)(0xba.toByte)
+      val fileStream = new CheckedInputStream(new ByteArrayInputStream(data), new CRC32)
+
+      val centralFileHeader = new CentralFileHeader(fileName, fileSize, offset, timeStamp, fileStream.getChecksum.getValue.toInt)
+
+      def readStream(s: InputStream): Array[Byte] = Stream.continually(s.read).takeWhile(_ != -1).toArray.map(_.toByte)
+    }
+    
+    
+  }
+
+}
