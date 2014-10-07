819cd5c0825218b0efcc85337daa612904f3b85d
Adam Hooper <adam@adamhooper.com>
2014-10-07 16:11:07 -0400

    Yet another try at Travis

--- deps/redis/build.sh
+++ deps/redis/build.sh
@@ -7,5 +7,4 @@ VERSION="2.8.17"
 DIR="$(dirname "$0")"
 SRCDIR="$DIR/redis-$VERSION"
 
-(cd "$SRCDIR"/deps/jemalloc && ./configure && make) # Will this work in Travis?
 (cd "$SRCDIR" && bash -c "make redis-server")
