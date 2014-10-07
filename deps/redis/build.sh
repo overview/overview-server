942451b5978e7e26e050b650927f04a2f2f4d0e3
Adam Hooper <adam@adamhooper.com>
2014-10-07 14:20:05 -0400

    Try to work around a Travis error

--- deps/redis/build.sh
+++ deps/redis/build.sh
@@ -7,4 +7,4 @@ VERSION="2.8.17"
 DIR="$(dirname "$0")"
 SRCDIR="$DIR/redis-$VERSION"
 
-(cd "$SRCDIR" && make redis-server)
+(cd "$SRCDIR" && sh -c "make redis-server") # trying to work around Travis failure
