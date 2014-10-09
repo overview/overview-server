460c870ae9be59294937c674ead59b10dcc17f9a
Jonas Karlsson <thejonas@gmail.com>
2014-10-09 15:16:03 -0400

    Read from dynamically composed streams

--- app/models/archive/ComposedInputStream.scala
+++ app/models/archive/ComposedInputStream.scala
@@ -1,7 +1,39 @@
 package models.archive
 
 import java.io.InputStream
+import scala.collection.mutable.Queue
 
 abstract class ComposedInputStream extends InputStream {
+
+  override def read: Int = readCurrentStream(_.read)
+
+  override def read(buffer: Array[Byte]): Int = readCurrentStream(_.read(buffer))
+
+  override def read(buffer: Array[Byte], offset: Int, length: Int): Int =
+    readCurrentStream(_.read(buffer, offset, length))
+
+  protected var subStreamGenerators: List[() => InputStream]
+
+  private def currentStream: Option[InputStream] =
+    currentStreamValue.orElse(initializeStream)
+
+  private var currentStreamValue: Option[InputStream] = None
+
+  private def initializeStream: Option[InputStream] = {
+    currentStreamValue = subStreamGenerators.headOption.map(_())
+    currentStreamValue
+  }
+
+  private def readCurrentStream(readFn: InputStream => Int): Int = currentStream.map { s =>
+    val n = readFn(s)
+    if (n != -1) n
+    else continueToNextStream(readFn)
+  }.getOrElse(-1)
+
+  private def continueToNextStream(readFn: InputStream => Int): Int = {
+    subStreamGenerators = subStreamGenerators.tail
+    initializeStream
+    readCurrentStream(readFn)
+  }
 
 }
