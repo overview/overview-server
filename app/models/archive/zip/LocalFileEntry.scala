1ba13bcaea29f5ddfc37be424104e2b7e1b503f4
Jonas Karlsson <thejonas@gmail.com>
2014-10-10 09:51:56 -0400

    Calculate crc on demand

--- app/models/archive/zip/LocalFileEntry.scala
+++ app/models/archive/zip/LocalFileEntry.scala
@@ -1,8 +1,45 @@
 package models.archive.zip
 
 import models.archive.ArchiveEntry
+import java.io.InputStream
+import java.io.ByteArrayInputStream
+import java.util.zip.CRC32
 
-class LocalFileEntry(entry: ArchiveEntry)  {
+class LocalFileEntry(entry: ArchiveEntry) {
 
-  def crc: Option[Int] = None
+  def crc: Int = getOrComputeCrc
+
+  def stream: InputStream = {
+
+    new ByteArrayInputStream(Array.empty)
+  }
+
+  private val BufferSize = 8192
+
+  private var crcValue: Option[Int] = None
+
+  private def getOrComputeCrc: Int = crcValue.getOrElse {
+    crcValue = Some(computeCrc(entry.data()))
+    crcValue.get
+  }
+
+  private def computeCrc(input: InputStream): Int = {
+    val buffer = new Array[Byte](BufferSize)
+    val checker = new CRC32()
+    
+    def checkStream: Int = {
+      val n = input.read(buffer)
+      if (n == -1) checker.getValue.toInt
+      else {
+        checker.update(buffer.take(n))
+        checkStream
+      }
+    }
+    
+    checkStream
+  }
+  
+
+
+
 }
--- test/models/archive/zip/LocalFileEntrySpec.scala
+++ test/models/archive/zip/LocalFileEntrySpec.scala
@@ -5,38 +5,47 @@ import org.specs2.specification.Scope
 import java.io.InputStream
 import java.io.ByteArrayInputStream
 import models.archive.ArchiveEntry
+import java.util.zip.CRC32
 
 class LocalFileEntrySpec extends Specification {
-  
+
   "LocalFileEntry" should {
-    
-    "start with uninitialized crc" in new LocalFileContext {
+
+    "read stream to set crc when accessed" in new LocalFileContext {
       val localFileEntry = new LocalFileEntry(archiveEntry)
+
+      streamRequestedCount must be equalTo 0
+
+      localFileEntry.crc must be equalTo crc
       
-      localFileEntry.crc must beNone
+      streamRequestedCount must be equalTo 1
     }
-    
-    "set crc32 after generating stream" in {
-      todo
-    }
-    
+
     "write values to stream" in {
       todo
     }
-  
-    
-    
+
   }
 
 }
 
+trait LocalFileContext extends Scope {
 
-trait  LocalFileContext extends Scope {
-  
   val numberOfBytes = 52
   val data = Array.range(1, numberOfBytes).map(_.toByte)
-  def stream(): InputStream = new ByteArrayInputStream(data)
-  
+  val crc = {
+    val checker = new CRC32()
+    
+    checker.update(data)
+    checker.getValue.toInt
+  }
+  var streamRequestedCount = 0
+
+  def stream(): InputStream = {
+    streamRequestedCount += 1
+    new ByteArrayInputStream(data)
+  }
+
   val archiveEntry = ArchiveEntry(numberOfBytes, "filename", stream)
-  
+
 }
