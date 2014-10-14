53b9c16c7b443b05d02463ca5a83c80d53bea6d8
Adam Hooper <adam@adamhooper.com>
2014-10-14 19:00:16 -0400

    Export JUnit test results from sbt

--- project/Build.scala
+++ project/Build.scala
@@ -29,7 +29,8 @@ object ApplicationBuild extends Build with ProjectSettings {
   }
 
   val ourTestWithNoDbOptions = Seq(
-    Tests.Argument(TestFrameworks.Specs2, "xonly")/*,
+    Tests.Argument(TestFrameworks.Specs2, "xonly"),
+    Tests.Argument("junitxml", "console")/*,
     Tests.Setup { loader =>
       // Load Logger so configurations happen in the right order
       loader.loadClass("org.slf4j.LoggerFactory")
