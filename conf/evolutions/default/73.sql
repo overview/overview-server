a9f63cfb5d33440d8447c42da9d7ea6f4a06dfe2
Jonas Karlsson <thejonas@gmail.com>
2014-10-20 09:59:15 -0400

    Don't store size with page

--- conf/evolutions/default/73.sql
+++ conf/evolutions/default/73.sql
@@ -4,7 +4,6 @@ BEGIN;
 
 ALTER TABLE file ADD COLUMN contents_size BIGINT;
 ALTER TABLE file ADD COLUMN view_size BIGINT;
-ALTER TABLE page ADD COLUMN size BIGINT;
 
 COMMIT;
 
@@ -12,7 +11,6 @@ COMMIT;
 
 BEGIN;
 
-ALTER TABLE page DROP COLUMN size;
 ALTER TABLE file DROP COLUMN view_size;
 ALTER TABLE file DROP COLUMN contents_size;
 
