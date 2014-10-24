935c3a64fa6732711deab89aed7edeb46e3a3dbc
Jonas Karlsson <thejonas@gmail.com>
2014-10-24 10:04:17 -0400

    Test page entry values

--- test/models/archive/ArchiveEntryFactorySpec.scala
+++ test/models/archive/ArchiveEntryFactorySpec.scala
@@ -26,6 +26,9 @@ class ArchiveEntryFactorySpec extends Specification with Mockito {
 
       entry must beSome { e: ArchiveEntry =>
         e.name must be equalTo s"$name - page $pageNumber"
+        e.size must be equalTo pageSize
+        val s = e.data()
+        there was one(factory.mockStorage).pageDataStream(pageId)
       }
     }
     
@@ -86,9 +89,10 @@ class ArchiveEntryFactorySpec extends Specification with Mockito {
     override protected val storage = smartMock[Storage]
     storage.findFile(any) returns Some(file)
     storage.largeObjectInputStream(oid) returns smartMock[InputStream]
-
+    
     storage.findPageSize(any) returns pageSize
-
+    storage.pageDataStream(any) returns Some(smartMock[InputStream])
+    
     def mockStorage = storage
   }
 }
