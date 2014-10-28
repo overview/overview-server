4adc649d88144c26e80589de9747aa2dc4f9749b
Adam Hooper <adam@adamhooper.com>
2014-10-28 12:01:59 -0400

    Remove mocha-jenkins-reporter [finishes #81585550]

--- travis/wait-and-test-integration.sh
+++ travis/wait-and-test-integration.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Waits a certain amount of time (for things to start up, presumably) and
-# then runs the given command.
+# then runs integration tests, outputting to test/integration/test-results.xml.
 #
 # Why? Because this is the easiest way to get a Travis system up and running:
 # "./run --sh travis/wait-and-test-integration.sh". A better way would be for the
@@ -9,4 +9,4 @@
 # but that would take more work.
 
 sleep 30
-"$(dirname "$0")"/../auto/test-integration.sh
+(cd "$(dirname "$0")/../test/integration" && npm run-script test-xunit)
