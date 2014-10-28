cbb7c61c5399b215979d2b7be58967053f6dab39
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-28 02:55:58 +0000

    create a non-root user

--- znmeb-docker/build-image.bash
+++ znmeb-docker/build-image.bash
@@ -3,3 +3,4 @@
 docker run ubuntu 'apt-get update && apt-get upgrade'
 docker run ubuntu 'apt-get install postgresql git openjdk-jdk make gcc'
 docker run ubuntu 'apt-get install vim wget'
+docker run ubuntu 'useradd -m znmeb -G sudo'
