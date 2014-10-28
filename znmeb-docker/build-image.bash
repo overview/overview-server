77cb494fb69beba0053742116553f722f1ec22ff
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-28 02:18:16 +0000

    need make and gcc to build Redis

--- znmeb-docker/build-image.bash
+++ znmeb-docker/build-image.bash
@@ -1,4 +1,4 @@
 #! /bin/bash -v
 
 docker run ubuntu 'apt-get update && apt-get upgrade'
-docker run ubuntu 'apt-get install postgresql git openjdk-jdk'
+docker run ubuntu 'apt-get install postgresql git openjdk-jdk make gcc'
