c285e927c396beb88c41fb817098c1c373eafcdf
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-27 22:59:08 -0700

    absolute path, since we know it

--- znmeb-docker/build-image.bash
+++
@@ -1,6 +0,0 @@
-#! /bin/bash -v
-
-docker run ubuntu 'apt-get update && apt-get upgrade'
-docker run ubuntu 'apt-get install postgresql git openjdk-jdk make gcc'
-docker run ubuntu 'apt-get install vim wget'
-docker run ubuntu 'useradd -m znmeb -G sudo'
