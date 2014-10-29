0d91b68792e9a8b3486fbbe4c1f4bc7595f68cea
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-29 01:52:27 -0700

    override the start command

--- znmeb-docker/overview-developer/start.bash
+++ znmeb-docker/overview-developer/start.bash
@@ -2,4 +2,4 @@
 
 docker kill overview-developer
 docker rm overview-developer
-docker run -it -p 9000:9000 --name overview-developer overview-developer
+docker run -it -p 9000:9000 --name overview-developer overview-developer /bin/bash
