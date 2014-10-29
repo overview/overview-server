77284909139d1ca9583a0e12e78d4ea6169ac6be
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-29 03:27:41 -0700

    go back to interactive

--- znmeb-docker/overview-developer/start.bash
+++ znmeb-docker/overview-developer/start.bash
@@ -2,4 +2,4 @@
 
 docker kill overview-developer
 docker rm overview-developer
-docker run -d -p 9000:9000 --name overview-developer overview-developer sh -c 'while true; do sleep 15; done'
+docker run -it -p 9000:9000 --name overview-developer overview-developer /bin/bash
