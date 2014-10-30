a4d3c9eb9ecee65734134703da37942e4083b571
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-30 02:07:41 -0700

    script to install Overview dev dependencies

---
+++ znmeb-docker/overview-developer/trusty-workstation.bash
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install -y \
+  build-essential \
+  docker.io \
+  openjdk-7-jdk \
+  postgresql \
+  unzip \
+  vim \
+  zip
+git config --global push.default simple
+git config --global user.email "znmeb@znmeb.net"
+git config --global user.name "M. Edward (Ed) Borasky"
