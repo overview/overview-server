a8cae694d76d043bfa218faaa9e378c18bf3fd2c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-31 02:20:16 -0700

    working - but it'll get factored out tomorrow

--- znmeb-docker/overview-release/run.bash
+++ znmeb-docker/overview-release/run.bash
@@ -2,4 +2,18 @@
 
 docker kill overview-release
 docker rm overview-release
-docker run -it -p 9000:9000 --name overview-release overview-release /bin/bash
+
+# run the developer-built image to export the release zipfile
+docker run -d \
+  --name=overview-developer-built \
+  -v /home/overview \
+  overview-developer-built \
+  sh -c "while true; do sleep 15; done"
+
+# run the release interactively
+docker run -it \
+  -p 9000:9000 \
+  --name overview-release \
+  --volumes-from overview-developer-built \
+  overview-release \
+  /bin/bash
