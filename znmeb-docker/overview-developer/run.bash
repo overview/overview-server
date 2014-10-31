830fea6a3061cb22d27a9b915e20cee930716b78
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-30 23:16:16 -0700

    volume mount for release zipfile

--- znmeb-docker/overview-developer/run.bash
+++ znmeb-docker/overview-developer/run.bash
@@ -2,4 +2,9 @@
 
 docker kill overview-developer
 docker rm overview-developer
-docker run -it -p 9000:9000 --name overview-developer overview-developer /bin/bash
+docker run -it \
+  -p 9000:9000 \
+  --name="overview-developer" \
+  -v ../overview-release/:/home/overview/overview-release/ \
+  overview-developer \
+  /bin/bash
