f7b9884b9068636705660956cdb2b64b6bdb9652
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-11-02 19:30:45 -0800

    smoke test time

---
+++ znmeb-release/Dockerfiles/release/run.bash
@@ -0,0 +1,24 @@
+#! /bin/bash -v
+#
+# Copyright (C) 2013 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+# log in first
+docker login
+
+# build a temporary image with the unpacked zipfile
+docker build -t temp .
+
+# now make the release
+docker rm make-release
+docker run -it \
+  --name="make-release" \
+  temp
+
+docker commit make-release znmeb/overview-release
