6a130c1ab2deaba7bb628dea332a4588ea2b1abc
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-11-03 18:01:13 -0800

    cleanup

--- znmeb-release/Dockerfiles/source/run.bash
+++ znmeb-release/Dockerfiles/source/run.bash
@@ -9,5 +9,4 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-# always fetch the source
-docker build --no-cache=true -t znmeb/overview-source .
+docker build -t znmeb/overview-source .
