a0cbb943adb5d567e15e5254420e686878c84ce6
Jonas Karlsson <thejonas@gmail.com>
2014-11-11 15:56:53 -0500

    create objects with storage

--- app/models/archive/FileViewInfo.scala
+++ app/models/archive/FileViewInfo.scala
@@ -2,7 +2,7 @@ package models.archive
 
 import java.io.InputStream
 
-case class FileViewInfo(documentTitle: String, viewOid: Long, size: Long) 
+case class FileViewInfo(documentTitle: String, viewOid: Long, size: Long)
 
 abstract class FileViewInfo1(documentTitle: String, viewOid: Long, size: Long) extends DocumentViewInfo {
 
@@ -10,9 +10,28 @@ abstract class FileViewInfo1(documentTitle: String, viewOid: Long, size: Long) e
     ArchiveEntry(asPdf(removePdf(documentTitle)), size, largeObjectInputStream(viewOid) _)
 
   private def largeObjectInputStream(oid: Long)(): InputStream = storage.largeObjectInputStream(oid)
-  
+
   protected val storage: Storage
   protected trait Storage {
     def largeObjectInputStream(oid: Long): InputStream
+  }
+}
+
+object FileViewInfo1 {
+  import controllers.util.PlayLargeObjectInputStream
+
+  private val LOBufferSize = 1024 * 1024
+  
+  def apply(documentTitle: String, viewOid: Long, size: Long): FileViewInfo1 =
+    new DbFileViewInfo(documentTitle, viewOid, size)
+
+  private class DbFileViewInfo(documentTitle: String, viewOid: Long, size: Long)
+      extends FileViewInfo1(documentTitle, viewOid, size) {
+
+    override protected val storage = new DbStorage
+
+    protected class DbStorage extends Storage {
+      override def largeObjectInputStream(oid: Long): InputStream = new PlayLargeObjectInputStream(oid, LOBufferSize)
+    }
   }
 }
--- app/models/archive/PageViewInfo.scala
+++ app/models/archive/PageViewInfo.scala
@@ -6,19 +6,41 @@ case class PageViewInfo(documentTitle: String, pageNumber: Int, pageId: Long, si
 
 abstract class PageViewInfo1(documentTitle: String, pageNumber: Int, pageId: Long, size: Long) extends DocumentViewInfo {
 
-  def archiveEntry: ArchiveEntry = 
+  def archiveEntry: ArchiveEntry =
     ArchiveEntry(fileNameWithPage(removePdf(documentTitle), pageNumber), size, pageDataStream(pageId) _)
 
-    
   private def fileNameWithPage(fileName: String, pageNumber: Int): String =
     asPdf(s"$fileName p$pageNumber")
 
-    
   private def pageDataStream(pageId: Long)(): InputStream =
     storage.pageDataStream(pageId).get
 
   protected val storage: Storage
   protected trait Storage {
     def pageDataStream(pageId: Long): Option[InputStream]
+  }
+}
+
+object PageViewInfo1 {
+  import java.io.ByteArrayInputStream
+  import models.OverviewDatabase
+  import org.overviewproject.database.Slick.simple._
+  import org.overviewproject.models.tables.Pages
+
+  def apply(documentTitle: String, pageNumber: Int, pageId: Long, size: Long): PageViewInfo1 =
+    new DbPageViewInfo(documentTitle, pageNumber, pageId, size)
+
+  private class DbPageViewInfo(documentTitle: String, pageNumber: Int, pageId: Long, size: Long)
+      extends PageViewInfo1(documentTitle, pageNumber, pageId, size) {
+
+    override protected val storage = new DbStorage
+
+    protected class DbStorage extends Storage {
+      override def pageDataStream(pageId: Long): Option[InputStream] =
+        OverviewDatabase.withSlickSession { implicit session =>
+          val q = Pages.filter(_.id === pageId).map(_.data)
+          q.firstOption.map(new ByteArrayInputStream(_))
+        }
+    }
   }
 }
