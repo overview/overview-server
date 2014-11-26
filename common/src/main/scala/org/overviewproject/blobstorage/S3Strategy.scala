76ca99e793c82a668f2491fabdf37e19a5d41bfa
Adam Hooper <adam@adamhooper.com>
2014-11-26 15:26:13 -0500

    Add S3Strategy for blob storage
    [finishes #83429790]

--- common/src/main/scala/org/overviewproject/blobstorage/S3Strategy.scala
+++ common/src/main/scala/org/overviewproject/blobstorage/S3Strategy.scala
@@ -1,14 +1,166 @@
 package org.overviewproject.blobstorage
 
+import com.amazonaws.services.s3.{AmazonS3,AmazonS3Client}
+import com.amazonaws.services.s3.transfer.TransferManager
+import com.amazonaws.services.s3.model.{AmazonS3Exception,DeleteObjectsRequest,ObjectMetadata}
+import com.amazonaws.event.{ProgressEvent,ProgressEventType,ProgressListener}
 import java.io.InputStream
+import java.nio.file.Files
+import java.util.UUID
 import play.api.libs.iteratee.Enumerator
-import scala.concurrent.Future
+import scala.concurrent.{Future,Promise,blocking}
+import scala.util.Try
 
+/** Uses S3 to send and receive files.
+  *
+  * Right now, this is implemented using the Java SDK. Straight HTTP requests
+  * would probably save us some threads and temporary files.
+  */
 trait S3Strategy extends BlobStorageStrategy {
-  override def get(location: String): Future[Enumerator[Array[Byte]]] = ???
-  override def delete(location: String): Future[Unit] = ???
-  override def create(locationPrefix: String, inputStream: InputStream, nBytes: Long): Future[String] = ???
+  protected val s3: AmazonS3
+  protected val transferManager: TransferManager
+
+  private case class Location(bucket: String, key: String)
+  private val LocationRegex = """^s3:([-\w]+):([-\w]+)$""".r
+  private def stringToLocation(s: String) = s match {
+    case LocationRegex(bucket, key) => Location(bucket, key)
+    case _ => throw new IllegalArgumentException("Invalid location string: '" + s + "'")
+  }
+
+  private case class LocationPrefix(bucket: String)
+  private val LocationPrefixRegex = """^s3:([-\w]+)$""".r
+  private def stringToLocationPrefix(s: String) = s match {
+    case LocationPrefixRegex(bucket) => LocationPrefix(bucket)
+    case _ => throw new IllegalArgumentException("Invalid location prefix: '" + s + '"')
+  }
+
+  override def get(locationString: String): Future[Enumerator[Array[Byte]]] = {
+    /*
+     * We'll download to a temporary file, then create an Enumerator from it.
+     *
+     * Why? Well ... AWS offers an interface to grab an InputStream, but that's
+     * a blocking interface so it doesn't help much. And we can't listen to the
+     * number of bytes read in a ProgressEvent: TransferManager downloads
+     * chunks of a file in parallel, so we can't know how many bytes of the
+     * file are _sequentially_ readable.
+     *
+     * There are a few spots this method blocks, because we think the
+     * consequences are minor and the time savings are substantial:
+     *
+     * - We determine a download filename by creating it.
+     * - We delete the download file using File.delete()
+     * - We use Enumerator.fromFile() (this one isn't our bad, really....)
+     *
+     * We also assume the OS allows deleting an open file.
+     */
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val location = stringToLocation(locationString)
+
+    // This is blocking, but /tmp ought to be very fast
+    val filePath = Files.createTempFile("overview-s3-transfer-", ".tmp")
+    val file = filePath.toFile
+
+    val download = transferManager.download(location.bucket, location.key, file)
+
+    val promise = Promise[Enumerator[Array[Byte]]]()
+
+    download.addProgressListener(new ProgressListener {
+      override def progressChanged(event: ProgressEvent) = event.getEventType match {
+        case ProgressEventType.TRANSFER_COMPLETED_EVENT => {
+          val ret = Enumerator.fromFile(file)
+          file.delete
+          promise.success(ret)
+        }
+        case ProgressEventType.TRANSFER_FAILED_EVENT => {
+          file.delete
+          promise.failure(blocking(download.waitForException()))
+        }
+        case _ => ()
+      }
+    })
+
+    promise.future
+  }
+
+  override def delete(location: String): Future[Unit] = deleteMany(Seq(location))
+
+  override def deleteMany(locationStrings: Seq[String]): Future[Unit] = {
+    /*
+     * Send delete requests to S3, grouped by bucket, max 1000 per request.
+     *
+     * If any one fails, the others will still continue but we'll throw an
+     * exception. That meets the API promise.
+     *
+     * Amazon's Java SDK does not provide a non-blocking way to do this. We
+     * may wish to convert to its REST API to avoid that shortcoming.
+     */
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    val MaxKeysPerRequest = 1000
+
+    val locations = locationStrings.map(stringToLocation)
+    val requests: Seq[DeleteObjectsRequest] = locations
+      .groupBy(_.bucket)       // Map[bucketName:String,locations:Seq[Location]]
+      .mapValues(_.map(_.key)) // Map[bucketName:String,keys:Seq[String]]
+      .toSeq                   // Seq[(bucketName:String,keys:Seq[String])]
+      .flatMap({ (bucket: String, keys: Seq[String]) =>
+        keys.grouped(MaxKeysPerRequest).map((group) => (bucket, group))
+        // Seq[(bucketName:String,keyGroup:Seq[String])], max 1000 keys per group
+      }.tupled)
+      .map({ (bucket: String, keyGroup: Seq[String]) =>
+        new DeleteObjectsRequest(bucket).withKeys(keyGroup: _*)
+      }.tupled)
+
+    Future.traverse(requests) { (request: DeleteObjectsRequest) => Future[Unit] { blocking {
+      try {
+        s3.deleteObjects(request)
+      } catch {
+        case ex: AmazonS3Exception if ex.getStatusCode == 404 =>
+      }
+    }}}
+      .map((_) => ()) // Seq[Unit] => Unit. side-feature: it looks like a wink
+  }
+
+  override def create(locationPrefixString: String, inputStream: InputStream, nBytes: Long): Future[String] = {
+    /*
+     * We generate a random filename and upload to it.
+     */
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    val locationPrefix = stringToLocationPrefix(locationPrefixString)
+    val key = UUID.randomUUID.toString
+
+    val metadata = new ObjectMetadata()
+    metadata.setContentLength(nBytes)
+
+    val promise = Promise[String]()
+    val upload = transferManager.upload(locationPrefix.bucket, key, inputStream, metadata)
+
+    def complete: Unit = {
+      val result = Try(blocking {
+        upload.waitForCompletion()
+        s"s3:${locationPrefix.bucket}:${key}"
+      })
+      promise.complete(result)
+    }
+
+    upload.addProgressListener(new ProgressListener {
+      override def progressChanged(event: ProgressEvent) = event.getEventType match {
+        case ProgressEventType.TRANSFER_COMPLETED_EVENT => complete
+        case ProgressEventType.TRANSFER_FAILED_EVENT => complete
+        case _ => ()
+      }
+    })
+
+    promise.future
+  }
 }
 
 object S3Strategy extends S3Strategy {
+  override lazy val s3 = {
+    val awsCredentials = BlobStorageConfig.awsCredentials
+    new AmazonS3Client(awsCredentials)
+  }
+
+  override lazy val transferManager = new TransferManager(s3)
 }
---
+++ common/src/test/scala/org/overviewproject/blobstorage/S3StrategySpec.scala
@@ -0,0 +1,243 @@
+package org.overviewproject.blobstorage
+
+import com.amazonaws.AmazonClientException
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.{AmazonS3Exception,DeleteObjectsRequest,ObjectMetadata}
+import com.amazonaws.services.s3.transfer.{Download,TransferManager,Upload}
+import com.amazonaws.event.{ProgressEvent,ProgressEventType,ProgressListener}
+import java.io.{File,InputStream}
+import java.nio.file.Files
+import org.specs2.matcher.{Expectable,Matcher}
+
+class S3StrategySpec extends StrategySpecification {
+  class DeleteManyRequestLike(bucket: String, keys: Seq[String]) extends Matcher[DeleteObjectsRequest] {
+    def apply[S <: DeleteObjectsRequest](req: Expectable[S]) = {
+      import scala.collection.JavaConverters.asScalaBufferConverter
+      result(
+        bucket == req.value.getBucketName && keys == req.value.getKeys.asScala.toSeq.map(_.getKey),
+        s"${req.description} is the correct request",
+        s"${req.description} has the wrong bucket (should be ${bucket}) or the wrong keys (should be ${keys.toString})",
+        req
+      )
+    }
+  }
+  def deleteManyRequestLike(bucket: String, keys: Seq[String]) = new DeleteManyRequestLike(bucket, keys)
+
+  class MockTransferManager extends TransferManager {
+    case class LastDownload(bucket: String, key: String, file: File)
+    case class LastUpload(bucket: String, key: String, inputStream: InputStream, metadata: ObjectMetadata)
+
+    var lastDownload: Option[LastDownload] = None
+    var lastUpload: Option[LastUpload] = None
+    var lastProgressListener: Option[ProgressListener] = None
+    var lastException: Option[AmazonClientException] = None
+
+    def succeed = {
+      lastProgressListener.foreach { pl =>
+        pl.progressChanged(new ProgressEvent(ProgressEventType.TRANSFER_COMPLETED_EVENT))
+      }
+    }
+
+    def fail(ex: AmazonClientException) = {
+      lastException = Some(ex)
+      lastProgressListener.foreach { pl =>
+        pl.progressChanged(new ProgressEvent(ProgressEventType.TRANSFER_FAILED_EVENT))
+      }
+    }
+
+    override def download(bucket: String, key: String, file: File) = {
+      lastDownload = Some(LastDownload(bucket, key, file))
+      val ret = mock[Download]
+      ret.addProgressListener(any[ProgressListener]) answers { (pl) => lastProgressListener = Some(pl.asInstanceOf[ProgressListener]) }
+      ret.waitForException answers { (_) => lastException.get }
+      ret.waitForCompletion answers { (_) => if (!lastException.isEmpty) throw lastException.get }
+      ret
+    }
+
+    override def upload(bucket: String, key: String, inputStream: InputStream, metadata: ObjectMetadata) = {
+      lastUpload = Some(LastUpload(bucket, key, inputStream, metadata))
+      val ret = mock[Upload]
+      ret.addProgressListener(any[ProgressListener]) answers { (pl) => lastProgressListener = Some(pl.asInstanceOf[ProgressListener]) }
+      ret.waitForException answers { (_) => lastException.get }
+      ret.waitForCompletion answers { (_) => if (!lastException.isEmpty) throw lastException.get }
+      ret
+    }
+  }
+
+  trait S3BaseScope extends BaseScope {
+    val mockS3 = mock[AmazonS3]
+    val mockTransferManager = new MockTransferManager
+    object TestStrategy extends S3Strategy {
+      override val s3 = mockS3
+      override val transferManager = mockTransferManager
+    }
+  }
+
+  "#get" should {
+    "fail when the location is invalid" in new S3BaseScope {
+      TestStrategy.get("foo:bar") must throwA[IllegalArgumentException]
+      TestStrategy.get("s3::bar") must throwA[IllegalArgumentException]
+      TestStrategy.get("s3:foo:") must throwA[IllegalArgumentException]
+      TestStrategy.get("s3:foo:bar:baz") must throwA[IllegalArgumentException]
+    }
+
+    "provide contents when the tempfile is full" in new S3BaseScope {
+      val futureEnumerator = TestStrategy.get("s3:foo:bar")
+      Files.write(mockTransferManager.lastDownload.get.file.toPath, "file contents".getBytes("utf-8"))
+      mockTransferManager.succeed
+      val enumerator = await(futureEnumerator)
+      consume(enumerator) must beEqualTo("file contents".getBytes("utf-8"))
+    }
+
+    "delete the file after enumerating" in new S3BaseScope {
+      val futureEnumerator = TestStrategy.get("s3:foo:bar")
+      mockTransferManager.succeed
+      val enumerator = await(futureEnumerator)
+      consume(enumerator)
+      mockTransferManager.lastDownload.get.file.exists must beFalse
+    }
+
+    "delete the file before enumerator is consumed, in case enumeration fails" in new S3BaseScope {
+      // This won't work on Windows. It _will_ work with Docker.
+      val futureEnumerator = TestStrategy.get("s3:foo:bar")
+      mockTransferManager.succeed
+      val enumerator = await(futureEnumerator)
+      mockTransferManager.lastDownload.get.file.exists must beFalse
+    }
+
+    "fail when the transfer fails" in new S3BaseScope {
+      val ex = new AmazonS3Exception("boo")
+      val futureEnumerator = TestStrategy.get("s3:foo:bar")
+      mockTransferManager.fail(ex)
+      await(futureEnumerator) must throwA(ex)
+    }
+
+    "delete the file when the transfer fails" in new S3BaseScope {
+      val ex = new AmazonS3Exception("boo")
+      val futureEnumerator = TestStrategy.get("s3:foo:bar")
+      mockTransferManager.fail(ex)
+      await(futureEnumerator) must throwA[Exception] // "must throwA" to ignore the exception
+      mockTransferManager.lastDownload.get.file.exists must beFalse
+    }
+  }
+
+  "#delete" should {
+    "fail when the location is invalid" in new S3BaseScope {
+      TestStrategy.delete("foo:bar") must throwA[IllegalArgumentException]
+      TestStrategy.delete("s3::bar") must throwA[IllegalArgumentException]
+      TestStrategy.delete("s3:foo:") must throwA[IllegalArgumentException]
+      TestStrategy.delete("s3:foo:bar:baz") must throwA[IllegalArgumentException]
+    }
+
+    "succeed on success" in new S3BaseScope {
+      await(TestStrategy.delete("s3:foo:bar")) must beEqualTo(())
+      there was one(mockS3).deleteObjects(deleteManyRequestLike("foo", Seq("bar")))
+    }
+
+    "succeed when the bucket does not exist" in new S3BaseScope {
+      // S3 returns a 404 when the bucket does not exist
+      val ex = new AmazonS3Exception("boo")
+      ex.setStatusCode(404)
+      mockS3.deleteObjects(any) throws ex
+      await(TestStrategy.delete("s3:foo:bar")) must beEqualTo(())
+    }
+
+    "succeed when the object does not exist" in new S3BaseScope {
+      // S3 already succeeds when the object does not exist
+    }
+  }
+
+  "#deleteMany" should {
+    "fail when one location is invalid" in new S3BaseScope {
+      TestStrategy.deleteMany(Seq("s3:foo:bar", "foo:bar")) must throwA[IllegalArgumentException]
+      TestStrategy.deleteMany(Seq("s3:foo:bar", "s3::bar")) must throwA[IllegalArgumentException]
+      TestStrategy.deleteMany(Seq("s3:foo:bar", "s3:foo:")) must throwA[IllegalArgumentException]
+      TestStrategy.deleteMany(Seq("s3:foo:bar", "s3:foo:bar:baz")) must throwA[IllegalArgumentException]
+    }
+
+    "succeed on success" in new S3BaseScope {
+      await(TestStrategy.deleteMany(Seq("s3:foo:bar", "s3:foo:baz"))) must beEqualTo(())
+      there was one(mockS3).deleteObjects(deleteManyRequestLike("foo", Seq("bar", "baz")))
+    }
+
+    "succeed when deleting from multiple buckets" in new S3BaseScope {
+      await(TestStrategy.deleteMany(Seq("s3:foo:bar", "s3:bar:baz"))) must beEqualTo(())
+      there was one(mockS3).deleteObjects(deleteManyRequestLike("foo", Seq("bar")))
+      there was one(mockS3).deleteObjects(deleteManyRequestLike("bar", Seq("baz")))
+    }
+
+    "batch requests by 1,000" in new S3BaseScope {
+      // 1,000 is the Amazon-documented limit
+      val keys1 = Range(0, 1000).toSeq.map(_.toString)
+      val keys2 = Range(1000, 1002).toSeq.map(_.toString)
+
+      val locations = (keys1 ++ keys2).map((s) => "s3:foo:" + s)
+
+      await(TestStrategy.deleteMany(locations)) must beEqualTo(())
+      there was one(mockS3).deleteObjects(deleteManyRequestLike("foo", keys1))
+      there was one(mockS3).deleteObjects(deleteManyRequestLike("foo", keys2))
+    }
+
+    "succeed when a bucket does not exist" in new S3BaseScope {
+      // S3 returns a 404 when the bucket does not exist
+      val ex = new AmazonS3Exception("boo")
+      ex.setStatusCode(404)
+      mockS3.deleteObjects(any[DeleteObjectsRequest]) throws ex
+      await(TestStrategy.deleteMany(Seq("s3:foo:bar", "s3:foo:baz"))) must beEqualTo(())
+    }
+
+    "succeed when an object does not exist" in new S3BaseScope {
+      // S3 already succeeds when the object does not exist
+    }
+
+    "succeed without sending a request when there are no locations" in new S3BaseScope {
+      await(TestStrategy.deleteMany(Seq[String]())) must beEqualTo(())
+      there was no(mockS3).deleteObjects(any)
+    }
+  }
+
+  "#create" should {
+    "fail when the location prefix is invalid" in new S3BaseScope {
+      TestStrategy.create("foo:bar", utf8InputStream("content 1"), 9) must throwA[IllegalArgumentException]
+      TestStrategy.create("s3:foo:", utf8InputStream("content 1"), 9) must throwA[IllegalArgumentException]
+      TestStrategy.create("s3:", utf8InputStream("content 1"), 9) must throwA[IllegalArgumentException]
+    }
+
+    "create random UUID filenames" in new S3BaseScope {
+      val LocationRegex = "^s3:foo:[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$".r
+      val name1Future = TestStrategy.create("s3:foo", utf8InputStream("content 1"), 9)
+      mockTransferManager.succeed
+      val name2Future = TestStrategy.create("s3:foo", utf8InputStream("content 1"), 9)
+      mockTransferManager.succeed
+      await(name1Future) must beMatching(LocationRegex)
+      await(name2Future) must beMatching(LocationRegex)
+      await(name1Future) must not(beEqualTo(await(name2Future)))
+    }
+
+    "send the uploads to S3" in new S3BaseScope {
+      val UuidRegex = "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$".r
+
+      val inputStream = utf8InputStream("content 1")
+      val future = TestStrategy.create("s3:foo", inputStream, 9)
+      mockTransferManager.lastUpload must beSome
+      mockTransferManager.lastUpload.get.bucket must beEqualTo("foo")
+      mockTransferManager.lastUpload.get.key must beMatching(UuidRegex)
+      mockTransferManager.lastUpload.get.inputStream must beEqualTo(inputStream)
+      mockTransferManager.lastUpload.get.metadata.getContentLength must beEqualTo(9)
+    }
+
+    "succeed when the upload succeeds" in new S3BaseScope {
+      val LocationRegex = "^s3:foo:[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$".r
+      val future = TestStrategy.create("s3:foo", utf8InputStream("content"), 7)
+      mockTransferManager.succeed
+      await(future) must beMatching(LocationRegex)
+    }
+
+    "fail when the upload fails" in new S3BaseScope {
+      val ex = new AmazonS3Exception("boo")
+      val future = TestStrategy.create("s3:foo", utf8InputStream("content"), 7)
+      mockTransferManager.fail(ex)
+      await(future) must throwA(ex)
+    }
+  }
+}
