f5767d2e430a793631de5d8eef88916d4a70496c
Jonas Karlsson <thejonas@gmail.com>
2014-11-26 08:17:06 -0500

    Extract helper methods

---
+++ common/src/test/scala/org/overviewproject/blobstorage/PageByteAStrategySpec.scala
@@ -0,0 +1,63 @@
+package org.overviewproject.blobstorage
+
+import org.overviewproject.models.tables.Files
+import org.overviewproject.models.File
+import org.overviewproject.models.tables.Pages
+import org.overviewproject.models.Page
+import org.overviewproject.test.SlickSpecification
+
+class PageByteAStrategySpec extends SlickSpecification {
+
+  "PageByteAStrategy" should {
+
+    "#get" should {
+
+      "return an enumerator from data" in new ExistingPageScope {
+        val future = TestPageByteAStrategy.get(s"pagebytea:${page.id}")
+        //val enumerator = await(future)
+        val bytesRead = Array[Byte](4,5,6) //consume(enumerator)
+        
+        bytesRead must be equalTo data
+      }
+    }
+
+    "#delete" should {
+
+    }
+
+    "#create" should {
+
+    }
+  }
+
+  object DbFactory {
+    import org.overviewproject.database.Slick.simple._
+
+    private val insertFileInvoker = (Files returning Files).insertInvoker
+    private val insertPageInvoker = (Pages returning Pages).insertInvoker
+
+    def insertFile(implicit session: Session): File = {
+      val file = File(0l, 1, 10l, 10l, "name", Some(100l), Some(100l))
+      insertFileInvoker.insert(file)
+    }
+    
+    def insertPage(fileId: Long, data: Array[Byte])(implicit session: Session): Page = {
+      val page = Page(0l, fileId, 1, 1, Some(data), None)
+      insertPageInvoker.insert(page)
+    }
+  }
+  
+  trait PageBaseScope extends DbScope {
+    object TestPageByteAStrategy extends PageByteAStrategy
+  }
+  
+  trait ExistingFileScope extends PageBaseScope {
+    val file = DbFactory.insertFile
+  }
+  
+  trait ExistingPageScope extends ExistingFileScope {
+    val data = Array[Byte](1, 2, 3)
+    val page = DbFactory.insertPage(file.id, data)
+  }
+}
+
--- common/src/test/scala/org/overviewproject/blobstorage/StrategySpecification.scala
+++ common/src/test/scala/org/overviewproject/blobstorage/StrategySpecification.scala
@@ -1,24 +1,15 @@
 package org.overviewproject.blobstorage
 
-import java.io.ByteArrayInputStream
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
-import play.api.libs.iteratee.{Enumerator,Iteratee}
-import scala.concurrent.{Await,Future}
-import scala.concurrent.duration.Duration
+
 
 class StrategySpecification
   extends Specification
+  with StrategySpecHelper
   with Mockito
 {
-  def await[T](future: Future[T]): T = Await.result(future, Duration.Inf)
-  def byteArrayInputStream(bytes: Array[Byte]) = new ByteArrayInputStream(bytes)
-  def utf8InputStream(string: String) = byteArrayInputStream(string.getBytes("utf-8"))
-  def consume(enumerator: Enumerator[Array[Byte]]): Array[Byte] = {
-    val f = Iteratee.consume[Array[Byte]]()
-    await(Iteratee.flatten(enumerator(f)).run)
-  }
 
   trait BaseScope extends Scope {
   }
