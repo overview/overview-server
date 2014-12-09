efe87e1bdef0524bf001410d513d19db19c7315d
Adam Hooper <adam@adamhooper.com>
2014-12-09 18:03:14 -0500

    Implement FileBackend. Fixes crashers.

--- app/controllers/backend/FileBackend.scala
+++ app/controllers/backend/FileBackend.scala
@@ -11,7 +11,15 @@ trait FileBackend {
 }
 
 trait DbFileBackend extends FileBackend { self: DbBackend =>
-  override def show(id: Long) = ???
+  override def show(id: Long) = db { session => DbFileBackend.show(session, id) }
+}
+
+object DbFileBackend {
+  import org.overviewproject.database.Slick.simple._
+
+  private lazy val byId = Compiled { (id: Column[Long]) => Files.filter(_.id === id) }
+
+  private def show(session: Session, id: Long): Option[File] = byId(id).firstOption(session)
 }
 
 object FileBackend extends DbFileBackend with DbBackend
---
+++ test/controllers/backend/DbFileBackendSpec.scala
@@ -0,0 +1,27 @@
+package controllers.backend
+
+import org.overviewproject.models.File
+
+class DbFileBackendSpec extends DbBackendSpecification {
+  trait BaseScope extends DbScope {
+    val backend = new TestDbBackend(session) with DbFileBackend
+  }
+
+  "#show" should {
+    trait ShowScope extends BaseScope {
+      val file = factory.file()
+    }
+
+    "show a File" in new ShowScope {
+      val ret = await(backend.show(file.id))
+      ret must beSome
+      ret.map(_.id) must beSome(file.id)
+      ret.map(_.name) must beSome(file.name)
+      // etc
+    }
+
+    "not show a non-File" in new ShowScope {
+      await(backend.show(file.id + 1)) must beNone
+    }
+  }
+}
