8a9068e19c138eb2795a733b3e688b3e04372d8a
Jonas Karlsson <thejonas@gmail.com>
2014-12-12 16:00:01 -0500

    Organize delete methods

--- documentset-worker/src/main/scala/org/overviewproject/database/DocumentSetDeleter.scala
+++ documentset-worker/src/main/scala/org/overviewproject/database/DocumentSetDeleter.scala
@@ -9,43 +9,65 @@ import scala.slick.jdbc.StaticQuery.interpolation
 trait DocumentSetDeleter {
 
   protected def db[A](block: Session => A): Future[A]
-  
-  def delete(documentSetId: Long): Future[Unit] = db { implicit session => 
-    val documents = Documents.filter(_.documentSetId === documentSetId) 
-    val documentSetUser = DocumentSetUsers.filter(_.documentSetId === documentSetId)
-    val documentSet = DocumentSets.filter(_.id === documentSetId) 
 
-    val uploadedFileId = UploadedFiles.filter(_.id in documentSet.map(_.uploadedFileId)).map(_.id).firstOption
-    val uploadedFiles = UploadedFiles.filter(_.id === uploadedFileId)
+  def delete(documentSetId: Long): Future[Unit] = db { implicit session =>
+    val uploadedFileId = findUploadedFileId(documentSetId)
+
+    releaseFiles(documentSetId)
+
+    deleteUserAddedData(documentSetId)
+    deleteCore(documentSetId)
     
+    deleteUploadedFile(uploadedFileId)
+  }
+
+  private def findUploadedFileId(documentSetId: Long)(implicit session: Session): Option[Long] =
+    DocumentSets.filter(_.id === documentSetId).map(_.uploadedFileId).firstOption.flatten
+
+  // The minimal set of components, common to all document sets
+  private def deleteCore(documentSetId: Long)(implicit session: Session): Unit = {
+    val documents = Documents.filter(_.documentSetId === documentSetId)
+    val documentSetUser = DocumentSetUsers.filter(_.documentSetId === documentSetId)
+    val documentSet = DocumentSets.filter(_.id === documentSetId)
+
+    documents.delete
+    documentSetUser.delete
+    documentSet.delete
+
+  }
+
+  // Artifacts added by the user interacting with the system
+  private def deleteUserAddedData(documentSetId: Long)(implicit session: Session): Unit = {
     val tags = Tags.filter(_.documentSetId === documentSetId)
     val searchResults = SearchResults.filter(_.documentSetId === documentSetId)
-    
-    
+
+    tags.delete
+    searchResults.delete
+  }
+
+  private def deleteUploadedFile(uploadedFileId: Option[Long])(implicit session: Session): Unit =
+    uploadedFileId.map { uid => UploadedFiles.filter(_.id === uid).delete }
+
+  // Decrement reference counts on Files and Pages
+  // Assume something else deletes them when reference count is 0
+  // Use raw sql because Slick does not support decrement 
+  private def releaseFiles(documentSetId: Long)(implicit session: Session): Unit = {
     val fileReferences = sqlu"""
       UPDATE file SET reference_count = reference_count - 1
       WHERE reference_count > 0 AND id IN 
         (SELECT file_id FROM document where document_set_id  = $documentSetId)
      """
-     fileReferences.execute 
-     
-     val pageReferences = sqlu"""
+
+    val pageReferences = sqlu"""
        UPDATE page SET reference_count = reference_count - 1
        WHERE reference_count > 0 AND file_id IN
         (SELECT file_id FROM document where document_set_id  = $documentSetId)
      """
-    pageReferences.execute
-    
-    tags.delete
-    searchResults.delete
-    
-    documents.delete
-    documentSetUser.delete
-    documentSet.delete
 
-    uploadedFiles.delete
-    
+    fileReferences.execute
+    pageReferences.execute
+
   }
-  
+
 }
 
