6e295e4d96e433e0cfffb8553c0c05818dc8ba86
Jonas Karlsson <thejonas@gmail.com>
2014-12-11 19:01:52 -0500

    Delete uploaded CSV file

--- documentset-worker/src/main/scala/org/overviewproject/database/DocumentSetDeleter.scala
+++ documentset-worker/src/main/scala/org/overviewproject/database/DocumentSetDeleter.scala
@@ -2,7 +2,7 @@ package org.overviewproject.database
 
 import scala.concurrent.Future
 import org.overviewproject.database.Slick.simple._
-import org.overviewproject.models.tables.{ Documents, DocumentSets, DocumentSetUsers}
+import org.overviewproject.models.tables.{ Documents, DocumentSets, DocumentSetUsers, UploadedFiles }
 
 trait DocumentSetDeleter {
 
@@ -13,9 +13,15 @@ trait DocumentSetDeleter {
     val documentSetUser = DocumentSetUsers.filter(_.documentSetId === documentSetId)
     val documentSet = DocumentSets.filter(_.id === documentSetId) 
 
+    val uploadedFileId = UploadedFiles.filter(_.id in documentSet.map(_.uploadedFileId)).map(_.id).firstOption
+    val uploadedFiles = UploadedFiles.filter(_.id === uploadedFileId)
+    
     documents.delete
     documentSetUser.delete
     documentSet.delete
-   
+
+    uploadedFiles.delete
+    
   }
 }
+
--- documentset-worker/src/test/scala/org/overviewproject/database/DocumentSetDeleterSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/database/DocumentSetDeleterSpec.scala
@@ -1,9 +1,9 @@
 package org.overviewproject.database
 
 import org.overviewproject.test._
-import org.overviewproject.models.tables.{ Documents, DocumentSets }
+import org.overviewproject.models.tables.{ Documents, DocumentSets, UploadedFiles }
 import org.overviewproject.database.Slick.simple._
-import org.overviewproject.models.{ Document, DocumentSet }
+import org.overviewproject.models.{ Document, DocumentSet, UploadedFile }
 import java.sql.Timestamp
 import scala.concurrent.Future
 import org.overviewproject.test.factories.DbFactory
@@ -17,18 +17,40 @@ class DocumentSetDeleterSpec extends SlickSpecification {
 
       findDocumentSet(documentSet.id) must beEmpty
     }
+
+    "delete csv uploads" in new CsvUploadScope {
+      await { deleter.delete(documentSet.id) }
+
+      findDocumentSet(documentSet.id) must beEmpty
+      findUploadedFile must beEmpty
+    }
   }
 
   trait BasicDocumentSetScope extends DbScope {
     val deleter = new TestDocumentSetDeleter
-    val documentSet = factory.documentSet()
+    val documentSet = createDocumentSet
+
     val documents = Seq.fill(10)(factory.document(documentSetId = documentSet.id))
     factory.documentSetUser(documentSetId = documentSet.id)
-    
+
     def findDocumentSet(documentSetId: Long)(implicit session: Session): Option[DocumentSet] = {
       val q = DocumentSets.filter(_.id === documentSetId)
       q.firstOption
     }
+
+    def createDocumentSet: DocumentSet = factory.documentSet()
+  }
+
+  trait CsvUploadScope extends BasicDocumentSetScope {
+
+    override def createDocumentSet: DocumentSet = {
+      val uploadedFile = factory.uploadedFile(size = 100l)
+      factory.documentSet(uploadedFileId = Some(uploadedFile.id))
+    }
+
+    def findUploadedFile(implicit session: Session): Option[UploadedFile] = 
+      UploadedFiles.firstOption
+
   }
 
   class TestDocumentSetDeleter(implicit session: Session) extends DocumentSetDeleter {
