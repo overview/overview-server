2b8473c96a530f270a8feb0c746b2d986302a213
Jonas Karlsson <thejonas@gmail.com>
2014-12-15 17:02:03 -0500

    Extract a little function

--- documentset-worker/src/main/scala/org/overviewproject/database/DocumentSetCreationJobDeleter.scala
+++ documentset-worker/src/main/scala/org/overviewproject/database/DocumentSetCreationJobDeleter.scala
@@ -5,23 +5,24 @@ import org.overviewproject.blobstorage.BlobStorage
 import org.overviewproject.database.Slick.simple._
 import org.overviewproject.models.tables.DocumentSetCreationJobs
 
-
 trait DocumentSetCreationJobDeleter extends SlickClient {
 
   def deleteByDocumentSet(documentSetId: Long): Future[Unit] = db { implicit session =>
     val documentSetCreationJob = DocumentSetCreationJobs.filter(_.documentSetId === documentSetId)
-    
+
     val uploadedCsvOids = documentSetCreationJob.map(_.contentsOid).list.flatten
-    
-    uploadedCsvOids.map { oid =>
-      val csvUploadLocation = s"pglo:$oid"
-      
-      blobStorage.delete(csvUploadLocation)
-    }
+
+    uploadedCsvOids.map(deleteContent)
     
     documentSetCreationJob.delete
   }
-  
+
+  private def deleteContent(oid: Long) = {
+    val csvUploadLocation = s"pglo:$oid"
+
+    blobStorage.delete(csvUploadLocation)
+  }
+
   protected val blobStorage: BlobStorage
 }
 
