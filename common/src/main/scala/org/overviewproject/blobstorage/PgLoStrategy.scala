d889587fc24ec174dfbcc79556c4bb83eed1919c
Adam Hooper <adam@adamhooper.com>
2014-12-15 17:14:56 -0500

    Implement PgLoStrategy [finishes #84590508]
    Test this by downloading archives that have not been split into pages.

--- common/src/main/scala/org/overviewproject/blobstorage/PgLoStrategy.scala
+++ common/src/main/scala/org/overviewproject/blobstorage/PgLoStrategy.scala
@@ -1,14 +1,153 @@
 package org.overviewproject.blobstorage
 
 import java.io.InputStream
+import org.postgresql.PGConnection
+import org.postgresql.util.PSQLException
 import play.api.libs.iteratee.Enumerator
-import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Future,blocking}
+import scala.slick.jdbc.JdbcBackend.Session
+
+import org.overviewproject.database.DB
+import org.overviewproject.postgres.{LO,LargeObject}
 
 trait PgLoStrategy extends BlobStorageStrategy {
-  override def get(location: String): Future[Enumerator[Array[Byte]]] = ???
-  override def delete(location: String): Future[Unit] = ???
-  override def create(locationPrefix: String, inputStream: InputStream, nBytes: Long): Future[String] = ???
+
+  /** Size of each Array[Byte] we push.
+    *
+    * Larger causes blocking operations to block longer. Smaller causes more
+    * database connections.
+    */
+  protected val BufferSize = 4 * 1024 * 1024 // 4MB, chosen at random
+
+  /** Execute code with a PGConnection. */
+  protected def withPgConnection[A](code: PGConnection => A): A
+  /** Execute code in Slick. */
+  protected def withSlickSession[A](code: Session => A): A
+
+  /** Execute code with the given LargeObject. */
+  private def withLargeObject[A](loid: Long)(f: LargeObject => A): A = {
+    blocking {
+      withPgConnection { pgConnection =>
+        LO.withLargeObject(loid)({ lo => f(lo) })(pgConnection)
+      }
+    }
+  }
+
+  /** Execute code with the given newly-created LargeObject. */
+  private def withNewLargeObject[A](f: LargeObject => A): A = {
+    blocking {
+      withPgConnection { pgConnection =>
+        LO.withLargeObject({ lo => f(lo) })(pgConnection)
+      }
+    }
+  }
+
+  private def locationToOid(location: String): Long = {
+    if (!location.startsWith("pglo:")) throw new IllegalArgumentException("Invalid prefix on location: " + location)
+    location.substring(5).toLong
+  }
+
+  /** Throws an exception iff we cannot access the given large object. */
+  private def testLargeObject(loid: Long): Unit = withLargeObject(loid) { lo => () }
+
+  /** Returns an Enumerator over the given large object. */
+  private def enumerateLargeObject(loid: Long): Enumerator[Array[Byte]] = {
+    val buffer = new Array[Byte](BufferSize)
+
+    // Reads [position,position+BufferSize) bytes.
+    //
+    // Returns Some(newPosition, bytes) if there are more bytes; otherwise
+    // returns None
+    def continue(position: Int): Future[Option[(Int,Array[Byte])]] = {
+      Future(withLargeObject(loid) { lo =>
+        lo.seek(position)
+        val nBytes = lo.read(buffer, 0, BufferSize)
+        val newPosition = position + nBytes
+        nBytes match {
+          case 0 => None
+          case _ => Some(newPosition, buffer.take(nBytes)) // always a copy
+        }
+      })
+    }
+
+    Enumerator.unfoldM(0)(continue _)
+  }
+
+  override def get(location: String): Future[Enumerator[Array[Byte]]] = {
+    val loid = locationToOid(location)
+    Future {
+      testLargeObject(loid); // fail fast
+      enumerateLargeObject(loid);
+    }
+  }
+
+  override def delete(location: String): Future[Unit] = deleteMany(Seq(location))
+
+  override def deleteMany(locations: Seq[String]): Future[Unit] = {
+    // LO.delete() puts the connection in an inconsistent state if the loid is
+    // invalid. But our contract states the loid may be invalid. So we can't
+    // use LO.delete() ... or lo_unlink() even. We need some nifty SQL.
+
+    val loids = locations.map(locationToOid _)
+
+    Future(blocking {
+      import org.overviewproject.database.Slick.simple._
+      import scala.slick.jdbc.StaticQuery
+
+      val q = s"""
+        DO $$$$
+        DECLARE
+          loids BIGINT[] := ARRAY[${loids.mkString(",")}];
+          loid BIGINT;
+        BEGIN
+          FOREACH loid IN ARRAY loids LOOP
+            BEGIN
+              PERFORM lo_unlink(loid);
+            EXCEPTION
+              WHEN undefined_object THEN NULL;
+            END;
+          END LOOP;
+        END$$$$;
+      """
+
+      withSlickSession { session =>
+        StaticQuery.updateNA(q).apply().execute(session)
+      }
+    })
+  }
+
+  private def copy(inputStream: InputStream, lo: LargeObject, nBytes: Long) = {
+    val buffer = new Array[Byte](BufferSize)
+    var remaining = nBytes
+    while (remaining > 0) {
+      val bytesRead = inputStream.read(buffer, 0, BufferSize)
+      lo.add(buffer.take(bytesRead))
+      remaining -= bytesRead
+    }
+  }
+
+  override def create(locationPrefix: String, inputStream: InputStream, nBytes: Long): Future[String] = {
+    if (locationPrefix != "pglo") throw new IllegalArgumentException("locationPrefix must be pglo; got: " + locationPrefix);
+    Future(withNewLargeObject { lo: LargeObject =>
+      copy(inputStream, lo, nBytes)
+      "pglo:" + lo.oid
+    })
+  }
 }
 
 object PgLoStrategy extends PgLoStrategy {
+  override protected def withPgConnection[A](f: PGConnection => A): A = {
+    DB.withConnection { connection =>
+      val pgConnection = DB.pgConnection(connection)
+      f(pgConnection)
+    }
+  }
+
+  override protected def withSlickSession[A](f: Session => A): A = {
+    DB.withConnection { connection =>
+      val session = DB.slickSession(connection)
+      f(session)
+    }
+  }
 }
---
+++ common/src/test/scala/org/overviewproject/blobstorage/PgLoStrategySpec.scala
@@ -0,0 +1,172 @@
+package org.overviewproject.blobstorage
+
+import java.io.{ByteArrayInputStream,IOException,InputStream}
+import org.postgresql.PGConnection
+import org.postgresql.util.PSQLException
+import play.api.libs.iteratee.Iteratee
+import scala.slick.jdbc.JdbcBackend.Session
+
+import org.overviewproject.database.DB
+import org.overviewproject.postgres.LO
+import org.overviewproject.test.SlickSpecification
+
+class PgLoStrategySpec extends SlickSpecification with StrategySpecification {
+  trait PgLoBaseScope extends DbScope with BaseScope {
+    def _withPgConnection[A](f: PGConnection => A) = {
+      val pgConnection = DB.pgConnection(connection)
+      f(pgConnection)
+    }
+
+    def _withSlickSession[A](f: Session => A) = {
+      val session = DB.slickSession(connection)
+      f(session)
+    }
+
+    // Each test runs in a transaction. Make sure the _code_ uses the same
+    // connection, even though it's in a Future.
+    object TestStrategy extends PgLoStrategy {
+      override protected val BufferSize = 10 // to prove we paginate
+      override protected def withPgConnection[A](f: PGConnection => A) = _withPgConnection(f)
+      override protected def withSlickSession[A](f: Session => A) = _withSlickSession(f)
+    }
+
+    def createLargeObject(data: Array[Byte]): Long = {
+      _withPgConnection { pgConnection =>
+        LO.withLargeObject({ lo =>
+          lo.add(data)
+          lo.oid // return loid
+        })(pgConnection)
+      }
+    }
+
+    def readLargeObject(loid: Long): Array[Byte] = {
+      _withPgConnection { pgConnection =>
+        LO.withLargeObject(loid)({ lo =>
+          val buffer = new Array[Byte](1000)
+          val size = lo.read(buffer, 0, buffer.length)
+          buffer.take(size)
+        })(pgConnection)
+      }
+    }
+
+    def largeObjectExists(loid: Long): Boolean = {
+      // XXX this will break if called twice!
+      val SqlStateUndefinedObject = "42704" // http://www.postgresql.org/docs/9.3/static/errcodes-appendix.html
+      _withPgConnection { pgConnection =>
+        try {
+          LO.withLargeObject(loid)({ lo => ()})(pgConnection)
+          true
+        } catch {
+          case e: PSQLException if (e.getSQLState() == SqlStateUndefinedObject) => false
+        }
+      }
+    }
+  }
+
+  "#get" should {
+    trait GetScope extends PgLoBaseScope
+
+    "throw an exception when location does not look like pglo:12345" in new GetScope {
+      TestStrategy.get("pglo") must throwA[IllegalArgumentException]
+      TestStrategy.get("pglo:abcd") must throwA[IllegalArgumentException]
+      TestStrategy.get("pglo:18446744073709552001") must throwA[IllegalArgumentException]
+    }
+
+    "throw a delayed exception when the location points at an invalid large object" in new GetScope {
+      await(TestStrategy.get("pglo:123")) must throwA[Exception]
+    }
+
+    "return an Enumerator of the file" in new GetScope {
+      val data = "foo".getBytes("utf-8")
+      val loid = createLargeObject(data)
+      val enumerator = await(TestStrategy.get("pglo:" + loid))
+      await(enumerator.run(Iteratee.consume())) must beEqualTo(data)
+    }
+
+    "fill the buffer multiple times in the Enumerator if necessary" in new GetScope {
+      val data = "foo bar baz moo mar maz".getBytes("utf-8")
+      val loid = createLargeObject(data)
+      val enumerator = await(TestStrategy.get("pglo:" + loid))
+      await(enumerator.run(Iteratee.consume())) must beEqualTo(data)
+    }
+  }
+
+  "#delete" should {
+    trait DeleteScope extends PgLoBaseScope
+
+    "throw an exception when location does not look like pglo:12345" in new DeleteScope {
+      TestStrategy.delete("pglo") must throwA[IllegalArgumentException]
+      TestStrategy.delete("pglo:abcd") must throwA[IllegalArgumentException]
+      TestStrategy.delete("pglo:18446744073709552001") must throwA[IllegalArgumentException]
+    }
+
+    "succeed when the large object does not exist" in new DeleteScope {
+      await(TestStrategy.delete("pglo:123")) must beEqualTo(())
+    }
+
+    "delete the blob when the large object exists" in new DeleteScope {
+      val data = "foo".getBytes("utf-8")
+      val loid = createLargeObject(data)
+      await(TestStrategy.delete("pglo:" + loid)) must beEqualTo(())
+      largeObjectExists(loid) must beFalse
+    }
+  }
+
+  "#deleteMany" should {
+    trait DeleteManyScope extends PgLoBaseScope
+
+    "throw an exception when location does not look like pglo:12345" in new DeleteManyScope {
+      TestStrategy.deleteMany(Seq("pglo:123", "pglo")) must throwA[IllegalArgumentException]
+      TestStrategy.deleteMany(Seq("pglo:123", "pglo:abcd")) must throwA[IllegalArgumentException]
+      TestStrategy.deleteMany(Seq("pglo:123", "pglo:18446744073709552001")) must throwA[IllegalArgumentException]
+    }
+
+    "delete large objects" in new DeleteManyScope {
+      val loid1 = createLargeObject("foo".getBytes("utf-8"))
+      val loid2 = createLargeObject("bar".getBytes("utf-8"))
+      await(TestStrategy.deleteMany(Seq("pglo:" + loid1, "pglo:" + loid2))) must beEqualTo(())
+      // largeObjectExists(loid1) must beEqualTo(false)
+      largeObjectExists(loid2) must beEqualTo(false) // largeObjectExists() breaks if called twice
+    }
+
+    "delete even when a large object does not exist" in new DeleteManyScope {
+      val loid1 = createLargeObject("foo".getBytes("utf-8"))
+      val loid2 = loid1 + 1;
+      val loid3 = createLargeObject("bar".getBytes("utf-8"))
+      await(TestStrategy.deleteMany(Seq("pglo:" + loid1, "pglo:" + loid2, "pglo:" + loid3))) must beEqualTo(())
+      // largeObjectExists(loid1) must beEqualTo(false)
+      largeObjectExists(loid3) must beEqualTo(false) // largeObjectExists() breaks if called twice
+    }
+  }
+
+  "#create" should {
+    trait CreateScope extends PgLoBaseScope {
+      val data = "foo bar baz moo mar maz".getBytes("utf-8")
+      val dataAsStream = new ByteArrayInputStream(data)
+      def go = TestStrategy.create("pglo", dataAsStream, data.length)
+    }
+
+    "throw an exception when location prefix is not pglo" in new CreateScope {
+      TestStrategy.create("pglop", dataAsStream, data.length) must throwA[IllegalArgumentException]
+      TestStrategy.create("pglo:", dataAsStream, data.length) must throwA[IllegalArgumentException]
+      TestStrategy.create("pgl", dataAsStream, data.length) must throwA[IllegalArgumentException]
+    }
+
+    "return a valid location" in new CreateScope {
+      await(go) must beMatching("^pglo:\\d+$".r)
+    }
+
+    "store the file" in new CreateScope {
+      val location = await(go)
+      val loid = location.split(":")(1).toLong
+      readLargeObject(loid) must beEqualTo(data)
+    }
+
+    "fail when storing the file fails" in new CreateScope {
+      val evilStream = new InputStream {
+        override def read = throw new IOException("this method intentionally left broken")
+      }
+      await(TestStrategy.create("pglo", evilStream, 10)) must throwA[IOException]
+    }
+  }
+}
