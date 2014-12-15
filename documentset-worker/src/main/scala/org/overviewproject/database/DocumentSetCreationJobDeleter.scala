8841e4afbe1fee3656d373ea81f8cea9afa836a6
Jonas Karlsson <thejonas@gmail.com>
2014-12-15 17:00:52 -0500

    Delete uploaded csv

--- documentset-worker/src/main/scala/org/overviewproject/database/DocumentSetCreationJobDeleter.scala
+++ documentset-worker/src/main/scala/org/overviewproject/database/DocumentSetCreationJobDeleter.scala
@@ -1,6 +1,7 @@
 package org.overviewproject.database
 
 import scala.concurrent.Future
+import org.overviewproject.blobstorage.BlobStorage
 import org.overviewproject.database.Slick.simple._
 import org.overviewproject.models.tables.DocumentSetCreationJobs
 
@@ -9,8 +10,18 @@ trait DocumentSetCreationJobDeleter extends SlickClient {
 
   def deleteByDocumentSet(documentSetId: Long): Future[Unit] = db { implicit session =>
     val documentSetCreationJob = DocumentSetCreationJobs.filter(_.documentSetId === documentSetId)
-
+    
+    val uploadedCsvOids = documentSetCreationJob.map(_.contentsOid).list.flatten
+    
+    uploadedCsvOids.map { oid =>
+      val csvUploadLocation = s"pglo:$oid"
+      
+      blobStorage.delete(csvUploadLocation)
+    }
+    
     documentSetCreationJob.delete
   }
+  
+  protected val blobStorage: BlobStorage
 }
 
--- documentset-worker/src/test/scala/org/overviewproject/database/DocumentSetCreationJobDeleterSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/database/DocumentSetCreationJobDeleterSpec.scala
@@ -1,36 +1,57 @@
 package org.overviewproject.database
 
 import org.overviewproject.test.SlickSpecification
+import org.overviewproject.blobstorage.BlobStorage
 import org.overviewproject.database.Slick.simple._
 import org.overviewproject.test.SlickClientInSession
 import org.overviewproject.models.DocumentSetCreationJob
 import org.overviewproject.models.DocumentSetCreationJobType._
 import org.overviewproject.models.tables.DocumentSetCreationJobs
+import org.specs2.mock.Mockito
 
-class DocumentSetCreationJobDeleterSpec extends SlickSpecification {
+class DocumentSetCreationJobDeleterSpec extends SlickSpecification with Mockito {
 
   "DocumentSetCreationJobDeleter" should {
-    
+
     "delete a job" in new JobScope {
-     await { deleter.deleteByDocumentSet(documentSet.id) }
-      
+      await { deleter.deleteByDocumentSet(documentSet.id) }
+
       DocumentSetCreationJobs.list must beEmpty
     }
-    
-    
+
+    "delete uploaded csv" in new CsvImportJobScope {
+      await { deleter.deleteByDocumentSet(documentSet.id) }
+      
+      DocumentSetCreationJobs.list must beEmpty
+      
+      there was one(deleter.mockBlobStorage).delete(s"pglo:$oid")
+    }
+
   }
-  
-  
+
   trait JobScope extends DbScope {
     val deleter = new TestDocumentSetDeleter
-    
-    val documentSet = factory.documentSet()   
+
+    val documentSet = factory.documentSet()
     val job = createJob
 
-    def createJob: DocumentSetCreationJob = 
+    def createJob: DocumentSetCreationJob =
       factory.documentSetCreationJob(documentSetId = documentSet.id, jobType = Recluster, treeTitle = Some("tree"))
   }
-  
-  class TestDocumentSetDeleter(implicit val session: Session) extends DocumentSetCreationJobDeleter with SlickClientInSession
-  
+
+  trait CsvImportJobScope extends JobScope {
+    def oid = 1l
+
+    override def createJob =
+      factory.documentSetCreationJob(documentSetId = documentSet.id, jobType = CsvUpload, contentsOid = Some(oid))
+  }
+
+  class TestDocumentSetDeleter(implicit val session: Session) extends DocumentSetCreationJobDeleter 
+  with SlickClientInSession {
+    
+	override protected val blobStorage = mock[BlobStorage]
+	
+	def mockBlobStorage = blobStorage
+  }
+
 }
