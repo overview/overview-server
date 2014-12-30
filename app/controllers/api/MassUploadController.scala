76098375f49faf863bea6fba0a271e40570f1cfc
Adam Hooper <adam@adamhooper.com>
2014-12-30 08:58:48 -0500

    Add util.MassUploadControllerMethods
    [refs #83712908]

--- app/controllers/api/MassUploadController.scala
+++ app/controllers/api/MassUploadController.scala
@@ -11,10 +11,10 @@ import controllers.auth.Authorities.anyUser
 import controllers.backend.{ FileGroupBackend, GroupedFileUploadBackend }
 import controllers.forms.MassUploadControllerForm
 import controllers.iteratees.GroupedFileUploadIteratee
-import controllers.util.JobQueueSender
+import controllers.util.{MassUploadControllerMethods,JobQueueSender}
 import models.orm.stores.{ DocumentSetCreationJobStore, DocumentSetStore, DocumentSetUserStore }
 import models.OverviewDatabase
-import org.overviewproject.models.{ApiToken,FileGroup,GroupedFileUpload}
+import org.overviewproject.models.GroupedFileUpload
 import org.overviewproject.jobs.models.ClusterFileGroup
 import org.overviewproject.tree.orm.{DocumentSet,DocumentSetCreationJob,DocumentSetUser}
 import org.overviewproject.tree.Ownership
@@ -29,52 +29,15 @@ trait MassUploadController extends ApiController {
   protected val uploadIterateeFactory: (GroupedFileUpload,Long) => Iteratee[Array[Byte],Unit]
 
   /** Starts or resumes a file upload.  */
-  def create(guid: UUID) = EssentialAction { request =>
-    def badRequest(message: String): Iteratee[Array[Byte],Result] = {
-        Iteratee.ignore.map(_ => BadRequest(jsonError(message)))
-    }
-
-    def findOrCreateFileGroup(apiToken: ApiToken): Future[FileGroup] = {
-      val attributes = FileGroup.CreateAttributes(apiToken.createdBy, Some(apiToken.token))
-      fileGroupBackend.findOrCreate(attributes)
-    }
-
-    def findOrCreateGroupedFileUpload(fileGroup: FileGroup, info: MassUploadController.RequestInfo): Future[GroupedFileUpload] = {
-      val attributes = GroupedFileUpload.CreateAttributes(
-        fileGroup.id,
-        guid,
-        info.contentType,
-        info.filename,
-        info.total
-      )
-      groupedFileUploadBackend.findOrCreate(attributes)
-    }
-
-    def createIteratee(upload: GroupedFileUpload, info: MassUploadController.RequestInfo): Iteratee[Array[Byte],Result] = {
-      if (info.start > upload.uploadedSize) {
-        badRequest(s"Tried to resume past last uploaded byte. Resumed at byte ${info.start}, but only ${upload.uploadedSize} bytes have been uploaded.")
-      } else {
-        uploadIterateeFactory(upload, info.start).map(_ => Created)
-      }
-    }
-
-    val futureApiToken: Future[Either[Result,ApiToken]] = apiTokenFactory.loadAuthorizedApiToken(request, anyUser)
-    val futureIteratee: Future[Iteratee[Array[Byte],Result]] = futureApiToken.flatMap(_ match {
-      case Left(result) => Future.successful(Iteratee.ignore.map(_ => result))
-      case Right(apiToken) => {
-        MassUploadController.RequestInfo.fromRequest(request) match {
-          case Some(info) => {
-            for {
-              fileGroup <- findOrCreateFileGroup(apiToken)
-              groupedFileUpload <- findOrCreateGroupedFileUpload(fileGroup, info)
-            } yield createIteratee(groupedFileUpload, info)
-          }
-          case None => Future.successful(badRequest(("Request must have Content-Range or Content-Length header")))
-        }
-      }
-    })
-
-    Iteratee.flatten(futureIteratee)
+  def create(guid: UUID): EssentialAction = {
+    MassUploadControllerMethods.Create(
+      guid,
+      fileGroupBackend,
+      groupedFileUploadBackend,
+      apiTokenFactory,
+      uploadIterateeFactory,
+      true
+    )
   }
 
   /** Responds to a HEAD request.
@@ -238,36 +201,6 @@ object MassUploadController extends MassUploadController {
       )
 
       JobQueueSender.send(command)
-    }
-  }
-
-  private case class RequestInfo(filename: String, contentType: String, start: Long, total: Long)
-  
-  private object RequestInfo {
-    def fromRequest(request: RequestHeader): Option[RequestInfo] = {
-      val contentType = request.headers.get(CONTENT_TYPE).getOrElse("")
-      val contentDisposition = request.headers.get(CONTENT_DISPOSITION)
-      val filename: String = contentDisposition.flatMap(ContentDisposition(_).filename).getOrElse("")
-
-      def one(start: Long, total: Long) = RequestInfo(filename, contentType, start, total)
-
-      // A string matching "(\d{0,18})" cannot throw an exception when converted to Long.
-      val rangeResults = request.headers.get(CONTENT_RANGE).flatMap { contentRanges =>
-        """^bytes (\d{0,18})-\d+/(\d{0,18})$""".r.findFirstMatchIn(contentRanges).map { rangeMatch =>
-          val List(start, total) = rangeMatch.subgroups.take(2)
-
-          one(start.toLong, total.toLong)
-        }
-      }
-      
-      val lengthResults = request.headers.get(CONTENT_LENGTH).flatMap { contentLengths =>
-        """^(\d{0,18})$""".r.findFirstMatchIn(contentLengths).map { lengthMatch =>
-          val List(total) = lengthMatch.subgroups.take(1)
-          one(0L, total.toLong)
-        }
-      }
-
-      (rangeResults ++ lengthResults).headOption
     }
   }
 }
