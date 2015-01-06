ad99915385fab3ed3d38fc12a1e19332eb383a9d
Jonas Karlsson <thejonas@gmail.com>
2015-01-06 15:04:41 -0500

    Create Slick model and table

---
+++ common/src/main/scala/org/overviewproject/models/DocumentSetCreationJobNode.scala
@@ -0,0 +1,6 @@
+package org.overviewproject.models
+
+case class DocumentSetCreationJobNode(
+  documentSetId: Long,
+  nodeId: Long
+)
---
+++ common/src/main/scala/org/overviewproject/models/tables/DocumentSetCreationJobNodes.scala
@@ -0,0 +1,13 @@
+package org.overviewproject.models.tables
+
+import org.overviewproject.database.Slick.simple._
+import org.overviewproject.models.DocumentSetCreationJobNode
+
+class DocumentSetCreationJobNodesImpl(tag: Tag) extends Table[DocumentSetCreationJobNode](tag, "document_set_creation_job_node") {
+  def documentSetCreationJobId = column[Long]("document_set_creation_job_id")
+  def nodeId = column[Long]("node_id")
+
+  def * = (documentSetCreationJobId, nodeId) <> ((DocumentSetCreationJobNode.apply _).tupled, DocumentSetCreationJobNode.unapply)
+}
+
+object DocumentSetCreationJobNodes extends TableQuery(new DocumentSetCreationJobNodesImpl(_))
