b733330b9d4abb4edb9c524c0f493fb137ec4d28
Jonas Karlsson <thejonas@gmail.com>
2015-01-09 15:44:04 -0500

    Implement restart

--- worker/src/main/scala/org/overviewproject/util/DocumentSetCreationJobRestarter.scala
+++ worker/src/main/scala/org/overviewproject/util/DocumentSetCreationJobRestarter.scala
@@ -2,9 +2,9 @@ package org.overviewproject.util
 
 import org.overviewproject.models.DocumentSetCreationJob
 import org.overviewproject.models.DocumentSetCreationJobState._
+import scala.concurrent.ExecutionContext
 
 trait DocumentSetCreationJobRestarter extends NewJobRestarter {
-
 
   override protected def removeInterruptedJobData: Unit =
     storage.deleteDocuments(job.documentSetId)
@@ -15,4 +15,34 @@ trait DocumentSetCreationJobRestarter extends NewJobRestarter {
     def deleteDocuments(jobId: Long): Unit
   }
 
+}
+
+object DocumentSetCreationJobRestarter {
+
+  def apply(job: DocumentSetCreationJob)(implicit executionContext: ExecutionContext): DocumentSetCreationJobRestarter = ???
+
+  private class DocumentSetCreationJobRestarterWithStorage(val job: DocumentSetCreationJob)(
+    implicit executionContext: ExecutionContext) extends DocumentSetCreationJobRestarter {
+
+    import scala.concurrent.{ Await, Future }
+    import scala.concurrent.duration.Duration
+    import org.overviewproject.database.SlickSessionProvider
+
+    override protected val storage = new DbSyncedStorage
+    
+    class DbSyncedStorage extends DocumentStorage {
+      private def await[A](block: => Future[A]): A =
+        Await.result(block, Duration.Inf)
+
+      private val cleaner = new DocumentSetCleaner with SlickSessionProvider {
+        override implicit protected val executor = executionContext
+      }
+
+      override def updateValidJob(job: DocumentSetCreationJob): Unit =
+        await(cleaner.updateValidJob(job))
+
+      override def deleteDocuments(documentSetId: Long): Unit =
+        await(cleaner.deleteDocuments(documentSetId))
+    }
+  }
 }
