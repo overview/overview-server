50180ddb76683904801daa96c4003f90f8dc62ec
Jonas Karlsson <thejonas@gmail.com>
2015-01-15 10:37:54 -0500

    Use SlickSessionProvider

--- worker/src/main/scala/org/overviewproject/util/JobRestarter.scala
+++ worker/src/main/scala/org/overviewproject/util/JobRestarter.scala
@@ -3,34 +3,36 @@ package org.overviewproject.util
 import org.overviewproject.models.DocumentSetCreationJob
 import org.overviewproject.models.DocumentSetCreationJobState._
 import org.overviewproject.models.tables.DocumentSetCreationJobMappings
+import org.overviewproject.database.SlickSessionProvider
 
 trait JobRestarter {
   val MaxRetryAttempts = Configuration.getInt("max_job_retry_attempts")
-  
+
   protected val job: DocumentSetCreationJob
   protected val storage: Storage
-  
-  def restart: Unit = 
-    if (job.retryAttempts < MaxRetryAttempts ) attemptRestart
+
+  def restart: Unit =
+    if (job.retryAttempts < MaxRetryAttempts) attemptRestart
     else failJob
-  
-    
+
   protected def attemptRestart: Unit = {
-      removeInterruptedJobData
-      storage.updateValidJob(job.copy(state = NotStarted, retryAttempts = job.retryAttempts + 1))
-    }
-  
-  protected def failJob: Unit = 
+    removeInterruptedJobData
+    storage.updateValidJob(job.copy(state = NotStarted, retryAttempts = job.retryAttempts + 1))
+  }
+
+  protected def failJob: Unit =
     storage.updateValidJob(job.copy(state = Error, statusDescription = "max_retry_attempts"))
-    
-  protected def removeInterruptedJobData: Unit 
-  
+
+  protected def removeInterruptedJobData: Unit
+
   protected trait Storage {
     def updateValidJob(job: DocumentSetCreationJob)
   }
 }
 
-object JobRestarter extends DocumentSetCreationJobMappings {
+object JobRestarter extends DocumentSetCreationJobMappings with SlickSessionProvider {
+  import scala.concurrent.Await
+  import scala.concurrent.duration.Duration
   import scala.concurrent.ExecutionContext.Implicits.global
   import org.overviewproject.database.Database
   import org.overviewproject.database.Slick.simple._
@@ -38,22 +40,24 @@ object JobRestarter extends DocumentSetCreationJobMappings {
   import org.overviewproject.models.DocumentSetCreationJobType._
   import org.overviewproject.models.DocumentSetCreationJobState._
   import org.overviewproject.models.tables.DocumentSetCreationJobs
-  
-  
+
   def restartInterruptedJobs: Unit = {
     interruptedJobs.flatMap(createRestarter).map(_.restart)
-  } 
-
-  private def interruptedJobs: Seq[DocumentSetCreationJob] = Database.withSlickSession { implicit session =>
-    DocumentSetCreationJobs.filter(_.state === InProgress).list
   }
-  
+
+  // FIXME: make async
+  private def interruptedJobs: Seq[DocumentSetCreationJob] = {
+    val jobList = db { implicit session =>
+      DocumentSetCreationJobs.filter(_.state === InProgress).list
+    }
+    Await.result(jobList, Duration.Inf)
+  }
+
   private def createRestarter(job: DocumentSetCreationJob): Option[JobRestarter] = job.jobType match {
-    case Recluster => Some(ClusteringJobRestarter(job))
+    case Recluster     => Some(ClusteringJobRestarter(job))
     case DocumentCloud => Some(DocumentSetCreationJobRestarter(job, SearchIndex))
-    case CsvUpload => Some(DocumentSetCreationJobRestarter(job, SearchIndex))
-    case _ => None
+    case CsvUpload     => Some(DocumentSetCreationJobRestarter(job, SearchIndex))
+    case _             => None
   }
 
-  
 }
