d0f04ed9de0a8b3340b328793dde99c2bfeb8739
Jonas Karlsson <thejonas@gmail.com>
2015-01-15 10:40:54 -0500

    Remove Slick references

--- common/src/main/scala/org/overviewproject/database/Database.scala
+++ common/src/main/scala/org/overviewproject/database/Database.scala
@@ -1,18 +1,10 @@
 package org.overviewproject.database
 
 import java.sql.Connection
-import javax.sql.{ DataSource => JDataSource }
-import scala.slick.jdbc.JdbcBackend.{ Database => JDatabase, Session }
 
 object Database extends TransactionProvider {
 
   def transactionBlock[A](block: Connection => A): A = {
     DB.withTransaction(block)
-  }
-
-  private lazy val db = JDatabase.forDataSource(DB.getDataSource()) 
-  
-  def withSlickSession[A](block: Session => A): A = {
-    db.withSession(block)
   }
 }
