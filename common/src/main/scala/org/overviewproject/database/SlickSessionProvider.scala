875c6f37bce4a62618262b66870ff62e54b0bae3
Jonas Karlsson <thejonas@gmail.com>
2015-01-15 14:12:32 -0500

    Add parent trait

--- common/src/main/scala/org/overviewproject/database/SlickSessionProvider.scala
+++ common/src/main/scala/org/overviewproject/database/SlickSessionProvider.scala
@@ -4,14 +4,14 @@ import scala.concurrent.{ blocking, ExecutionContext, Future }
 import scala.slick.jdbc.JdbcBackend.{ Database => SlickDatabase }
 import org.overviewproject.database.Slick.simple.Session
 
-trait SlickSessionProvider {
+trait SlickSessionProvider extends SlickClient {
 
-  private lazy val db = SlickDatabase.forDataSource(DB.getDataSource())
+  private lazy val slickDb = SlickDatabase.forDataSource(DB.getDataSource())
   
   
   protected def db[A](block: Session => A)(implicit executor: ExecutionContext): Future[A] = Future {
     blocking {
-      db.withSession { session =>
+      slickDb.withSession { session =>
         block(session)
       }
     }
