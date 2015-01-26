dba91b9de6d1e30661ec5bbaefe7436b00ece7b9
Jonas Karlsson <thejonas@gmail.com>
2015-01-26 17:43:47 -0500

    Add Slick model

---
+++ common/src/main/scala/org/overviewproject/models/TempDocumentSetFile.scala
@@ -0,0 +1,6 @@
+package org.overviewproject.models
+
+case class TempDocumentSetFile(
+  documentSetId: Long,
+  fileId: Long
+)
---
+++ common/src/main/scala/org/overviewproject/models/tables/TempDocumentSetFiles.scala
@@ -0,0 +1,14 @@
+package org.overviewproject.models.tables
+
+import org.overviewproject.database.Slick.simple._
+import org.overviewproject.models.TempDocumentSetFile
+
+class TempDocumentSetFilesImpl(tag: Tag) extends Table[TempDocumentSetFile](tag, "temp_document_set_file") { 
+  def documentSetId = column[Long]("document_set_id")
+  def fileId = column[Long]("file_id")
+  def pk = primaryKey("temp_document_set_file_pkey", (documentSetId, fileId))
+  
+  def * = (documentSetId, fileId) <> (TempDocumentSetFile.tupled, TempDocumentSetFile.unapply)
+}
+
+object TempDocumentSetFiles extends TableQuery(new TempDocumentSetFilesImpl(_))
