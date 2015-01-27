14fc735a4973afb03c077a78003a21dbdfc45205
Jonas Karlsson <thejonas@gmail.com>
2015-01-27 17:26:09 -0500

    add withTransaction method

--- common/src/main/scala/org/overviewproject/database/SlickClient.scala
+++ common/src/main/scala/org/overviewproject/database/SlickClient.scala
@@ -6,4 +6,19 @@ import org.overviewproject.database.Slick.simple._
 trait SlickClient {
   
   protected def db[A](block: Session => A)(implicit executor: ExecutionContext): Future[A]
+  
+  protected def withTransaction[A](block: Session => A)(implicit session: Session): A = {
+    val connection = session.conn
+    
+    if (connection.getAutoCommit) {
+      connection.setAutoCommit(false)
+      
+      val r = block(session)
+      
+      connection.commit
+      connection.setAutoCommit(true)
+      r
+    }
+    else block(session)
+  }
 }
--- common/src/main/scala/org/overviewproject/database/SlickSessionProvider.scala
+++ common/src/main/scala/org/overviewproject/database/SlickSessionProvider.scala
@@ -3,17 +3,19 @@ package org.overviewproject.database
 import scala.concurrent.{ blocking, ExecutionContext, Future }
 import scala.slick.jdbc.JdbcBackend.{ Database => SlickDatabase }
 import org.overviewproject.database.Slick.simple.Session
+import java.sql.Connection
 
 trait SlickSessionProvider extends SlickClient {
 
   private lazy val slickDb = SlickDatabase.forDataSource(DB.getDataSource())
   
   
-  protected def db[A](block: Session => A)(implicit executor: ExecutionContext): Future[A] = Future {
+  override protected def db[A](block: Session => A)(implicit executor: ExecutionContext): Future[A] = Future {
     blocking {
       slickDb.withSession { session =>
         block(session)
       }
     }
   }
+  
 }
