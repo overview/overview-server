7774f4447f7ce07944edb101a263375361bdcf1e
Jonas Karlsson <thejonas@gmail.com>
2015-01-29 18:12:35 -0500

    Create actor

--- documentset-worker/src/main/scala/org/overviewproject/background/filegroupcleanup/DeletedFileGroupCleaner.scala
+++ documentset-worker/src/main/scala/org/overviewproject/background/filegroupcleanup/DeletedFileGroupCleaner.scala
@@ -1,25 +1,48 @@
 package org.overviewproject.background.filegroupcleanup
 
-import akka.actor.{ Actor, ActorRef }
+import scala.concurrent.Future
+import akka.actor.{ Actor, ActorRef, Props }
+import akka.pattern.pipe
 import org.overviewproject.background.filegroupcleanup.FileGroupRemovalRequestQueueProtocol._
 
+
+/**
+ * Looks for deleted [[FileGroup]]s on start up, and sends removal requests to
+ * the [[FileGroupRemovalRequestQueue]].
+ * 
+ * When requests have been sent, the actor terminates. This functionality probably
+ * doesn't need to be an actor.
+ */
 trait DeletedFileGroupCleaner extends Actor {
   import context._
 
-  private case object RemoveDeletedFileGroups
+  protected case object RemoveDeletedFileGroups
+  protected case object RequestsSent
 
   override def preStart = self ! RemoveDeletedFileGroups
 
   override def receive = {
-    case RemoveDeletedFileGroups => {
-      deletedFileGroupFinder.deletedFileGroupIds.map {
-        _.foreach(fileGroupRemovalRequestQueue ! RemoveFileGroup(_))
-      }
-      context.stop(self)
-    }
-
+    case RemoveDeletedFileGroups => requestRemovals pipeTo self
+    case RequestsSent =>  context.stop(self)
   }
+
+  private def requestRemovals =
+    deletedFileGroupFinder.deletedFileGroupIds.map { ids =>
+      ids.foreach(fileGroupRemovalRequestQueue ! RemoveFileGroup(_))
+      RequestsSent
+    }
 
   protected val deletedFileGroupFinder: DeletedFileGroupFinder
   protected val fileGroupRemovalRequestQueue: ActorRef
+}
+
+
+object DeletedFileGroupCleaner {
+  def apply(fileGroupRemovalRequestQueue: ActorRef): Props = 
+    Props(new DeletedFileGroupCleanerImpl(fileGroupRemovalRequestQueue))
+  
+  private class DeletedFileGroupCleanerImpl(fileGroupRemovalRequestQueueActor: ActorRef) extends DeletedFileGroupCleaner {
+    override protected val deletedFileGroupFinder = DeletedFileGroupFinder()
+    override protected val fileGroupRemovalRequestQueue = fileGroupRemovalRequestQueueActor
+  }
 }
