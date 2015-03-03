5b6413a4d166766c084098d121111f75d00d9628
Adam Hooper <adam@adamhooper.com>
2015-03-03 11:18:52 -0500

    Optimize incrementing refcounts while cloning

--- worker/src/main/scala/org/overviewproject/clone/DocumentCloner.scala
+++ worker/src/main/scala/org/overviewproject/clone/DocumentCloner.scala
@@ -13,7 +13,7 @@ object DocumentCloner extends InDatabaseCloner {
     val cloneResult = super.clone(sourceDocumentSetId, cloneDocumentSetId)
 
     Logger.debug("updating ref counts")
-    updateFileRefCounts(sourceDocumentSetId)
+    updateFileRefCounts(cloneDocumentSetId)
     Logger.debug("done with ref counts")
     cloneResult
   }
@@ -32,23 +32,21 @@ object DocumentCloner extends InDatabaseCloner {
              FROM document WHERE document_set_id = {sourceDocumentSetId}    	
         """)
 
-  private def updateFileRefCounts(sourceDocumentSetId: Long): Unit = {
+  private def updateFileRefCounts(cloneDocumentSetId: Long): Unit = {
     implicit val c: Connection = Database.currentConnection
 
     SQL("""
-        SELECT * FROM file 
-          WHERE id IN 
-            (SELECT file_id FROM document
-               WHERE document_set_id = {documentSetId})
-          ORDER BY id
-          FOR UPDATE
-        """).on("documentSetId" -> sourceDocumentSetId).execute
-
-    
-    SQL("""
-    	UPDATE file SET reference_count = reference_count + 1
-          WHERE id IN 
-            (SELECT file_id FROM document WHERE document_set_id = {documentSetId})
-    	    """).on("documentSetId" -> sourceDocumentSetId).executeUpdate
+      WITH file_ids AS (
+        SELECT file_id
+        FROM document
+        WHERE document_set_id = {documentSetId}
+          AND file_id IS NOT NULL
+        FOR UPDATE
+      )
+      UPDATE file
+      SET reference_count = reference_count + 1
+      WHERE EXISTS (SELECT 1 FROM file_ids WHERE file_id = file.id)
+      AND reference_count > 0
+    """).on('documentSetId -> cloneDocumentSetId).executeUpdate
   }
 }
