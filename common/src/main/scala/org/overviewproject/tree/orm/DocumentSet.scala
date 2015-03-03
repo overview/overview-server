47e608438a1d61ce1c999bcee4728e57afd35a0a
Adam Hooper <adam@adamhooper.com>
2015-03-03 11:13:28 -0500

    Add orm.DocumentSet#toDocumentSet

--- common/src/main/scala/org/overviewproject/tree/orm/DocumentSet.scala
+++ common/src/main/scala/org/overviewproject/tree/orm/DocumentSet.scala
@@ -4,6 +4,8 @@ import java.sql.Timestamp
 import org.squeryl.KeyedEntity
 import org.squeryl.annotations.Column
 import scala.language.implicitConversions
+
+import org.overviewproject.models.{DocumentSet=>GoodDocumentSet}
 import org.overviewproject.util.DocumentSetVersion
 
 case class DocumentSet(
@@ -21,6 +23,20 @@ case class DocumentSet(
 
   // https://www.assembla.com/spaces/squeryl/tickets/68-add-support-for-full-updates-on-immutable-case-classes#/followers/ticket:68
   override def isPersisted(): Boolean = (id > 0)
+
+  def toDocumentSet: GoodDocumentSet = GoodDocumentSet(
+    id,
+    title,
+    query,
+    isPublic,
+    createdAt,
+    documentCount,
+    documentProcessingErrorCount,
+    importOverflowCount,
+    uploadedFileId,
+    version,
+    deleted
+  )
 }
 
 object DocumentSet {
