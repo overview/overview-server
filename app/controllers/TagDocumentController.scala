b81c4f9bb7978c08442e5a149b31084f59e97fd9
Adam Hooper <adam@adamhooper.com>
2015-04-01 11:53:59 -0400

    Add GET /documentsets/:id/tags/count
    [refs #91513230]

--- app/controllers/TagDocumentController.scala
+++ app/controllers/TagDocumentController.scala
@@ -1,10 +1,11 @@
 package controllers
 
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json.{JsObject,JsNumber}
 import scala.concurrent.Future
 
 import controllers.auth.{AuthorizedAction,AuthorizedRequest}
-import controllers.auth.Authorities.userOwningTag
+import controllers.auth.Authorities.{userViewingDocumentSet,userOwningTag}
 import controllers.backend.{SelectionBackend,TagDocumentBackend}
 
 trait TagDocumentController extends Controller {
@@ -18,6 +19,20 @@ trait TagDocumentController extends Controller {
       selection <- selectionBackend.findOrCreate(request.user.email, sr)
       ids <- selection.getAllDocumentIds
     } yield ids
+  }
+
+  def count(documentSetId: Long) = AuthorizedAction(userViewingDocumentSet(documentSetId)).async { request =>
+    for {
+      ids <- getDocumentIds(documentSetId, request)
+      counts <- tagDocumentBackend.count(documentSetId, ids)
+    } yield {
+      val fields: Seq[(String,JsNumber)] = counts.toSeq.map { (x: (Long,Int)) =>
+        val key = x._1.toString
+        val number = BigDecimal(x._2)
+        key -> JsNumber(number)
+      }
+      Ok(JsObject(fields))
+    }
   }
 
   def createMany(documentSetId: Long, tagId: Long) = AuthorizedAction(userOwningTag(documentSetId, tagId)).async { implicit request =>
--- test/controllers/TagDocumentControllerSpec.scala
+++ test/controllers/TagDocumentControllerSpec.scala
@@ -1,18 +1,53 @@
 package controllers
 
+import org.specs2.matcher.JsonMatchers
 import org.specs2.specification.Scope
 import scala.concurrent.Future
 
 import controllers.backend.{SelectionBackend,TagDocumentBackend}
 import models.{SelectionLike,SelectionRequest}
 
-class TagDocumentControllerSpec extends ControllerSpecification {
+class TagDocumentControllerSpec extends ControllerSpecification with JsonMatchers {
   trait BaseScope extends Scope {
     val mockSelectionBackend = smartMock[SelectionBackend]
     val mockTagDocumentBackend = smartMock[TagDocumentBackend]
     val controller = new TagDocumentController {
       override val selectionBackend = mockSelectionBackend
       override val tagDocumentBackend = mockTagDocumentBackend
+    }
+  }
+
+  "#count" should {
+    trait CountScope extends BaseScope {
+      val documentSetId = 1L
+      val formBody: Seq[(String,String)] = Seq()
+      lazy val request = fakeAuthorizedRequest.withFormUrlEncodedBody(formBody: _*)
+      lazy val result = controller.count(documentSetId)(request)
+
+      val mockSelection = mock[SelectionLike]
+      mockSelection.getAllDocumentIds returns Future.successful(Seq(2L, 3L, 4L))
+      mockSelectionBackend.findOrCreate(any, any) returns Future.successful(mockSelection)
+      mockTagDocumentBackend.count(any, any) returns Future.successful(Map())
+    }
+
+    "return 200 Ok with empty JSON" in new CountScope {
+      h.status(result) must beEqualTo(h.OK)
+      h.contentAsString(result) must beEqualTo("{}")
+    }
+
+    "return JSON" in new CountScope {
+      mockTagDocumentBackend.count(any, any) returns Future.successful(Map(5L -> 6, 7L -> 8))
+      val json = h.contentAsString(result)
+      json must /("5" -> 6)
+      json must /("7" -> 8)
+    }
+
+    "findOrCreate a Selection and use its IDs" in new CountScope {
+      override val formBody = Seq("q" -> "moo")
+      h.status(result)
+      there was one(mockSelectionBackend).findOrCreate(request.user.email, SelectionRequest(documentSetId, q="moo"))
+      there was one(mockSelection).getAllDocumentIds
+      there was one(mockTagDocumentBackend).count(documentSetId, Seq(2L, 3L, 4L))
     }
   }
 
