cca4514dd2a8a1567ad7c2ca9113d1ec155e8f9e
Jonas Karlsson <thejonas@gmail.com>
2015-04-09 14:34:50 -0400

    Convert files to pdf

---
+++ documentset-worker/src/main/scala/org/overviewproject/jobhandler/filegroup/task/step/CreateFileWithView.scala
@@ -0,0 +1,65 @@
+package org.overviewproject.jobhandler.filegroup.task.step
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.blocking
+import scala.concurrent.Future
+import org.overviewproject.blobstorage.BlobBucketId
+import org.overviewproject.database.Slick.simple._
+import org.overviewproject.database.SlickClient
+import org.overviewproject.models.File
+import org.overviewproject.models.GroupedFileUpload
+import org.overviewproject.models.TempDocumentSetFile
+import org.overviewproject.models.tables.Files
+import org.overviewproject.models.tables.GroupedFileUploads
+import org.overviewproject.models.tables.TempDocumentSetFiles
+import java.io.InputStream
+import scala.util.control.Exception.ultimately
+import org.overviewproject.jobhandler.filegroup.task.DocumentConverter
+
+trait CreateFileWithView extends TaskStep with LargeObjectMover with SlickClient {
+  protected val documentSetId: Long
+  protected val uploadedFileId: Long
+  protected val converter: DocumentConverter
+  
+  protected val nextStep: File => TaskStep
+
+  override def execute: Future[TaskStep] =
+    for {
+      upload <- findUpload
+      contentsLocation <- moveLargeObjectToBlobStorage(upload.contentsOid, upload.size, BlobBucketId.FileContents)
+      (viewLocation, viewSize) <- createView(upload)
+      file = File(0l, 11, upload.name, contentsLocation, upload.size, viewLocation, viewSize)
+      savedFile <- writeFile(file)
+    } yield nextStep(savedFile)
+
+  private def findUpload: Future[GroupedFileUpload] = db { implicit session =>
+    GroupedFileUploads.filter(_.id === uploadedFileId).first
+  }
+
+  private def createView(upload: GroupedFileUpload): Future[(String, Long)] = blocking {
+    withLargeObjectInputStream(upload.contentsOid) { stream =>
+      converter.withStreamAsPdf(upload.guid, upload.name, stream){ (viewStream, viewSize) =>
+        blobStorage.create(BlobBucketId.FileView, viewStream, viewSize)
+          .map((_, viewSize))
+      }
+    }
+  }
+
+  private def writeFile(file: File): Future[File] = db { implicit session =>
+    withTransaction(writeFileAndTempDocumentSetFile(file)(_))
+  }
+
+  private def writeFileAndTempDocumentSetFile(file: File)(implicit session: Session): File = {
+    val fileId = (Files returning Files.map(_.id)) += file
+    TempDocumentSetFiles += TempDocumentSetFile(documentSetId, fileId)
+    file.copy(id = fileId)
+  }
+
+  private def withLargeObjectInputStream[T](oid: Long)(f: InputStream => T): T = {
+    val stream = largeObjectInputStream(oid)
+
+    ultimately(stream.close) {
+      f(stream)
+    }
+  }
+}
---
+++ documentset-worker/src/test/scala/org/overviewproject/jobhandler/filegroup/task/step/CreateFileWithViewSpec.scala
@@ -0,0 +1,89 @@
+package org.overviewproject.jobhandler.filegroup.task.step
+
+import org.overviewproject.test.DbSpecification
+import org.specs2.mock.Mockito
+import org.overviewproject.models.File
+import org.overviewproject.models.tables.Files
+import org.overviewproject.test.SlickClientInSession
+import org.overviewproject.blobstorage.BlobStorage
+import java.io.InputStream
+import scala.slick.jdbc.JdbcBackend.Session
+import scala.concurrent.Future
+import org.overviewproject.blobstorage.BlobBucketId
+import org.overviewproject.jobhandler.filegroup.task.DocumentConverter
+import java.util.UUID
+
+class CreateFileWithViewSpec extends DbSpecification with Mockito {
+
+  "CreateFileWithView" should {
+
+    "create a File with a PDF view" in new ConvertScope {
+      await(createFileWithView.execute)
+
+      val file = findFile
+
+      file.map(f => (f.contentsLocation, f.contentsSize, f.viewLocation, f.viewSize)) must
+        beSome(contentsLocation, contentsSize, viewLocation, viewSize)
+    }
+
+    "return next step with File" in new ConvertScope {
+      val NextStep(file) = await(createFileWithView.execute)
+      
+      val savedFile = findFile
+      
+      file must beEqualTo (savedFile.get)
+    }
+
+    trait ConvertScope extends DbScope {
+
+      val contentsLocation = "content:location"
+      val contentsSize = 100l
+      val viewLocation = "view:location"
+      val viewSize = 200l
+
+      val fileGroup = factory.fileGroup()
+      val upload = factory.groupedFileUpload(fileGroupId = fileGroup.id, size = contentsSize)
+
+      val createFileWithView = new TestCreateFileWithView
+
+      def findFile: Option[File] = {
+        import org.overviewproject.database.Slick.simple._
+        Files.firstOption(session)
+      }
+
+      class NullConverter extends DocumentConverter {
+        override def withStreamAsPdf[T](guid: UUID, filename: String, inputStream: InputStream)
+          (f: (InputStream, Long) => T) = f(inputStream, viewSize)
+      }
+
+      case class NextStep(file: File) extends TaskStep {
+        override def execute = Future.successful(this)
+      }
+      
+      
+      class TestCreateFileWithView(implicit val session: Session) extends CreateFileWithView with SlickClientInSession {
+        override protected val documentSetId = 1l
+        override protected val uploadedFileId = upload.id
+        override protected val nextStep = { f: File => NextStep(f) }
+        
+        override protected val blobStorage = smartMock[BlobStorage]
+        override protected def largeObjectInputStream(oid: Long) = smartMock[InputStream]
+        override protected val converter = new NullConverter
+        
+        blobStorage.create(
+          be(BlobBucketId.FileContents),
+          any,
+          be_===(contentsSize)) returns Future.successful(contentsLocation)
+          
+        blobStorage.create(
+          be(BlobBucketId.FileView),
+          any,
+          be_===(viewSize)) returns Future.successful(viewLocation)
+          
+      }
+
+
+    }
+  }
+
+}
