da209da494a987427c8a4f30eec50a57bd53466e
Jonas Karlsson <thejonas@gmail.com>
2015-05-01 17:13:07 -0400

    Added comment to say This code is broken

--- documentset-worker/src/main/scala/org/overviewproject/jobhandler/filegroup/DocumentIdSupplier.scala
+++ documentset-worker/src/main/scala/org/overviewproject/jobhandler/filegroup/DocumentIdSupplier.scala
@@ -7,6 +7,24 @@ object DocumentIdSupplierProtocol {
   case class IdRequestResponse(documentIds: Seq[Long])
 }
 
+/**
+ * Responds to requests for document ids for a specified document set.
+ * 
+ * Multiple concurrent workers need ids for documents they are creating. [[DocumentIdSupplier]] 
+ * is a source for serially created ids, ensuring that the ids generated do not overlap
+ * with previously used ids.
+ * Request responses are created by [[DocumentIdGenerator]]s, one for each document set id.
+ * To avoid concurrent requests for the same document set id, the creation of a [[DocumentIdGenerator]]
+ * blocks while reading the database to find the last document id used.
+ * In this implementation, requests may block even when not strictly necessary - even if the actor receives
+ * request for different document set ids, the actor will respond to a request before starting to process 
+ * the next one. In the current setup with only two workers this unnecessary blocking should not cause too
+ * much unneeded delay.
+ * In this implementation the [[DocumentIdGenerator]] is never released, because the [[DocumentIdSupplier]]
+ * does not know when the requested ids have actually been written to the database. Eventually the table of
+ * [[DocumentIdGenerator]]s may consume all available memory, but in practice this won't be a problem (hopefully).
+ * 
+ */
 trait DocumentIdSupplier extends Actor {
   import DocumentIdSupplierProtocol._
   
