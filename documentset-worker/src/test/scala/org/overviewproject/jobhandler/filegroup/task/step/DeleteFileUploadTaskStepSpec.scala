4bade9f4f853ee9dae5fe43e74f19dc33318edc7
Jonas Karlsson <thejonas@gmail.com>
2015-05-22 17:55:05 -0400

    Remove multithreading from test [Fixes #87013330]

--- documentset-worker/src/test/scala/org/overviewproject/jobhandler/filegroup/task/step/DeleteFileUploadTaskStepSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/jobhandler/filegroup/task/step/DeleteFileUploadTaskStepSpec.scala
@@ -1,7 +1,6 @@
 package org.overviewproject.jobhandler.filegroup.task.step
 
 import scala.concurrent.Promise
-
 import org.overviewproject.database.DocumentSetCreationJobDeleter
 import org.overviewproject.database.DocumentSetDeleter
 import org.overviewproject.database.FileGroupDeleter
@@ -10,6 +9,7 @@ import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
 import org.specs2.time.NoTimeConversions
+import scala.concurrent.Future
 
 
 class DeleteFileUploadTaskStepSpec extends Specification with Mockito with NoTimeConversions {
@@ -19,59 +19,42 @@ class DeleteFileUploadTaskStepSpec extends Specification with Mockito with NoTim
     "complete when all deleters complete" in new FileUploadScope {
       val result = step.execute
 
-      result.isCompleted must beFalse
-
-      jobDeleted.success(())
-      tempFileDeleted.success(())
-      documentSetDeleted.success(())
-      fileGroupDeleted.success(())
-
       result must beEqualTo(FinalStep).await
     }
 
     "delete job before starting other deleters" in new FileUploadScope {
+      implicit val order = inOrder(
+          mockJobDeleter,
+          mockTempFileDeleter,
+          mockDocumentSetDeleter,
+          mockFileGroupDeleter)
+          
       val result = step.execute
 
-      tempFileDeleted.success(())
-      documentSetDeleted.success(())
-      fileGroupDeleted.success(())
 
-      there was one(mockJobDeleter).deleteByDocumentSet(documentSetId)
-      there was no(mockTempFileDeleter).delete(documentSetId)
-      there was no(mockDocumentSetDeleter).delete(documentSetId)
-      there was no(mockFileGroupDeleter).delete(fileGroupId)
+      there was one(mockJobDeleter).deleteByDocumentSet(documentSetId) andThen
+      one(mockTempFileDeleter).delete(documentSetId) andThen
+      one(mockDocumentSetDeleter).delete(documentSetId) andThen
+      one(mockFileGroupDeleter).delete(fileGroupId)
     }
 
-    "delete temp files before starting other deleters" in new FileUploadScope {
-      val result = step.execute
-
-      jobDeleted.success(())
-      documentSetDeleted.success(())
-      fileGroupDeleted.success(())
-
-      there was one(mockTempFileDeleter).delete(documentSetId)
-      there was no(mockDocumentSetDeleter).delete(documentSetId)
-      there was no(mockFileGroupDeleter).delete(fileGroupId)
-    }
   }
 
   trait FileUploadScope extends Scope {
     val documentSetId = 1l
     val fileGroupId = 13l
-    val jobDeleted = Promise[Unit]()
-    val documentSetDeleted = Promise[Unit]()
-    val fileGroupDeleted = Promise[Unit]()
-    val tempFileDeleted = Promise[Unit]()
 
     val mockJobDeleter = smartMock[DocumentSetCreationJobDeleter]
     val mockDocumentSetDeleter = smartMock[DocumentSetDeleter]
     val mockFileGroupDeleter = smartMock[FileGroupDeleter]
     val mockTempFileDeleter = smartMock[TempFileDeleter]
 
-    mockJobDeleter.deleteByDocumentSet(documentSetId) returns jobDeleted.future
-    mockDocumentSetDeleter.delete(documentSetId) returns documentSetDeleted.future
-    mockFileGroupDeleter.delete(fileGroupId) returns fileGroupDeleted.future
-    mockTempFileDeleter.delete(documentSetId) returns tempFileDeleted.future
+    def success = Future.successful(())
+    
+    mockJobDeleter.deleteByDocumentSet(documentSetId) returns success
+    mockDocumentSetDeleter.delete(documentSetId) returns success
+    mockFileGroupDeleter.delete(fileGroupId) returns success
+    mockTempFileDeleter.delete(documentSetId) returns success
 
     val step = new TestDeleteFileUploadTaskStep(documentSetId, fileGroupId)
 
