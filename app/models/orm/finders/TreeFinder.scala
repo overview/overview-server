d20b2502026d3a4ed20c5e36eb1d5e16c08b07df
Adam Hooper <adam@adamhooper.com>
2015-06-03 12:15:06 -0400

    Nix BaseTreeFinder
    (The methods are all in TreeFinder now)

--- app/models/orm/finders/TreeFinder.scala
+++ app/models/orm/finders/TreeFinder.scala
@@ -1,6 +1,47 @@
 package models.orm.finders
 
 import models.orm.Schema
-import org.overviewproject.tree.orm.finders.BaseTreeFinder
+import org.squeryl.Table
+import scala.language.postfixOps
 
-object TreeFinder extends BaseTreeFinder(Schema.trees)
+import org.overviewproject.postgres.SquerylEntrypoint._
+import org.overviewproject.tree.orm.finders.{DocumentSetComponentFinder,FinderResult}
+import org.overviewproject.tree.orm.Tree
+
+object TreeFinder extends DocumentSetComponentFinder[Tree] {
+  override protected val table = Schema.trees
+
+  type TreeFinderResult = FinderResult[Tree]
+
+  /** All Trees for the given document sets.
+    *
+    * These are ordered from newest to oldest.
+    */
+  def byDocumentSets(documentSets: Iterable[Long]): TreeFinderResult = {
+    from(table)(t =>
+      where(t.documentSetId in documentSets)
+      select(t)
+      orderBy(t.createdAt desc)
+    )
+  }
+
+  /** All Trees for the given document set.
+    *
+    * These are ordered from newest to oldest.
+    */
+  override def byDocumentSet(documentSet: Long) : TreeFinderResult = {
+    from(table)(t =>
+      where(t.documentSetId === documentSet)
+      select(t)
+      orderBy(t.createdAt desc)
+    )
+  }
+  
+  def byId(id: Long): TreeFinderResult = {
+    table.where(_.id === id)
+  }
+
+  def byJobId(jobId: Long): TreeFinderResult = {
+    table.where(_.jobId === jobId)
+  }
+}
