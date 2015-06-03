0be108ace5cc829f8893243645698be5add572c8
Adam Hooper <adam@adamhooper.com>
2015-06-02 20:30:42 -0400

    Remove some old Stores

--- documentset-worker/src/main/scala/org/overviewproject/database/orm/stores/GroupedFileUploadStore.scala
+++
@@ -1,7 +0,0 @@
-package org.overviewproject.database.orm.stores
-
-import org.overviewproject.database.orm.Schema
-import org.overviewproject.postgres.SquerylEntrypoint._
-import org.overviewproject.tree.orm.stores.BaseGroupedFileUploadStore
-
-object GroupedFileUploadStore extends BaseGroupedFileUploadStore(Schema.groupedFileUploads) 
--- worker/src/main/scala/org/overviewproject/persistence/orm/stores/GroupedFileUploadStore.scala
+++
@@ -1,6 +0,0 @@
-package org.overviewproject.persistence.orm.stores
-
-import org.overviewproject.tree.orm.stores.BaseStore
-import org.overviewproject.persistence.orm.Schema
-
-object GroupedFileUploadStore extends BaseStore(Schema.groupedFileUploads)
