7155c20f70c4ea344d1869b39872eba5c29b2a51
Jonas Karlsson <thejonas@gmail.com>
2015-06-24 15:31:25 -0400

    Start OCR step if no font found

--- documentset-worker/src/main/scala/org/overviewproject/jobhandler/filegroup/task/step/ExtractTextWithOcr.scala
+++ documentset-worker/src/main/scala/org/overviewproject/jobhandler/filegroup/task/step/ExtractTextWithOcr.scala
@@ -12,6 +12,8 @@ trait ExtractTextWithOcr extends UploadedFileProcessStep {
   override protected lazy val filename = file.name
 
   protected val nextStep: Seq[DocumentData] => TaskStep
+  protected def startOcr(file: File, document: PdfDocument): TaskStep
+
   protected val pdfProcessor: PdfProcessor
 
   protected trait PdfProcessor {
@@ -21,6 +23,15 @@ trait ExtractTextWithOcr extends UploadedFileProcessStep {
   override protected def doExecute: Future[TaskStep] = for {
     pdfDocument <- pdfProcessor.loadFromBlobStorage(file.viewLocation)
   } yield ultimately(pdfDocument.close) {
-    nextStep(Seq(PdfFileDocumentData(file.name, file.id, pdfDocument.text)))
+    pdfDocument.textWithFonts.fold(
+        _ => startOcr(file, pdfDocument),
+        startNextStep)
+    
+  }
+
+      
+  private def startNextStep(text: String): TaskStep = {
+    val documentInfo = Seq(PdfFileDocumentData(file.name, file.id, text)) 
+    nextStep(documentInfo)
   }
 }
--- documentset-worker/src/test/scala/org/overviewproject/jobhandler/filegroup/task/step/ExtractTextWithrOcrSpec.scala
+++ documentset-worker/src/test/scala/org/overviewproject/jobhandler/filegroup/task/step/ExtractTextWithrOcrSpec.scala
@@ -13,44 +13,64 @@ class ExtractTextWithrOcrSpec extends Specification with Mockito {
   "ExtractTextWithOcr" should {
 
     "attempt to extract text without OCR" in new PdfWithTextContext {
-      val extractTextStep = new TestExtractTextWithOcr
 
       val r = extractTextStep.execute
 
       r must be_==(NextStep(documentData)).await
     }
 
-    "start ocr step if needed" in {
-      todo
+    "start ocr step if needed" in new PdfWithNoTextContext {
+      val r = extractTextStep.execute
+
+      r must be_==(OcrStep(pdfFile, document)).await
     }
   }
 
-  trait PdfWithTextContext extends Scope {
+  trait PdfContext extends Scope {
     val fileName = "file name"
     val viewLocation = "view location"
     val fileId = 1l
-    val text = "extracted text"
-    val documentData = Seq(PdfFileDocumentData(fileName, fileId, text))
-    
-    class TestExtractTextWithOcr extends ExtractTextWithOcr {
+    val pdfFile = smartMock[File]
+    pdfFile.name returns fileName
+    pdfFile.viewLocation returns viewLocation
+    pdfFile.id returns fileId
+
+    class TestExtractTextWithOcr(document: PdfDocument) extends ExtractTextWithOcr {
       override protected val executor: ExecutionContext = implicitly
       override protected val documentSetId = 1l
-
-      override protected val file = smartMock[File]
-      file.name returns fileName
-      file.viewLocation returns viewLocation
-      file.id returns fileId
+      override protected val file = pdfFile
       
-      val pdfDocument = smartMock[PdfDocument]
-      pdfDocument.text returns text
       override protected val pdfProcessor = smartMock[PdfProcessor]
-      pdfProcessor.loadFromBlobStorage(viewLocation) returns Future.successful(pdfDocument)
-      
+      pdfProcessor.loadFromBlobStorage(viewLocation) returns Future.successful(document)
+
       override protected val nextStep = NextStep(_)
+      override protected def startOcr(f: File, d: PdfDocument): TaskStep = OcrStep(f, d)
     }
+
+  }
+
+  trait PdfWithTextContext extends PdfContext {
+    val text = "extracted text"
+    val documentData = Seq(PdfFileDocumentData(fileName, fileId, text))
+    val document = smartMock[PdfDocument]
+    document.textWithFonts returns Right(text)
+
+    val extractTextStep = new TestExtractTextWithOcr(document)
+
+  }
+
+  trait PdfWithNoTextContext extends PdfContext {
+    val document = smartMock[PdfDocument]
+    document.textWithFonts returns Left("")
+
+    val extractTextStep = new TestExtractTextWithOcr(document)
+
   }
 
   case class NextStep(documentData: Seq[DocumentData]) extends TaskStep {
+    override def execute = Future.successful(this)
+  }
+  case class OcrStep(file: File, document: PdfDocument) extends TaskStep {
     override def execute = Future.successful(this)
   }
 
