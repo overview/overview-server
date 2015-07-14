3413772a8552de9d07d33df92f2eadc52d7e11d3
Adam Hooper <adam@adamhooper.com>
2015-07-14 19:06:05 -0400

    Whoops. Make unit tests pass again.
    Unit tests *and* dev environment. Wow, I'm really covering all the
    bases, eh?

--- app/modules/DbConnection.scala
+++ app/modules/DbConnection.scala
@@ -3,6 +3,7 @@ package modules
 import com.zaxxer.hikari.HikariDataSource
 import javax.inject._
 import play.api.inject.ApplicationLifecycle
+import play.api.{Mode,Play}
 import scala.concurrent.Future
 
 import org.overviewproject.database.DB
@@ -17,7 +18,12 @@ import org.overviewproject.database.DB
   * the application's ClassLoader and create another. That leaks every
   * singleton in the old ClassLoader. So DB.dataSource will stay open forever.
   *
-  * Load this module, and the database will shut down at the right time.
+  * Load this module, and the database will shut down at the right time. It
+  * will only register its shutdown hook in dev mode, because dev is the only
+  * mode that leaks a ClassLoader. (Test, in particular, starts up and shuts
+  * down dozens of Applications, all in the same ClassLoader; and then the
+  * process, which was forked, ends. So we definitely don't want to shut down
+  * in Test mode.)
   *
   * And why is this file so complicated? Because I don't really understand Play
   * modules, and I'm not in the mood to. Just scratching an itch here!
@@ -26,5 +32,7 @@ import org.overviewproject.database.DB
 class DbConnection @Inject() (lifecycle: ApplicationLifecycle) {
   val dataSource: HikariDataSource = DB.dataSource
 
-  lifecycle.addStopHook { () => Future.successful(dataSource.shutdown) }
+  if (Play.current.mode == Mode.Dev) { // Either `current` is set, or the caller shouldn't have called this
+    lifecycle.addStopHook { () => Future.successful(dataSource.shutdown) }
+  }
 }
