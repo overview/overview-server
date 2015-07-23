9a0b95af638e121d48ff4a2b944b778b1cf802c2
Jonas Karlsson <thejonas@gmail.com>
2015-07-23 15:51:47 -0400

    Translate language string to ISO 639-2 format

--- common/src/main/scala/org/overviewproject/util/SupportedLanguage.scala
+++ common/src/main/scala/org/overviewproject/util/SupportedLanguage.scala
@@ -2,7 +2,7 @@ package org.overviewproject.util
 
 import java.util.Locale
 
-case class SupportedLanguage(val languageCode: String) {
+case class SupportedLanguage(languageCode: String) {
   val locale : Locale = new Locale(languageCode)
 
   def displayLanguage(callerLocale: Locale) : String = locale.getDisplayLanguage(callerLocale)
--- common/src/main/scala/org/overviewproject/util/SupportedLanguages.scala
+++ common/src/main/scala/org/overviewproject/util/SupportedLanguages.scala
@@ -3,21 +3,24 @@ package org.overviewproject.util
 import java.util.Locale
 
 object SupportedLanguages {
-  val languageCodes = Set(
-    "ar",
-    "ca",
-    "de",
-    "en",
-    "es",
-    "fr",
-    "sv",
-    "nl",
-    "it",
-    "pt",
-    "ru"
+  // Supported languages, iso639-1 mapped to iso639-2 format
+  private val iso639Codes = Map(
+    "ar" -> "ara",
+    "ca" -> "cat",
+    "de" -> "deu",
+    "en" -> "eng",
+    "es" -> "spa",
+    "fr" -> "fra",
+    "sv" -> "swe",
+    "nl" -> "nld",
+    "it" -> "ita",
+    "pt" -> "por",
+    "ru" -> "rus"
   )
 
-  val languages : Set[SupportedLanguage] = languageCodes.map(SupportedLanguage.apply)
+  val languageCodes = iso639Codes.keySet
+  
+  val languages : Set[SupportedLanguage] = languageCodes.map(SupportedLanguage)
 
   val defaultLanguage : SupportedLanguage = SupportedLanguage("en")
 
@@ -25,4 +28,7 @@ object SupportedLanguages {
   def languagesSortedInCallerLocale(callerLocale: Locale) : Seq[SupportedLanguage] = {
     languages.toIndexedSeq.sortBy((lang) => lang.displayLanguage(callerLocale))
   }
+  
+  /** @returns the iso639-2 language code, if the language is supported */
+  def asIso639_2(iso639_1Code: String): Option[String] = iso639Codes.get(iso639_1Code)
 }
