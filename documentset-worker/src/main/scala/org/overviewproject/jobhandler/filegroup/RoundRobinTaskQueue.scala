9a878953f4bc111116b9b52a28ec816e8fdfd44e
Jonas Karlsson <thejonas@gmail.com>
2015-07-31 17:42:14 -0400

    Hand out tasks in round robin order

--- documentset-worker/src/main/scala/org/overviewproject/jobhandler/filegroup/RoundRobinTaskQueue.scala
+++ documentset-worker/src/main/scala/org/overviewproject/jobhandler/filegroup/RoundRobinTaskQueue.scala
@@ -4,21 +4,51 @@ import scala.collection.mutable
 import org.overviewproject.jobhandler.filegroup.task.FileGroupTaskWorkerProtocol.TaskWorkerTask
 
 class RoundRobinTaskQueue extends TaskQueue {
-  override def isEmpty: Boolean = queue.isEmpty
-  
+  override def isEmpty: Boolean = order.isEmpty
+
   override def addTask(task: TaskWorkerTask): TaskQueue = {
-    queue += task
+    val documentSetQueue = queues.getOrElseUpdate(task.documentSetId, mutable.Queue.empty)
+
+    documentSetQueue += task
+    if (!order.exists(_ == task.documentSetId)) order += task.documentSetId
+    
     this
   }
-  
+
   override def addTasks(tasks: Iterable[TaskWorkerTask]): TaskQueue = {
-    queue ++= tasks
+    tasks.map(addTask)
     this
   }
-  
-  override def dequeue: TaskWorkerTask = queue.dequeue
-  override def dequeueAll(p: TaskWorkerTask => Boolean): Seq[TaskWorkerTask] = queue.dequeueAll(p)
-  
-  private val queue: mutable.Queue[TaskWorkerTask] = mutable.Queue.empty
+
+  override def dequeue: TaskWorkerTask = {
+    val documentSetId = order.dequeue
+
+    val taskQueue = queues.get(documentSetId).get
+
+    val task = taskQueue.dequeue
+    
+    if (taskQueue.isEmpty) queues -= task.documentSetId
+    else order += documentSetId
+    
+    task
+  }
+
+  override def dequeueAll(p: TaskWorkerTask => Boolean): Seq[TaskWorkerTask] = {
+    val tasks = order.map(
+      queues.get(_).get
+        .dequeueAll(p))
+
+    queues.keys.map { ds =>
+      if (queues.get(ds).get.isEmpty) {
+        queues -= ds
+        order.dequeueAll(_ == ds)
+      }
+    }
+
+    tasks.flatten
+  }
+
+  private val order: mutable.Queue[Long] = mutable.Queue.empty
+  private val queues: mutable.Map[Long, mutable.Queue[TaskWorkerTask]] = mutable.HashMap.empty
 
 }
