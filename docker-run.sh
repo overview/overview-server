95f2dfd3121802326a9f9dca435274d7606c5aa5
Jonas Karlsson <thejonas@gmail.com>
2015-08-18 10:24:30 -0400

    Persist data

--- docker-run.sh
+++ docker-run.sh
@@ -2,10 +2,11 @@
 
 
 # Start support services
-docker run --name database -h overview-database -d overview/database
+docker run --name database -h overview-database -v /opt/overview-data/postgres:/var/lib/postgresql/data -d overview/database
 docker run --name message-broker -h overview-messagebroker -d overview/message-broker
 docker run --name redis -h overview-redis -d redis:2.8
 docker run --name searchindex -h overview-searchindex \
+  -v /opt/overview-data/elasticsearch:/usr/share/elasticsearch/data \
   -d elasticsearch:1.7 elasticsearch \
   -Des.cluster.name="DevSearchIndex" \
   -Des.node.name="SearchIndex" \
@@ -25,11 +26,13 @@ docker run --name documentset-worker \
   --link database:overview-database \
   --link message-broker:overview-messagebroker \
   --link searchindex:overview-searchindex \
-   -d overview/documentset-worker
+  -v /opt/overview-data/blob-storage:/var/lib/overview/blob-storage \
+  -d overview/documentset-worker
 
 docker run --name worker \
   --link database:database \
   --link searchindex:overview-searchindex \
+  -v /opt/overview-data/blob-storage:/var/lib/overview/blob-storage \
   -d overview/worker
 
 docker run --name web \
@@ -37,5 +40,6 @@ docker run --name web \
   --link message-broker:overview-messagebroker \
   --link searchindex:overview-searchindex \
   --link redis:overview-redis \
+  -v /opt/overview-data/blob-storage:/var/lib/overview/blob-storage \
   -p 9000:9000 \
   -d overview/web
