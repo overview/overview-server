1404a4f90baab3b3cb2034228934d9a11f876946
Jonas Karlsson <thejonas@gmail.com>
2015-08-19 09:55:34 -0400

    Only name containers once

--- docker-run.sh
+++ docker-run.sh
@@ -2,10 +2,10 @@
 
 
 # Start support services
-docker run --name database -h overview-database -v /opt/overview-data/postgres:/var/lib/postgresql/data -d overview/database
-docker run --name message-broker -h overview-messagebroker -d overview/message-broker
-docker run --name redis -h overview-redis -d redis:2.8
-docker run --name searchindex -h overview-searchindex \
+docker run --name overview-database -v /opt/overview-data/postgres:/var/lib/postgresql/data -d overview/database
+docker run --name overview-messagebroker -d overview/message-broker
+docker run --name overview-redis -d redis:2.8
+docker run --name overview-searchindex \
   -v /opt/overview-data/elasticsearch:/usr/share/elasticsearch/data \
   -d elasticsearch:1.7 elasticsearch \
   -Des.cluster.name="DevSearchIndex" \
@@ -16,30 +16,30 @@ docker run --name searchindex -h overview-searchindex \
 
 # Wait for evolutions to be applied before starting other services
 docker run --name db-evolution-applier \
-  --link database:overview-database \
+  --link overview-database \
   --rm overview/db-evolution-applier
 
 
 # Start overview services
 
 docker run --name documentset-worker \
-  --link database:overview-database \
-  --link message-broker:overview-messagebroker \
-  --link searchindex:overview-searchindex \
+  --link overview-database \
+  --link overview-messagebroker \
+  --link overview-searchindex \
   -v /opt/overview-data/blob-storage:/var/lib/overview/blob-storage \
   -d overview/documentset-worker
 
 docker run --name worker \
-  --link database:database \
-  --link searchindex:overview-searchindex \
+  --link overview-database \
+  --link overview-searchindex \
   -v /opt/overview-data/blob-storage:/var/lib/overview/blob-storage \
   -d overview/worker
 
 docker run --name web \
-  --link database:database \
-  --link message-broker:overview-messagebroker \
-  --link searchindex:overview-searchindex \
-  --link redis:overview-redis \
+  --link overview-database \
+  --link overview-messagebroker \
+  --link overview-searchindex \
+  --link overview-redis \
   -v /opt/overview-data/blob-storage:/var/lib/overview/blob-storage \
   -p 9000:9000 \
   -d overview/web
