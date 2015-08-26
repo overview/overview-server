86de9b10e5795f469d183513ed35083eecace1f1
Adam Hooper <adam@adamhooper.com>
2015-08-25 21:13:26 -0400

    Whoops. Nix obsolete Scala unit tests

--- test/controllers/TagControllerSpec.scala
+++ test/controllers/TagControllerSpec.scala
@@ -143,35 +143,4 @@ class TagControllerSpec extends ControllerSpecification with JsonMatchers {
       ))
     }
   }
-
-  "indexJsonWithTree" should {
-    class IndexJsonWithTreeScope extends BaseScope {
-      val documentSetId = 1L
-      val treeId = 2L
-      def tagsWithCounts : Seq[(Tag,Long,Long)] = Seq()
-      mockStorage.findTagsWithCounts(anyInt, anyInt) answers { (_) => tagsWithCounts }
-      val result = controller.indexJsonWithTree(documentSetId, treeId)(fakeAuthorizedRequest)
-    }
-
-    "show an empty list" in new IndexJsonWithTreeScope {
-      h.status(result) must beEqualTo(h.OK)
-      h.contentAsString(result) must beEqualTo("[]")
-    }
-
-    "prevent caching of result" in new IndexJsonWithTreeScope {
-      h.header(h.CACHE_CONTROL, result) must beSome("max-age=0")
-    }
-
-    "show a full list" in new IndexJsonWithTreeScope {
-      override def tagsWithCounts = Seq(
-        (Tag(documentSetId=documentSetId, id=1, name="tag1", color="111111"), 5L, 5L),
-        (Tag(documentSetId=documentSetId, id=2, name="tag2", color="222222"), 10L, 8L)
-      )
-      h.status(result) must beEqualTo(h.OK)
-      h.contentAsJson(result) must beEqualTo(Json.arr(
-        Json.obj("id" -> 1, "name" -> "tag1", "color" -> "#111111", "size" -> 5, "sizeInTree" -> 5),
-        Json.obj("id" -> 2, "name" -> "tag2", "color" -> "#222222", "size" -> 10, "sizeInTree" -> 8)
-      ))
-    }
-  }
 }
