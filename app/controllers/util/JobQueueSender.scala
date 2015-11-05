221935fc0aabd5dc75901f05432dfe79526c5d5d
Adam Hooper <adam@adamhooper.com>
2015-11-05 17:19:24 -0500

    Make JobQueueSender survive restarts

--- app/controllers/util/JobQueueSender.scala
+++ app/controllers/util/JobQueueSender.scala
@@ -1,9 +1,9 @@
 package controllers.util
 
-import akka.actor.{ActorSelection,ActorSystem}
-import com.typesafe.config.ConfigFactory
+import akka.actor.ActorSelection
 
 import com.overviewdocs.messages.DocumentSetCommands
+import modules.RemoteActorSystemModule
 
 trait JobQueueSender {
   protected def documentSetWorkerActor: ActorSelection
@@ -14,17 +14,7 @@ trait JobQueueSender {
 }
 
 object JobQueueSender extends JobQueueSender {
-  // FIXME survive restarts in development
-  override protected lazy val documentSetWorkerActor = find("DocumentSetMessageBroker")
-
-  private lazy val config = ConfigFactory.load.getConfig("documentset-worker")
-  private lazy val hostname = config.getString("message_broker_hostname")
-  private lazy val port = config.getInt("message_broker_port")
-  private lazy val rootPath = s"akka.tcp://WorkerActorSystem@${hostname}:${port}/user/supervised"
-
-  private lazy val remoteSystem = ActorSystem.create("documentset-worker", config)
-
-  private def find(name: String): ActorSelection = {
-    remoteSystem.actorSelection(rootPath + "/"+ name)
+  override protected lazy val documentSetWorkerActor = {
+    play.api.Play.current.injector.instanceOf[RemoteActorSystemModule].documentSetWorkerActor
   }
 }
---
+++ app/modules/RemoteActorSystemModule.scala
@@ -0,0 +1,41 @@
+package modules
+
+import akka.actor.{ActorSelection,ActorSystem}
+import com.typesafe.config.ConfigFactory
+import javax.inject._
+import play.api.inject.ApplicationLifecycle
+import scala.concurrent.Future
+
+/** Provides a RemoteActorSystem, to communicate with documentset-worker.
+  *
+  * We have three behaviors:
+  *
+  * * In production, the actor system starts on first reference and stops on
+  *   shutdown.
+  * * In dev, the actor system stops every time the app shuts down. (This is
+  *   why we *need* this module: otherwise we try to listen on the same port
+  *   every time the server restarts.)
+  * * In test, we never ever reference this ActorSystem.
+  */
+@Singleton
+class RemoteActorSystemModule @Inject() (lifecycle: ApplicationLifecycle) {
+  private lazy val config = ConfigFactory.load.getConfig("documentset-worker")
+  private lazy val hostname = config.getString("message_broker_hostname")
+  private lazy val port = config.getInt("message_broker_port")
+  private lazy val rootPath = s"akka.tcp://WorkerActorSystem@${hostname}:${port}/user/supervised"
+
+  lazy val remoteActorSystem: ActorSystem = {
+    val ret = ActorSystem.create("documentset-worker", config)
+
+    lifecycle.addStopHook { () =>
+      ret.shutdown
+      Future.successful(())
+    }
+
+    ret
+  }
+
+  lazy val documentSetWorkerActor: ActorSelection = {
+    remoteActorSystem.actorSelection(rootPath + "/DocumentSetMessageBroker")
+  }
+}
