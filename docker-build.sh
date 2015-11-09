258169ded9e9b26318e3bd3fe947a5467a7ecb73
Adam Hooper <adam@adamhooper.com>
2015-11-09 17:37:43 -0500

    Update Docker scripts. WIP.

--- docker-build.sh
+++ docker-build.sh
@@ -2,16 +2,14 @@
 
 cd "$(dirname "$0")"
 
-
 # Support services
-docker build -t overview/database docker/database
+docker build --rm -t overview/database docker/database
+docker build --rm -t overview/searchindex docker/searchindex
 
 # Create a base with everything built and staged
-docker build -t overview/overview-base  docker/overview-base
+docker build --rm -t overview/overview-base docker/overview-base
 
-# Overview services
-docker build -t overview/db-evolution-applier docker/db-evolution-applier
-docker build -t overview/documentset-worker docker/documentset-worker
-docker build -t overview/worker docker/worker
-
-docker build -t overview/web docker/web
+docker build --rm -t overview/db-evolution-applier docker/db-evolution-applier
+docker build --rm -t overview/documentset-worker docker/documentset-worker
+docker build --rm -t overview/worker docker/worker
+docker build --rm -t overview/web docker/web
