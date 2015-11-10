6b0488dc360d2de7781d50b358f1c860fc1da374
Adam Hooper <adam@adamhooper.com>
2015-11-10 10:14:14 -0500

    Move Docker stuff around. WIP.

--- auto/docker-build.sh
+++ auto/docker-build.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")"/..
 
 # Support services
 docker build --rm -t overview/database docker/database
---
+++ auto/docker-publish.sh
@@ -0,0 +1,6 @@
+docker push overview/database
+docker push overview/searchindex
+docker push overview/db-evolution-applier
+docker push overview/documentset-worker
+docker push overview/worker
+docker push overview/web
