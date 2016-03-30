ded65cd50981bce18c04d5a5467879a3652b5b75
Adam Hooper <adam@adamhooper.com>
2016-03-30 08:28:54 -0400

    Textify error messages.
    I don't know why an error message would contain \u0000. But evidently,
    one does.

--- worker/src/main/scala/com/overviewdocs/jobhandler/filegroup/task/WriteDocumentProcessingError.scala
+++ worker/src/main/scala/com/overviewdocs/jobhandler/filegroup/task/WriteDocumentProcessingError.scala
@@ -4,7 +4,7 @@ import scala.concurrent.{ExecutionContext,Future}
 
 import com.overviewdocs.database.HasDatabase
 import com.overviewdocs.models.tables.DocumentProcessingErrors
-import com.overviewdocs.util.Logger
+import com.overviewdocs.util.{Logger,Textify}
 
 object WriteDocumentProcessingError extends HasDatabase {
   private lazy val logger = Logger.forClass(getClass)
@@ -16,6 +16,17 @@ object WriteDocumentProcessingError extends HasDatabase {
   def apply(documentSetId: Long, filename: String, message: String)(implicit ec: ExecutionContext): Future[Unit] = {
     logger.info("Error processing {} in docset {}: {}", filename, documentSetId, message)
 
-    database.runUnit(inserter.+=((documentSetId, filename, message)))
+    /*
+     * [adam, 2016-03-30] Somehow, it's possible for a PDF error to contain
+     * \u0000. I have no idea how, and I have no idea whether we should be
+     * escaping that text _here_. I guess the logic is, we should be able to
+     * handle any error message.
+     *
+     * Really, I'm frustrated by Postgres. What does it have against \u0000,
+     * anyway?
+     */
+    val escapedMessage = Textify(message)
+
+    database.runUnit(inserter.+=((documentSetId, filename, escapedMessage)))
   }
 }
