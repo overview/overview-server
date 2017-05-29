7168e14922e52b294ea39767d8dd02a66d61ec5f
Adam Hooper <adam@adamhooper.com>
2017-05-29 09:56:00 -0400

    Nix another override def before

--- worker/src/test/scala/com/overviewdocs/database/background/filecleanup/FileRemovalRequestQueueSpec.scala
+++ worker/src/test/scala/com/overviewdocs/database/background/filecleanup/FileRemovalRequestQueueSpec.scala
@@ -3,7 +3,6 @@ package com.overviewdocs.background.filecleanup
 import akka.actor.ActorRef
 import akka.actor.Props
 import akka.testkit.TestProbe
-import org.specs2.mutable.Before
 import org.specs2.mutable.Specification
 import scala.concurrent.duration.Duration
 
@@ -14,17 +13,24 @@ import com.overviewdocs.test.ActorSystemContext
 class FileRemovalRequestQueueSpec extends Specification {
   sequential
 
+  trait QueueScope extends ActorSystemContext {
+    class TestFileRemovalRequestQueue(val fileRemover: ActorRef) extends FileRemovalRequestQueue
+
+    lazy val fileRemover: TestProbe = TestProbe()
+    lazy val fileRemovalQueue: ActorRef = system.actorOf(Props(new TestFileRemovalRequestQueue(fileRemover.ref)))
+  }
+
   "FileRemovalRequestQueue" should {
 
     "send a request on startup" in new QueueScope {
+      fileRemovalQueue
       fileRemover.expectMsg(RemoveDeletedFiles)
     }
 
     "send only one request if new request are received before previous request is complete" in new QueueScope {
        fileRemovalQueue ! RemoveFiles
        fileRemovalQueue ! RemoveFiles
-       
-       
+
        fileRemover.expectMsg(RemoveDeletedFiles)
        fileRemover.reply(FileRemovalComplete)
        fileRemover.expectMsg(RemoveDeletedFiles)
@@ -32,25 +38,15 @@ class FileRemovalRequestQueueSpec extends Specification {
 
        fileRemover.expectNoMsg(Duration.Zero)
     }
-    
+
     "send a request received after a previous one completes" in new QueueScope {
+      fileRemovalQueue
+
       fileRemover.expectMsg(RemoveDeletedFiles)
       fileRemover.reply(FileRemovalComplete)
-      
+
       fileRemovalQueue ! RemoveFiles
       fileRemover.expectMsg(RemoveDeletedFiles)
-    } 
-    
-  }
-
-  abstract class QueueScope extends ActorSystemContext with Before {
-    var fileRemover: TestProbe = _
-    var fileRemovalQueue: ActorRef = _
-
-    override def before = {
-      fileRemover = TestProbe()
-      fileRemovalQueue = system.actorOf(Props(new TestFileRemovalRequestQueue(fileRemover.ref)))
     }
   }
-  class TestFileRemovalRequestQueue(val fileRemover: ActorRef) extends FileRemovalRequestQueue
 }
