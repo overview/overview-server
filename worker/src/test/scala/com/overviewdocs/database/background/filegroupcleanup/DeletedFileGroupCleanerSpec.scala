4dc04fbcac4d96cde0a8eb0ed6b36b135ab88e8e
Adam Hooper <adam@adamhooper.com>
2017-05-29 10:00:46 -0400

    Nix another override def before

--- worker/src/test/scala/com/overviewdocs/database/background/filegroupcleanup/DeletedFileGroupCleanerSpec.scala
+++ worker/src/test/scala/com/overviewdocs/database/background/filegroupcleanup/DeletedFileGroupCleanerSpec.scala
@@ -7,46 +7,40 @@ import org.specs2.mutable.Specification
 import org.specs2.mock.Mockito
 import com.overviewdocs.background.filegroupcleanup.FileGroupRemovalRequestQueueProtocol._
 import com.overviewdocs.test.ActorSystemContext
-import org.specs2.mutable.Before
 
 
 class DeletedFileGroupCleanerSpec extends Specification with Mockito {
   sequential
-  
+
+  trait DeletedFileGroupScope extends ActorSystemContext {
+    val deletedFileGroupIds = Seq(1L, 2L, 3L)
+
+    lazy val supervisor = TestProbe()
+    lazy val queue = TestProbe()
+    lazy val cleaner: ActorRef = {
+      val ret = system.actorOf(Props(new TestDeletedFileGroupCleaner(queue.ref, deletedFileGroupIds)))
+      supervisor.watch(ret)
+      ret
+    }
+
+    class TestDeletedFileGroupCleaner(queue: ActorRef, ids: Seq[Long]) extends DeletedFileGroupCleaner {
+      override protected val deletedFileGroupFinder = smartMock[DeletedFileGroupFinder]
+      deletedFileGroupFinder.indexIds returns Future.successful(ids)
+
+      override protected val fileGroupRemovalRequestQueue = queue
+    }
+  }
+
   "DeletedFileGroupCleaner" should {
-    
     "send FileGroup removal requests to queue on start up" in new DeletedFileGroupScope {
-      val requests = queue.receiveN(numberOfFileGroups)
-      
+      cleaner
+      val requests = queue.receiveN(3)
       requests must containTheSameElementsAs(deletedFileGroupIds.map(RemoveFileGroup))
     }
-    
+
     "die after requests have been sent" in new DeletedFileGroupScope {
+      cleaner
       supervisor.expectTerminated(cleaner)
     }
-  }
-
-  abstract class DeletedFileGroupScope extends ActorSystemContext with Before {
-    val numberOfFileGroups = 3
-    val deletedFileGroupIds = Seq.range[Long](0, numberOfFileGroups)
-    var supervisor: TestProbe = _ 
-    var queue: TestProbe = _
-    var cleaner: ActorRef = _
-
-    override def before = {
-      supervisor = TestProbe()
-      queue = TestProbe()
-      cleaner = system.actorOf(Props(new TestDeletedFileGroupCleaner(queue.ref, deletedFileGroupIds)))
-      
-      supervisor watch cleaner
-    }
-  }
-  
-  class TestDeletedFileGroupCleaner(queue: ActorRef, ids: Seq[Long]) extends DeletedFileGroupCleaner {
-    import context._
-    override protected val deletedFileGroupFinder = smartMock[DeletedFileGroupFinder]
-    deletedFileGroupFinder.indexIds returns Future { ids }
-    
-    override protected val fileGroupRemovalRequestQueue = queue
   }
 }
