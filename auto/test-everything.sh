e2a77a10b3e4594549d1853c74878a89a9cdaf91
Adam Hooper <adam@adamhooper.com>
2017-07-20 14:01:12 -0400

    s/overview-server/web

--- auto/test-everything.sh
+++ auto/test-everything.sh
@@ -47,7 +47,7 @@ sleep 20
 echo "Launching Overview..."
 unzip -o -q overview-server.zip
 overview-server/db-evolution-applier/db-evolution-applier
-overview-server/frontend/overview-server &
+overview-server/web/web &
 PIDS="$!"
 overview-server/worker/worker &
 PIDS="$PIDS $!"
@@ -57,7 +57,7 @@ until $(echo | nc localhost 9000 -w 1 2>/dev/null); do sleep 1; done
 echo "Waiting an additional 60s for system to calm down..." >&2
 sleep 60
 
-(cd "$DIR"/test/integration && npm run-script test-with-jenkins) || true
+(cd "$DIR"/web/test/integration && npm run-script test-with-jenkins) || true
 
 kill $PIDS
 wait
