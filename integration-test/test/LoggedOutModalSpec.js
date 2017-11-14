bd4a7a45d53c8ff9cc0e524aa575d50965700d94
Adam Hooper <adam@adamhooper.com>
2017-11-14 17:05:38 -0500

    At long last: fix LoggedOutModalSpec

--- integration-test/test/LoggedOutModalSpec.js
+++ integration-test/test/LoggedOutModalSpec.js
@@ -13,22 +13,26 @@ describe('LoggedOutModal', function() {
     // These tests tell a story: they must be executed sequentially.
 
     it('should show a modal when a logged out user tries to perform an ajax action', async function() {
-      // There's a race here: we're loading the document list, _and_ we'll start
-      // a search. Either can cause the error: we don't care which
+      await this.b.click('nav .dropdown-toggle a')
+      await this.b.click({ link: 'Edit Fields' })
+      await this.b.click({ button: 'Add Field' })
+
+      // Okay, here comes the ajax action. Log out!
       await this.b.driver.manage().deleteAllCookies()
-      await this.b.sendKeys('test\uE007', 'input[name=query]')
+      await this.b.sendKeys('test', '#metadata-schema-editor-app input[name=name]')
+      await this.b.click({ button: 'Add Field' })
 
       await this.b.assertExists('#logged-out-modal', { wait: 'pageLoad' })
     })
 
     it('should redirect to the login page on clicking the login button', async function() {
-      await this.b.click({ tag: 'button', contains: 'Log back in' })
+      await this.b.click({ button: 'Log back in' })
       await this.b.assertExists('form[action="/login"]', { wait: 'pageLoad' })
     })
 
     // we need to log back in so that asUserWithDocumentSet's
     // afterAll hook will pass, so we might as well test the redirect too.
-    it('logging back in should redirect to document set', async function() {
+    it('should go to the document set after you log back in', async function() {
       await this.b.sendKeys(this.userEmail, '.session-form [name=email]')
       await this.b.sendKeys(this.userEmail, '.session-form [name=password]')
       await this.b.click('.session-form [type=submit]')
