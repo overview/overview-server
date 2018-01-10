d470e97b84b1dabf668b5fb73c52ac976dfff2b3
Adam Hooper <adam@adamhooper.com>
2018-01-10 12:26:39 -0500

    Add comments about redis AUTH ""

--- web/app/modules/RedisModule.scala
+++ web/app/modules/RedisModule.scala
@@ -8,14 +8,27 @@ import play.api.Configuration
 import play.api.inject.ApplicationLifecycle
 import scala.concurrent.Future
 
-case class RedisConfiguration(host: String, port: Int, password: String)
+case class RedisConfiguration(
+  host: String,
+  port: Int,
+  password: String
+)
 
 /** Provides a RedisClient. */
 @Singleton
 class RedisModule @Inject() (actorSystem: ActorSystem, configuration: Configuration, lifecycle: ApplicationLifecycle) {
   lazy val client: RedisClient = {
-    val config = RedisConfiguration(configuration.get[String]("redis.host"), configuration.get[Int]("redis.port"), password=configuration.get[String]("redis.password"))
-    val ret = new RedisClient(config.host, config.port, password=Option(config.password))(actorSystem)
+    val config = RedisConfiguration(
+      configuration.get[String]("redis.host"),
+      configuration.get[Int]("redis.port"),
+      password=configuration.get[String]("redis.password")
+    )
+    val ret = new RedisClient(
+      config.host,
+      config.port,
+      // Empty password = no auth. https://github.com/antirez/redis/issues/3676
+      password=Option(config.password)
+    )(actorSystem)
 
     lifecycle.addStopHook { () => Future.successful(client.stop) }
 
