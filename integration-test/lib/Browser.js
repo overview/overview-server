bda8670befbd73c8a2246e55a800bad5571b15a5
Adam Hooper <adam@adamhooper.com>
2018-02-08 09:57:21 -0500

    Allow SCREENSHOT_ERRORS=true for integration tests
    It's hard to track down races. This should help.

--- integration-test/lib/Browser.js
+++ integration-test/lib/Browser.js
@@ -177,9 +177,11 @@ module.exports = class Browser {
     try {
       await this.find(locator, Object.assign({}, options, { throwOnNull: true }))
     } catch (e) {
-      //const filename = `assertExists-failed-${(Math.random() * 9999999).toFixed(0)}.png`
-      //console.log(`Saving failure screenshot to ${filename}`)
-      //await this.saveScreenshot(filename)
+      if (process.env.SCREENSHOT_ERRORS === 'true') {
+        const filename = `assertExists-failed-${(Math.random() * 9999999).toFixed(0)}.png`
+        console.log(`Saving failure screenshot to ${filename}`)
+        await this.saveScreenshot(filename)
+      }
       throw e
     }
   }
@@ -216,9 +218,11 @@ module.exports = class Browser {
         }
       }
     } catch (e) {
-      //const filename = `assertNotExists-failed-${(Math.random() * 9999999).toFixed(0)}.png`
-      //console.log(`Saving failure screenshot to ${filename}`)
-      //await this.saveScreenshot(filename)
+      if (process.env.SCREENSHOT_ERRORS === 'true') {
+        const filename = `assertNotExists-failed-${(Math.random() * 9999999).toFixed(0)}.png`
+        console.log(`Saving failure screenshot to ${filename}`)
+        await this.saveScreenshot(filename)
+      }
       throw e
     }
   }
@@ -441,9 +445,11 @@ module.exports = class Browser {
       if (retval) return
     } while (new Date() - start < timeout)
 
-    //const filename = `wait-failed-${(Math.random() * 9999999).toFixed(0)}.png`
-    //console.log(`Saving failure screenshot to ${filename}`)
-    //await this.saveScreenshot(filename)
+    if (process.env.SCREENSHOT_ERRORS === 'true') {
+      const filename = `wait-failed-${(Math.random() * 9999999).toFixed(0)}.png`
+      console.log(`Saving failure screenshot to ${filename}`)
+      await this.saveScreenshot(filename)
+    }
 
     throw new Error(`Timed out waiting for ${message}`)
   }
