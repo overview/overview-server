e14227e28b39de3c09e85f9da0cb901ef2e95d07
Adam Hooper <adam@adamhooper.com>
2018-02-28 11:11:05 -0500

    Test File2Writer edge cases

--- worker/src/main/scala/com/overviewdocs/ingest/models/CreatedFile2.scala
+++ worker/src/main/scala/com/overviewdocs/ingest/models/CreatedFile2.scala
@@ -11,16 +11,19 @@ import com.overviewdocs.models.{BlobStorageRef,File2}
   */
 case class CreatedFile2(
   id: Long,
-  documentSetId: Long,                    // for ingest, later
-  rootId: Option[Long],                   // to create children while processing
-  parentId: Option[Long],                 // to create children while processing
-  indexInParent: Int,                     // to delete, in case of processing error
-  filename: String,                       // for processing
-  contentType: String,                    // for processing
-  languageCode: String,                   // for processing
-  metadata: File2.Metadata,               // to create children while processing
-  pipelineOptions: File2.PipelineOptions, // for processing
-  blobOpt: Option[BlobStorageRefWithSha1] // for processing
+  documentSetId: Long,                     // for ingest, later
+  rootId: Option[Long],                    // to create children while processing
+  parentId: Option[Long],                  // to create children while processing
+  indexInParent: Int,                      // to delete, in case of processing error
+  filename: String,                        // for processing
+  contentType: String,                     // for processing
+  languageCode: String,                    // for processing
+  metadata: File2.Metadata,                // to create children while processing
+  pipelineOptions: File2.PipelineOptions,  // for processing
+  blobOpt: Option[BlobStorageRefWithSha1], // for processing
+  ownsBlob: Boolean,                       // for delete/resume
+  thumbnailLocationOpt: Option[String],    // for delete/resume
+  ownsThumbnail: Boolean                   // for delete/resume
 ) {
   def asWrittenFile2Opt: Option[WrittenFile2] = blobOpt match {
     case Some(blob) => Some(WrittenFile2(id, documentSetId, rootId, parentId, filename, contentType, languageCode, metadata, pipelineOptions, blob))
