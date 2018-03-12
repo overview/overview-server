711819959fc201a546118708d2bc4c73628c3b29
Adam Hooper <adam@adamhooper.com>
2018-03-12 11:01:25 -0400

    docker-run.sh: use `docker exec` when we can
    This means we run web, worker, and (eventually) converters on the same
    Docker container. This isn't ideal; but it unifies the two common cases:
    
    1. `./dev` runs both Web and Worker -- hence, Worker is on localhost
    2. Developer runs `./sbt web/run` in one tab and `./sbt worker/~reStart`
       in another -- hence, before this commit, Worker is in a separate
       container.
    
    Ideally they'd always run in separate containers and we'd use DNS to
    connect them. But `./sbt` is so slow and memory-intensive that we
    optimize by only running it once in the common, `./dev` case. That means
    we need to configure the `worker` and `web` to connect to one another
    via `localhost`.
    
    This commit makes `localhost` work in case 2.

--- auto/docker-run.sh
+++ auto/docker-run.sh
@@ -15,24 +15,31 @@
 # pristene container, before any commands (except bash) run on it.
 
 if [ ! -f /this-is-overview-dev-on-docker ]; then
-  # Failure: https://github.com/docker/compose/issues/4076 -- this affects Jenkins
-  # So if we run this, we won't see any output:
-  #exec docker-compose run --rm --no-deps dev "$@"
-  # Solution: call Docker directly
   DIR="$(realpath "$(dirname "$0")"/..)"
-  CMD="docker run --rm -it --network overviewserver_default --volume overviewserver_database-data:/var/lib/postgresql/data --volume overviewserver_search-data:/var/lib/overview/search --volume overviewserver_blob-storage-data:/var/lib/overview/blob-storage --volume overviewserver_homedir:/root --volume $DIR:/app"
 
-  # Publish port 9000=>80, if nothing else is running on it
-  if [ -z "$(docker ps -q -f publish=80 -f ancestor=overviewserver_dev:latest)" ]; then
-    echo "Exposing localhost:9000" >&2
-    CMD="$CMD --publish 127.0.0.1:9000:80"
+  RUNNING_CONTAINER=$(docker ps -q -f name=overviewserver_dev)
+  if [ -z "$RUNNING_CONTAINER" ]; then
+    # If overviewserver_dev is not running, use `docker run`
+    CMD="docker run --rm -it --network overviewserver_default --volume overviewserver_database-data:/var/lib/postgresql/data --volume overviewserver_search-data:/var/lib/overview/search --volume overviewserver_blob-storage-data:/var/lib/overview/blob-storage --volume overviewserver_homedir:/root --volume $DIR:/app"
+
+    # Publish port 9000=>80, if nothing else is running on it
+    if [ -z "$(docker ps -q -f publish=80 -f ancestor=overviewserver_dev:latest)" ]; then
+      echo "Exposing localhost:9000" >&2
+      CMD="$CMD --publish 127.0.0.1:9000:80"
+    fi
+
+    DOCKER_OPTIONS=
+    # Add docker options _before_ the image name
+    while [ "x$1" != "x${1#-}" ]; do
+      DOCKER_OPTIONS="$DOCKER_OPTIONS $1"
+      shift
+    done
+
+    CMD="$CMD $DOCKER_OPTIONS overviewserver_dev:latest"
+  else
+    # overviewserver_dev is already running: use `docker exec` within it
+    CMD="docker exec -it ${RUNNING_CONTAINER}"
   fi
 
-  DOCKER_OPTIONS=
-  # Add docker options _before_ the image name
-  while [ "x$1" != "x${1#-}" ]; do
-    DOCKER_OPTIONS="$DOCKER_OPTIONS $1"
-    shift
-  done
-  exec $CMD $DOCKER_OPTIONS overviewserver_dev:latest "$@"
+  exec $CMD "$@"
 fi
