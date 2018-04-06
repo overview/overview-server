14e3e04caaa5f6dec9cdb97cb8eb58da2b26a55a
Adam Hooper <adam@adamhooper.com>
2018-04-06 01:37:20 -0400

    Avoid crash when user cancels twice

--- worker/src/main/scala/com/overviewdocs/ingest/model/ResumedFileGroupJob.scala
+++ worker/src/main/scala/com/overviewdocs/ingest/model/ResumedFileGroupJob.scala
@@ -10,6 +10,9 @@ case class ResumedFileGroupJob(
   def fileGroupId: Long = fileGroup.id
   def documentSetId: Long = fileGroup.addToDocumentSetId.get
   def isComplete: Boolean = fileGroup.nFiles.get == progressState.nFilesIngested
-  def cancel: Unit = progressState.cancel.success(akka.Done)
   def isCanceled: Boolean = progressState.cancel.isCompleted
+
+  def cancel: Unit = {
+    progressState.cancel.trySuccess(akka.Done)
+  }
 }
