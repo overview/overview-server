b107eae2b5366ad5b2d0789028ce0a00e426d7e0
Adam Hooper <adam@adamhooper.com>
2018-04-09 10:25:50 -0400

    When resuming, remember a job is canceled

--- worker/src/main/scala/com/overviewdocs/ingest/model/ResumedFileGroupJob.scala
+++ worker/src/main/scala/com/overviewdocs/ingest/model/ResumedFileGroupJob.scala
@@ -10,7 +10,7 @@ case class ResumedFileGroupJob(
   def fileGroupId: Long = fileGroup.id
   def documentSetId: Long = fileGroup.addToDocumentSetId.get
   def isComplete: Boolean = fileGroup.nFiles.get == progressState.nFilesIngested
-  def isCanceled: Boolean = progressState.cancel.isCompleted
+  def isCanceled: Boolean = progressState.cancel.isCompleted || fileGroup.deleted
 
   def cancel: Unit = {
     progressState.cancel.trySuccess(akka.Done)
