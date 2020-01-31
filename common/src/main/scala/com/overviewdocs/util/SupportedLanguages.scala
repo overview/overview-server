83e221a62d7ac00981afcddf19943c563337b1b1
Adam Hooper <adam@adamhooper.com>
2020-01-31 13:53:44 -0500

    Add Czech-language document support

--- common/src/main/scala/com/overviewdocs/util/SupportedLanguages.scala
+++ common/src/main/scala/com/overviewdocs/util/SupportedLanguages.scala
@@ -3,26 +3,23 @@ package com.overviewdocs.util
 import java.util.Locale
 
 object SupportedLanguages {
-  // Supported languages, iso639-1 mapped to iso639-2 format
-  // If we want to have different language support for stop-word removal and OCR
-  // a more sophisticated approach will be required.
-  private val iso639Codes = Map(
-    "ar" -> "ara",
-    "ca" -> "cat",
-    "de" -> "deu",
-    "en" -> "eng",
-    "es" -> "spa",
-    "fr" -> "fra",
-    "sv" -> "swe",
-    "nl" -> "nld",
-    "no" -> "nor",
-    "it" -> "ita",
-    "pt" -> "por",
-    "ro" -> "ron",
-    "ru" -> "rus"
+  // Supported languages, in ISO639-2 format
+  val languageCodes = Set(
+    "ar",
+    "ca",
+    "cs",
+    "de",
+    "en",
+    "es",
+    "fr",
+    "sv",
+    "nl",
+    "no",
+    "it",
+    "pt",
+    "ro",
+    "ru"
   )
-
-  val languageCodes = iso639Codes.keySet
   
   val languages : Set[SupportedLanguage] = languageCodes.map(SupportedLanguage)
 
@@ -32,7 +29,4 @@ object SupportedLanguages {
   def languagesSortedInCallerLocale(callerLocale: Locale) : Seq[SupportedLanguage] = {
     languages.toIndexedSeq.sortBy((lang) => lang.displayLanguage(callerLocale))
   }
-  
-  /** @returns the iso639-2 language code, if the language is supported */
-  def asIso639_2(iso639_1Code: String): Option[String] = iso639Codes.get(iso639_1Code)
 }
