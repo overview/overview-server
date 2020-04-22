8d78e4bd475cc4d25bdc9e62711023632b81eb18
Adam Hooper <adam@adamhooper.com>
2020-04-22 15:08:35 -0400

    Fix migrations for Postgres 12

--- db-evolution-applier/src/main/resources/migration/V104__file_group_progress.sql
+++ db-evolution-applier/src/main/resources/migration/V104__file_group_progress.sql
@@ -4,12 +4,12 @@
 
 ALTER TABLE file_group
   ADD COLUMN add_to_document_set_id BIGINT REFERENCES document_set (id),
-  ADD COLUMN lang VARCHAR CHECK (lang IS NULL = add_to_document_set_id IS NULL),
-  ADD COLUMN split_documents BOOLEAN CHECK (split_documents IS NULL = add_to_document_set_id IS NULL),
-  ADD COLUMN n_files INT CHECK (n_files IS NULL = add_to_document_set_id IS NULL),
-  ADD COLUMN n_bytes BIGINT CHECK (n_files IS NULL = add_to_document_set_id IS NULL),
-  ADD COLUMN n_files_processed INT CHECK (n_files_processed IS NULL = add_to_document_set_id IS NULL),
-  ADD COLUMN n_bytes_processed BIGINT CHECK (n_bytes_processed IS NULL = add_to_document_set_id IS NULL),
+  ADD COLUMN lang VARCHAR CHECK ((lang IS NULL) = (add_to_document_set_id IS NULL)),
+  ADD COLUMN split_documents BOOLEAN CHECK ((split_documents IS NULL) = (add_to_document_set_id IS NULL)),
+  ADD COLUMN n_files INT CHECK ((n_files IS NULL) = (add_to_document_set_id IS NULL)),
+  ADD COLUMN n_bytes BIGINT CHECK ((n_files IS NULL) = (add_to_document_set_id IS NULL)),
+  ADD COLUMN n_files_processed INT CHECK ((n_files_processed IS NULL) = (add_to_document_set_id IS NULL)),
+  ADD COLUMN n_bytes_processed BIGINT CHECK ((n_bytes_processed IS NULL) = (add_to_document_set_id IS NULL)),
   ADD COLUMN estimated_completion_time TIMESTAMP WITH TIME ZONE,
   DROP COLUMN completed;
 
--- db-evolution-applier/src/main/resources/migration/V113__file_group_ocr.sql
+++ db-evolution-applier/src/main/resources/migration/V113__file_group_ocr.sql
@@ -1,4 +1,4 @@
 ALTER TABLE file_group
-ADD COLUMN ocr BOOLEAN CHECK (ocr IS NULL = add_to_document_set_id IS NULL);
+ADD COLUMN ocr BOOLEAN CHECK ((ocr IS NULL) = (add_to_document_set_id IS NULL));
 
 COMMENT ON COLUMN file_group.ocr IS 'When true, find text on pages with OCR; when false, do not';
